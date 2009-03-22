a74f2af61cb83e104cc9cc5ebd9292f14b6802fa
antirez <antirez@gmail.com>
2009-03-22 14:59:05 +0100

    redis-cli now checks the arity of vararg commnads

--- redis-cli.c
+++ redis-cli.c
@@ -224,7 +224,7 @@ static int cliSendCommand(int argc, char **argv) {
     }
 
     if ((rc->arity > 0 && argc != rc->arity) ||
-        (rc->arity < 0 && argc < rc->arity)) {
+        (rc->arity < 0 && argc < -rc->arity)) {
             fprintf(stderr,"Wrong number of arguments for '%s'\n",rc->name);
             return 1;
     }
