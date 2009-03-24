9eb00f21f0afd16b311daa0795b1d45ca7da8874
antirez <antirez@gmail.com>
2009-03-24 14:22:42 +0100

    protocol fix in SORT reply with null elements

--- redis.c
+++ redis.c
@@ -2772,7 +2772,7 @@ static void sortCommand(redisClient *c) {
 
             if (sop->type == REDIS_SORT_GET) {
                 if (!val || val->type != REDIS_STRING) {
-                    addReply(c,shared.minus1);
+                    addReply(c,shared.nullbulk);
                 } else {
                     addReplySds(c,sdscatprintf(sdsempty(),"$%d\r\n",
                         sdslen(val->ptr)));
