5a7c647e9acb6034e2bfbe879b1cba4209a76c05
antirez <antirez@gmail.com>
2009-04-29 10:33:29 +0200

    Check for fork() failure in background saving

--- redis.c
+++ redis.c
@@ -1879,6 +1879,11 @@ static int rdbSaveBackground(char *filename) {
         }
     } else {
         /* Parent */
+        if (childpid == -1) {
+            redisLog(REDIS_WARNING,"Can't save in background: fork: %s",
+                strerror(errno));
+            return REDIS_ERR;
+        }
         redisLog(REDIS_NOTICE,"Background saving started by pid %d",childpid);
         server.bgsaveinprogress = 1;
         return REDIS_OK;
