1904ecc165735a859ef6a25fc7a8bd0c14505153
antirez <antirez@gmail.com>
2009-05-11 19:45:47 +0200

    timestamp in log lines

--- redis.c
+++ redis.c
@@ -556,7 +556,12 @@ void redisLog(int level, const char *fmt, ...)
     va_start(ap, fmt);
     if (level >= server.verbosity) {
         char *c = ".-*";
-        fprintf(fp,"%c ",c[level]);
+        char buf[64];
+        time_t now;
+
+        now = time(NULL);
+        strftime(buf,64,"%d %b %H:%M:%S",gmtime(&now));
+        fprintf(fp,"%s %c ",buf,c[level]);
         vfprintf(fp, fmt, ap);
         fprintf(fp,"\n");
         fflush(fp);
