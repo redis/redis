8cca9b82773e2ca3d13bc1bb009d4e4d98141021
antirez <antirez@gmail.com>
2009-05-24 09:21:35 +0200

    Warns if using the default config

--- redis.c
+++ redis.c
@@ -4002,6 +4002,8 @@ int main(int argc, char **argv) {
     } else if (argc > 2) {
         fprintf(stderr,"Usage: ./redis-server [/path/to/redis.conf]\n");
         exit(1);
+    } else {
+        redisLog(REDIS_WARNING,"Warning: no config file specified, using the default config. In order to specify a config file use 'redis-server /path/to/redis.conf'");
     }
     initServer();
     if (server.daemonize) daemonize();
