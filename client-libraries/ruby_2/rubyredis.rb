a56785f73cc3a2e09571f46941caf56133551400
antirez <antirez@gmail.com>
2009-05-26 01:12:46 +0200

    RubyRedis: set TCP_NODELAY TCP socket option to to disable the neagle algorithm. Makes a huge difference under some OS, notably Linux

--- client-libraries/ruby_2/rubyredis.rb
+++ client-libraries/ruby_2/rubyredis.rb
@@ -116,6 +116,7 @@ class RedisClient
         else
             sock = TCPSocket.new(host, port, 0)
         end
+        sock.setsockopt Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1
 
         # If the timeout is set we set the low level socket options in order
         # to make sure a blocking read will return after the specified number
