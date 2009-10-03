c32514972cbf2255d6c78e0ba4f2c7d7b3f652f5
antirez <antirez@gmail.com>
2009-10-03 10:54:27 +0200

    benchmark now outputs the right command line to shorten the TIME_WAIT interval on Mac OS X when keep alive is set

--- benchmark.c
+++ benchmark.c
@@ -421,7 +421,7 @@ int main(int argc, char **argv) {
     parseOptions(argc,argv);
 
     if (config.keepalive == 0) {
-        printf("WARNING: keepalive disabled, you probably need 'echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse' in order to use a lot of clients/requests\n");
+        printf("WARNING: keepalive disabled, you probably need 'echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse' for Linux and 'sudo sysctl -w net.inet.tcp.msl=1000' for Mac OS X in order to use a lot of clients/requests\n");
     }
 
     do {
