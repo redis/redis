0aad7a1938c015c59d7ccdedd3dae8c2132170db
antirez <antirez@gmail.com>
2009-10-23 22:39:25 +0200

    zrange now starts to work. zadd still does not support update and will crash or leak or b000mmmmm

--- redis.c
+++ redis.c
@@ -3865,7 +3865,7 @@ static void zrangeCommand(redisClient *c) {
 
             addReplySds(c,sdscatprintf(sdsempty(),"*%d\r\n",rangelen));
             for (j = 0; j < rangelen; j++) {
-                ele = ln->forward[0]->obj;
+                ele = ln->obj;
                 addReplyBulkLen(c,ele);
                 addReply(c,ele);
                 addReply(c,shared.crlf);
