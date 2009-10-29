4aa701c11aae2b645583e78a808452ae7e1918da
antirez <antirez@gmail.com>
2009-10-29 11:38:32 +0100

    Fixed issue 72 (SLAVEOF shutdowns redis-server on malformed reply)

--- redis.c
+++ redis.c
@@ -5071,6 +5071,11 @@ static int syncWithMaster(void) {
             strerror(errno));
         return REDIS_ERR;
     }
+    if (buf[0] != '$') {
+        close(fd);
+        redisLog(REDIS_WARNING,"Bad protocol from MASTER, the first byte is not '$', are you sure the host and port are right?");
+        return REDIS_ERR;
+    }
     dumpsize = atoi(buf+1);
     redisLog(REDIS_NOTICE,"Receiving %d bytes data dump from MASTER",dumpsize);
     /* Read the bulk write data on a temp file */
