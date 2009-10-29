fa4c0aba85fca17e2526831d1afa530724479ed5
antirez <antirez@gmail.com>
2009-10-29 11:43:40 +0100

    Fixed Issue 74 (ERR just returned on invalid password), now the error message is -ERR invalid password.

--- redis.c
+++ redis.c
@@ -2721,7 +2721,7 @@ static void authCommand(redisClient *c) {
       addReply(c,shared.ok);
     } else {
       c->authenticated = 0;
-      addReply(c,shared.err);
+      addReplySds(c,sdscatprintf(sdsempty(),"-ERR invalid password\r\n"));
     }
 }
 
