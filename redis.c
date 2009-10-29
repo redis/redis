6c9385e01d127f8ffa6a783b2a571d8ac4b73666
antirez <antirez@gmail.com>
2009-10-29 11:31:55 +0100

    Fixed issue 77 (Incorrect time in log files) thanks to youwantalex

--- redis.c
+++ redis.c
@@ -671,7 +671,7 @@ static void redisLog(int level, const char *fmt, ...) {
         time_t now;
 
         now = time(NULL);
-        strftime(buf,64,"%d %b %H:%M:%S",gmtime(&now));
+        strftime(buf,64,"%d %b %H:%M:%S",localtime(&now));
         fprintf(fp,"%s %c ",buf,c[level]);
         vfprintf(fp, fmt, ap);
         fprintf(fp,"\n");
