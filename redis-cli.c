e2aba0f9ec23d00610b315f8163d7a0ec255c370
antirez <antirez@gmail.com>
2009-11-01 15:31:54 +0100

    redis-cli now makes clear when the returned string is an integer

--- redis-cli.c
+++ redis-cli.c
@@ -238,6 +238,7 @@ static int cliReadReply(int fd) {
         cliReadSingleLineReply(fd);
         return 1;
     case '+':
+        return cliReadSingleLineReply(fd);
     case ':':
         printf("(integer) ");
         return cliReadSingleLineReply(fd);
