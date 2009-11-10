060f6be6bf2976bad5bd6007d21e7e6ff081c92b
antirez <antirez@gmail.com>
2009-11-10 10:26:09 +0100

    Fixed issue 92 in redis: redis-cli (nil) return value lacks CR/LF

--- redis-cli.c
+++ redis-cli.c
@@ -191,7 +191,7 @@ static int cliReadBulkReply(int fd) {
     bulklen = atoi(replylen);
     if (bulklen == -1) {
         sdsfree(replylen);
-        printf("(nil)");
+        printf("(nil)\n");
         return 0;
     }
     reply = zmalloc(bulklen);
