ccb5332c5e6644a56b2af1ba3248f882d4f1bb60
antirez <antirez@gmail.com>
2009-11-18 19:02:20 +0100

    more writev tests/work

--- benchmark.c
+++ benchmark.c
@@ -565,6 +565,15 @@ int main(int argc, char **argv) {
         aeMain(config.el);
         endBenchmark("LRANGE (first 100 elements)");
 
+        prepareForBenchmark();
+        c = createClient();
+        if (!c) exit(1);
+        c->obuf = sdscat(c->obuf,"LRANGE mylist 0 299\r\n");
+        prepareClientForReply(c,REPLY_MBULK);
+        createMissingClients(c);
+        aeMain(config.el);
+        endBenchmark("LRANGE (first 300 elements)");
+
         printf("\n");
     } while(config.loop);
 
