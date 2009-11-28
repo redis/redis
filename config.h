37be27653817f0c4c18d695ceea082e0bb7bf462
antirez <antirez@gmail.com>
2009-11-28 20:48:53 +0100

    enable kqueue/kevent only for Mac OS X 10.6.x as it seems that 10.5.x has a broken implementation of this syscalls.

--- config.h
+++ config.h
@@ -31,7 +31,7 @@
 #define HAVE_EPOLL 1
 #endif
 
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
+#if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
 #define HAVE_KQUEUE 1
 #endif
 
