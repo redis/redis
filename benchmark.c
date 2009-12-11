61c47ecd2551a729032ff277620d3eea7c7ed080
antirez <antirez@gmail.com>
2009-12-12 00:04:20 +0100

    better handling of non blocking connect on redis-benchmark: EPIPE on read does not print an error message now

--- benchmark.c
+++ benchmark.c
@@ -322,7 +322,8 @@ static void writeHandler(aeEventLoop *el, int fd, void *privdata, int mask)
         int len = sdslen(c->obuf) - c->written;
         int nwritten = write(c->fd, ptr, len);
         if (nwritten == -1) {
-            fprintf(stderr, "Writing to socket: %s\n", strerror(errno));
+            if (errno != EPIPE)
+                fprintf(stderr, "Writing to socket: %s\n", strerror(errno));
             freeClient(c);
             return;
         }
