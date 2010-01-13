85a2775298a85b80ccaaf31082c479b7968158b1
antirez <antirez@gmail.com>
2010-01-13 13:41:38 -0500

    removed a useless debugging message

--- redis.c
+++ redis.c
@@ -5399,8 +5399,6 @@ static void sortCommand(redisClient *c) {
         }
         dictReleaseIterator(di);
     }
-    printf("**************************** %d == %d\n",
-        j, vectorlen);
     redisAssert(j == vectorlen);
 
     /* Now it's time to load the right scores in the sorting vector */
