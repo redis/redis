6fa987e390f12388e1597ab5c46f58618c859912
antirez <antirez@gmail.com>
2010-01-15 14:02:18 -0500

    Create swap file only if not exists

--- redis.c
+++ redis.c
@@ -7035,9 +7035,13 @@ static void vmInit(void) {
 
     expandVmSwapFilename();
     redisLog(REDIS_NOTICE,"Using '%s' as swap file",server.vm_swap_file);
-    server.vm_fp = fopen(server.vm_swap_file,"r+b");
+    if ((server.vm_fp = fopen(server.vm_swap_file,"r+b")) == NULL) {
+        server.vm_fp = fopen(server.vm_swap_file,"w+b");
+    }
     if (server.vm_fp == NULL) {
-        redisLog(REDIS_WARNING,"Impossible to open the swap file. Exiting.");
+        redisLog(REDIS_WARNING,
+            "Impossible to open the swap file: %s. Exiting.",
+            strerror(errno));
         exit(1);
     }
     server.vm_fd = fileno(server.vm_fp);
