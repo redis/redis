459f52a8aadefc39bd2fe0fb75b9b29b09e1936e
antirez <antirez@gmail.com>
2010-01-15 13:55:52 -0500

    I hate warnings

--- redis.c
+++ redis.c
@@ -7865,7 +7865,7 @@ static void debugCommand(redisClient *c) {
                 "+Key at:%p refcount:%d, value at:%p refcount:%d "
                 "encoding:%d serializedlength:%lld\r\n",
                 (void*)key, key->refcount, (void*)val, val->refcount,
-                val->encoding, rdbSavedObjectLen(val,NULL)));
+                val->encoding, (long long) rdbSavedObjectLen(val,NULL)));
         } else {
             addReplySds(c,sdscatprintf(sdsempty(),
                 "+Key at:%p refcount:%d, value swapped at: page %llu "
