59146ef3c107d59b6522b8a2f5539a55703adee1
antirez <antirez@gmail.com>
2010-02-06 18:53:39 +0100

    DEBUG OBJECT provide info about serialized object length even when VM is disabled

--- redis.c
+++ redis.c
@@ -8171,8 +8171,8 @@ static void debugCommand(redisClient *c) {
         }
         key = dictGetEntryKey(de);
         val = dictGetEntryVal(de);
-        if (server.vm_enabled && (key->storage == REDIS_VM_MEMORY ||
-                                  key->storage == REDIS_VM_SWAPPING)) {
+        if (!server.vm_enabled || (key->storage == REDIS_VM_MEMORY ||
+                                   key->storage == REDIS_VM_SWAPPING)) {
             addReplySds(c,sdscatprintf(sdsempty(),
                 "+Key at:%p refcount:%d, value at:%p refcount:%d "
                 "encoding:%d serializedlength:%lld\r\n",
