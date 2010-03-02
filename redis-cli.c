e3c7f0027fe857a200425218c1faa39b9a6407e0
Damian Janowski & Michel Martens <damian.janowski+soveran@gmail.com>
2010-03-02 15:47:31 -0300

    Remove trailing newline in interactive mode.

--- redis-cli.c
+++ redis-cli.c
@@ -439,7 +439,10 @@ static char *prompt(char *line, int size) {
         retval = fgets(line, size, stdin);
     } while (retval && *line == '\n');
 
+    line[strlen(line) - 1] = '\0';
+
     fpurge(stdin);
+
     return retval;
 }
 
