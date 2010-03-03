386b43c6b31ba8c91cd5c46e23fb411542b7d71f
antirez <antirez@gmail.com>
2010-03-03 20:08:16 +0100

    fpurge call removed from redis-cli

--- redis-cli.c
+++ redis-cli.c
@@ -441,10 +441,7 @@ static char *prompt(char *line, int size) {
         printf(">> ");
         retval = fgets(line, size, stdin);
     } while (retval && *line == '\n');
-
     line[strlen(line) - 1] = '\0';
-
-    fpurge(stdin);
 
     return retval;
 }
