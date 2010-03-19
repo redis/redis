aa42c9a7916e29964a565b66a2446a42f8bf483b
antirez <antirez@gmail.com>
2010-03-19 19:02:24 +0100

    Removed a stupid overriding of config values due to a wrong cut&paste

--- redis.c
+++ redis.c
@@ -6484,9 +6484,6 @@ static sds genRedisInfoString(void) {
     int j;
     char hmem[64];
 
-    server.hash_max_zipmap_entries = REDIS_HASH_MAX_ZIPMAP_ENTRIES;
-    server.hash_max_zipmap_value = REDIS_HASH_MAX_ZIPMAP_VALUE;
-  
     bytesToHuman(hmem,zmalloc_used_memory());
     info = sdscatprintf(sdsempty(),
         "redis_version:%s\r\n"
