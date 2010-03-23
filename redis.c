e940927314582b6e6260ed87e73841d69dd88f29
antirez <antirez@gmail.com>
2010-03-23 20:04:55 +0100

    with --help states that you can use - as config file name to feed config via stdin

--- redis.c
+++ redis.c
@@ -9200,6 +9200,7 @@ static void version() {
 
 static void usage() {
     fprintf(stderr,"Usage: ./redis-server [/path/to/redis.conf]\n");
+    fprintf(stderr,"       ./redis-server - (read config from stdin)\n");
     exit(1);
 }
 
