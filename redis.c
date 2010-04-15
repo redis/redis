6e9e463f8014cbde1eaabe4c59045471e1871b74
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-04-15 15:38:48 +0200

    use shared replies for hset

--- redis.c
+++ redis.c
@@ -6242,13 +6242,13 @@ static robj *hashLookupWriteOrCreate(redisClient *c, robj *key) {
 
 /* ============================= Hash commands ============================== */
 static void hsetCommand(redisClient *c) {
-    int update = 0;
+    int update;
     robj *o;
 
     if ((o = hashLookupWriteOrCreate(c,c->argv[1])) == NULL) return;
     hashTryConversion(o,c->argv,2,3);
     update = hashReplace(o,c->argv[2],c->argv[3]);
-    addReplySds(c,sdscatprintf(sdsempty(),":%d\r\n",update == 0));
+    addReply(c, update ? shared.czero : shared.cone);
     server.dirty++;
 }
 
