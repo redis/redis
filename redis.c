177727542c871330445700299aed803a1384b0ed
antirez <antirez@gmail.com>
2010-04-18 00:03:49 +0200

    Guru mediation -> meditation

--- redis.c
+++ redis.c
@@ -9909,7 +9909,7 @@ static void _redisAssert(char *estr, char *file, int line) {
 
 static void _redisPanic(char *msg, char *file, int line) {
     redisLog(REDIS_WARNING,"!!! Software Failure. Press left mouse button to continue");
-    redisLog(REDIS_WARNING,"Guru Mediation: %s #%s:%d",msg,file,line);
+    redisLog(REDIS_WARNING,"Guru Meditation: %s #%s:%d",msg,file,line);
 #ifdef HAVE_BACKTRACE
     redisLog(REDIS_WARNING,"(forcing SIGSEGV in order to print the stack trace)");
     *((char*)-1) = 'x';
