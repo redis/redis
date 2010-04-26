a88a2af6c5a5248227dd5436a93a637000e88d8c
antirez <antirez@gmail.com>
2010-04-26 18:39:39 +0200

    Initial support for quoted strings in redis-cli

--- redis-cli.c
+++ redis-cli.c
@@ -34,6 +34,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #include "anet.h"
 #include "sds.h"
@@ -460,32 +461,95 @@ static char **convertToSds(int count, char** args) {
   return sds;
 }
 
+static char **splitArguments(char *line, int *argc) {
+    char *p = line;
+    char *current = NULL;
+    char **vector = NULL;
+
+    *argc = 0;
+    while(1) {
+        /* skip blanks */
+        while(*p && isspace(*p)) p++;
+        if (*p) {
+            /* get a token */
+            int inq=0; /* set to 1 if we are in "quotes" */
+            int done = 0;
+
+            if (current == NULL) current = sdsempty();
+            while(!done) {
+                if (inq) {
+                    if (*p == '\\' && *(p+1)) {
+                        char c;
+
+                        p++;
+                        switch(*p) {
+                        case 'n': c = '\n'; break;
+                        case 'r': c = '\r'; break;
+                        case 't': c = '\t'; break;
+                        case 'b': c = '\b'; break;
+                        case 'a': c = '\a'; break;
+                        default: c = *p; break;
+                        }
+                        current = sdscatlen(current,&c,1);
+                    } else if (*p == '"') {
+                        done = 1;
+                    } else {
+                        current = sdscatlen(current,p,1);
+                    }
+                } else {
+                    switch(*p) {
+                    case ' ':
+                    case '\n':
+                    case '\r':
+                    case '\t':
+                    case '\0':
+                        done=1;
+                        break;
+                    case '"':
+                        inq=1;
+                        break;
+                    default:
+                        current = sdscatlen(current,p,1);
+                        break;
+                    }
+                }
+                if (*p) p++;
+            }
+            /* add the token to the vector */
+            vector = zrealloc(vector,((*argc)+1)*sizeof(char*));
+            vector[*argc] = current;
+            (*argc)++;
+            current = NULL;
+        } else {
+            return vector;
+        }
+    }
+}
+
+#define LINE_BUFLEN 4096
 static void repl() {
-    int size = 4096, max = size >> 1, argc;
-    char *line;
-    char **ap, *args[max];
+    int argc, j;
+    char *line, **argv;
 
     while((line = linenoise("redis> ")) != NULL) {
         if (line[0] != '\0') {
-          linenoiseHistoryAdd(line);
-          argc = 0;
-
-          for (ap = args; (*ap = strsep(&line, " \t")) != NULL;) {
-              if (**ap != '\0') {
-                  if (argc >= max) break;
-                  if (strcasecmp(*ap,"quit") == 0 || strcasecmp(*ap,"exit") == 0)
-                      exit(0);
-                  ap++;
-                  argc++;
-              }
-          }
-
-          cliSendCommand(argc, convertToSds(argc, args), 1);
+            argv = splitArguments(line,&argc);
+            linenoiseHistoryAdd(line);
+            if (argc > 0) {
+                if (strcasecmp(argv[0],"quit") == 0 ||
+                    strcasecmp(argv[0],"exit") == 0)
+                        exit(0);
+                else
+                    cliSendCommand(argc, argv, 1);
+            }
+            /* Free the argument vector */
+            for (j = 0; j < argc; j++)
+                sdsfree(argv[j]);
+            free(argv);
         }
-
+        /* linenoise() returns malloc-ed lines like readline() */
         free(line);
     }
-
     exit(0);
 }
 
