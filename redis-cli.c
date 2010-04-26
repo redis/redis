095e805e891798435944aa9082d45b3c69ed2dcf
antirez <antirez@gmail.com>
2010-04-26 19:56:16 +0200

    don't output the newline when stdout is not a tty

--- redis-cli.c
+++ redis-cli.c
@@ -271,7 +271,6 @@ static int cliReadBulkReply(int fd) {
             zfree(reply);
             return 1;
         }
-        if (reply[bulklen-1] != '\n') printf("\n");
     } else {
         /* If you are producing output for the standard output we want
          * a more interesting output with quoted characters and so forth */
