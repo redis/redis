7aaaad507060b131ff18d4637ea63499828076d6
antirez <antirez@metal.(none)>
2010-04-29 20:13:23 +0200

    redis-cli minor fix (less segfault is better)

--- redis-cli.c
+++ redis-cli.c
@@ -359,11 +359,11 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
     int fd, j, retval = 0;
     sds cmd;
 
-    config.raw_output = (rc->flags & CMDFLAG_RAWOUTPUT);
     if (!rc) {
         fprintf(stderr,"Unknown command '%s'\n",argv[0]);
         return 1;
     }
+    config.raw_output = (rc->flags & CMDFLAG_RAWOUTPUT);
 
     if ((rc->arity > 0 && argc != rc->arity) ||
         (rc->arity < 0 && argc < -rc->arity)) {
