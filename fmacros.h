af090ffa5279a9faca4ea974d908d58d0fc77844
antirez <antirez@gmail.com>
2010-05-02 15:08:06 +0200

    compilation fix for mac os x

--- fmacros.h
+++ fmacros.h
@@ -2,7 +2,13 @@
 #define _REDIS_FMACRO_H
 
 #define _BSD_SOURCE
+
+#ifdef __linux__
 #define _XOPEN_SOURCE 700
+#else
+#define _XOPEN_SOURCE
+#endif
+
 #define _LARGEFILE_SOURCE
 #define _FILE_OFFSET_BITS 64
 
