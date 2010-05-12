5107436cdf7c4ec25b7ea5ff4de87dfc10eeb2db
antirez <antirez@gmail.com>
2010-05-12 21:51:48 +0200

    added overflow check in the double -> long long conversion trick to avoid integer overflows. I think this was not needed in practical terms, but it is safer

--- redis.c
+++ redis.c
@@ -3513,7 +3513,7 @@ static int rdbSaveDoubleValue(FILE *fp, double val) {
         len = 1;
         buf[0] = (val < 0) ? 255 : 254;
     } else {
-        if (val == ((long long)val))
+        if (val > LLONG_MAX && val < LLONG_MIN && val == ((long long)val))
             ll2string((char*)buf+1,sizeof(buf),(long long)val);
         else
             snprintf((char*)buf+1,sizeof(buf)-1,"%.17g",val);
