c7dadd15131c2d9cd8f3db468c060c37a4bf1714
antirez <antirez@gmail.com>
2010-05-18 16:39:57 +0200

    git hash 00000000 in reelase.h when git is not found enabled again after some shell scripting fix that is now compatible with most shells

--- mkreleasehdr.sh
+++ mkreleasehdr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GIT_SHA1=$(git show-ref --head --hash=8 2> /dev/null | head -n1)
+GIT_SHA1=$( (git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1)
 GIT_DIRTY=$(git diff 2> /dev/null | wc -l)
 test -f release.h || touch release.h
 (cat release.h | grep SHA1 | grep $GIT_SHA1) && \
