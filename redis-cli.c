a2f4f8711a7ded05d950fe0b784a5e4c7abeb298
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-05-26 18:22:05 +0200

    build command outside while loop

--- redis-cli.c
+++ redis-cli.c
@@ -252,18 +252,17 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
         return 1;
     }
 
-    while(repeat--) {
-        /* Build the command to send */
-        cmd = sdscatprintf(sdsempty(),"*%d\r\n",argc);
-        for (j = 0; j < argc; j++) {
-            cmd = sdscatprintf(cmd,"$%lu\r\n",
-                (unsigned long)sdslen(argv[j]));
-            cmd = sdscatlen(cmd,argv[j],sdslen(argv[j]));
-            cmd = sdscatlen(cmd,"\r\n",2);
-        }
-        anetWrite(fd,cmd,sdslen(cmd));
-        sdsfree(cmd);
+    /* Build the command to send */
+    cmd = sdscatprintf(sdsempty(),"*%d\r\n",argc);
+    for (j = 0; j < argc; j++) {
+        cmd = sdscatprintf(cmd,"$%lu\r\n",
+            (unsigned long)sdslen(argv[j]));
+        cmd = sdscatlen(cmd,argv[j],sdslen(argv[j]));
+        cmd = sdscatlen(cmd,"\r\n",2);
+    }
 
+    while(repeat--) {
+        anetWrite(fd,cmd,sdslen(cmd));
         while (config.monitor_mode) {
             cliReadSingleLineReply(fd,0);
         }
@@ -277,7 +276,6 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
         }
 
         retval = cliReadReply(fd);
-
         if (retval) {
             return retval;
         }
