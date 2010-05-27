8a3b0d2d9a2eb34f1277e22c3f0185b0bfe21959
antirez <antirez@gmail.com>
2010-05-28 00:23:27 +0200

    Added Git sha1 and dirty status in redis-server -v output

--- redis.c
+++ redis.c
@@ -10906,7 +10906,8 @@ static void daemonize(void) {
 }
 
 static void version() {
-    printf("Redis server version %s\n", REDIS_VERSION);
+    printf("Redis server version %s (%s:%d)\n", REDIS_VERSION,
+        REDIS_GIT_SHA1, atoi(REDIS_GIT_DIRTY) > 0);
     exit(0);
 }
 
