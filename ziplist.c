ffc15852676131003d1f5e468467f15f57a8e0bf
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-06-01 17:57:51 +0200

    added stress test for heavy i/o in ziplists

--- ziplist.c
+++ ziplist.c
@@ -581,6 +581,7 @@ void ziplistRepr(unsigned char *zl) {
 }
 
 #ifdef ZIPLIST_TEST_MAIN
+#include <sys/time.h>
 
 unsigned char *createList() {
     unsigned char *zl = ziplistNew();
@@ -608,6 +609,35 @@ unsigned char *createIntList() {
     sprintf(buf, "much much longer non integer");
     zl = ziplistPush(zl, (unsigned char*)buf, strlen(buf), ZIPLIST_TAIL);
     return zl;
+}
+
+long long usec(void) {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return (((long long)tv.tv_sec)*1000000)+tv.tv_usec;
+}
+
+void stress(int pos, int num, int maxsize, int dnum) {
+    int i,j,k;
+    unsigned char *zl;
+    char posstr[2][5] = { "HEAD", "TAIL" };
+    long long start;
+    for (i = 0; i < maxsize; i+=dnum) {
+        zl = ziplistNew();
+        for (j = 0; j < i; j++) {
+            zl = ziplistPush(zl,(unsigned char*)"quux",4,ZIPLIST_TAIL);
+        }
+
+        /* Do num times a push+pop from pos */
+        start = usec();
+        for (k = 0; k < num; k++) {
+            zl = ziplistPush(zl,(unsigned char*)"quux",4,pos);
+            zl = ziplistDeleteRange(zl,0,1);
+        }
+        printf("List size: %8d, bytes: %8d, %dx push+pop (%s): %6lld usec\n",
+            i,ZIPLIST_BYTES(zl),num,posstr[pos],usec()-start);
+        zfree(zl);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -918,6 +948,13 @@ int main(int argc, char **argv) {
         printf("SUCCESS\n");
     }
 
+    printf("Stress with variable ziplist size:\n");
+    {
+        stress(ZIPLIST_HEAD,100000,16384,256);
+        stress(ZIPLIST_TAIL,100000,16384,256);
+    }
+
     return 0;
 }
+
 #endif
