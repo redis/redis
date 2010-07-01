f9231c2c9a0ebc890f3304af924c9112265ec145
antirez <antirez@gmail.com>
2010-07-01 20:21:46 +0200

    Fixed MONITOR output for consistency: now integer encoded values are also formatted like this: "3932"

--- redis.c
+++ redis.c
@@ -2555,7 +2555,7 @@ static void replicationFeedMonitors(list *monitors, int dictid, robj **argv, int
 
     for (j = 0; j < argc; j++) {
         if (argv[j]->encoding == REDIS_ENCODING_INT) {
-            cmdrepr = sdscatprintf(cmdrepr, "%ld", (long)argv[j]->ptr);
+            cmdrepr = sdscatprintf(cmdrepr, "\"%ld\"", (long)argv[j]->ptr);
         } else {
             cmdrepr = sdscatrepr(cmdrepr,(char*)argv[j]->ptr,
                         sdslen(argv[j]->ptr));
