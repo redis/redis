d3b958c3fc32be66dac929ede482ef5fe8f5b462
antirez <antirez@gmail.com>
2010-07-01 20:22:46 +0200

    Fixed MONITOR output for consistency: now integer encoded values are also formatted like this: "3932"

--- src/replication.c
+++ src/replication.c
@@ -91,7 +91,7 @@ void replicationFeedMonitors(list *monitors, int dictid, robj **argv, int argc) 
 
     for (j = 0; j < argc; j++) {
         if (argv[j]->encoding == REDIS_ENCODING_INT) {
-            cmdrepr = sdscatprintf(cmdrepr, "%ld", (long)argv[j]->ptr);
+            cmdrepr = sdscatprintf(cmdrepr, "\"%ld\"", (long)argv[j]->ptr);
         } else {
             cmdrepr = sdscatrepr(cmdrepr,(char*)argv[j]->ptr,
                         sdslen(argv[j]->ptr));
