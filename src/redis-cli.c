b37ca6edb10faa0ebcf54a7d23cee31d895fe5b1
antirez <antirez@gmail.com>
2010-08-24 18:08:09 +0200

    Issue 179 fixed, now redis-cli is able to parse correctly multi bulk replies with elements that are errors

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -169,6 +169,7 @@ static int cliReadBulkReply(int fd) {
 static int cliReadMultiBulkReply(int fd) {
     sds replylen = cliReadLine(fd);
     int elements, c = 1;
+    int retval = 0;
 
     if (replylen == NULL) return 1;
     elements = atoi(replylen);
@@ -182,10 +183,10 @@ static int cliReadMultiBulkReply(int fd) {
     }
     while(elements--) {
         printf("%d. ", c);
-        if (cliReadReply(fd)) return 1;
+        if (cliReadReply(fd)) retval = 1;
         c++;
     }
-    return 0;
+    return retval;
 }
 
 static int cliReadReply(int fd) {
