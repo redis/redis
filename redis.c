dda93651f7dd782ba5cf8c8c19053a792dab80f3
antirez <antirez@gmail.com>
2010-08-24 16:27:40 +0200

    slave now detect lost connection during SYNC, fixing Issue 173

--- redis.c
+++ redis.c
@@ -8074,9 +8074,9 @@ static int syncWithMaster(void) {
         int nread, nwritten;
 
         nread = read(fd,buf,(dumpsize < 1024)?dumpsize:1024);
-        if (nread == -1) {
+        if (nread <= 0) {
             redisLog(REDIS_WARNING,"I/O error trying to sync with MASTER: %s",
-                strerror(errno));
+                (nread == -1) ? strerror(errno) : "connection lost");
             close(fd);
             close(dfd);
             return REDIS_ERR;
