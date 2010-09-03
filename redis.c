29a8c8d20b13d297cfd4a1f473727d15e9c3b2dc
antirez <antirez@gmail.com>
2010-09-03 11:44:05 +0200

    fix for the above fix

--- redis.c
+++ redis.c
@@ -9383,7 +9383,7 @@ static void vmThreadedIOCompletedJob(aeEventLoop *el, int fd, void *privdata,
     REDIS_NOTUSED(mask);
     REDIS_NOTUSED(privdata);
 
-    if (privdata == NULL) trytoswap = 0; /* check the comments above... */
+    if (privdata != NULL) trytoswap = 0; /* check the comments above... */
 
     /* For every byte we read in the read side of the pipe, there is one
      * I/O job completed to process. */
