5ca2f0c49894878be47161f667ae0daf70bb6fd3
antirez <antirez@gmail.com>
2010-09-22 16:09:30 +0200

    preventive conflict resolution to merge pietern/zset-mem

--- src/t_zset.c
+++ src/t_zset.c
@@ -355,7 +355,8 @@ void zaddGenericCommand(redisClient *c, robj *key, robj *ele, double scoreval, i
             *score = scoreval;
         }
         if (isnan(*score)) {
-            addReplyError(c,"resulting score is not a number (NaN)");
+            addReplySds(c,
+                sdsnew("-ERR resulting score is not a number (NaN)\r\n"));
             zfree(score);
             /* Note that we don't need to check if the zset may be empty and
              * should be removed here, as we can only obtain Nan as score if
