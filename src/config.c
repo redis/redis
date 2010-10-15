9f8ded8ced44fb4c764a8c7dde113ea870ce9c3e
antirez <antirez@gmail.com>
2010-10-15 12:29:05 +0200

    CONFIG RESETSTAT no longer resets the server uptime. Now keyspace hits/misses are reset as well.

--- src/config.c
+++ src/config.c
@@ -475,10 +475,11 @@ void configCommand(redisClient *c) {
         configGetCommand(c);
     } else if (!strcasecmp(c->argv[1]->ptr,"resetstat")) {
         if (c->argc != 2) goto badarity;
+        server.stat_keyspace_hits = 0;
+        server.stat_keyspace_misses = 0;
         server.stat_numcommands = 0;
         server.stat_numconnections = 0;
         server.stat_expiredkeys = 0;
-        server.stat_starttime = time(NULL);
         addReply(c,shared.ok);
     } else {
         addReplyError(c,
