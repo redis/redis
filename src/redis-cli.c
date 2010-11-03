339b9dc2d2e7d319e29581b367a1027365186cc3
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-11-03 17:07:10 +0100

    Put duration in parenthesis

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -432,8 +432,9 @@ static void repl() {
                             cliPrintContextErrorAndExit();
                     }
                     elapsed = mstime()-start_time;
-                    if (elapsed > 500) printf("%.2f seconds\n",
-                        (double)elapsed/1000);
+                    if (elapsed >= 500) {
+                        printf("(%.2fs)\n",(double)elapsed/1000);
+                    }
                 }
             }
             /* Free the argument vector */
