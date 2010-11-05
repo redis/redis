feecb608ed0ec8f9932960d5fa8f02e551ffbe59
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-11-05 10:22:52 +0100

    Use multi-bulk protocol in SET benchmark

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -467,12 +467,13 @@ int main(int argc, char **argv) {
 
         prepareForBenchmark("SET");
         c = createClient(REDIS_REPLY_STATUS);
-        c->obuf = sdscat(c->obuf,"SET foo_rand000000000000 ");
+        c->obuf = sdscat(c->obuf,"*3\r\n$3\r\nSET\r\n$20\r\nfoo_rand000000000000\r\n");
         {
             char *data = zmalloc(config.datasize+2);
             memset(data,'x',config.datasize);
             data[config.datasize] = '\r';
             data[config.datasize+1] = '\n';
+            c->obuf = sdscatprintf(c->obuf,"$%d\r\n",config.datasize);
             c->obuf = sdscatlen(c->obuf,data,config.datasize+2);
         }
         createMissingClients(c);
