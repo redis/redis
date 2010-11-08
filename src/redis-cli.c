11fd0c422b627d73d563916c6f559d3c0a021f26
antirez <antirez@gmail.com>
2010-11-08 16:26:02 +0100

    now redis-cli is able to show the Git SHA1 in the version output

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -67,6 +67,7 @@ static struct config {
 } config;
 
 static void usage();
+char *redisGitSHA1(void);
 
 /*------------------------------------------------------------------------------
  * Utility functions
@@ -351,7 +352,7 @@ static int parseOptions(int argc, char **argv) {
 "automatically used as last argument.\n"
             );
         } else if (!strcmp(argv[i],"-v")) {
-            printf("redis-cli shipped with Redis version %s\n", REDIS_VERSION);
+            printf("redis-cli shipped with Redis version %s (%s)\n", REDIS_VERSION, redisGitSHA1());
             exit(0);
         } else {
             break;
