59bd44d1c883b52507358996d1436d466c377e60
Damian Janowski & Michel Martens <damian.janowski+soveran@gmail.com>
2010-11-09 11:00:54 -0300

    Remove warning.

--- src/t_list.c
+++ src/t_list.c
@@ -818,6 +818,24 @@ int handleClientsWaitingListPush(redisClient *c, robj *key, robj *ele) {
     return 1;
 }
 
+int checkTimeout(redisClient *c, robj *object, time_t *timeout) {
+    long long lltimeout;
+
+    if (getLongLongFromObject(object, &lltimeout) != REDIS_OK) {
+        addReplyError(c, "timeout is not an integer");
+        return REDIS_ERR;
+    }
+
+    if (lltimeout < 0) {
+        addReplyError(c, "timeout is negative");
+        return REDIS_ERR;
+    }
+
+    *timeout = lltimeout;
+
+    return REDIS_OK;
+}
+
 /* Blocking RPOP/LPOP */
 void blockingPopGenericCommand(redisClient *c, int where) {
     robj *o;
@@ -879,24 +897,6 @@ void blockingPopGenericCommand(redisClient *c, int where) {
     /* If the list is empty or the key does not exists we must block */
     if (timeout > 0) timeout += time(NULL);
     blockForKeys(c, c->argv + 1, c->argc - 2, timeout, NULL);
-}
-
-int checkTimeout(redisClient *c, robj *object, time_t *timeout) {
-    long long lltimeout;
-
-    if (getLongLongFromObject(object, &lltimeout) != REDIS_OK) {
-        addReplyError(c, "timeout is not an integer");
-        return REDIS_ERR;
-    }
-
-    if (lltimeout < 0) {
-        addReplyError(c, "timeout is negative");
-        return REDIS_ERR;
-    }
-
-    *timeout = lltimeout;
-
-    return REDIS_OK;
 }
 
 void blpopCommand(redisClient *c) {
