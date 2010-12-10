586500c0ef32f480dba9bc10aedb3f72bd525118
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-12-10 12:06:24 +0100

    Typo

--- src/t_string.c
+++ src/t_string.c
@@ -88,7 +88,7 @@ static int getBitOffsetFromArgument(redisClient *c, robj *o, size_t *offset) {
     if (getLongLongFromObjectOrReply(c,o,&loffset,err) != REDIS_OK)
         return REDIS_ERR;
 
-    /* Limit offset to SIZE_T_MAX or 1GB in bytes */
+    /* Limit offset to SIZE_T_MAX or 512MB in bytes */
     if ((loffset < 0) ||
         ((unsigned long long)loffset >= (unsigned)SIZE_T_MAX) ||
         ((unsigned long long)loffset >> 3) >= (512*1024*1024))
