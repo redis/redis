b215a496a4fcab6b198ce4344b4c1b94c5b1eb12
antirez <antirez@gmail.com>
2010-12-10 17:03:38 +0100

    faster INCR doing far less allocation in common cases

--- src/t_string.c
+++ src/t_string.c
@@ -144,8 +144,23 @@ void incrDecrCommand(redisClient *c, long long incr) {
 
     o = lookupKeyWrite(c->db,c->argv[1]);
     if (o != NULL && checkType(c,o,REDIS_STRING)) return;
-    if (getLongLongFromObjectOrReply(c,o,&value,NULL) != REDIS_OK) return;
 
+    /* Fast path if the object is integer encoded and is not shared. */
+    if (o && o->refcount == 1 && o->encoding == REDIS_ENCODING_INT) {
+        long long newval = ((long)o->ptr) + incr;
+
+        if (newval >= LONG_MIN && newval <= LONG_MAX) {
+            o->ptr = (void*) (long) newval;
+            touchWatchedKey(c->db,c->argv[1]);
+            server.dirty++;
+            addReplyLongLong(c,newval);
+            return;
+        }
+        /* ... else take the usual safe path */
+    }
+
+    /* Otherwise we create a new object and replace the old one. */
+    if (getLongLongFromObjectOrReply(c,o,&value,NULL) != REDIS_OK) return;
     value += incr;
     o = createStringObjectFromLongLong(value);
     dbReplace(c->db,c->argv[1],o);
