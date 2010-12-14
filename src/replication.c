2b2eca1f5643260ae7ca6bec70c263ff0db5a8e3
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-12-14 17:39:34 +0100

    Zero-pad timestamps in MONITOR output
    Original report and fix:
    http://code.google.com/p/redis/issues/detail?id=404

--- src/replication.c
+++ src/replication.c
@@ -88,7 +88,7 @@ void replicationFeedMonitors(list *monitors, int dictid, robj **argv, int argc) 
     struct timeval tv;
 
     gettimeofday(&tv,NULL);
-    cmdrepr = sdscatprintf(cmdrepr,"%ld.%ld ",(long)tv.tv_sec,(long)tv.tv_usec);
+    cmdrepr = sdscatprintf(cmdrepr,"%ld.%06ld ",(long)tv.tv_sec,(long)tv.tv_usec);
     if (dictid != 0) cmdrepr = sdscatprintf(cmdrepr,"(db %d) ", dictid);
 
     for (j = 0; j < argc; j++) {
