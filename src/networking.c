401c3e213c89d98bbeda399b4e17fbd636b80ac0
antirez <antirez@gmail.com>
2010-12-15 16:07:49 +0100

    bulk transfers limited to 512 MB as this is the new limit of all the redis strings

--- src/networking.c
+++ src/networking.c
@@ -804,7 +804,7 @@ int processMultibulkBuffer(redisClient *c) {
                 bulklen = strtol(c->querybuf+pos+1,&eptr,10);
                 tolerr = (eptr[0] != '\r');
                 if (tolerr || bulklen == LONG_MIN || bulklen == LONG_MAX ||
-                    bulklen < 0 || bulklen > 1024*1024*1024)
+                    bulklen < 0 || bulklen > 512*1024*1024)
                 {
                     addReplyError(c,"Protocol error: invalid bulk length");
                     setProtocolError(c,pos);
