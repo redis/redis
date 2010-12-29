d021221086c9a6c5af404f3017791605b70c592d
antirez <antirez@gmail.com>
2010-12-29 17:02:43 +0100

    version set to 2.3.0

--- src/diskstore.c
+++ src/diskstore.c
@@ -97,7 +97,7 @@ int dsOpen(void) {
     /* New disk store, create the directory structure now, as creating
      * them in a lazy way is not a good idea, after very few insertions
      * we'll need most of the 65536 directories anyway. */
-    if (mkdir(path) == -1) {
+    if (mkdir(path,0644) == -1) {
         redisLog(REDIS_WARNING,"Disk store init failed creating dir %s: %s",
             path, strerror(errno));
         return REDIS_ERR;
