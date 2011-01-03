6be6b703c6e2c811b0c2fe987138298a8ce185e7
Chip <chip@pobox.com>
2011-01-03 11:37:43 -0800

    don't assume size_t == long

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -143,7 +143,7 @@ static void randomizeClientKey(client c) {
 
     for (i = 0; i < c->randlen; i++) {
         r = random() % config.randomkeys_keyspacelen;
-        snprintf(buf,sizeof(buf),"%012lu",r);
+        snprintf(buf,sizeof(buf),"%012lu",(unsigned long)r);
         memcpy(c->randptr[i],buf,12);
     }
 }
