0a0f83ab2c02ebf1a9029a473e23ef72d037cac0
antirez <antirez@gmail.com>
2011-01-09 19:01:44 +0100

    DEBUG FLUSHCACHE needs to wait that everything was synched on disk

--- src/debug.c
+++ src/debug.c
@@ -182,6 +182,9 @@ void debugCommand(redisClient *c) {
             addReplyError(c, "DEBUG FLUSHCACHE called with diskstore off.");
             return;
         } else {
+            /* To flush the whole cache we need to wait for everything to
+             * be flushed on disk... */
+            cacheForcePointInTime();
             emptyDb();
             addReply(c,shared.ok);
             return;
