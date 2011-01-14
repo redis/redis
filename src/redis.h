ebb07fb4bbe971460df206f5c2fded037207d7b0
antirez <antirez@gmail.com>
2011-01-14 10:20:02 +0100

    implemented two new INFO fields showing the size of clients max input and output buffers.

--- src/redis.h
+++ src/redis.h
@@ -656,6 +656,8 @@ void addReplyDouble(redisClient *c, double d);
 void addReplyLongLong(redisClient *c, long long ll);
 void addReplyMultiBulkLen(redisClient *c, long length);
 void *dupClientReplyValue(void *o);
+void getClientsMaxBuffers(unsigned long *longest_output_list,
+                          unsigned long *biggest_input_buffer);
 
 #ifdef __GNUC__
 void addReplyErrorFormat(redisClient *c, const char *fmt, ...)
