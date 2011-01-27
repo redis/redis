71791e7a8ed2de79f6bcaf2056ee6e502fcb0a65
antirez <antirez@gmail.com>
2011-01-27 16:52:37 +0100

    propagate key eviction to slaves and AOF, like for expires, so that replicas and AOFs are always consistent even in maxmemory mode.

--- src/redis.c
+++ src/redis.c
@@ -1457,6 +1457,7 @@ void freeMemoryIfNeeded(void) {
             /* Finally remove the selected key. */
             if (bestkey) {
                 robj *keyobj = createStringObject(bestkey,sdslen(bestkey));
+                propagateExpire(db,keyobj);
                 dbDelete(db,keyobj);
                 server.stat_evictedkeys++;
                 decrRefCount(keyobj);
