a63773fae521c9b831e50226850f5af2a9bc3e90
antirez <antirez@gmail.com>
2011-03-04 16:24:30 +0100

    AOF file descriptor leak fix backported from 2.2

--- redis.c
+++ redis.c
@@ -8418,8 +8418,10 @@ int loadAppendOnlyFile(char *filename) {
     struct redis_stat sb;
     int appendonly = server.appendonly;
 
-    if (redis_fstat(fileno(fp),&sb) != -1 && sb.st_size == 0)
+    if (fp && redis_fstat(fileno(fp),&sb) != -1 && sb.st_size == 0) {
+        fclose(fp);
         return REDIS_ERR;
+    }
 
     if (fp == NULL) {
         redisLog(REDIS_WARNING,"Fatal error: can't open the append log file for reading: %s",strerror(errno));
