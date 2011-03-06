699465c31e2f35b6be53b4da2aa27258e322e480
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-03-06 20:46:40 +0100

    Store SELECTed database for reconnect (issue #468)

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -264,11 +264,9 @@ static int cliAuth() {
 /* Send SELECT dbnum to the server */
 static int cliSelect() {
     redisReply *reply;
-    char dbnum[16];
     if (config.dbnum == 0) return REDIS_OK;
 
-    snprintf(dbnum,sizeof(dbnum),"%d",config.dbnum);
-    reply = redisCommand(context,"SELECT %s",dbnum);
+    reply = redisCommand(context,"SELECT %d",config.dbnum);
     if (reply != NULL) {
         freeReplyObject(reply);
         return REDIS_OK;
@@ -492,6 +490,10 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
         if (cliReadReply(output_raw) != REDIS_OK) {
             free(argvlen);
             return REDIS_ERR;
+        } else {
+            /* Store database number when SELECT was successfully executed. */
+            if (!strcasecmp(command,"select") && argc == 2)
+                config.dbnum = atoi(argv[1]);
         }
     }
 
