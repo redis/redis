6c390c0b2303247c16f42160fec3fd609cb99cb7
antirez <antirez@gmail.com>
2011-03-30 18:12:51 +0200

    minor change with no actual effects til the loading node config code is in place

--- src/cluster.c
+++ src/cluster.c
@@ -94,9 +94,9 @@ void clusterInit(void) {
          * by the createClusterNode() function. */
         redisLog(REDIS_NOTICE,"No cluster configuration found, I'm %.40s",
             server.cluster.myself->name);
+        clusterAddNode(server.cluster.myself);
         saveconf = 1;
     }
-    clusterAddNode(server.cluster.myself);
     if (saveconf) clusterSaveConfigOrDie();
     /* We need a listening TCP port for our cluster messaging needs */
     server.cfd = anetTcpServer(server.neterr,
