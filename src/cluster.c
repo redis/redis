f797c7dc176c833e6aa412c557d7fedd59dc1124
antirez <antirez@gmail.com>
2011-04-05 13:57:28 +0200

    bad data on RESTORE can no longer crash the server but create a memory leak with some input string

--- src/cluster.c
+++ src/cluster.c
@@ -1140,8 +1140,10 @@ void restoreCommand(redisClient *c) {
 
     /* Finally create the object from the serialized dump and
      * store it at the specified key. */
-    o = rdbLoadObject(data[0],fp);
-    if (o == NULL) {
+    if ((data[0] > 4 && data[0] < 9) ||
+         data[0] > 11 ||
+        (o = rdbLoadObject(data[0],fp)) == NULL)
+    {
         addReplyError(c,"Bad data format.");
         fclose(fp);
         return;
