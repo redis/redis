9d89e99c9946ab199d295db6faa1953252d7b80c
antirez <antirez@gmail.com>
2011-04-13 11:42:05 +0200

    no longer useful debugging printf removed

--- src/cluster.c
+++ src/cluster.c
@@ -994,7 +994,6 @@ int clusterNodeGetSlotBit(clusterNode *n, int slot) {
 int clusterAddSlot(clusterNode *n, int slot) {
     redisAssert(clusterNodeSetSlotBit(n,slot) == 0);
     server.cluster.slots[slot] = n;
-    printf("SLOT %d added to %.40s\n", slot, n->name);
     return REDIS_OK;
 }
 
