1eb713a4c1e601bfc86963f7eab3fe36a0a65b17
antirez <antirez@gmail.com>
2011-04-29 14:31:18 +0200

    CLUSTER KEYSLOT command

--- src/cluster.c
+++ src/cluster.c
@@ -1205,6 +1205,10 @@ void clusterCommand(redisClient *c) {
             (unsigned long)sdslen(info)));
         addReplySds(c,info);
         addReply(c,shared.crlf);
+    } else if (!strcasecmp(c->argv[1]->ptr,"keyslot") && c->argc == 3) {
+        sds key = c->argv[2]->ptr;
+
+        addReplyLongLong(c,keyHashSlot(key,sdslen(key)));
     } else {
         addReplyError(c,"Wrong CLUSTER subcommand or number of arguments");
     }
