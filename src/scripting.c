b28fa1813167b1c36860857911cd78d802ba6a29
antirez <antirez@gmail.com>
2011-05-01 15:48:26 +0200

    Multi bulk to Lua table now working

--- src/scripting.c
+++ src/scripting.c
@@ -98,17 +98,15 @@ char *redisProtocolToLuaType_MultiBulk(lua_State *lua, char *reply) {
     long long mbulklen;
     int j = 0;
 
-    printf("--%s-- (%d)\n", reply,(int)(p-reply-1));
     string2ll(reply+1,p-reply-1,&mbulklen);
     p += 2;
     if (mbulklen == -1) {
         lua_pushnil(lua);
         return p;
     }
-    printf("BL: %lld\n", mbulklen);
     lua_newtable(lua);
     for (j = 0; j < mbulklen; j++) {
-        lua_pushnumber(lua,j);
+        lua_pushnumber(lua,j+1);
         p = redisProtocolToLuaType(lua,p);
         lua_settable(lua,-3);
     }
