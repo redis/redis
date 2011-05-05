4763ecc9add311467c9c3852a81664a0b3005919
antirez <antirez@gmail.com>
2011-05-05 17:52:19 +0200

    missing return caused protocol desync in CLUSTER SETSLOT

--- src/cluster.c
+++ src/cluster.c
@@ -1241,6 +1241,7 @@ void clusterCommand(redisClient *c) {
             server.cluster.importing_slots_from[slot] = NULL;
         } else {
             addReplyError(c,"Invalid CLUSTER SETSLOT action or number of arguments");
+            return;
         }
         clusterSaveConfigOrDie();
         addReply(c,shared.ok);
