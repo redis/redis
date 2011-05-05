46834808fe31e7cbf08d6c9f7638a54c5c8bff8a
antirez <antirez@gmail.com>
2011-05-05 17:56:12 +0200

    CLUSTER SETSLOT STABLE fixed

--- src/cluster.c
+++ src/cluster.c
@@ -1239,6 +1239,7 @@ void clusterCommand(redisClient *c) {
             server.cluster.importing_slots_from[slot] = n;
         } else if (!strcasecmp(c->argv[3]->ptr,"stable") && c->argc == 4) {
             server.cluster.importing_slots_from[slot] = NULL;
+            server.cluster.migrating_slots_to[slot] = NULL;
         } else {
             addReplyError(c,"Invalid CLUSTER SETSLOT action or number of arguments");
             return;
