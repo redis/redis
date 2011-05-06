ca0e7452e45b27f93f2846070d653acdf1bbc633
antirez <antirez@gmail.com>
2011-05-06 17:30:54 +0200

    Lua max exec time fixes: redisLog() to log the event moved before the lua_error() call that does a long jump so never returns. Aesthetic fixes in error message.

--- src/scripting.c
+++ src/scripting.c
@@ -206,9 +206,9 @@ void luaMaskCountHook(lua_State *lua, lua_Debug *ar) {
     if (server.lua_time_limit <= 0) return;
     elapsed = (ustime()/1000) - server.lua_time_start;
     if (elapsed >= server.lua_time_limit) {
-        lua_pushstring(lua,"Script aborted for max execution time...");
+        redisLog(REDIS_NOTICE,"Lua script aborted for max execution time after %lld milliseconds of running time.",elapsed);
+        lua_pushstring(lua,"Script aborted for max execution time.");
         lua_error(lua);
-        redisLog(REDIS_NOTICE,"Lua script aborted for max execution time after %lld milliseconds of running time",elapsed);
     }
 }
 
