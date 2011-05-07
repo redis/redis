d121c5d71bf79a0d0fbd1467da71be5d7bd2b48c
antirez <antirez@gmail.com>
2011-05-07 11:40:29 +0200

    Lua memory usage in INFO

--- src/redis.c
+++ src/redis.c
@@ -1320,6 +1320,7 @@ sds genRedisInfoString(char *section) {
             "used_memory_rss:%zu\r\n"
             "used_memory_peak:%zu\r\n"
             "used_memory_peak_human:%s\r\n"
+            "used_memory_lua:%lld\r\n"
             "mem_fragmentation_ratio:%.2f\r\n"
             "mem_allocator:%s\r\n",
             zmalloc_used_memory(),
@@ -1327,6 +1328,7 @@ sds genRedisInfoString(char *section) {
             zmalloc_get_rss(),
             server.stat_peak_memory,
             peak_hmem,
+            ((long long)lua_gc(server.lua,LUA_GCCOUNT,0))*1024LL,
             zmalloc_get_fragmentation_ratio(),
             REDIS_MALLOC
             );
