5464e52314fdc3450448cd93214c56b41c276bb7
Louis-Philippe Perron <lp@spiralix.org>
2011-05-14 12:31:08 -0400

    basic sandbox unit tests

--- tests/unit/scripting.tcl
+++ tests/unit/scripting.tcl
@@ -13,5 +13,45 @@ start_server {
         assert_equal "1 2 3 4 5" [r eval "return {1,2,3,4,5}" 0]
         assert_error "ERR Error*lua error*" {r eval "return error('lua error')" 0}
     }
+    
+    test "EVAL - sandbox allowed libraries - base" {
+        reconnect
+        assert_match "function:*" [r eval "return tostring(ipairs)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries - package" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(package)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries - table" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(table)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries - os" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(os)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries - string" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(string)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries - math" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(math)" 0]
+    }
+    
+    test "EVAL - sandbox allowed libraries debug" {
+        reconnect
+        assert_match "table:*" [r eval "return tostring(debug)" 0]
+    }
+    
+    test "EVAL - sandbox disallowed libraries - io" {
+        reconnect
+        assert_error "*attempt to index global 'io' (a nil value)*" {r eval "return io.flush()"  0}
+    }
 
 }
