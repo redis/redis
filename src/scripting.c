e927a246108e78dfaf6e02d604a674234e5116d3
antirez <antirez@gmail.com>
2011-05-16 18:36:07 +0200

    Fix for wrong error level detection in redis.log()

--- src/scripting.c
+++ src/scripting.c
@@ -209,11 +209,11 @@ int luaLogCommand(lua_State *lua) {
         luaPushError(lua, "redis.log() requires two arguments or more.");
         return 1;
     } else if (!lua_isnumber(lua,-argc)) {
-        luaPushError(lua, "First argument must be a number.");
+        luaPushError(lua, "First argument must be a number (log level).");
         return 1;
     }
     level = lua_tonumber(lua,-argc);
-    if (level < REDIS_DEBUG && level > REDIS_WARNING) {
+    if (level < REDIS_DEBUG || level > REDIS_WARNING) {
         luaPushError(lua, "Invalid debug level.");
         return 1;
     }
