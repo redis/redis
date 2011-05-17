7f9022e26457460bdc817496a8e819e5f150c945
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-05-17 17:13:04 +0200

    Fix up Makefile to statically link jemalloc

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -51,6 +51,11 @@
 #define calloc(count,size) tc_calloc(count,size)
 #define realloc(ptr,size) tc_realloc(ptr,size)
 #define free(ptr) tc_free(ptr)
+#elif defined(USE_JEMALLOC)
+#define malloc(size) je_malloc(size)
+#define calloc(count,size) je_calloc(count,size)
+#define realloc(ptr,size) je_realloc(ptr,size)
+#define free(ptr) je_free(ptr)
 #endif
 
 #define update_zmalloc_stat_alloc(__n,__size) do { \
