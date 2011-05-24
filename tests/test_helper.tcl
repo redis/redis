b02e0388e3f10f53791ab95432fd0bdb00022823
antirez <antirez@gmail.com>
2011-05-24 18:40:37 +0200

    Scripting tests added

--- tests/test_helper.tcl
+++ tests/test_helper.tcl
@@ -125,6 +125,7 @@ proc execute_everything {} {
     execute_tests "integration/aof"
 #    execute_tests "integration/redis-cli"
     execute_tests "unit/pubsub"
+    execute_tests "unit/scripting"
 
     # run tests with VM enabled
     set ::global_overrides {vm-enabled yes}
