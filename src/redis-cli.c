442c748d841bb3603ba1194527ca9da6a1b3cd21
antirez <antirez@gmail.com>
2011-05-28 15:25:48 +0200

    redis-cli: Use the repetiton prefix after a reconnection.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -696,7 +696,8 @@ static void repl() {
 
                         /* If we still cannot send the command print error.
                          * We'll try to reconnect the next time. */
-                        if (cliSendCommand(argc,argv,1) != REDIS_OK)
+                        if (cliSendCommand(argc-skipargs,argv+skipargs,repeat)
+                            != REDIS_OK)
                             cliPrintContextError();
                     }
                     elapsed = mstime()-start_time;
