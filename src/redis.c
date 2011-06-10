73b9e8aedfc42d4b4b9f2902969e8bc219736f3d
antirez <antirez@gmail.com>
2011-06-10 14:44:06 +0200

    debugging printf removed

--- src/redis.c
+++ src/redis.c
@@ -692,7 +692,6 @@ int serverCron(struct aeEventLoop *eventLoop, long long id, void *clientData) {
          }
 
          /* Trigger an AOF rewrite if needed */
-         printf("%d\n", server.auto_aofrewrite_perc);
          if (server.auto_aofrewrite_perc &&
              server.appendonly_current_size > server.auto_aofrewrite_min_size)
          {
