d630abcdaf869345fed8fe508774d0318d1d82ff
antirez <antirez@gmail.com>
2011-06-10 14:52:47 +0200

    Added new INFO fields related to the new AOF auto rewrite feature

--- src/redis.c
+++ src/redis.c
@@ -1393,6 +1393,16 @@ sds genRedisInfoString(char *section) {
             server.lastsave,
             server.bgrewritechildpid != -1);
 
+        if (server.appendonly) {
+            info = sdscatprintf(info,
+                "aof_current_size:%lld\r\n"
+                "aof_base_size:%lld\r\n"
+                "aof_pending_rewrite:%d\r\n",
+                (long long) server.appendonly_current_size,
+                (long long) server.auto_aofrewrite_base_size,
+                server.aofrewrite_scheduled);
+        }
+
         if (server.loading) {
             double perc;
             time_t eta, elapsed;
