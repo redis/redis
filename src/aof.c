9e40bce3fa178c5a79188f63ab6f1acf81500a53
antirez <antirez@gmail.com>
2011-06-10 18:35:16 +0200

    different message on BGREWRITEAOF when it is just scheduled and not started.

--- src/aof.c
+++ src/aof.c
@@ -619,7 +619,7 @@ void bgrewriteaofCommand(redisClient *c) {
         addReplyError(c,"Background append only file rewriting already in progress");
     } else if (server.bgsavechildpid != -1) {
         server.aofrewrite_scheduled = 1;
-        addReplyStatus(c,"Background append only file rewriting started");
+        addReplyStatus(c,"Background append only file rewriting scheduled");
     } else if (rewriteAppendOnlyFileBackground() == REDIS_OK) {
         addReplyStatus(c,"Background append only file rewriting started");
     } else {
