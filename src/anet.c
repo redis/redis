a528c6dd72a296fe312c2a573c7b98dfb8486d49
Geoff Garside <geoff@geoffgarside.co.uk>
2011-06-17 00:55:00 +0100

    Add anetSetReuseAddr(err, fd) static function.
    Extract setting SO_REUSEADDR socket option into separate function
    so the same code can be more easily used by anetCreateSocket and
    other functions.

--- src/anet.c
+++ src/anet.c
@@ -129,17 +129,24 @@ int anetResolve(char *err, char *host, char *ipbuf, size_t ipbuf_len)
     return ANET_OK;
 }
 
+static int anetSetReuseAddr(char *err, int fd) {
+    int yes = 1;
+    /* Make sure connection-intensive things like the redis benckmark
+     * will be able to close/open sockets a zillion of times */
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        anetSetError(err, "setsockopt SO_REUSEADDR: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
 static int anetCreateSocket(char *err, int domain) {
-    int s, on = 1;
+    int s;
     if ((s = socket(domain, SOCK_STREAM, 0)) == -1) {
         anetSetError(err, "creating socket: %s", strerror(errno));
         return ANET_ERR;
     }
-
-    /* Make sure connection-intensive things like the redis benckmark
-     * will be able to close/open sockets a zillion of times */
-    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
-        anetSetError(err, "setsockopt SO_REUSEADDR: %s", strerror(errno));
+    if (anetSetReuseAddr(err,s) == ANET_ERR) {
         return ANET_ERR;
     }
     return s;
