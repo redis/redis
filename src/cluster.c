c8d7b9cde434f103ded9414b5e28e4869a7d1ac5
Geoff Garside <geoff@geoffgarside.co.uk>
2011-06-17 20:37:45 +0100

    Use inet_pton(3) in clusterCommand.
    Replace inet_aton(3) call with the more future proof inet_pton(3)
    function which is capable of handling additional address families.

--- src/cluster.c
+++ src/cluster.c
@@ -1237,7 +1237,7 @@ void clusterCommand(redisClient *c) {
         long port;
 
         /* Perform sanity checks on IP/port */
-        if (inet_aton(c->argv[2]->ptr,&sa.sin_addr) == 0) {
+        if (inet_pton(AF_INET,c->argv[0]->ptr,&(sa.sin_addr)) == 0) {
             addReplyError(c,"Invalid IP address in MEET");
             return;
         }
