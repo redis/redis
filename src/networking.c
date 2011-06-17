30836721f210f474c317d464f3231b660fcc22b8
Geoff Garside <geoff@geoffgarside.co.uk>
2011-06-17 19:54:17 +0100

    Update anetTcpAccept & anetPeerToString calls.
    Add the additional ip buffer length argument to function calls of
    anetTcpAccept and anetPeerToString in network.c and cluster.c

--- src/networking.c
+++ src/networking.c
@@ -557,7 +557,7 @@ void acceptTcpHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
     REDIS_NOTUSED(mask);
     REDIS_NOTUSED(privdata);
 
-    cfd = anetTcpAccept(server.neterr, fd, cip, &cport);
+    cfd = anetTcpAccept(server.neterr, fd, cip, sizeof(cip), &cport);
     if (cfd == AE_ERR) {
         redisLog(REDIS_WARNING,"Accepting client connection: %s", server.neterr);
         return;
@@ -1129,7 +1129,7 @@ sds getClientInfoString(redisClient *client) {
     int emask;
 
     if (!(client->flags & REDIS_UNIX_SOCKET))
-        anetPeerToString(client->fd,ip,&port);
+        anetPeerToString(client->fd,ip,sizeof(ip),&port);
     p = flags;
     if (client->flags & REDIS_SLAVE) {
         if (client->flags & REDIS_MONITOR)
@@ -1210,7 +1210,7 @@ void clientCommand(redisClient *c) {
             int port;
 
             client = listNodeValue(ln);
-            if (anetPeerToString(client->fd,ip,&port) == -1) continue;
+            if (anetPeerToString(client->fd,ip,sizeof(ip),&port) == -1) continue;
             snprintf(addr,sizeof(addr),"%s:%d",ip,port);
             if (strcmp(addr,c->argv[2]->ptr) == 0) {
                 addReply(c,shared.ok);
