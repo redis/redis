5b88f1e6d2d1d7a2f10661a9bf7a89e0f17456b9
Geoff Garside <geoff@geoffgarside.co.uk>
2011-06-18 19:54:05 +0100

    Change anetTcpGenericConnect to use AF_UNSPEC.
    This allows anetTcpGenericConnect to try to connect to AF_INET6
    addresses in addition to any resolved AF_INET addresses.

--- src/anet.c
+++ src/anet.c
@@ -164,7 +164,7 @@ static int anetTcpGenericConnect(char *err, char *addr, int port, int flags)
 
     snprintf(_port,6,"%d",port);
     memset(&hints,0,sizeof(hints));
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
     if ((rv = getaddrinfo(addr,_port,&hints,&servinfo)) != 0) {
