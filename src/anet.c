227fd8ac276ba2e4f250205c66d773a8308a2b2c
Geoff Garside <geoff@geoffgarside.co.uk>
2011-06-18 14:47:38 +0100

    Update anetTcpAccept to handle AF_INET6 addresses.
    Change the sockaddr_in to sockaddr_storage which is capable of storing
    both AF_INET and AF_INET6 sockets. Uses the sockaddr_storage ss_family
    to correctly return the printable IP address and port.

--- src/anet.c
+++ src/anet.c
@@ -374,13 +374,20 @@ static int anetGenericAccept(char *err, int s, struct sockaddr *sa, socklen_t *l
 
 int anetTcpAccept(char *err, int s, char *ip, size_t ip_len, int *port) {
     int fd;
-    struct sockaddr_in sa;
+    struct sockaddr_storage sa;
     socklen_t salen = sizeof(sa);
     if ((fd = anetGenericAccept(err,s,(struct sockaddr*)&sa,&salen)) == ANET_ERR)
         return ANET_ERR;
 
-    if (ip) inet_ntop(sa.sin_family,(void*)&(sa.sin_addr),ip,ip_len);
-    if (port) *port = ntohs(sa.sin_port);
+    if (sa.ss_family == AF_INET) {
+        struct sockaddr_in *s = (struct sockaddr_in *)&sa;
+        if (ip) inet_ntop(AF_INET,(void*)&(s->sin_addr),ip,ip_len);
+        if (port) *port = ntohs(s->sin_port);
+    } else {
+        struct sockaddr_in6 *s = (struct sockaddr_in6 *)&sa;
+        if (ip) inet_ntop(AF_INET6,(void*)&(s->sin6_addr),ip,ip_len);
+        if (port) *port = ntohs(s->sin6_port);
+    }
     return fd;
 }
 
