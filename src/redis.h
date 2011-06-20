c1c9d551da6dd534c8dae051a3a7e64bf7db6bfb
antirez <antirez@gmail.com>
2011-06-20 17:07:18 +0200

    Fix for bug 561 and other related problems

--- src/redis.h
+++ src/redis.h
@@ -819,6 +819,7 @@ void addReplyMultiBulkLen(redisClient *c, long length);
 void *dupClientReplyValue(void *o);
 void getClientsMaxBuffers(unsigned long *longest_output_list,
                           unsigned long *biggest_input_buffer);
+void rewriteClientCommandVector(redisClient *c, int argc, ...);
 
 #ifdef __GNUC__
 void addReplyErrorFormat(redisClient *c, const char *fmt, ...)
