15bc1cc1bcd4174e7116b5429f1f31f792d13d2e
antirez <antirez@gmail.com>
2011-06-25 12:42:55 +0200

    colorized test output shows scripting tests with X

--- tests/support/test.tcl
+++ tests/support/test.tcl
@@ -80,6 +80,9 @@ proc colored_dot {tags passed} {
         } elseif {[lsearch $tags basic] != -1} {
             set colorcode {35}
             set ch B
+        } elseif {[lsearch $tags scripting] != -1} {
+            set colorcode {36}
+            set ch X
         } else {
             set colorcode {37}
             set ch .
