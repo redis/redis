5ca75751ed0ff6d37f52b2ff9c9ebc43b5459b00
antirez <antirez@gmail.com>
2011-09-13 16:19:37 +0200

    typo fixed in bio.c

--- src/bio.c
+++ src/bio.c
@@ -85,7 +85,7 @@ void *bioProcessBackgroundJobs(void *arg) {
         listNode *ln;
 
         /* The loop always starts with the lock hold. */
-        if (listLength(server.io_newjobs) == 0) {
+        if (listLength(bio_jobs) == 0) {
             pthread_cond_wait(&bio_condvar,&bio_mutex);
             continue;
         }
@@ -99,6 +99,7 @@ void *bioProcessBackgroundJobs(void *arg) {
 
         /* Process the job accordingly to its type. */
         if (job->type == REDIS_BIO_CLOSE_FILE) {
+            printf("Closing file...\n");
             close((long)job->data);
         } else {
             redisPanic("Wrong job type in bioProcessBackgroundJobs().");
