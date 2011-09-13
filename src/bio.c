a72c0423b43def624d69d27f844bb2f89d32cbff
antirez <antirez@gmail.com>
2011-09-13 16:53:33 +0200

    debug message removed

--- src/bio.c
+++ src/bio.c
@@ -100,7 +100,6 @@ void *bioProcessBackgroundJobs(void *arg) {
 
         /* Process the job accordingly to its type. */
         if (job->type == REDIS_BIO_CLOSE_FILE) {
-            printf("Closing file...\n");
             close((long)job->data);
         } else {
             redisPanic("Wrong job type in bioProcessBackgroundJobs().");
