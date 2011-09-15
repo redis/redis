96674b6d95c8990cc7dae885b27c76f377ddeb91
antirez <antirez@gmail.com>
2011-09-15 19:32:25 +0200

    Fixed a bug in the average latency metering of redis-cli --latency

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -765,6 +765,7 @@ static void latencyMode(void) {
         } else {
             if (latency < min) min = latency;
             if (latency > max) max = latency;
+            tot += latency;
             avg = (double) tot/count;
         }
         printf("\x1b[0G\x1b[2Kmin: %lld, max: %lld, avg: %.2f (%lld samples)",
