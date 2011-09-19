de9264deac93148dc4527e14ffc88c639e065236
Geoff Garside <geoff@geoffgarside.co.uk>
2011-09-19 23:31:41 +0100

    Add static anetV6Only() function.
    This function sets the IPV6_V6ONLY option to 1 to use separate stack
    IPv6 sockets.

--- src/anet.c
+++ src/anet.c
@@ -301,6 +301,16 @@ static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len) {
     return ANET_OK;
 }
 
+static int anetV6Only(char *err, int s) {
+    int yes = 1;
+    if (setsockopt(s,IPPROTO_IPV6,IPV6_V6ONLY,&yes,sizeof(yes)) == -1) {
+        anetSetError(err, "setsockopt: %s", strerror(errno));
+        close(s);
+        return ANET_ERR;
+    }
+    return ANET_OK;
+}
+
 int anetTcpServer(char *err, int port, char *bindaddr)
 {
     int s, rv;
