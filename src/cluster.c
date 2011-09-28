1ef8b0a96d9b1a9f3a69914ece8e20b3c3d4c64c
antirez <antirez@gmail.com>
2011-09-28 18:28:36 +0200

    show node with myself flag always as connected in CLUSTER NODES output.

--- src/cluster.c
+++ src/cluster.c
@@ -1099,7 +1099,8 @@ sds clusterGenNodesDescription(void) {
         ci = sdscatprintf(ci,"%ld %ld %s",
             (long) node->ping_sent,
             (long) node->pong_received,
-            node->link ? "connected" : "disconnected");
+            (node->link || node->flags & REDIS_NODE_MYSELF) ?
+                        "connected" : "disconnected");
 
         /* Slots served by this instance */
         start = -1;
