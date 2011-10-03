c01043baf1b58c064f6ced68abc3c512a022d61a
dvir volk <dvir@doit9.com>
2011-10-03 11:29:01 +0200

    changed license to gpl v3

--- utils/install_server.sh
+++ utils/install_server.sh
@@ -1,10 +1,26 @@
 #! /bin/sh
+#
+#    Copyright (c) 2011, Dvir Volk <dvirsky at gmail dot com >
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+#
 # Interactive service installer for redis server
 # this generates a redis config file and an /etc/init.d script, and installs them
 # this scripts should be run as root
 #
-# Contributed by Dvir Volk dvirsky at gmail dot com 2011
-# This code is contributed to public domain
 
 
 die () {
