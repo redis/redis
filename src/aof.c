ff15dba09390b8b0a864e57fb0fdd84b20d4193d
antirez <antirez@gmail.com>
2011-10-08 10:58:57 +0200

    not used parameter for redisLog() removed. Error message grammar fixed. Thanks to @ardsrk.

--- src/aof.c
+++ src/aof.c
@@ -52,7 +52,7 @@ int startAppendOnly(void) {
     if (rewriteAppendOnlyFileBackground() == REDIS_ERR) {
         server.appendonly = 0;
         close(server.appendfd);
-        redisLog(REDIS_WARNING,"Used tried to switch on AOF via CONFIG, I can't trigger a background AOF rewrite operation. Check the above logs for more info about the error.",strerror(errno));
+        redisLog(REDIS_WARNING,"User tried turning on AOF with CONFIG SET but I can't trigger a background AOF rewrite operation. Check the above logs for more info about the error.");
         return REDIS_ERR;
     }
     return REDIS_OK;
