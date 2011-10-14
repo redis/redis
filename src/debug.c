80ad7189e8de69ce5f7950dcdea4789be7db7014
antirez <antirez@gmail.com>
2011-10-14 14:30:41 +0200

    More informative error when DEBUG RELOAD fails.

--- src/debug.c
+++ src/debug.c
@@ -222,7 +222,7 @@ void debugCommand(redisClient *c) {
         }
         emptyDb();
         if (rdbLoad(server.dbfilename) != REDIS_OK) {
-            addReply(c,shared.err);
+            addReplyError(c,"Error trying to load the RDB dump");
             return;
         }
         redisLog(REDIS_WARNING,"DB reloaded by DEBUG RELOAD");
