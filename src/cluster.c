e0aab1fc79a89bf0c6ed639e7823b27e73acd1db
antirez <antirez@gmail.com>
2011-10-17 16:44:08 +0200

    MIGRATE +NOKEY error was missing the final CRLF causing a protocol error.

--- src/cluster.c
+++ src/cluster.c
@@ -1530,7 +1530,7 @@ void migrateCommand(redisClient *c) {
      * nothing to migrate (for instance the key expired in the meantime), but
      * we include such information in the reply string. */
     if ((o = lookupKeyRead(c->db,c->argv[3])) == NULL) {
-        addReplySds(c,sdsnew("+NOKEY"));
+        addReplySds(c,sdsnew("+NOKEY\r\n"));
         return;
     }
     
