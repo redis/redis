b6fe152b83ebc130c11192099b61b95bc0c227e3
antirez <antirez@gmail.com>
2011-10-18 17:12:16 +0200

    Fix to the previous fix for issue #145, make sure to return only after the event was unregistered.

--- src/replication.c
+++ src/replication.c
@@ -376,18 +376,18 @@ void syncWithMaster(aeEventLoop *el, int fd, void *privdata, int mask) {
     REDIS_NOTUSED(privdata);
     REDIS_NOTUSED(mask);
 
+    redisLog(REDIS_NOTICE,"Non blocking connect for SYNC fired the event.");
+    /* This event should only be triggered once since it is used to have a
+     * non-blocking connect(2) to the master. It has been triggered when this
+     * function is called, so we can delete it. */
+    aeDeleteFileEvent(server.el,fd,AE_READABLE|AE_WRITABLE);
+
     /* If this event fired after the user turned the instance into a master
      * with SLAVEOF NO ONE we must just return ASAP. */
     if (server.replstate == REDIS_REPL_NONE) {
         close(fd);
         return;
     }
-
-    redisLog(REDIS_NOTICE,"Non blocking connect for SYNC fired the event.");
-    /* This event should only be triggered once since it is used to have a
-     * non-blocking connect(2) to the master. It has been triggered when this
-     * function is called, so we can delete it. */
-    aeDeleteFileEvent(server.el,fd,AE_READABLE|AE_WRITABLE);
 
     /* AUTH with the master if required. */
     if(server.masterauth) {
