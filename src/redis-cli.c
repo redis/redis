7d31a020eaa316416a81984912d9645e9ddd07ac
antirez <antirez@gmail.com>
2011-10-28 17:43:04 +0200

    Fixed a memory leak in redis-cli.c

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -676,6 +676,7 @@ static void repl() {
 
             if (argv == NULL) {
                 printf("Invalid argument(s)\n");
+                free(line);
                 continue;
             } else if (argc > 0) {
                 if (strcasecmp(argv[0],"quit") == 0 ||
