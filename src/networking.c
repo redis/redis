ca908473e8298e617d099a57ae1fb8db4bccdfa9
antirez <antirez@gmail.com>
2011-11-04 11:18:15 +0100

    A comment moved a few lines for clarity.

--- src/networking.c
+++ src/networking.c
@@ -789,10 +789,10 @@ int processMultibulkBuffer(redisClient *c) {
             /* Not enough data (+2 == trailing \r\n) */
             break;
         } else {
+#ifdef REDIS_MBULK_BIG_ARG
             /* Optimization: if the buffer contanins JUST our bulk element
              * instead of creating a new object by *copying* the sds we
              * just use the current sds string. */
-#ifdef REDIS_MBULK_BIG_ARG
             if (pos == 0 &&
                 c->bulklen >= REDIS_MBULK_BIG_ARG &&
                 (signed) sdslen(c->querybuf) == c->bulklen+2)
