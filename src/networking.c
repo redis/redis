7558b1fe4d416de237ee24034282f5a37eb75e33
antirez <antirez@gmail.com>
2011-11-25 16:09:16 +0100

    log client protocol errors for log level >= verbose

--- src/networking.c
+++ src/networking.c
@@ -698,6 +698,12 @@ int processInlineBuffer(redisClient *c) {
 /* Helper function. Trims query buffer to make the function that processes
  * multi bulk requests idempotent. */
 static void setProtocolError(redisClient *c, int pos) {
+    if (server.verbosity >= REDIS_VERBOSE) {
+        sds client = getClientInfoString(c);
+        redisLog(REDIS_VERBOSE,
+            "Protocol error from client: %s", client);
+        sdsfree(client);
+    }
     c->flags |= REDIS_CLOSE_AFTER_REPLY;
     c->querybuf = sdsrange(c->querybuf,pos,-1);
 }
