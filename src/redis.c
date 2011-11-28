a675e5b1715be87b1913fdf42b872cd0c1f9df3c
antirez <antirez@gmail.com>
2011-11-28 11:04:03 +0100

    two new AOF related INFO fields that can be interesting information for debugging.

--- src/redis.c
+++ src/redis.c
@@ -1339,10 +1339,14 @@ sds genRedisInfoString(void) {
         info = sdscatprintf(info,
             "aof_current_size:%lld\r\n"
             "aof_base_size:%lld\r\n"
-            "aof_pending_rewrite:%d\r\n",
+            "aof_pending_rewrite:%d\r\n"
+            "aof_buffer_length:%zu\r\n"
+            "aof_pending_bio_fsync:%llu\r\n",
             (long long) server.appendonly_current_size,
             (long long) server.auto_aofrewrite_base_size,
-            server.aofrewrite_scheduled);
+            server.aofrewrite_scheduled,
+            sdslen(server.aofbuf),
+            bioPendingJobsOfType(REDIS_BIO_AOF_FSYNC));
     }
 
     if (server.masterhost) {
