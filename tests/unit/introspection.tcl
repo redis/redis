8a6b21da619d6de6fc5a2ce186c87b7b971a6a52
antirez <antirez@gmail.com>
2011-12-19 10:21:50 +0100

    unit/introspection.tcl added

---
+++ tests/unit/introspection.tcl
@@ -0,0 +1,5 @@
+start_server {tags {"introspection"}} {
+    test {CLIENT LIST} {
+        r client list
+    } {*addr=*:* fd=* idle=* flags=N db=9 sub=0 psub=0 qbuf=0 obl=0 oll=0 events=r cmd=client*}
+}
