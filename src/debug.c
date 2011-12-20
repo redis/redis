efb6022529469813cd946f6bb22d762dc20e606d
antirez <antirez@gmail.com>
2011-12-20 17:52:57 +0100

    Do not propagate DEBUG LOADAOF

--- src/debug.c
+++ src/debug.c
@@ -233,6 +233,7 @@ void debugCommand(redisClient *c) {
             addReply(c,shared.err);
             return;
         }
+        server.dirty = 0; /* Prevent AOF / replication */
         redisLog(REDIS_WARNING,"Append Only File loaded by DEBUG LOADAOF");
         addReply(c,shared.ok);
     } else if (!strcasecmp(c->argv[1]->ptr,"object") && c->argc == 3) {
