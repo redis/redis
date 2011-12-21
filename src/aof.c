b941417c1ba4d7720a16f6b56790e2af9fa978a3
antirez <antirez@gmail.com>
2011-12-21 16:37:22 +0100

    Log when the child rewriting the AOF is killed by stopAppendOnly()

--- src/aof.c
+++ src/aof.c
@@ -31,6 +31,8 @@ void stopAppendOnly(void) {
     if (server.aof_child_pid != -1) {
         int statloc;
 
+        redisLog(REDIS_NOTICE,"Killing running AOF rewrite child: %ld",
+            (long) server.aof_child_pid);
         if (kill(server.aof_child_pid,SIGKILL) != -1)
             wait3(&statloc,0,NULL);
         /* reset the buffer accumulating changes while the child saves */
