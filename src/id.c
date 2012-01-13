dd86c601a364616ecbdbb7b64cc39575c7d0f5e7
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-01-14 00:05:38 +0100

    Add a 128-bit k-ordered unique id generator
    This is similar to what snowflake and the recent boundary
    solution do, but it makes sense to use redis for that type
    of use cases for people wanting a simple way to get
    incremental ids in distributed systems without an additional
    daemon requirement.
    
    Unique IDs are composed as follows:
    
    * epoch seconds: 4 bytes
    * epoch mseconds: 4 bytes
    * host name: 6 bytes
    * sequence id: 2 bytes
    
    host name is truncated to 6 chars, so the appropriate config
    directive id-generation-name should be set on each machines
    wanting to yield ids if truncating hostname do 6 chars does
    not suffice.

---
+++ src/id.c
@@ -0,0 +1,27 @@
+#include "redis.h"
+#include <sys/time.h>
+
+u_int16_t sequence;
+
+void incridCommand(redisClient *c) {
+    struct timeval tv;
+    sds id_buf;
+
+    if ((gettimeofday(&tv, NULL)) == -1) {
+        addReplyError(c, "cannot get time of day");
+	return;
+    }
+
+    id_buf = sdscatprintf(sdsempty(),
+                          "0x%08lx%08x%02x%02x%02x%02x%02x%02x%04x\r\n",
+			  tv.tv_sec,
+                          tv.tv_usec,
+                          server.id_generation_name[0],
+                          server.id_generation_name[1],
+                          server.id_generation_name[2],
+                          server.id_generation_name[3],
+                          server.id_generation_name[4],
+                          server.id_generation_name[5],
+                          sequence++);
+    addReplySds(c, id_buf);
+}
