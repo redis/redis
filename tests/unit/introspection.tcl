3c0602ff418ff1a1b18440a6994fd73226786251
antirez <antirez@gmail.com>
2012-01-23 16:19:49 +0100

    CLIENT LIST test fixed (there is a new omem field in the output)

--- tests/unit/introspection.tcl
+++ tests/unit/introspection.tcl
@@ -1,5 +1,5 @@
 start_server {tags {"introspection"}} {
     test {CLIENT LIST} {
         r client list
-    } {*addr=*:* fd=* idle=* flags=N db=9 sub=0 psub=0 qbuf=0 obl=0 oll=0 events=r cmd=client*}
+    } {*addr=*:* fd=* idle=* flags=N db=9 sub=0 psub=0 qbuf=0 obl=0 oll=0 omem=0 events=r cmd=client*}
 }
