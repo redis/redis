57a5e54ddcbaf0df15771faad3344fa6990e2227
antirez <antirez@gmail.com>
2012-03-13 13:26:33 +0100

    Added a qbuf-free field to CLIENT LIST output.

--- src/networking.c
+++ src/networking.c
@@ -1111,7 +1111,7 @@ sds getClientInfoString(redisClient *client) {
     if (emask & AE_WRITABLE) *p++ = 'w';
     *p = '\0';
     return sdscatprintf(sdsempty(),
-        "addr=%s:%d fd=%d age=%ld idle=%ld flags=%s db=%d sub=%d psub=%d qbuf=%lu obl=%lu oll=%lu omem=%lu events=%s cmd=%s",
+        "addr=%s:%d fd=%d age=%ld idle=%ld flags=%s db=%d sub=%d psub=%d qbuf=%lu qbuf-free=%lu obl=%lu oll=%lu omem=%lu events=%s cmd=%s",
         ip,port,client->fd,
         (long)(now - client->ctime),
         (long)(now - client->lastinteraction),
@@ -1120,6 +1120,7 @@ sds getClientInfoString(redisClient *client) {
         (int) dictSize(client->pubsub_channels),
         (int) listLength(client->pubsub_patterns),
         (unsigned long) sdslen(client->querybuf),
+        (unsigned long) sdsavail(client->querybuf),
         (unsigned long) client->bufpos,
         (unsigned long) listLength(client->reply),
         getClientOutputBufferMemoryUsage(client),
