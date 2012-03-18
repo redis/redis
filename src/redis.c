ea693f028246300e9ca7ee5efc9112436f53145c
antirez <antirez@gmail.com>
2012-03-18 18:03:27 +0100

    More memory tests implemented. Default number of iterations lowered to a more acceptable value of 50.

--- src/redis.c
+++ src/redis.c
@@ -2262,7 +2262,7 @@ int main(int argc, char **argv) {
             strcmp(argv[1], "-h") == 0) usage();
         if (strcmp(argv[1], "--test-memory") == 0) {
             if (argc == 3) {
-                memtest(atoi(argv[2]),300);
+                memtest(atoi(argv[2]),50);
                 exit(0);
             } else {
                 fprintf(stderr,"Please specify the amount of memory to test in megabytes.\n");
