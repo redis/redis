83f0c677f84f9abd2c22120d777750ed135ebfd6
antirez <antirez@gmail.com>
2012-03-18 17:24:48 +0100

    Number of iteration of --test-memory is now 300 (several minutes per gigabyte). Memtest86 and Memtester links are also displayed while running the test.

--- src/redis.c
+++ src/redis.c
@@ -1979,7 +1979,7 @@ int main(int argc, char **argv) {
             strcmp(argv[1], "-h") == 0) usage();
         if (strcmp(argv[1], "--test-memory") == 0) {
             if (argc == 3) {
-                memtest(atoi(argv[2]),10000);
+                memtest(atoi(argv[2]),300);
                 exit(0);
             } else {
                 fprintf(stderr,"Please specify the amount of memory to test in megabytes.\n");
