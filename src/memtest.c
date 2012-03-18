1a197a3c1ae8c1014ca3f5ce0b0e436c550b52bb
antirez <antirez@gmail.com>
2012-03-18 17:24:48 +0100

    Number of iteration of --test-memory is now 300 (several minutes per gigabyte). Memtest86 and Memtester links are also displayed while running the test.

--- src/memtest.c
+++ src/memtest.c
@@ -24,11 +24,13 @@ void memtest_progress_start(char *title, int pass) {
 
     printf("\x1b[H\x1b[2J");    /* Cursor home, clear screen. */
     /* Fill with dots. */
-    for (j = 0; j < ws.ws_col*ws.ws_row; j++) printf(".");
+    for (j = 0; j < ws.ws_col*(ws.ws_row-2); j++) printf(".");
+    printf("Please keep the test running several minutes per GB of memory.\n");
+    printf("Also check http://www.memtest86.com/ and http://pyropus.ca/software/memtester/");
     printf("\x1b[H\x1b[2K");          /* Cursor home, clear current line.  */
     printf("%s [%d]\n", title, pass); /* Print title. */
     progress_printed = 0;
-    progress_full = ws.ws_col*(ws.ws_row-1);
+    progress_full = ws.ws_col*(ws.ws_row-3);
     fflush(stdout);
 }
 
