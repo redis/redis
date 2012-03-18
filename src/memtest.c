74760d3ccd0c4bc21b8743d5044253465ebc301e
antirez <antirez@gmail.com>
2012-03-18 17:27:56 +0100

    Fixed typo.

--- src/memtest.c
+++ src/memtest.c
@@ -129,7 +129,7 @@ void memtest(size_t megabytes, int passes) {
     }
     memtest_test(megabytes,passes);
     printf("\nYour memory passed this test.\n");
-    printf("Please if you are stil in doubt use the following two tools:\n");
+    printf("Please if you are still in doubt use the following two tools:\n");
     printf("1) memtest86: http://www.memtest86.com/\n");
     printf("2) memtester: http://pyropus.ca/software/memtester/\n");
     exit(0);
