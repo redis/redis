188a17ed563ef59bffa8b47b365079f264859b90
antirez <antirez@gmail.com>
2012-03-24 11:52:56 +0100

    Add used allocator in redis-server -v output.

--- src/redis.c
+++ src/redis.c
@@ -2185,8 +2185,8 @@ void daemonize(void) {
 }
 
 void version() {
-    printf("Redis server version %s (%s:%d)\n", REDIS_VERSION,
-        redisGitSHA1(), atoi(redisGitDirty()) > 0);
+    printf("Redis server v=%s sha=%s:%d malloc=%s\n", REDIS_VERSION,
+        redisGitSHA1(), atoi(redisGitDirty()) > 0, ZMALLOC_LIB);
     exit(0);
 }
 
