c2672a06cdf411d882c6d2a58a6c84fcbc57b4c7
antirez <antirez@gmail.com>
2012-03-30 10:39:34 +0200

    Purely aesthetic code change.

--- src/replication.c
+++ src/replication.c
@@ -26,8 +26,8 @@ void replicationFeedSlaves(list *slaves, int dictid, robj **argv, int argc) {
             robj *selectcmd;
 
             if (dictid >= 0 && dictid < REDIS_SHARED_SELECT_CMDS) {
-                incrRefCount(shared.select[dictid]);
                 selectcmd = shared.select[dictid];
+                incrRefCount(selectcmd);
             } else {
                 selectcmd = createObject(REDIS_STRING,
                     sdscatprintf(sdsempty(),"select %d\r\n",dictid));
