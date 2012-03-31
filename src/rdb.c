11dae1711fa799e23eacc4f142c09b425c75a8b8
antirez <antirez@gmail.com>
2012-03-31 17:08:40 +0200

    Write RDB magic using a REDIS_RDB_VERSION define that is defined inside rdb.h

--- src/rdb.c
+++ src/rdb.c
@@ -602,6 +602,7 @@ int rdbSave(char *filename) {
     dictIterator *di = NULL;
     dictEntry *de;
     char tmpfile[256];
+    char magic[10];
     int j;
     long long now = mstime();
     FILE *fp;
@@ -616,7 +617,8 @@ int rdbSave(char *filename) {
     }
 
     rioInitWithFile(&rdb,fp);
-    if (rdbWriteRaw(&rdb,"REDIS0004",9) == -1) goto werr;
+    snprintf(magic,sizeof(magic),"REDIS%04d",REDIS_RDB_VERSION);
+    if (rdbWriteRaw(&rdb,magic,9) == -1) goto werr;
 
     for (j = 0; j < server.dbnum; j++) {
         redisDb *db = server.db+j;
