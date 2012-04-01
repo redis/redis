776d58fe5dec607d1376cbf098de233e4c7cdae8
Leo P <shell@slact.net>
2012-03-31 22:06:16 -0400

    Added config option incrbyfloat-precision, the decimal point precision of the command result. Default to 17.

--- src/util.c
+++ src/util.c
@@ -329,6 +329,12 @@ int d2string(char *buf, size_t len, double value) {
     return len;
 }
 
+/* Generate format string for sprintf conversion of long double to string 
+ * with given amount for precision past the decimal point */
+void longDoubleFormatString(int precision, char *format) {
+    snprintf(format, 128, "%%.%iLf", precision);
+}
+
 /* Generate the Redis "Run ID", a SHA1-sized random number that identifies a
  * given execution of Redis, so that if you are talking with an instance
  * having run_id == A, and you reconnect and it has run_id == B, you can be
