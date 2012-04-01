a1fbb329ade652c5c0ecdeeea3d58bbe5768dd3e
Leo P <shell@slact.net>
2012-03-31 21:52:50 -0400

    Changed INCRBYFLOAT and HINCRBYFLOAT to store floats in scientific notation or as a decimal as needed. (These commands previously stored a decimal representation of the float with a fixed precision after the decimal. Floating point fraction (mantissa) precision is still limited to 17 decimal digits.

--- src/object.c
+++ src/object.c
@@ -56,16 +56,7 @@ robj *createStringObjectFromLongDouble(long double value) {
      * that is "non surprising" for the user (that is, most small decimal
      * numbers will be represented in a way that when converted back into
      * a string are exactly the same as what the user typed.) */
-    len = snprintf(buf,sizeof(buf),"%.17Lf", value);
-    /* Now remove trailing zeroes after the '.' */
-    if (strchr(buf,'.') != NULL) {
-        char *p = buf+len-1;
-        while(*p == '0') {
-            p--;
-            len--;
-        }
-        if (*p == '.') len--;
-    }
+    len = snprintf(buf,sizeof(buf),"%.17Lg", value);
     return createStringObject(buf,len);
 }
 
