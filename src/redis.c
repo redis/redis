f2f2ba1b3ab5147684143ebfb2981c14c9a0bd27
antirez <antirez@gmail.com>
2012-04-11 10:56:55 +0200

    Comment typo fixed. Clusetr -> Cluster.

--- src/redis.c
+++ src/redis.c
@@ -2042,7 +2042,7 @@ sds genRedisInfoString(char *section) {
         }
     }
 
-    /* Clusetr */
+    /* Cluster */
     if (allsections || defsections || !strcasecmp(section,"cluster")) {
         if (sections++) info = sdscat(info,"\r\n");
         info = sdscatprintf(info,
