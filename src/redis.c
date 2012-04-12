d298825803ee32513ad08f415a93f7f0a7c362c2
antirez <antirez@gmail.com>
2012-04-12 11:50:18 +0200

    Print arch bits with redis-server -v

--- src/redis.c
+++ src/redis.c
@@ -2251,8 +2251,12 @@ void daemonize(void) {
 }
 
 void version() {
-    printf("Redis server v=%s sha=%s:%d malloc=%s\n", REDIS_VERSION,
-        redisGitSHA1(), atoi(redisGitDirty()) > 0, ZMALLOC_LIB);
+    printf("Redis server v=%s sha=%s:%d malloc=%s bits=%d\n",
+        REDIS_VERSION,
+        redisGitSHA1(),
+        atoi(redisGitDirty()) > 0,
+        ZMALLOC_LIB,
+        sizeof(long) == 4 ? 32 : 64);
     exit(0);
 }
 
