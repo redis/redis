4c442e9d88c493a16f4f1197413b063c7205d812
antirez <antirez@gmail.com>
2012-04-12 11:49:52 +0200

    memtest.c: integer overflow fixed.

--- src/memtest.c
+++ src/memtest.c
@@ -47,7 +47,7 @@ void memtest_progress_end(void) {
 }
 
 void memtest_progress_step(size_t curr, size_t size, char c) {
-    size_t chars = (curr*progress_full)/size, j;
+    size_t chars = ((unsigned long long)curr*progress_full)/size, j;
 
     for (j = 0; j < chars-progress_printed; j++) {
         printf("%c",c);
