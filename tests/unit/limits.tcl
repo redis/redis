96aeca4b9d084ceff6bc8ee56a2bf58bab61d3db
antirez <antirez@gmail.com>
2012-04-17 10:04:42 +0200

    Less false positives in maxclients test, hopefully.

--- tests/unit/limits.tcl
+++ tests/unit/limits.tcl
@@ -2,9 +2,10 @@ start_server {tags {"limits"} overrides {maxclients 10}} {
     test {Check if maxclients works refusing connections} {
         set c 0
         catch {
-            while 1 {
+            while {$c < 50} {
                 incr c
                 redis_deferring_client
+                after 100
             }
         } e
         assert {$c > 8 && $c <= 10}
