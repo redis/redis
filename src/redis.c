727d6dd52a3e778403d2ef66a516498ff25f3c02
antirez <antirez@gmail.com>
2012-04-18 11:31:24 +0200

    Two small fixes to maxclients handling.
    1) Don't accept maxclients set to < 0
    2) Allow maxclients < 1024, it is useful for testing.

--- src/redis.c
+++ src/redis.c
@@ -1135,7 +1135,6 @@ void adjustOpenFilesLimit(void) {
     rlim_t maxfiles = server.maxclients+32;
     struct rlimit limit;
 
-    if (maxfiles < 1024) maxfiles = 1024;
     if (getrlimit(RLIMIT_NOFILE,&limit) == -1) {
         redisLog(REDIS_WARNING,"Unable to obtain the current NOFILE limit (%s), assuming 1024 and setting the max clients configuration accordingly.",
             strerror(errno));
