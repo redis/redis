0122cc4f42792066b2c4e8c0bcc500b5bfc00f42
antirez <antirez@gmail.com>
2012-04-18 20:53:37 +0200

    redis-cli --bigkeys output modified to be simpler to read..

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1030,7 +1030,7 @@ static void findBigKeys(void) {
         reply3 = redisCommand(context,"%s %s", sizecmd, reply1->str);
         if (reply3 && reply3->type == REDIS_REPLY_INTEGER) {
             if (biggest[type] < reply3->integer) {
-                printf("Biggest %s so far: %s, size: %llu\n",
+                printf("[%6s] %s | biggest so far with size %llu\n",
                     typename[type], reply1->str,
                     (unsigned long long) reply3->integer);
                 biggest[type] = reply3->integer;
