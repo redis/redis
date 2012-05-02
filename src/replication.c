9b43b1ef4d646922fc904fda61ff5ab67a2581db
antirez <antirez@gmail.com>
2012-05-02 21:45:01 +0200

    Remove useless trailing space in SYNC command sent to master.

--- src/replication.c
+++ src/replication.c
@@ -423,7 +423,7 @@ void syncWithMaster(aeEventLoop *el, int fd, void *privdata, int mask) {
     }
 
     /* Issue the SYNC command */
-    if (syncWrite(fd,"SYNC \r\n",7,server.repl_syncio_timeout*1000) == -1) {
+    if (syncWrite(fd,"SYNC\r\n",6,server.repl_syncio_timeout*1000) == -1) {
         redisLog(REDIS_WARNING,"I/O error writing to MASTER: %s",
             strerror(errno));
         goto error;
