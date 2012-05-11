346825c7ed8e5e57704ff2603a51435249b7c59e
antirez <antirez@gmail.com>
2012-05-11 10:45:12 +0200

    redis-cli pipe mode: handle EAGAIN while writing to socket.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1052,9 +1052,13 @@ static void pipeMode(void) {
                     ssize_t nwritten = write(fd,obuf+obuf_pos,obuf_len);
                     
                     if (nwritten == -1) {
-                        fprintf(stderr, "Error writing to the server: %s\n",
-                            strerror(errno));
-                        exit(1);
+                        if (errno != EAGAIN) {
+                            fprintf(stderr, "Error writing to the server: %s\n",
+                                strerror(errno));
+                            exit(1);
+                        } else {
+                            nwritten = 0;
+                        }
                     }
                     obuf_len -= nwritten;
                     obuf_pos += nwritten;
