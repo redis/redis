3a682eb3bf670ab1805c5f7dfd9208d84b6e38e3
Robert Allen <zircote@gmail.com>
2012-07-02 07:16:48 -0500

    added hostname to `INFO` command to `server` section This is to ease the facilitation of data and performance collection

--- src/redis.c
+++ src/redis.c
@@ -1866,6 +1866,7 @@ sds genRedisInfoString(char *section) {
         uname(&name);
         info = sdscatprintf(info,
             "# Server\r\n"
+            "hostname:%s\r\n"
             "redis_version:%s\r\n"
             "redis_git_sha1:%s\r\n"
             "redis_git_dirty:%d\r\n"
@@ -1879,6 +1880,7 @@ sds genRedisInfoString(char *section) {
             "uptime_in_seconds:%ld\r\n"
             "uptime_in_days:%ld\r\n"
             "lru_clock:%ld\r\n",
+            name.nodename,
             REDIS_VERSION,
             redisGitSHA1(),
             strtol(redisGitDirty(),NULL,10) > 0,
