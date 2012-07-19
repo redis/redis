a696bad92ada4d8b331322d8113b7f25ce6fad78
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2012-07-19 21:37:34 +0900

    use nanosleep instead of usleep
    SUSv3 says that:
    	The useconds argument shall be less than one million. If the value of
    	useconds is 0, then the call has no effect.
    and actually NetBSD's implementation rejects such a value with EINVAL.
    use nanosleep which has no such a limitation instead.

--- src/debug.c
+++ src/debug.c
@@ -298,8 +298,11 @@ void debugCommand(redisClient *c) {
     } else if (!strcasecmp(c->argv[1]->ptr,"sleep") && c->argc == 3) {
         double dtime = strtod(c->argv[2]->ptr,NULL);
         long long utime = dtime*1000000;
+        struct timespec tv;
 
-        usleep(utime);
+        tv.tv_sec = utime / 1000000;
+        tv.tv_nsec = (utime % 1000000) * 1000;
+        nanosleep(&tv, NULL);
         addReply(c,shared.ok);
     } else {
         addReplyError(c,
