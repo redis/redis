d918e6f127f60efd7b9fd23ba3413eb441240e0f
antirez <antirez@gmail.com>
2012-07-24 18:20:05 +0200

    Sentinel: debugging message removed.

--- src/sentinel.c
+++ src/sentinel.c
@@ -930,7 +930,6 @@ void sentinelLinkEstablishedCallback(const redisAsyncContext *c, int status) {
 }
 
 void sentinelDisconnectCallback(const redisAsyncContext *c, int status) {
-    printf("DISCONNECT CALLBACK CALLED: %p (%p)\n", (void*)c, (void*)c->data);
     sentinelDisconnectInstanceFromContext(c);
 }
 
