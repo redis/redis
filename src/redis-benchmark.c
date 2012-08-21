227b4293647d51244771d16f0ad14bc94a2b3ad9
antirez <antirez@gmail.com>
2012-08-21 17:31:44 +0200

    redis-benchmark: disable big buffer cleanup in hiredis context.
    This new hiredis features allows us to reuse a previous context reader
    buffer even if already very big in order to maximize performances with
    big payloads (Usually hiredis re-creates buffers when they are too big
    and unused in order to save memory).

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -263,6 +263,8 @@ static client createClient(char *cmd, size_t len) {
             fprintf(stderr,"%s: %s\n",config.hostsocket,c->context->errstr);
         exit(1);
     }
+    /* Suppress hiredis cleanup of unused buffers for max speed. */
+    c->context->reader->maxbuf = 0;
     /* Queue N requests accordingly to the pipeline size. */
     c->obuf = sdsempty();
     for (j = 0; j < config.pipeline; j++)
