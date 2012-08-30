e480c279bc28832ca78a33f7f52c0b57ae5a16f1
Jokea <tfengjun@gmail.com>
2012-08-30 15:08:19 +0800

    Force expire all timer events when system clock skew is detected.
    When system time changes back, the timer will not worker properly
    hence some core functionality of redis will stop working(e.g. replication,
    bgsave, etc). See issue #633 for details.
    
    The patch saves the previous time and when a system clock skew is detected,
    it will force expire all timers.
    
    Modiifed by @antirez: the previous time was moved into the eventLoop
    structure to make sure the library is still thread safe as long as you
    use different event loops into different threads (otherwise you need
    some synchronization). More comments added about the reasoning at the
    base of the patch, that's worth reporting here:
    
    /* If the system clock is moved to the future, and then set back to the
     * right value, time events may be delayed in a random way. Often this
     * means that scheduled operations will not be performed soon enough.
     *
     * Here we try to detect system clock skews, and force all the time
     * events to be processed ASAP when this happens: the idea is that
     * processing events earlier is less dangerous than delaying them
     * indefinitely, and practice suggests it is. */

--- src/ae.h
+++ src/ae.h
@@ -88,6 +88,7 @@ typedef struct aeEventLoop {
     int maxfd;   /* highest file descriptor currently registered */
     int setsize; /* max number of file descriptors tracked */
     long long timeEventNextId;
+    time_t lastTime;     /* Used to detect system clock skew */
     aeFileEvent *events; /* Registered events */
     aeFiredEvent *fired; /* Fired events */
     aeTimeEvent *timeEventHead;
