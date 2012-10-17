ee5a6df101ae27f2209ae358d2b5e0e0ada1cb36
Geoff Garside <geoff@geoffgarside.co.uk>
2012-10-17 22:32:21 +0100

    Update calls to anetPeerToString to include ip_len.

--- src/replication.c
+++ src/replication.c
@@ -293,7 +293,7 @@ void replicationFeedMonitors(redisClient *c, list *monitors, int dictid, robj **
     int j, port;
     sds cmdrepr = sdsnew("+");
     robj *cmdobj;
-    char ip[32];
+    char ip[REDIS_IP_STR_LEN];
     struct timeval tv;
 
     gettimeofday(&tv,NULL);
@@ -303,7 +303,7 @@ void replicationFeedMonitors(redisClient *c, list *monitors, int dictid, robj **
     } else if (c->flags & REDIS_UNIX_SOCKET) {
         cmdrepr = sdscatprintf(cmdrepr,"[%d unix:%s] ",dictid,server.unixsocket);
     } else {
-        anetPeerToString(c->fd,ip,&port);
+        anetPeerToString(c->fd,ip,sizeof(ip),&port);
         cmdrepr = sdscatprintf(cmdrepr,"[%d %s:%d] ",dictid,ip,port);
     }
 
