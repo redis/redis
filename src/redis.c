99d7dbe669c1531012b0f33a8a04af9fca76d593
antirez <antirez@gmail.com>
2012-10-22 19:21:47 +0200

    A filed called slave_read_only added in INFO output.
    This was an important information missing from the INFO output in the
    replication section.
    
    It obviously reflects if the slave is read only or not.

--- src/redis.c
+++ src/redis.c
@@ -2070,7 +2070,10 @@ sds genRedisInfoString(char *section) {
                     (long)server.unixtime-server.repl_down_since);
             }
             info = sdscatprintf(info,
-                "slave_priority:%d\r\n", server.slave_priority);
+                "slave_priority:%d\r\n"
+                "slave_read_only:%d\r\n",
+                server.slave_priority,
+                server.repl_slave_ro);
         }
         info = sdscatprintf(info,
             "connected_slaves:%lu\r\n",
