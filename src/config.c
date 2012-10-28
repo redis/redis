881f3ea461c9407d78f680de6cca4eca33719261
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2012-10-28 13:33:04 +0900

    don't assume time_t == long
    time_t is always 64bit on recent versions of NetBSD.

--- src/config.c
+++ src/config.c
@@ -833,8 +833,8 @@ void configGetCommand(redisClient *c) {
         int j;
 
         for (j = 0; j < server.saveparamslen; j++) {
-            buf = sdscatprintf(buf,"%ld %d",
-                    server.saveparams[j].seconds,
+            buf = sdscatprintf(buf,"%jd %d",
+                    (intmax_t)server.saveparams[j].seconds,
                     server.saveparams[j].changes);
             if (j != server.saveparamslen-1)
                 buf = sdscatlen(buf," ",1);
