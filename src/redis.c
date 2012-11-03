e34a35a51194650ce7ef7df1047012c7eaad2957
antirez <antirez@gmail.com>
2012-11-03 12:15:29 +0100

    Use the new unified protocol to send SELECT to slaves.
    SELECT was still transmitted to slaves using the inline protocol, that
    is conceived mostly for humans to type into telnet sessions, and is
    notably not understood by redis-cli --slave.
    
    Now the new protocol is used instead.

--- src/redis.c
+++ src/redis.c
@@ -1091,8 +1091,14 @@ void createSharedObjects(void) {
     shared.plus = createObject(REDIS_STRING,sdsnew("+"));
 
     for (j = 0; j < REDIS_SHARED_SELECT_CMDS; j++) {
+        char dictid_str[64];
+        int dictid_len;
+
+        dictid_len = ll2string(dictid_str,sizeof(dictid_str),j);
         shared.select[j] = createObject(REDIS_STRING,
-            sdscatprintf(sdsempty(),"select %d\r\n", j));
+            sdscatprintf(sdsempty(),
+                "*2\r\n$6\r\nSELECT\r\n$%d\r\n%s\r\n",
+                dictid_len, dictid_str));
     }
     shared.messagebulk = createStringObject("$7\r\nmessage\r\n",13);
     shared.pmessagebulk = createStringObject("$8\r\npmessage\r\n",14);
