037edfd495fb941c7458e6b048b0bcf2fa608903
NanXiao <xn212516@163.com>
2012-11-08 09:08:21 +0800

    Update src/config.h
    Hi, antirez:
        Solaris platform doesn't support HAVE_BACKTRACE, so this will introduce compile error in Solaris platform. So I suggest remove (__sun) definition.
        Please help to check it, thanks very much!!
    Best Regards
    Nan Xiao

--- src/config.h
+++ src/config.h
@@ -25,7 +25,7 @@
 #endif
 
 /* Test for backtrace() */
-#if defined(__APPLE__) || defined(__linux__) || defined(__sun)
+#if defined(__APPLE__) || defined(__linux__)
 #define HAVE_BACKTRACE 1
 #endif
 
