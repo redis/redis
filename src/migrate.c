ae3aeca8288bbb22c6ef2eac51827409e19e41df
antirez <antirez@gmail.com>
2012-11-12 18:56:29 +0100

    MIGRATE: fix fd leak due to missing close on error.

--- src/migrate.c
+++ src/migrate.c
@@ -158,6 +158,7 @@ void migrateCommand(redisClient *c) {
         return;
     }
     if ((aeWait(fd,AE_WRITABLE,timeout*1000) & AE_WRITABLE) == 0) {
+        close(fd);
         addReplySds(c,sdsnew("-IOERR error or timeout connecting to the client\r\n"));
         return;
     }
