49b645235100fc214468b608c1ba6cdbc320fa88
antirez <antirez@gmail.com>
2012-11-19 12:02:08 +0100

    Children creating AOF or RDB files now report memory used by COW.
    Finally Redis is able to report the amount of memory used by
    copy-on-write while saving an RDB or writing an AOF file in background.
    
    Note that this information is currently only logged (at NOTICE level)
    and not shown in INFO because this is less trivial (but surely doable
    with some minor form of interprocess communication).
    
    The reason we can't capture this information on the parent before we
    call wait3() is that the Linux kernel will release the child memory
    ASAP, and only retain the minimal state for the process that is useful
    to report the child termination to the parent.
    
    The COW size is obtained by summing all the Private_Dirty fields found
    in the "smap" file inside the proc filesystem for the process.
    
    All this is Linux specific and is not available on other systems.

--- src/aof.c
+++ src/aof.c
@@ -962,6 +962,13 @@ int rewriteAppendOnlyFileBackground(void) {
         if (server.sofd > 0) close(server.sofd);
         snprintf(tmpfile,256,"temp-rewriteaof-bg-%d.aof", (int) getpid());
         if (rewriteAppendOnlyFile(tmpfile) == REDIS_OK) {
+            size_t private_dirty = zmalloc_get_private_dirty();
+
+            if (private_dirty) {
+                redisLog(REDIS_NOTICE,
+                    "AOF rewrite: %lu MB of memory used by copy-on-write",
+                    private_dirty/(1024*1024));
+            }
             exitFromChild(0);
         } else {
             exitFromChild(1);
