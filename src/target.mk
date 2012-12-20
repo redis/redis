cd0d29272143b52910a8430dd0ef0fc177797f2c
Yossi Gottlieb <yossigo@gmail.com>
2012-12-20 22:11:08 +0200

    Single tree multi-architecture (32/64bit) builds.

---
+++ src/target.mk
@@ -0,0 +1,25 @@
+.SUFFIXES:
+
+ifndef OBJDIR
+  BUILD_ARCH ?= $(shell uname -m)
+  OBJDIR := obj.$(BUILD_ARCH)
+  export OBJDIR
+endif
+
+MAKETARGET = $(MAKE) --no-print-directory -C $@ -f $(CURDIR)/Makefile \
+		SRCDIR=$(CURDIR) $(MAKECMDGOALS)
+
+.PHONY: $(OBJDIR)
+$(OBJDIR):
+	+@[ -d $@ ] || mkdir -p $@
+	+@$(MAKETARGET)
+
+Makefile : ;
+%.mk :: ;
+
+% :: $(OBJDIR) ; :
+
+.PHONY: clean
+clean:
+	rm -rf $(OBJDIR)
+
