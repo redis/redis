c1d8fd71b3fe4fdf04ce88a3442aa23048fc846a
David Palm <dvdplm@gmail.com>
2013-01-02 20:22:26 +0100

    Add terabyte and petabyte support

--- src/redis.c
+++ src/redis.c
@@ -1890,7 +1890,14 @@ void bytesToHuman(char *s, unsigned long long n) {
     } else if (n < (1024LL*1024*1024*1024)) {
         d = (double)n/(1024LL*1024*1024);
         sprintf(s,"%.2fG",d);
+    } else if (n < (1024LL*1024*1024*1024*1024)) {
+        d = (double)n/(1024LL*1024*1024*1024);
+        sprintf(s,"%.2fT",d);
+    } else if (n < (1024LL*1024*1024*1024*1024*1024)) {
+        d = (double)n/(1024LL*1024*1024*1024*1024);
+        sprintf(s,"%.2fP",d);
     } else {
+        /* Let's hope we never need this */
         sprintf(s,"%lluB",n);
     }
 }
