fc2693b2593d8810f3e57bc91d80317115ea6bb0
Denis Uraganov <github@uraganov.net>
2013-01-09 21:59:43 -0800

    Add ability for non-interactive installation with support of custom configuration template
    Change-Id: I9f097345d645fb77e4d9f306836d8ffbf9f03668

--- utils/install_server.sh
+++ utils/install_server.sh
@@ -29,13 +29,61 @@
 # this generates a redis config file and an /etc/init.d script, and installs them
 # this scripts should be run as root
 
+
+# Functions
+###################################################
+help() {
+echo "
+  -h | --help         Show help
+  --non-interactive   If provided turn off interactive mode
+  --port              PORT for Redis instance
+  --config            Path of configuration
+  --config-tpl        Template for configuration file
+  --log               Path of Log file
+  --data              Path of date directory
+"
+}
+
 die () {
-	echo "ERROR: $1. Aborting!" 
+	echo "ERROR: $1. Aborting!"
 	exit 1
 }
 
+# Handle command line params
+###################################################
+# NOTE: This requires GNU getopt.  On Mac OS X, you get BSD getopt by default,
+# which doesn't work; see below.
+TEMP=`getopt -o h \
+             --long port:,config:,config-tpl:,log:,data:,non-interactive,help \
+             -n 'install_server.sh' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
 #Initial defaults
+###################################################
 _REDIS_PORT=6379
+_INTERACTIVE=1
+
+## Set params
+###################################################
+while true; do
+  case "$1" in
+    -h | --help ) help; exit;;
+    --port ) REDIS_PORT="$2"; shift 2 ;;
+    --config ) REDIS_CONFIG_FILE="$2"; shift 2 ;;
+    --config-tpl ) CUSTOM_CONFIG="$2"; shift 2 ;;
+    --log ) REDIS_LOG_FILE="$2"; shift 2 ;;
+    --data ) REDIS_DATA_DIR="$2"; shift 2 ;;
+    --non-interactive ) _INTERACTIVE=0; shift ;;
+    -- ) shift; break ;;
+    -* ) echo "$0: error - unrecognized option $1" 1>&2; help; exit 1;;
+    * ) break ;;
+  esac
+done
+
 
 echo "Welcome to the redis service installer"
 echo "This script will help you easily set up a running redis server
@@ -50,66 +98,100 @@ fi
 
 
 #Read the redis port
-read  -p "Please select the redis port for this instance: [$_REDIS_PORT] " REDIS_PORT 
-if [ ! `echo $REDIS_PORT | egrep "^[0-9]+\$"`  ] ; then
-	echo "Selecting default: $_REDIS_PORT"
-	REDIS_PORT=$_REDIS_PORT 
-fi
+if [ -z "$REDIS_PORT" ]; then
+    if [ $_INTERACTIVE -eq 1 ]
+    then
+        read  -p "Please select the redis port for this instance: [$_REDIS_PORT] " REDIS_PORT
+        if [ ! `echo $REDIS_PORT | egrep "^[0-9]+\$"`  ] ; then
+	       echo "Selecting default: $_REDIS_PORT"
+	       REDIS_PORT=$_REDIS_PORT
+        fi
+    else
+         REDIS_PORT=$_REDIS_PORT
+    fi
+fi;
+
+
 
 #read the redis config file
 _REDIS_CONFIG_FILE="/etc/redis/$REDIS_PORT.conf"
-read -p "Please select the redis config file name [$_REDIS_CONFIG_FILE] " REDIS_CONFIG_FILE
-if [ !"$REDIS_CONFIG_FILE" ] ; then
-	REDIS_CONFIG_FILE=$_REDIS_CONFIG_FILE
-	echo "Selected default - $REDIS_CONFIG_FILE"
-fi
+if [ -z "$REDIS_CONFIG_FILE" ]; then
+    if [ $_INTERACTIVE -eq 1 ]
+    then
+        read -p "Please select the redis config file name [$_REDIS_CONFIG_FILE] " REDIS_CONFIG_FILE
+        if [ !"$REDIS_CONFIG_FILE" ] ; then
+	        REDIS_CONFIG_FILE=$_REDIS_CONFIG_FILE
+	        echo "Selected default - $REDIS_CONFIG_FILE"
+        fi
+    else
+         REDIS_CONFIG_FILE=$_REDIS_CONFIG_FILE
+    fi
+fi;
 #try and create it
 mkdir -p `dirname "$REDIS_CONFIG_FILE"` || die "Could not create redis config directory"
 
 #read the redis log file path
 _REDIS_LOG_FILE="/var/log/redis_$REDIS_PORT.log"
-read -p "Please select the redis log file name [$_REDIS_LOG_FILE] " REDIS_LOG_FILE
-if [ !"$REDIS_LOG_FILE" ] ; then
-	REDIS_LOG_FILE=$_REDIS_LOG_FILE
-	echo "Selected default - $REDIS_LOG_FILE"
-fi
-
+if [ -z "$REDIS_LOG_FILE" ]; then
+    if [ $_INTERACTIVE  -eq 1 ]
+    then
+        read -p "Please select the redis log file name [$_REDIS_LOG_FILE] " REDIS_LOG_FILE
+        if [ !"$REDIS_LOG_FILE" ] ; then
+	        REDIS_LOG_FILE=$_REDIS_LOG_FILE
+	        echo "Selected default - $REDIS_LOG_FILE"
+        fi
+    else
+         REDIS_LOG_FILE=$_REDIS_LOG_FILE
+    fi
+fi;
 
 #get the redis data directory
 _REDIS_DATA_DIR="/var/lib/redis/$REDIS_PORT"
-read -p "Please select the data directory for this instance [$_REDIS_DATA_DIR] " REDIS_DATA_DIR
-if [ !"$REDIS_DATA_DIR" ] ; then
-	REDIS_DATA_DIR=$_REDIS_DATA_DIR
-	echo "Selected default - $REDIS_DATA_DIR"
-fi
+if [ -z "$REDIS_DATA_DIR" ]; then
+    if [ $_INTERACTIVE -eq 1 ]
+    then
+        read -p "Please select the data directory for this instance [$_REDIS_DATA_DIR] " REDIS_DATA_DIR
+        if [ !"$REDIS_DATA_DIR" ] ; then
+	        REDIS_DATA_DIR=$_REDIS_DATA_DIR
+	        echo "Selected default - $REDIS_DATA_DIR"
+        fi
+    else
+        REDIS_DATA_DIR=$_REDIS_DATA_DIR
+    fi
+fi;
 mkdir -p $REDIS_DATA_DIR || die "Could not create redis data directory"
 
 #get the redis executable path
-_REDIS_EXECUTABLE=`which redis-server`
-read -p "Please select the redis executable path [$_REDIS_EXECUTABLE] " REDIS_EXECUTABLE
+REDIS_EXECUTABLE=`which redis-server`
+
+if [ $_INTERACTIVE -eq 1 ]; then
+    read -p "Please select the redis executable path [$_REDIS_EXECUTABLE] " REDIS_EXECUTABLE
+fi
+
 if [ ! -f "$REDIS_EXECUTABLE" ] ; then
-	REDIS_EXECUTABLE=$_REDIS_EXECUTABLE
-	
-	if [ ! -f "$REDIS_EXECUTABLE" ] ; then
-		echo "Mmmmm...  it seems like you don't have a redis executable. Did you run make install yet?"
-		exit 1
-	fi
-	
+	echo "Mmmmm...  it seems like you don't have a redis executable. Did you run make install yet?"
+	exit 1
 fi
 
 
-#render the tmplates
+#render the templates
 TMP_FILE="/tmp/$REDIS_PORT.conf"
-DEFAULT_CONFIG="../redis.conf"
 INIT_TPL_FILE="./redis_init_script.tpl"
 INIT_SCRIPT_DEST="/etc/init.d/redis_$REDIS_PORT"
 PIDFILE="/var/run/redis_$REDIS_PORT.pid"
 
+#set custom config template
+if [ -z "$DEFAULT_CONFIG" ]; then
+	DEFAULT_CONFIG="../redis.conf"
+fi;
+if [ ! -f "$DEFAULT_CONFIG" ] ; then
+	die "Config template '$DEFAULT_CONFIG' not found"
+fi
 
 
 #check the default for redis cli
 CLI_EXEC=`which redis-cli`
-if [ ! "$CLI_EXEC" ] ; then 
+if [ ! "$CLI_EXEC" ] ; then
 	CLI_EXEC=`dirname $REDIS_EXECUTABLE`"/redis-cli"
 fi
 
@@ -121,7 +203,7 @@ SED_EXPR="s#^port [0-9]{4}\$#port ${REDIS_PORT}#;\
 s#^logfile .+\$#logfile ${REDIS_LOG_FILE}#;\
 s#^dir .+\$#dir ${REDIS_DATA_DIR}#;\
 s#^pidfile .+\$#pidfile ${PIDFILE}#;\
-s#^daemonize no\$#daemonize yes#;" 
+s#^daemonize no\$#daemonize yes#;"
 echo $SED_EXPR
 sed -r "$SED_EXPR" $DEFAULT_CONFIG  >> $TMP_FILE
 
@@ -159,7 +241,7 @@ REDIS_CHKCONFIG_INFO=\
 # Description: Redis daemon\n
 ### END INIT INFO\n\n"
 
-if [ !`which chkconfig` ] ; then 
+if [ !`which chkconfig` ] ; then
 	#combine the header and the template (which is actually a static footer)
 	echo $REDIS_INIT_HEADER > $TMP_FILE && cat $INIT_TPL_FILE >> $TMP_FILE || die "Could not write init script to $TMP_FILE"
 else
@@ -173,7 +255,7 @@ echo "Copied $TMP_FILE => $INIT_SCRIPT_DEST"
 
 #Install the service
 echo "Installing service..."
-if [ !`which chkconfig` ] ; then 
+if [ !`which chkconfig` ] ; then
 	#if we're not a chkconfig box assume we're able to use update-rc.d
 	update-rc.d redis_$REDIS_PORT defaults && echo "Success!"
 else
@@ -181,7 +263,7 @@ else
 	chkconfig --add redis_$REDIS_PORT && echo "Successfully added to chkconfig!"
 	chkconfig --level 345 redis_$REDIS_PORT on && echo "Successfully added to runlevels 345!"
 fi
-	
+
 /etc/init.d/redis_$REDIS_PORT start || die "Failed starting service..."
 
 #tada
