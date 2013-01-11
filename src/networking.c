1b2ac3b206e97e265c89351e095e7fabe674a506
antirez <antirez@gmail.com>
2013-01-11 18:50:40 +0100

    Typo fixed, ASCI -> ASCII.

--- src/networking.c
+++ src/networking.c
@@ -1211,7 +1211,7 @@ void clientCommand(redisClient *c) {
          * CLIENT LIST format will break. You should always be able to
          * split by space to get the different fields. */
         for (j = 0; j < len; j++) {
-            if (p[j] < '!' || p[j] > '~') { /* ASCI is assumed. */
+            if (p[j] < '!' || p[j] > '~') { /* ASCII is assumed. */
                 addReplyError(c,
                     "Client names cannot contain spaces, "
                     "newlines or special characters.");
