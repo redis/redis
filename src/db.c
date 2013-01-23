fdfb59beaea5a12b62f4db8f81281fff1c4a0ad8
antirez <antirez@gmail.com>
2013-01-23 17:04:18 +0100

    Initial test events for the new keyspace notification API.

--- src/db.c
+++ src/db.c
@@ -238,6 +238,7 @@ void delCommand(redisClient *c) {
     for (j = 1; j < c->argc; j++) {
         if (dbDelete(c->db,c->argv[j])) {
             signalModifiedKey(c->db,c->argv[j]);
+            notifyKeyspaceEvent("del",c->argv[j],c->db->id);
             server.dirty++;
             deleted++;
         }
@@ -385,6 +386,8 @@ void renameGenericCommand(redisClient *c, int nx) {
     dbDelete(c->db,c->argv[1]);
     signalModifiedKey(c->db,c->argv[1]);
     signalModifiedKey(c->db,c->argv[2]);
+    notifyKeyspaceEvent("rename_from",c->argv[1],c->db->id);
+    notifyKeyspaceEvent("rename_to",c->argv[2],c->db->id);
     server.dirty++;
     addReply(c,nx ? shared.cone : shared.ok);
 }
--- src/redis.c
+++ src/redis.c
@@ -666,6 +666,7 @@ void activeExpireCycle(void) {
 
                     propagateExpire(db,keyobj);
                     dbDelete(db,keyobj);
+                    notifyKeyspaceEvent("expired",keyobj,db->id);
                     decrRefCount(keyobj);
                     expired++;
                     server.stat_expiredkeys++;
@@ -2359,6 +2360,7 @@ int freeMemoryIfNeeded(void) {
                 delta -= (long long) zmalloc_used_memory();
                 mem_freed += delta;
                 server.stat_evictedkeys++;
+                notifyKeyspaceEvent("evicted",keyobj,db->id);
                 decrRefCount(keyobj);
                 keys_freed++;
 
