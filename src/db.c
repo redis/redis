d3cf778d827150f0d870f955ca3de0cd3454dcde
antirez <antirez@gmail.com>
2013-01-28 13:00:03 +0100

    Send 'expired' events when a key expires by lookup.

--- src/db.c
+++ src/db.c
@@ -532,6 +532,8 @@ int expireIfNeeded(redisDb *db, robj *key) {
     /* Delete the key */
     server.stat_expiredkeys++;
     propagateExpire(db,key);
+    notifyKeyspaceEvent(REDIS_NOTIFY_EXPIRED,
+        "expired",key,db->id);
     return dbDelete(db,key);
 }
 
