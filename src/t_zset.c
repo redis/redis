ad297a1a6782905394cb56b00ed6dbd08707fb04
antirez <antirez@gmail.com>
2013-01-29 13:50:01 +0100

    Z*STORE event fixed: generate del only if resulting sorted set is empty.

--- src/t_zset.c
+++ src/t_zset.c
@@ -1680,7 +1680,6 @@ void zunionInterGenericCommand(redisClient *c, robj *dstkey, int op) {
 
     if (dbDelete(c->db,dstkey)) {
         signalModifiedKey(c->db,dstkey);
-        notifyKeyspaceEvent(REDIS_NOTIFY_GENERIC,"del",dstkey,c->db->id);
         touched = 1;
         server.dirty++;
     }
@@ -1700,6 +1699,8 @@ void zunionInterGenericCommand(redisClient *c, robj *dstkey, int op) {
     } else {
         decrRefCount(dstobj);
         addReply(c,shared.czero);
+        if (touched)
+            notifyKeyspaceEvent(REDIS_NOTIFY_GENERIC,"del",dstkey,c->db->id);
     }
     zfree(src);
 }
