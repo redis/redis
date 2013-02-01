8529dd218f05eee158771d654a0d72a446cc2530
antirez <antirez@gmail.com>
2013-02-01 15:05:43 +0100

    SYNC not allowed with pending data on the static output buffer.

--- src/replication.c
+++ src/replication.c
@@ -457,8 +457,8 @@ void syncCommand(redisClient *c) {
      * the client about already issued commands. We need a fresh reply
      * buffer registering the differences between the BGSAVE and the current
      * dataset, so that we can copy to other slaves if needed. */
-    if (listLength(c->reply) != 0) {
-        addReplyError(c,"SYNC and PSYNC are invalid with pending input");
+    if (listLength(c->reply) != 0 || c->bufpos != 0) {
+        addReplyError(c,"SYNC and PSYNC are invalid with pending output");
         return;
     }
 
