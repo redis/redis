7404b958339ebe2101965d8c890b38dcc0e4abfd
antirez <antirez@gmail.com>
2013-02-13 13:38:20 +0100

    Avoid compiler warning by casting to match printf() specifier.

--- src/debug.c
+++ src/debug.c
@@ -704,7 +704,8 @@ int memtest_test_linux_anonymous_maps(void) {
 
         start_vect[regions] = start_addr;
         size_vect[regions] = size;
-        printf("Testing %lx %lu\n", start_vect[regions], size_vect[regions]);
+        printf("Testing %lx %lu\n", (unsigned long) start_vect[regions],
+                                    (unsigned long) size_vect[regions]);
         regions++;
     }
 
