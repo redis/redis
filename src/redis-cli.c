6b641f3aeb03b1608eb8b435f9e06c3f8b04773e
antirez <antirez@gmail.com>
2013-02-14 18:49:08 +0100

    redis-cli: update prompt on cluster redirection.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -533,6 +533,7 @@ static int cliReadReply(int output_raw_strings) {
             printf("-> Redirected to slot [%d] located at %s:%d\n",
                 slot, config.hostip, config.hostport);
         config.cluster_reissue_command = 1;
+        cliRefreshPrompt();
     }
 
     if (output) {
