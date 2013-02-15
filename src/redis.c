455da35c7f34a6f1e36d862ed6466a34948f3b4f
antirez <antirez@gmail.com>
2013-02-15 16:53:24 +0100

    Cluster: specific error code for cluster down condition.

--- src/redis.c
+++ src/redis.c
@@ -1662,7 +1662,7 @@ int processCommand(redisClient *c) {
         int hashslot;
 
         if (server.cluster->state != REDIS_CLUSTER_OK) {
-            addReplyError(c,"The cluster is down. Check with CLUSTER INFO for more information");
+            addReplySds(c,sdsnew("-CLUSTERDOWN The cluster is down. Use CLUSTER INFO for more information\r\n"));
             return REDIS_OK;
         } else {
             int ask;
