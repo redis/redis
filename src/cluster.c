3bb10d88780be5adac1bfd266a7f87a1e1df847c
charsyam <charsyam@gmail.com>
2013-02-21 01:22:24 -0800

    adding check error code
    adding check error code

--- src/cluster.c
+++ src/cluster.c
@@ -67,6 +67,8 @@ int clusterLoadConfig(char *filename) {
     while(fgets(line,maxline,fp) != NULL) {
         int argc;
         sds *argv = sdssplitargs(line,&argc);
+        if (argv == NULL) goto fmterr;
+
         clusterNode *n, *master;
         char *p, *s;
 
