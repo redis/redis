c6da9d9fac86a239e1381e9aa90cfedd08fa5511
antirez <antirez@gmail.com>
2013-02-21 16:31:22 +0100

    Call clusterUpdateState() after CLUSTER SETSLOT too.

--- src/cluster.c
+++ src/cluster.c
@@ -1440,6 +1440,7 @@ void clusterCommand(redisClient *c) {
             addReplyError(c,"Invalid CLUSTER SETSLOT action or number of arguments");
             return;
         }
+        clusterUpdateState();
         clusterSaveConfigOrDie();
         addReply(c,shared.ok);
     } else if (!strcasecmp(c->argv[1]->ptr,"info") && c->argc == 2) {
