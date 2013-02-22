f4093753e495799d2473f40cee29d5e47878372a
antirez <antirez@gmail.com>
2013-02-22 19:20:38 +0100

    Cluster: add cluster_size field in CLUSTER INFO output.

--- src/cluster.c
+++ src/cluster.c
@@ -1565,12 +1565,14 @@ void clusterCommand(redisClient *c) {
             "cluster_slots_pfail:%d\r\n"
             "cluster_slots_fail:%d\r\n"
             "cluster_known_nodes:%lu\r\n"
+            "cluster_size:%d\r\n"
             , statestr[server.cluster->state],
             slots_assigned,
             slots_ok,
             slots_pfail,
             slots_fail,
-            dictSize(server.cluster->nodes)
+            dictSize(server.cluster->nodes),
+            server.cluster->size
         );
         addReplySds(c,sdscatprintf(sdsempty(),"$%lu\r\n",
             (unsigned long)sdslen(info)));
