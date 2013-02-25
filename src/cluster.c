c2eb4a606f5bf534de6b6f68ed4051afdd5e3aa1
antirez <antirez@gmail.com>
2013-02-25 11:23:04 +0100

    Cluster: use CountKeysInSlot() when we just need the count.

--- src/cluster.c
+++ src/cluster.c
@@ -1534,13 +1534,7 @@ void clusterCommand(redisClient *c) {
             if (server.cluster->slots[slot] == server.cluster->myself &&
                 n != server.cluster->myself)
             {
-                int numkeys;
-                robj **keys;
-
-                keys = zmalloc(sizeof(robj*)*1);
-                numkeys = GetKeysInSlot(slot, keys, 1);
-                zfree(keys);
-                if (numkeys != 0) {
+                if (CountKeysInSlot(slot) != 0) {
                     addReplyErrorFormat(c, "Can't assign hashslot %d to a different node while I still hold keys for this hash slot.", slot);
                     return;
                 }
