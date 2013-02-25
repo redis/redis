18f537083aaf54b35e2a5aa0a683d031ac54e253
antirez <antirez@gmail.com>
2013-02-25 12:41:13 +0100

    Cluster: no limits for the count parameter of CLUSTER GETKEYSINSLOT.
    Not sure why I set a limit to 1 million keys, there is no reason for
    this artificial limit, and anyway this is s a stupid limit because it is
    already high enough to create latency issues. So let's the users shoot
    on their feet because maybe they just actually know what they are doing.

--- src/cluster.c
+++ src/cluster.c
@@ -1659,8 +1659,7 @@ void clusterCommand(redisClient *c) {
             return;
         if (getLongLongFromObjectOrReply(c,c->argv[3],&maxkeys,NULL) != REDIS_OK)
             return;
-        if (slot < 0 || slot >= REDIS_CLUSTER_SLOTS || maxkeys < 0 ||
-            maxkeys > 1024*1024) {
+        if (slot < 0 || slot >= REDIS_CLUSTER_SLOTS || maxkeys < 0) {
             addReplyError(c,"Invalid slot or number of keys");
             return;
         }
