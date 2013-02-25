544bbe5387b9c4fd925693b947fce574bd9854a5
antirez <antirez@gmail.com>
2013-02-25 12:40:32 +0100

    Cluster: validate slot number in CLUSTER COUNTKEYSINSLOT.

--- src/cluster.c
+++ src/cluster.c
@@ -1645,6 +1645,10 @@ void clusterCommand(redisClient *c) {
 
         if (getLongLongFromObjectOrReply(c,c->argv[2],&slot,NULL) != REDIS_OK)
             return;
+        if (slot < 0 || slot >= REDIS_CLUSTER_SLOTS) {
+            addReplyError(c,"Invalid slot");
+            return;
+        }
         addReplyLongLong(c,countKeysInSlot(slot));
     } else if (!strcasecmp(c->argv[1]->ptr,"getkeysinslot") && c->argc == 4) {
         long long maxkeys, slot;
