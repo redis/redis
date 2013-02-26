f288b07563c833ae7c3748692ea7e35d299c0082
antirez <antirez@gmail.com>
2013-02-26 15:03:27 +0100

    Cluster: log the event when we clear the FAIL flag.

--- src/cluster.c
+++ src/cluster.c
@@ -569,6 +569,9 @@ void clearNodeFailureIfNeeded(clusterNode *node) {
     if (node->flags & REDIS_NODE_FAIL && !node->numslaves) {
         node->flags &= ~REDIS_NODE_FAIL;
         clusterUpdateState();
+        redisLog(REDIS_NOTICE,
+            "Clear FAIL state for node %.40s: is reachable and has no slaves.",
+                node->name);
     }
 }
 
