d2b8281b3f2c19d6510bb066bca6d8ebd04a38dd
antirez <antirez@gmail.com>
2013-02-27 17:53:48 +0100

    Cluster: added a missing return on CLUSTER SETSLOT.

--- src/cluster.c
+++ src/cluster.c
@@ -1669,8 +1669,11 @@ void clusterCommand(redisClient *c) {
             /* CLUSTER SETSLOT <SLOT> NODE <NODE ID> */
             clusterNode *n = clusterLookupNode(c->argv[4]->ptr);
 
-            if (!n) addReplyErrorFormat(c,"Unknown node %s",
-                (char*)c->argv[4]->ptr);
+            if (!n) {
+                addReplyErrorFormat(c,"Unknown node %s",
+                    (char*)c->argv[4]->ptr);
+                return;
+            }
             /* If this hash slot was served by 'myself' before to switch
              * make sure there are no longer local keys for this hash slot. */
             if (server.cluster->slots[slot] == server.cluster->myself &&
