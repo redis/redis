9c8be6cab9e4ec6c4fad2e1dd01fb13f7032205b
Stam He <stamhe@163.com>
2013-02-27 11:53:11 +0800

    Set proctitle: avoid the use of __attribute__((constructor)).
    This cased a segfault in some Linux system and was GCC-specific.
    
    Commit modified by @antirez:
    
    1) Stripped away the part to set the proc title via config for now.
    2) Handle initialization of setproctitle only when the replacement
       is used.
    3) Don't require GCC now that the attribute constructor is no
       longer used.

--- src/config.h
+++ src/config.h
@@ -114,6 +114,8 @@
 
 #if (defined __linux || defined __APPLE__)
 #define USE_SETPROCTITLE
+#define INIT_SETPROCTITLE_REPLACEMENT
+void spt_init(int argc, char *argv[]);
 void setproctitle(const char *fmt, ...);
 #endif
 
