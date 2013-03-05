b7d085fc0dea44bb614da7edce2370521802250d
antirez <antirez@gmail.com>
2013-03-05 12:39:11 +0100

    Cluster: SLAVEOF command not allowed in cluster mode.

--- src/replication.c
+++ src/replication.c
@@ -1251,6 +1251,15 @@ void replicationUnsetMaster(void) {
 }
 
 void slaveofCommand(redisClient *c) {
+    /* SLAVEOF is not allowed in cluster mode as replication is automatically
+     * configured using the current address of the master node. */
+    if (server.cluster_enabled) {
+        addReplyError(c,"SLAVEOF not allowed in cluster mode.");
+        return;
+    }
+
+    /* The special host/port combination "NO" "ONE" turns the instance
+     * into a master. Otherwise the new master address is set. */
     if (!strcasecmp(c->argv[1]->ptr,"no") &&
         !strcasecmp(c->argv[2]->ptr,"one")) {
         if (server.masterhost) {
