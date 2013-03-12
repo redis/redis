4178a80282ac917a48771da2c2d1ee65bc562161
Damian Janowski <damian.janowski@gmail.com>
2013-03-12 14:37:50 -0300

    Abort when opening the RDB file results in an error other than ENOENT.
    This fixes cases where the RDB file does exist but can't be accessed for
    any reason. For instance, when the Redis process doesn't have enough
    permissions on the file.

--- src/redis.c
+++ src/redis.c
@@ -2736,7 +2736,7 @@ void loadDataFromDisk(void) {
             redisLog(REDIS_NOTICE,"DB loaded from disk: %.3f seconds",
                 (float)(ustime()-start)/1000000);
         } else if (errno != ENOENT) {
-            redisLog(REDIS_WARNING,"Fatal error loading the DB. Exiting.");
+            redisLog(REDIS_WARNING,"Fatal error loading the DB: %s. Exiting.",strerror(errno));
             exit(1);
         }
     }
