f93d9929d8bc2d97345070c9caa9509c0481496e
antirez <antirez@gmail.com>
2013-03-12 09:57:49 +0100

    redis-cli --bigkeys: don't crash with empty DBs.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1205,7 +1205,11 @@ static void findBigKeys(void) {
             fprintf(stderr, "RANDOMKEY error: %s\n",
                 reply1->str);
             exit(1);
+        } else if (reply1->type == REDIS_REPLY_NIL) {
+            fprintf(stderr, "It looks like the database is empty!\n");
+            exit(1);
         }
+
         /* Get the key type */
         reply2 = redisCommand(context,"TYPE %s",reply1->str);
         assert(reply2 && reply2->type == REDIS_REPLY_STATUS);
