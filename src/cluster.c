88221f88c05bd9b0ff6e7941006661e630507659
antirez <antirez@gmail.com>
2013-03-19 09:58:05 +0100

    Cluster: new command CLUSTER FLUSHSLOTS.
    It's just a simpler way to CLUSTER DELSLOTS with all the slots as
    arguments, in order to obtain a node without assigned slots for
    reconfiguration.

--- src/cluster.c
+++ src/cluster.c
@@ -1961,6 +1961,16 @@ void clusterCommand(redisClient *c) {
         o = createObject(REDIS_STRING,ci);
         addReplyBulk(c,o);
         decrRefCount(o);
+    } else if (!strcasecmp(c->argv[1]->ptr,"flushslots") && c->argc == 2) {
+        /* CLUSTER FLUSHSLOTS */
+        if (dictSize(server.db[0].dict) != 0) {
+            addReplyError(c,"DB must be empty to perform CLUSTER FLUSHSLOTS.");
+            return;
+        }
+        clusterDelNodeSlots(server.cluster->myself);
+        clusterUpdateState();
+        clusterSaveConfigOrDie();
+        addReply(c,shared.ok);
     } else if ((!strcasecmp(c->argv[1]->ptr,"addslots") ||
                !strcasecmp(c->argv[1]->ptr,"delslots")) && c->argc >= 3)
     {
