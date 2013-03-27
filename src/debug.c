32a83c8206a501f300e971f46cb6cd469731e74f
antirez <antirez@gmail.com>
2013-03-27 17:55:02 +0100

    DEBUG set-active-expire added.
    We need the ability to disable the activeExpireCycle() (active
    expired key collection) call for testing purposes.

--- src/debug.c
+++ src/debug.c
@@ -332,9 +332,14 @@ void debugCommand(redisClient *c) {
 
         usleep(utime);
         addReply(c,shared.ok);
+    } else if (!strcasecmp(c->argv[1]->ptr,"set-active-expire") &&
+               c->argc == 3)
+    {
+        server.active_expire_enabled = atoi(c->argv[2]->ptr);
+        addReply(c,shared.ok);
     } else {
-        addReplyError(c,
-            "Syntax error, try DEBUG [SEGFAULT|OBJECT <key>|SWAPIN <key>|SWAPOUT <key>|RELOAD]");
+        addReplyErrorFormat(c, "Unknown DEBUG subcommand '%s'",
+            (char*)c->argv[1]->ptr);
     }
 }
 
