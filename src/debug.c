47911b8630ccd2717ef678fdf2d975ea57d0e192
antirez <antirez@gmail.com>
2013-03-28 11:39:02 +0100

    Better DEBUG error message when num of arguments is wrong.

--- src/debug.c
+++ src/debug.c
@@ -336,7 +336,7 @@ void debugCommand(redisClient *c) {
         server.active_expire_enabled = atoi(c->argv[2]->ptr);
         addReply(c,shared.ok);
     } else {
-        addReplyErrorFormat(c, "Unknown DEBUG subcommand '%s'",
+        addReplyErrorFormat(c, "Unknown DEBUG subcommand or wrong number of arguments for '%s'",
             (char*)c->argv[1]->ptr);
     }
 }
