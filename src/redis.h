ed2d9881926d4b9fb0529414aeafd367cd29c1a6
antirez <antirez@gmail.com>
2013-04-02 14:05:50 +0200

    Throttle BGSAVE attempt on saving error.
    When a BGSAVE fails, Redis used to flood itself trying to BGSAVE at
    every next cron call, that is either 10 or 100 times per second
    depending on configuration and server version.
    
    This commit does not allow a new automatic BGSAVE attempt to be
    performed before a few seconds delay (currently 5).
    
    This avoids both the auto-flood problem and filling the disk with
    logs at a serious rate.
    
    The five seconds limit, considering a log entry of 200 bytes, will use
    less than 4 MB of disk space per day that is reasonable, the sysadmin
    should notice before of catastrofic events especially since by default
    Redis will stop serving write queries after the first failed BGSAVE.
    
    This fixes issue #849

--- src/redis.h
+++ src/redis.h
@@ -94,6 +94,7 @@
 #define REDIS_REPL_PING_SLAVE_PERIOD 10
 #define REDIS_RUN_ID_SIZE 40
 #define REDIS_OPS_SEC_SAMPLES 16
+#define REDIS_BGSAVE_RETRY_DELAY 5 /* Wait a few secs before trying again. */
 
 /* Protocol and I/O related defines */
 #define REDIS_MAX_QUERYBUF_LEN  (1024*1024*1024) /* 1GB max query buffer. */
@@ -593,6 +594,7 @@ struct redisServer {
     int rdb_compression;            /* Use compression in RDB? */
     int rdb_checksum;               /* Use RDB checksum? */
     time_t lastsave;                /* Unix time of last successful save */
+    time_t lastbgsave_try;          /* Unix time of last attempted bgsave */
     time_t rdb_save_time_last;      /* Time used by last RDB save run. */
     time_t rdb_save_time_start;     /* Current RDB save start time. */
     int lastbgsave_status;          /* REDIS_OK or REDIS_ERR */
