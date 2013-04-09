d1aee359c08b6fcac31a76a2e4a9b9bef81d2947
antirez <antirez@gmail.com>
2013-04-09 11:29:51 +0200

    Cluster: CONFIG SET cluster-node-timeout.

--- src/config.c
+++ src/config.c
@@ -784,6 +784,10 @@ void configSetCommand(redisClient *c) {
         if (getLongLongFromObject(o,&ll) == REDIS_ERR ||
             ll <= 0) goto badfmt;
         server.slave_priority = ll;
+    } else if (!strcasecmp(c->argv[2]->ptr,"cluster-node-timeout")) {
+        if (getLongLongFromObject(o,&ll) == REDIS_ERR ||
+            ll <= 0) goto badfmt;
+        server.cluster_node_timeout = ll;
     } else {
         addReplyErrorFormat(c,"Unsupported CONFIG parameter: %s",
             (char*)c->argv[2]->ptr);
