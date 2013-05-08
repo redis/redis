6f98ff1a7ae44e9c15348fd86da212693b6ed3be
antirez <antirez@gmail.com>
2013-05-08 11:58:26 +0200

    Test: various issues with the replication-4.tcl test fixed.
    The test actually worked, but vars for master and slave were inverted
    and sometimes used incorrectly.

--- tests/integration/replication-4.tcl
+++ tests/integration/replication-4.tcl
@@ -10,21 +10,22 @@ proc stop_bg_complex_data {handle} {
 start_server {tags {"repl"}} {
     start_server {} {
 
-        set master [srv 0 client]
-        set master_host [srv 0 host]
-        set master_port [srv 0 port]
+        set master [srv -1 client]
+        set master_host [srv -1 host]
+        set master_port [srv -1 port]
+        set slave [srv 0 client]
+
         set load_handle0 [start_bg_complex_data $master_host $master_port 9 100000]
         set load_handle1 [start_bg_complex_data $master_host $master_port 11 100000]
         set load_handle2 [start_bg_complex_data $master_host $master_port 12 100000]
 
         test {First server should have role slave after SLAVEOF} {
-            r -1 slaveof [srv 0 host] [srv 0 port]
+            $slave slaveof $master_host $master_port
             after 1000
-            s -1 role
+            s 0 role
         } {slave}
 
         test {Test replication with parallel clients writing in differnet DBs} {
-            lappend slave [srv 0 client]
             after 5000
             stop_bg_complex_data $load_handle0
             stop_bg_complex_data $load_handle1
@@ -37,7 +38,7 @@ start_server {tags {"repl"}} {
             }
             assert {[$master dbsize] > 0}
 
-            if {[r debug digest] ne [r -1 debug digest]} {
+            if {[$master debug digest] ne [$slave debug digest]} {
                 set csv1 [csvdump r]
                 set csv2 [csvdump {r -1}]
                 set fd [open /tmp/repldump1.txt w]
