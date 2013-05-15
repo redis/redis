901ceab20b9a39cb2196f5404f5b89a2af328ef8
antirez <antirez@gmail.com>
2013-05-15 11:15:18 +0200

    CONFIG REWRITE: correctly escape the notify-keyspace-events option.

--- src/config.c
+++ src/config.c
@@ -1358,7 +1358,9 @@ void rewriteConfigNotifykeyspaceeventsOption(struct rewriteConfigState *state) {
     sds line, flags;
 
     flags = keyspaceEventsFlagsToString(server.notify_keyspace_events);
-    line = sdscatprintf(sdsempty(),"%s %s", option, flags);
+    line = sdsnew(option);
+    line = sdscatlen(line, " ", 1);
+    line = sdscatrepr(line, flags, sdslen(flags));
     sdsfree(flags);
     rewriteConfigRewriteLine(state,option,line,force);
 }
