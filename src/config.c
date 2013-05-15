7d338fd982650926f228c681de815940b538142f
antirez <antirez@gmail.com>
2013-05-15 11:38:43 +0200

    CONFIG REWRITE: bindaddr -> bind.

--- src/config.c
+++ src/config.c
@@ -1548,7 +1548,7 @@ int rewriteConfig(char *path) {
     rewriteConfigYesNoOption(state,"daemonize",server.daemonize,0);
     rewriteConfigStringOption(state,"pidfile",server.pidfile,REDIS_DEFAULT_PID_FILE);
     rewriteConfigNumericalOption(state,"port",server.port,REDIS_SERVERPORT);
-    rewriteConfigStringOption(state,"bindaddr",server.bindaddr,NULL);
+    rewriteConfigStringOption(state,"bind",server.bindaddr,NULL);
     rewriteConfigStringOption(state,"unixsocket",server.unixsocket,NULL);
     rewriteConfigOctalOption(state,"unixsocketperm",server.unixsocketperm,REDIS_DEFAULT_UNIX_SOCKET_PERM);
     rewriteConfigNumericalOption(state,"timeout",server.maxidletime,REDIS_MAXIDLETIME);
