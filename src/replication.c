0000d5334df14cc281231b79a1d5f0e17d343c7a
antirez <antirez@gmail.com>
2013-05-25 01:22:27 +0200

    Make sure that REPLCONF ACK really has no return value.

--- src/replication.c
+++ src/replication.c
@@ -601,6 +601,7 @@ void replconfCommand(redisClient *c) {
                 c->repl_ack_off = offset;
             c->repl_ack_time = server.unixtime;
             /* Note: this command does not reply anything! */
+            return;
         } else {
             addReplyErrorFormat(c,"Unrecognized REPLCONF option: %s",
                 (char*)c->argv[j]->ptr);
