888400ebd57c85682edc147bb0f666af29c76d5b
antirez <antirez@gmail.com>
2013-05-29 19:56:33 +0200

    repl_offset field in INFO replication is now just offset.

--- src/redis.c
+++ src/redis.c
@@ -2344,7 +2344,7 @@ sds genRedisInfoString(char *section) {
 
                 info = sdscatprintf(info,
                     "slave%d:ip=%s,port=%d,state=%s,"
-                    "repl_offset=%lld,lag=%ld\r\n",
+                    "offset=%lld,lag=%ld\r\n",
                     slaveid,ip,slave->slave_listening_port,state,
                     slave->repl_ack_off, lag);
                 slaveid++;
