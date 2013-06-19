8e63fd07341f7a8ebc3b7455a84e09121bef9a5d
antirez <antirez@gmail.com>
2013-06-19 18:25:03 +0200

    Allow writes from scripts called by AOF loading in read-only slaves.
    This fixes issue #1163

--- src/scripting.c
+++ src/scripting.c
@@ -258,6 +258,7 @@ int luaRedisGenericCommand(lua_State *lua, int raise_error) {
                 "Write commands not allowed after non deterministic commands");
             goto cleanup;
         } else if (server.masterhost && server.repl_slave_ro &&
+                   !server.loading &&
                    !(server.lua_caller->flags & REDIS_MASTER))
         {
             luaPushError(lua, shared.roslaveerr->ptr);
