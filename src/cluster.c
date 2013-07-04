016ac38a216d1b701991a451e637ca584f804f93
antirez <antirez@gmail.com>
2013-07-04 18:49:49 +0200

    Cluster: use new anet.c listening socket creation API.

--- src/cluster.c
+++ src/cluster.c
@@ -252,7 +252,8 @@ void clusterInit(void) {
     if (saveconf) clusterSaveConfigOrDie();
     /* We need a listening TCP port for our cluster messaging needs */
     server.cfd = anetTcpServer(server.neterr,
-            server.port+REDIS_CLUSTER_PORT_INCR, server.bindaddr);
+            server.port+REDIS_CLUSTER_PORT_INCR, server.bindaddr,
+            server.bindaddr_count);
     if (server.cfd == -1) {
         redisLog(REDIS_WARNING, "Opening cluster TCP port: %s", server.neterr);
         exit(1);
