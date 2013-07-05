2160effc78463d6602082302486beb51eb6a999d
antirez <antirez@gmail.com>
2013-07-05 11:08:44 +0200

    Revert "Cluster: use new anet.c listening socket creation API."
    This reverts commit 016ac38a216d1b701991a451e637ca584f804f93.

--- src/cluster.c
+++ src/cluster.c
@@ -252,8 +252,7 @@ void clusterInit(void) {
     if (saveconf) clusterSaveConfigOrDie();
     /* We need a listening TCP port for our cluster messaging needs */
     server.cfd = anetTcpServer(server.neterr,
-            server.port+REDIS_CLUSTER_PORT_INCR, server.bindaddr,
-            server.bindaddr_count);
+            server.port+REDIS_CLUSTER_PORT_INCR, server.bindaddr);
     if (server.cfd == -1) {
         redisLog(REDIS_WARNING, "Opening cluster TCP port: %s", server.neterr);
         exit(1);
