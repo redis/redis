323e5e2058d9d83dd554ae21f7c732a84f574d1b
antirez <antirez@gmail.com>
2013-07-08 10:31:49 +0200

    Remove cluster stuff from closeListeningSockets() for 2.8.

--- src/redis.c
+++ src/redis.c
@@ -1895,8 +1895,6 @@ void closeListeningSockets(int unlink_unix_socket) {
 
     for (j = 0; j < server.ipfd_count; j++) close(server.ipfd[j]);
     if (server.sofd != -1) close(server.sofd);
-    if (server.cluster_enabled)
-        for (j = 0; j < server.cfd_count; j++) close(server.cfd[j]);
     if (unlink_unix_socket && server.unixsocket) {
         redisLog(REDIS_NOTICE,"Removing the unix socket file.");
         unlink(server.unixsocket); /* don't care if this fails */
