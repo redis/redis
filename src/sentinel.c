98d0abcecdf50a15a8aac31c3c9fd995091e6169
antirez <antirez@gmail.com>
2013-07-10 14:44:38 +0200

    Sentinel: make sure published addr/id buffer is large enough.
    With ipv6 support we need more space, so we account for the IP address
    max size plus what we need for the Run ID, port, flags.

--- src/sentinel.c
+++ src/sentinel.c
@@ -1839,7 +1839,7 @@ void sentinelPingInstance(sentinelRedisInstance *ri) {
         /* PUBLISH hello messages only to masters. */
         char ip[REDIS_IP_STR_LEN];
         if (anetSockName(ri->cc->c.fd,ip,sizeof(ip),NULL) != -1) {
-            char myaddr[128];
+            char myaddr[REDIS_IP_STR_LEN+128];
 
             // FIXME: IPv6 will break this due to nested : characters -geoffgarside
             snprintf(myaddr,sizeof(myaddr),"%s:%d:%s:%d",
