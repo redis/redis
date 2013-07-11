cf85b5ba812070442c450b5dbc6c5e12a6c0819b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2013-07-10 21:12:46 -0700

    va_copy must be matched by va_end
    Hat tip to @rfuchs. See: https://github.com/redis/hiredis/pull/178.
    
    Fixes #1187

--- src/sds.c
+++ src/sds.c
@@ -388,6 +388,7 @@ sds sdscatvprintf(sds s, const char *fmt, va_list ap) {
         buf[buflen-2] = '\0';
         va_copy(cpy,ap);
         vsnprintf(buf, buflen, fmt, cpy);
+        va_end(ap);
         if (buf[buflen-2] != '\0') {
             if (buf != staticbuf) zfree(buf);
             buflen *= 2;
