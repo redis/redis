1b696dc07ce137c4e5bfc7c7e35e0f9b751099fc
Jan-Erik Rediger <badboy@archlinux.us>
2013-07-11 17:47:55 +0200

    Wrap IPv6 in brackets in the prompt.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -116,7 +116,8 @@ static void cliRefreshPrompt(void) {
         len = snprintf(config.prompt,sizeof(config.prompt),"redis %s",
                        config.hostsocket);
     else
-        len = snprintf(config.prompt,sizeof(config.prompt),"redis %s:%d",
+        len = snprintf(config.prompt,sizeof(config.prompt),
+                       strchr(config.hostip,':') ? "[%s]:%d" : "%s:%d",
                        config.hostip, config.hostport);
     /* Add [dbnum] if needed */
     if (config.dbnum != 0)
