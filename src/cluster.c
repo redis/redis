3c68bb30ec12dffbdd8ca4a3a7d0402a5c335073
Ivan <dilcom3107@gmail.com>
2013-07-14 19:52:54 +0100

    fix small error in IP address validating

--- src/cluster.c
+++ src/cluster.c
@@ -2080,8 +2080,8 @@ void clusterCommand(redisClient *c) {
         long port;
 
         /* Perform sanity checks on IP/port */
-        if ((inet_pton(AF_INET,c->argv[0]->ptr,&(((struct sockaddr_in *)&sa)->sin_addr)) ||
-             inet_pton(AF_INET6,c->argv[0]->ptr,&(((struct sockaddr_in6 *)&sa)->sin6_addr))) == 0) {
+        if ((inet_pton(AF_INET,c->argv[2]->ptr,&(((struct sockaddr_in *)&sa)->sin_addr)) ||
+             inet_pton(AF_INET6,c->argv[2]->ptr,&(((struct sockaddr_in6 *)&sa)->sin6_addr))) == 0) {
             addReplyError(c,"Invalid IP address in MEET");
             return;
         }
