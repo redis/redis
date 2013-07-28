b3c8792f8e6d570503cf9d8e1b87c6915d70a1b6
ripcurld00d <boazyeah@gmail.com>
2013-07-28 20:11:58 +0300

    Add valgrind-3.6.0 support to unit tests

--- tests/support/server.tcl
+++ tests/support/server.tcl
@@ -17,10 +17,11 @@ proc check_valgrind_errors stderr {
     set buf [read $fd]
     close $fd
 
-    if {[regexp -- { at 0x} $buf] ||
+    if {([regexp -- { at 0x} $buf] &&
+         ![regexp -- {valgrind-[0-3].[0-6]} $::valgrind_version]) ||
         (![regexp -- {definitely lost: 0 bytes} $buf] &&
          ![regexp -- {no leaks are possible} $buf])} {
-        send_data_packet $::test_server_fd err "Valgrind error: $buf\n"
+        send_data_packet $::test_server_fd err "Valgrind error:\n $buf\n"
     }
 }
 
@@ -183,7 +184,12 @@ proc start_server {options {code undefined}} {
     set stderr [format "%s/%s" [dict get $config "dir"] "stderr"]
 
     if {$::valgrind} {
-        exec valgrind --suppressions=src/valgrind.sup --show-reachable=no --show-possibly-lost=no --leak-check=full src/redis-server $config_file > $stdout 2> $stderr &
+        if {[regexp -- {valgrind-[0-3].[0-6]} $::valgrind_version]} {
+            puts "attention: using old version of valgrind: $::valgrind_version"
+            exec valgrind --suppressions=src/valgrind.sup --show-reachable=no --leak-check=full src/redis-server $config_file > $stdout 2> $stderr &
+        } else {
+            exec valgrind --suppressions=src/valgrind.sup --show-reachable=no --show-possibly-lost=no --leak-check=full src/redis-server $config_file > $stdout 2> $stderr &
+        }
     } else {
         exec src/redis-server $config_file > $stdout 2> $stderr &
     }
