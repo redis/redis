9c3e6c3aa098f5b83a350b8817fc26c3a157814d
antirez <antirez@gmail.com>
2013-08-27 10:16:01 +0200

    Update server.lastbgsave_status when fork() fails.

--- src/rdb.c
+++ src/rdb.c
@@ -746,6 +746,7 @@ int rdbSaveBackground(char *filename) {
         /* Parent */
         server.stat_fork_time = ustime()-start;
         if (childpid == -1) {
+            server.lastbgsave_status = REDIS_ERR;
             redisLog(REDIS_WARNING,"Can't save in background: fork: %s",
                 strerror(errno));
             return REDIS_ERR;
