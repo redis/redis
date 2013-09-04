1fc83e65abaff22310cc4ba65a74eaa124b92fb2
antirez <antirez@gmail.com>
2013-09-04 10:35:53 +0200

    redis-cli: fix big keys search when the key no longer exist.
    The code freed a reply object that was never created, resulting in a
    segfault every time randomkey returned a key that was deleted before we
    queried it for size.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1295,7 +1295,6 @@ static void findBigKeys(void) {
         } else if (!strcmp(reply2->str,"none")) {
             freeReplyObject(reply1);
             freeReplyObject(reply2);
-            freeReplyObject(reply3);
             continue;
         } else {
             fprintf(stderr, "Unknown key type '%s' for key '%s'\n",
