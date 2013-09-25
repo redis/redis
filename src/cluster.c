32b5410af928b9d651a837f79c6c6002615a3048
antirez <antirez@gmail.com>
2013-09-25 12:38:36 +0200

    Cluster: add currentEpoch to CLUSTER INFO.

--- src/cluster.c
+++ src/cluster.c
@@ -2393,13 +2393,15 @@ void clusterCommand(redisClient *c) {
             "cluster_slots_fail:%d\r\n"
             "cluster_known_nodes:%lu\r\n"
             "cluster_size:%d\r\n"
+            "cluster_current_epoch:%llu\r\n"
             , statestr[server.cluster->state],
             slots_assigned,
             slots_ok,
             slots_pfail,
             slots_fail,
             dictSize(server.cluster->nodes),
-            server.cluster->size
+            server.cluster->size,
+            (unsigned long long) server.cluster->currentEpoch
         );
         addReplySds(c,sdscatprintf(sdsempty(),"$%lu\r\n",
             (unsigned long)sdslen(info)));
