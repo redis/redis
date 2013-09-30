b1875177198959710d9c9a348eb9f116b797b6bc
antirez <antirez@gmail.com>
2013-09-30 10:13:33 +0200

    Cluster: when upading the configEpoch for a node, save config on disk ASAP.

--- src/cluster.c
+++ src/cluster.c
@@ -924,8 +924,10 @@ int clusterProcessPacket(clusterLink *link) {
         if (senderCurrentEpoch > server.cluster->currentEpoch)
             server.cluster->currentEpoch = senderCurrentEpoch;
         /* Update the sender configEpoch if it is publishing a newer one. */
-        if (senderConfigEpoch > sender->configEpoch)
+        if (senderConfigEpoch > sender->configEpoch) {
             sender->configEpoch = senderConfigEpoch;
+            clusterSaveConfigOrDie();
+        }
     }
 
     /* Process packets by type. */
