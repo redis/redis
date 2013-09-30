81f614ef6f1eaf5c1c5699cf00b87c1c02da216e
antirez <antirez@gmail.com>
2013-09-30 11:53:18 +0200

    Add REWRITE to CONFIG subcommands help message.

--- src/config.c
+++ src/config.c
@@ -1732,7 +1732,7 @@ void configCommand(redisClient *c) {
         }
     } else {
         addReplyError(c,
-            "CONFIG subcommand must be one of GET, SET, RESETSTAT");
+            "CONFIG subcommand must be one of GET, SET, RESETSTAT, REWRITE");
     }
     return;
 
