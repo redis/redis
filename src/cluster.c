c9ab793e33c59ff27e04a4424c6d0438e24f06f5
SungBin_Hong <hsb0818@gmail.com>
2013-10-15 00:12:06 +0900

    Added feature to free memory in clusterLoadConfig function. (memory is "line")

--- src/cluster.c
+++ src/cluster.c
@@ -218,6 +218,7 @@ int clusterLoadConfig(char *filename) {
 
 fmterr:
     redisLog(REDIS_WARNING,"Unrecoverable error: corrupted cluster config file.");
+    zfree(line);
     fclose(fp);
     exit(1);
 }
