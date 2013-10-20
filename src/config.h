5f47be55ee76be56c552daff57de33c9e74dc4df
Rajiv Kurian <geetasen@gmail.com>
2013-10-20 00:08:22 -0700

    Use accept4 on linux, instead of fcntl to make a client socket non-blocking

--- src/config.h
+++ src/config.h
@@ -70,7 +70,6 @@
 #define HAVE_ACCEPT4 1
 #endif
 
-
 #if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
 #define HAVE_KQUEUE 1
 #endif
