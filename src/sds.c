bbea840e7e3aa8de8216df0fd61702e90e1d86ae
Eric Urban <hydrogen18@gmail.com>
2013-10-21 18:52:20 -0400

    Remove bogus quote in documentation

--- src/sds.c
+++ src/sds.c
@@ -43,7 +43,7 @@
  * The string is always null-termined (all the sds strings are, always) so
  * even if you create an sds string with:
  *
- * mystring = sdsnewlen("abc",3");
+ * mystring = sdsnewlen("abc",3);
  *
  * You can print the string with printf() as there is an implicit \0 at the
  * end of the string. However the string is binary safe and can contain
