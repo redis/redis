908eba5a8fd88354e9254cc312b0013ccf60121c
antirez <antirez@gmail.com>
2013-10-25 11:51:08 +0200

    SCAN: remove additional newlines to conform to Redis code base.

--- src/db.c
+++ src/db.c
@@ -391,12 +391,10 @@ void scanCommand(redisClient *c) {
             decrRefCount(kobj);
             listDelNode(keys, ln);
         }
-
         ln = ln_;
     }
 
     addReplyMultiBulkLen(c, 2);
-
     rv = snprintf(buf, sizeof(buf), "%lu", cursor);
     redisAssert(rv < sizeof(buf));
     addReplyBulkCBuffer(c, buf, rv);
@@ -415,7 +413,6 @@ cleanup:
         decrRefCount(kobj);
         listDelNode(keys, ln);
     }
-
     listRelease(keys);
 }
 
