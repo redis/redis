0471b908448682d7a9b19cdede6056648cdfd631
antirez <antirez@gmail.com>
2013-10-25 11:55:28 +0200

    SCAN: Fix test after option renamed from PATTERN to MATCH.

--- tests/unit/basic.tcl
+++ tests/unit/basic.tcl
@@ -798,14 +798,14 @@ start_server {tags {"basic"}} {
         assert_equal 1000 [llength $keys]
     }
 
-    test "SCAN PATTERN" {
+    test "SCAN MATCH" {
         r flushdb
         r debug populate 1000
 
         set cur 0
         set keys {}
         while 1 {
-            set res [r scan $cur pattern "key:1??"]
+            set res [r scan $cur match "key:1??"]
             set cur [lindex $res 0]
             set k [lindex $res 1]
             lappend keys $k
