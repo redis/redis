93d924ff1c70891e3fe93357988d68e158ddeda5
antirez <antirez@gmail.com>
2013-11-19 10:13:04 +0100

    Sentinel: sentinelFlushConfig() to CONFIG REWRITE + fsync.

--- src/redis.h
+++ src/redis.h
@@ -1146,6 +1146,7 @@ void appendServerSaveParams(time_t seconds, int changes);
 void resetServerSaveParams();
 struct rewriteConfigState; /* Forward declaration to export API. */
 void rewriteConfigRewriteLine(struct rewriteConfigState *state, char *option, sds line, int force);
+int rewriteConfig(char *path);
 
 /* db.c -- Keyspace access API */
 int removeExpire(redisDb *db, robj *key);
