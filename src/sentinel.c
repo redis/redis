b8a94463b751e55c9b3dfe08646738a13974c274
antirez <antirez@gmail.com>
2013-11-19 10:59:47 +0100

    Sentinel: rewriteConfigSentinelOption() sub-iterators var typo fixed.

--- src/sentinel.c
+++ src/sentinel.c
@@ -1417,7 +1417,7 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
         /* sentinel known-slave */
         di2 = dictGetIterator(master->slaves);
-        while((de = dictNext(di)) != NULL) {
+        while((de = dictNext(di2)) != NULL) {
             sentinelAddr *slave_addr;
 
             ri = dictGetVal(de);
@@ -1439,7 +1439,7 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
         /* sentinel known-sentinel */
         di2 = dictGetIterator(master->sentinels);
-        while((de = dictNext(di)) != NULL) {
+        while((de = dictNext(di2)) != NULL) {
             ri = dictGetVal(de);
             line = sdscatprintf(sdsempty(),
                 "sentinel known-sentinel %s %s %d",
