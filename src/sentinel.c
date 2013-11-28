a46f841df3c4ef5a60117f2db1f7e4c6fbacbceb
antirez <antirez@gmail.com>
2013-11-28 15:23:46 +0100

    Sentinel: log vote received from other Sentinels.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2653,6 +2653,11 @@ void sentinelReceiveIsMasterDownReply(redisAsyncContext *c, void *reply, void *p
             /* If the runid in the reply is not "*" the Sentinel actually
              * replied with a vote. */
             sdsfree(ri->leader);
+            if (ri->leader_epoch != r->element[2]->integer)
+                redisLog(REDIS_WARNING,
+                    "%s voted for %s %llu", ri->name,
+                    r->element[1]->str,
+                    (unsigned long long) r->element[2]->integer);
             ri->leader = sdsnew(r->element[1]->str);
             ri->leader_epoch = r->element[2]->integer;
         }
