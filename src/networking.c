0ff078d8d07c98f7c38af8858b4ec29c5623e7b2
Yossi Gottlieb <yossigo@gmail.com>
2013-12-08 12:57:03 +0200

    Return proper error on requests with an unbalanced number of quotes.

--- src/networking.c
+++ src/networking.c
@@ -868,6 +868,11 @@ int processInlineBuffer(redisClient *c) {
     aux = sdsnewlen(c->querybuf,querylen);
     argv = sdssplitargs(aux,&argc);
     sdsfree(aux);
+    if (argv == NULL) {
+        addReplyError(c,"Protocol error: unbalanced quotes in request");
+        setProtocolError(c,0);
+        return REDIS_ERR;
+    }
 
     /* Newline from slaves can be used to refresh the last ACK time.
      * This is useful for a slave to ping back while loading a big
