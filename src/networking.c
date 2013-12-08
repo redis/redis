6e70c011484018ca9946cc536b7ab4531580b8de
Yossi Gottlieb <yossigo@gmail.com>
2013-12-08 12:57:03 +0200

    Return proper error on requests with an unbalanced number of quotes.

--- src/networking.c
+++ src/networking.c
@@ -884,6 +884,11 @@ int processInlineBuffer(redisClient *c) {
     aux = sdsnewlen(c->querybuf,querylen);
     argv = sdssplitargs(aux,&argc);
     sdsfree(aux);
+    if (argv == NULL) {
+        addReplyError(c,"Protocol error: unbalanced quotes in request");
+        setProtocolError(c,0);
+        return REDIS_ERR;
+    }
 
     /* Leave data after the first line of the query in the buffer */
     sdsrange(c->querybuf,querylen+2,-1);
