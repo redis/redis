96614f5866b742901ebfd74d4e21eab73173d420
Samuel Chou <schou@barracuda.com>
2013-12-09 10:18:36 -0800

    Improve accuracy of HAVE_ATOMIC dependency check
    [I had to split out the clang check due to
     clang *really* not liking the __GLIBC_PREREQ macro; -matt]
    
    Closes #1456

--- src/config.h
+++ src/config.h
@@ -187,8 +187,13 @@ void setproctitle(const char *fmt, ...);
 
 #if (__i386 || __amd64 || __powerpc__) && __GNUC__
 #define GNUC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#if (GNUC_VERSION >= 40100) || defined(__clang__)
+#if defined(__clang__)
 #define HAVE_ATOMIC
+#endif
+#if (defined(__GLIBC__) && defined(__GLIBC_PREREQ))
+#if (GNUC_VERSION >= 40100 && __GLIBC_PREREQ(2, 6))
+#define HAVE_ATOMIC
+#endif
 #endif
 #endif
 
