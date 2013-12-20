658aff9d29f3846b5b60868b767fdc2c2cc1d4af
antirez <antirez@gmail.com>
2013-12-21 00:04:53 +0100

    Redis Cluster: move node failure reports logging from VERBOSE to NOTICE level.

--- src/cluster.c
+++ src/cluster.c
@@ -898,14 +898,14 @@ void clusterProcessGossipSection(clusterMsg *hdr, clusterLink *link) {
             {
                 if (flags & (REDIS_NODE_FAIL|REDIS_NODE_PFAIL)) {
                     if (clusterNodeAddFailureReport(node,sender)) {
-                        redisLog(REDIS_NOTICE,
+                        redisLog(REDIS_VERBOSE,
                             "Node %.40s reported node %.40s as not reachable.",
                             sender->name, node->name);
                     }
                     markNodeAsFailingIfNeeded(node);
                 } else {
                     if (clusterNodeDelFailureReport(node,sender)) {
-                        redisLog(REDIS_NOTICE,
+                        redisLog(REDIS_VERBOSE,
                             "Node %.40s reported node %.40s is back online.",
                             sender->name, node->name);
                     }
