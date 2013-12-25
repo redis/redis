d07d4a876c1e2803e90b29ff1a9f4f53022772fc
antirez <antirez@gmail.com>
2013-12-25 18:24:04 +0100

    Remove useless goto from anetTcpGenericConnect().

--- src/anet.c
+++ src/anet.c
@@ -255,10 +255,8 @@ static int anetTcpGenericConnect(char *err, char *addr, int port, int flags)
          * have a connected socket. Let's return to the caller. */
         goto end;
     }
-    if (p == NULL) {
+    if (p == NULL)
         anetSetError(err, "creating socket: %s", strerror(errno));
-        goto error;
-    }
 
 error:
     if (s != ANET_ERR) {
