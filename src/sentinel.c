964f6b17e9c9bf7b20efb56ac922c29985d9f31d
antirez <antirez@gmail.com>
2014-01-10 15:39:10 +0100

    Sentinel: SENTINEL REMOVE command added.
    The command totally removes a monitored master.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2521,6 +2521,15 @@ void sentinelCommand(redisClient *c) {
             sentinelFlushConfig();
             addReply(c,shared.ok);
         }
+    } else if (!strcasecmp(c->argv[1]->ptr,"remove")) {
+        /* SENTINEL REMOVE <name> */
+        sentinelRedisInstance *ri;
+
+        if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2]))
+            == NULL) return;
+        dictDelete(sentinel.masters,c->argv[2]->ptr);
+        sentinelFlushConfig();
+        addReply(c,shared.ok);
     } else {
         addReplyErrorFormat(c,"Unknown sentinel subcommand '%s'",
                                (char*)c->argv[1]->ptr);
