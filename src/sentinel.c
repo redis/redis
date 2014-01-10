52cf0975c780a02b720233c306b9c7e08d169b47
antirez <antirez@gmail.com>
2014-01-10 14:41:44 +0100

    Sentinel: added SENTINEL MASTER <name> command.
    With SENTINEL MASTERS it was already possible to list all the configured
    masters, but not a specific one.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2380,8 +2380,14 @@ void sentinelCommand(redisClient *c) {
     if (!strcasecmp(c->argv[1]->ptr,"masters")) {
         /* SENTINEL MASTERS */
         if (c->argc != 2) goto numargserr;
-
         addReplyDictOfRedisInstances(c,sentinel.masters);
+    } else if (!strcasecmp(c->argv[1]->ptr,"master")) {
+        /* SENTINEL MASTER <name> */
+        sentinelRedisInstance *ri;
+
+        if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2]))
+            == NULL) return;
+        addReplySentinelRedisInstance(c,ri);
     } else if (!strcasecmp(c->argv[1]->ptr,"slaves")) {
         /* SENTINEL SLAVES <master-name> */
         sentinelRedisInstance *ri;
