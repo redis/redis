8ed0b49525b8e0c790317b076330a8ead24f90c4
antirez <antirez@gmail.com>
2014-01-13 11:05:13 +0100

    Sentinel: fix wrong arity error message.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2539,7 +2539,7 @@ void sentinelCommand(redisClient *c) {
     return;
 
 numargserr:
-    addReplyErrorFormat(c,"Wrong number of commands for 'sentinel %s'",
+    addReplyErrorFormat(c,"Wrong number of arguments for 'sentinel %s'",
                           (char*)c->argv[1]->ptr);
 }
 
