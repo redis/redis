d5763dceaffcd2347e588352cceaf3a1615a4601
antirez <antirez@gmail.com>
2014-01-14 09:23:26 +0100

    SENTINEL SET master quorum implemented.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2667,6 +2667,12 @@ void sentinelSetCommand(redisClient *c) {
             sdsfree(ri->auth_pass);
             ri->auth_pass = strlen(value) ? sdsnew(value) : NULL;
             changes++;
+       } else if (!strcasecmp(option,"quorum")) {
+            /* quorum <count> */
+            if (getLongLongFromObject(o,&ll) == REDIS_ERR || ll <= 0)
+                goto badfmt;
+            ri->quorum = ll;
+            changes++;
         } else {
             addReplyErrorFormat(c,"Unknown option '%s' for SENTINEL SET",
                 option);
