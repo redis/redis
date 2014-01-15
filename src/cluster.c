25ddefdea300958f356365ab8830a9676193df70
antirez <antirez@gmail.com>
2014-01-15 11:33:41 +0100

    Cluster: range checking in getSlotOrReply() fixed.
    See issue #1426 on Github.

--- src/cluster.c
+++ src/cluster.c
@@ -2596,7 +2596,7 @@ int getSlotOrReply(redisClient *c, robj *o) {
     long long slot;
 
     if (getLongLongFromObject(o,&slot) != REDIS_OK ||
-        slot < 0 || slot > REDIS_CLUSTER_SLOTS)
+        slot < 0 || slot >= REDIS_CLUSTER_SLOTS)
     {
         addReplyError(c,"Invalid or out of range slot");
         return -1;
