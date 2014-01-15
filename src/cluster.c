3e948970fe81d2a209c5f9731123ca5cebfe1458
antirez <antirez@gmail.com>
2014-01-15 16:50:45 +0100

    Cluster: use the node blacklist in CLUSTER FORGET.
    CLUSTER FORGET is not useful if we can't remove a node from all the
    nodes of our cluster because of the Gossip protocol that keeps adding
    a given node to nodes where we already tried to remove it.
    
    So now CLUSTER FORGET implements a nodes blacklist that is set and
    checked by the Gossip section processing function. This way before a
    node is re-added at least 60 seconds must elapse since the FORGET
    execution.
    
    This means that redis-trib has some time to remove a node from a whole
    cluster. It is possible that in the future it will be uesful to raise
    the 60 sec figure to something bigger.

--- src/cluster.c
+++ src/cluster.c
@@ -967,8 +967,12 @@ void clusterProcessGossipSection(clusterMsg *hdr, clusterLink *link) {
              * Note that we require that the sender of this gossip message
              * is a well known node in our cluster, otherwise we risk
              * joining another cluster. */
-            if (sender && !(flags & REDIS_NODE_NOADDR))
+            if (sender &&
+                !(flags & REDIS_NODE_NOADDR) &&
+                !clusterBlacklistExists(g->nodename))
+            {
                 clusterStartHandshake(g->ip,ntohs(g->port));
+            }
         }
 
         /* Next node */
@@ -2875,6 +2879,7 @@ void clusterCommand(redisClient *c) {
             addReplyErrorFormat(c,"Unknown node %s", (char*)c->argv[2]->ptr);
             return;
         }
+        clusterBlacklistAddNode(n);
         clusterDelNode(n);
         clusterDoBeforeSleep(CLUSTER_TODO_UPDATE_STATE|CLUSTER_TODO_SAVE_CONFIG);
         addReply(c,shared.ok);
