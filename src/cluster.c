3d455393a6a874e49abe51543fb442d38073f07d
antirez <antirez@gmail.com>
2014-01-16 17:49:35 +0100

    Cluster: don't let a node forget its own master.
    redis-trib should make sure to reconfigure slaves of a node to remove
    from the cluster to replicate with other nodes before sending CLUSTER
    FORGET.

--- src/cluster.c
+++ src/cluster.c
@@ -2875,11 +2875,15 @@ void clusterCommand(redisClient *c) {
         /* CLUSTER FORGET <NODE ID> */
         clusterNode *n = clusterLookupNode(c->argv[2]->ptr);
 
-        if (n == server.cluster->myself) {
-            addReplyErrorFormat(c,"I tried hard but I can't forget myself...");
-            return;
-        } else if (!n) {
+        if (!n) {
             addReplyErrorFormat(c,"Unknown node %s", (char*)c->argv[2]->ptr);
+            return;
+        } else if (n == server.cluster->myself) {
+            addReplyError(c,"I tried hard but I can't forget myself...");
+            return;
+        } else if (server.cluster->myself->flags & REDIS_NODE_SLAVE &&
+                   server.cluster->myself->slaveof == n) {
+            addReplyError(c,"Can't forget my master!");
             return;
         }
         clusterBlacklistAddNode(n);
