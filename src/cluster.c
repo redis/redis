0c373207fa5092bb0f7cdaef5ac78b8e64614bfd
antirez <antirez@gmail.com>
2014-01-16 09:46:23 +0100

    Cluster: don't forget yourself with CLUSTER FORGET.

--- src/cluster.c
+++ src/cluster.c
@@ -2875,7 +2875,10 @@ void clusterCommand(redisClient *c) {
         /* CLUSTER FORGET <NODE ID> */
         clusterNode *n = clusterLookupNode(c->argv[2]->ptr);
 
-        if (!n) {
+        if (n == server.cluster->myself) {
+            addReplyErrorFormat(c,"I tried hard but I can't forget myself...");
+            return;
+        } else if (!n) {
             addReplyErrorFormat(c,"Unknown node %s", (char*)c->argv[2]->ptr);
             return;
         }
