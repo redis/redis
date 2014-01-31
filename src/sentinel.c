f0652c37a5dbca5e53d2df5aa1c14a521db5de69
antirez <antirez@gmail.com>
2014-01-31 10:13:36 +0100

    Sentinel: check arity for SENTINEL MASTER command.
    This fixes issue #1530.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2386,6 +2386,7 @@ void sentinelCommand(redisClient *c) {
         /* SENTINEL MASTER <name> */
         sentinelRedisInstance *ri;
 
+        if (c->argc != 3) goto numargserr;
         if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2]))
             == NULL) return;
         addReplySentinelRedisInstance(c,ri);
