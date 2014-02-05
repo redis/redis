c6f02fd67a6a51e127003acf9a6ee14299dfa9a0
antirez <antirez@gmail.com>
2014-02-05 15:52:38 +0100

    Cluster: CLUSTER FAILOVER replies with OK and logs the event.

--- src/cluster.c
+++ src/cluster.c
@@ -3317,6 +3317,8 @@ void clusterCommand(redisClient *c) {
         resetManualFailover();
         server.cluster->mf_end = mstime() + REDIS_CLUSTER_MF_TIMEOUT;
         clusterSendMFStart(myself->slaveof);
+        redisLog(REDIS_WARNING,"Manual failover user request accepted.");
+        addReply(c,shared.ok);
     } else {
         addReplyError(c,"Wrong CLUSTER subcommand or number of arguments");
     }
