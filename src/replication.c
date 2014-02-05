301a0cfc69e7d9168d52af85e2bcddc225982bff
antirez <antirez@gmail.com>
2014-02-05 16:38:10 +0100

    Check for EAGAIN in sendBulkToSlave().
    Sometime an osx master with a Linux server over a slow link caused
    a strange error where osx called the writable function for
    the socket but actually apparently there was no room in the socket
    buffer to accept the write: write(2) call returned an EAGAIN error,
    that was not checked, so we considered write(2) == 0 always as a connection
    reset, which was unfortunate since the bulk transfer has to start again.
    
    Also more errors are logged with the WARNING level in the same code path
    now.

--- src/replication.c
+++ src/replication.c
@@ -602,9 +602,11 @@ void sendBulkToSlave(aeEventLoop *el, int fd, void *privdata, int mask) {
         return;
     }
     if ((nwritten = write(fd,buf,buflen)) == -1) {
-        redisLog(REDIS_VERBOSE,"Write error sending DB to slave: %s",
-            strerror(errno));
-        freeClient(slave);
+        if (errno != EAGAIN) {
+            redisLog(REDIS_WARNING,"Write error sending DB to slave: %s",
+                strerror(errno));
+            freeClient(slave);
+        }
         return;
     }
     slave->repldboff += nwritten;
@@ -616,6 +618,7 @@ void sendBulkToSlave(aeEventLoop *el, int fd, void *privdata, int mask) {
         slave->repl_ack_time = server.unixtime;
         if (aeCreateFileEvent(server.el, slave->fd, AE_WRITABLE,
             sendReplyToClient, slave) == AE_ERR) {
+            redisLog(REDIS_WARNING,"Unable to register writable event for slave bulk transfer: %s", strerror(errno));
             freeClient(slave);
             return;
         }
