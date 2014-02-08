297050eb238838b762403213d2a5b994989ba8a1
charsyam <charsyam@gmail.com>
2014-02-08 23:08:46 +0900

    skip rdbLoad when it is slave and waking up

--- src/redis.c
+++ src/redis.c
@@ -3147,7 +3147,9 @@ int main(int argc, char **argv) {
     #ifdef __linux__
         linuxOvercommitMemoryWarning();
     #endif
-        loadDataFromDisk();
+        if (server.masterhost == NULL) {
+            loadDataFromDisk();
+        }
         if (server.cluster_enabled) {
             if (verifyClusterConfigWithData() == REDIS_ERR) {
                 redisLog(REDIS_WARNING,
