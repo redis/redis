bf670e0745100e1214e5d1ff74ff2e0d869a31ad
antirez <antirez@gmail.com>
2014-02-10 18:33:34 +0100

    Cluster: don't update slave's master if we don't know it.
    There is no way we can update the slave's node->slaveof pointer if we
    don't know the master (no node with such an ID in our tables).

--- src/cluster.c
+++ src/cluster.c
@@ -1363,7 +1363,7 @@ int clusterProcessPacket(clusterLink *link) {
                 }
 
                 /* Master node changed for this slave? */
-                if (sender->slaveof != master) {
+                if (master && sender->slaveof != master) {
                     if (sender->slaveof)
                         clusterNodeRemoveSlave(sender->slaveof,sender);
                     clusterNodeAddSlave(master,sender);
