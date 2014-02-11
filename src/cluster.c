4513d8fcd4a04e841feeb12735dffd066cd3f64a
antirez <antirez@gmail.com>
2014-02-11 10:18:24 +0100

    Cluster: UPDATE messages are the norm and verbose.
    Logging them at WARNING level was of little utility and of sure disturb.

--- src/cluster.c
+++ src/cluster.c
@@ -1430,7 +1430,7 @@ int clusterProcessPacket(clusterLink *link) {
                     if (server.cluster->slots[j]->configEpoch >
                         senderConfigEpoch)
                     {
-                        redisLog(REDIS_WARNING,
+                        redisLog(REDIS_VERBOSE,
                             "Node %.40s has old slots configuration, sending "
                             "an UPDATE message about %.40s",
                                 sender->name, server.cluster->slots[j]->name);
