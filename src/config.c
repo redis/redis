116617c5e79062faffd707d3cd9bb9cb4555cf76
antirez <antirez@gmail.com>
2014-02-13 14:32:44 +0100

    Log when CONFIG REWRITE goes bad.

--- src/config.c
+++ src/config.c
@@ -1824,6 +1824,7 @@ void configCommand(redisClient *c) {
             return;
         }
         if (rewriteConfig(server.configfile) == -1) {
+            redisLog(REDIS_WARNING,"CONFIG REWRITE failed: %s", strerror(errno));
             addReplyErrorFormat(c,"Rewriting config file: %s", strerror(errno));
         } else {
             addReply(c,shared.ok);
