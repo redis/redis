5efee4f0bb0916998453302b5862fb53f78bacc9
antirez <antirez@gmail.com>
2014-02-17 12:10:12 +0100

    Sentinel: better specify startup errors due to config file.
    Now it logs the file name if it is not accessible. Also there is a
    different error for the missing config file case, and for the non
    writable file case.

--- src/sentinel.c
+++ src/sentinel.c
@@ -417,8 +417,13 @@ void initSentinel(void) {
 void sentinelIsRunning(void) {
     redisLog(REDIS_WARNING,"Sentinel runid is %s", server.runid);
 
-    if (server.configfile == NULL || access(server.configfile,W_OK) == -1) {
-        redisLog(REDIS_WARNING,"Sentinel started without a config file, or config file not writable. Exiting...");
+    if (server.configfile == NULL) {
+        redisLog(REDIS_WARNING,
+            "Sentinel started without a config file. Exiting...");
+    } else if (access(server.configfile,W_OK) == -1) {
+        redisLog(REDIS_WARNING,
+            "Sentinel config file %s is not writable: %s. Exiting...",
+            server.configfile,strerror(errno));
         exit(1);
     }
 }
