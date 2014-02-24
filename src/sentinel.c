4af2acf2b0a7beba433fdc620b14c3783ac3194a
antirez <antirez@gmail.com>
2014-02-24 16:22:52 +0100

    Sentinel: added missing exit(1) after checking for config file.

--- src/sentinel.c
+++ src/sentinel.c
@@ -420,6 +420,7 @@ void sentinelIsRunning(void) {
     if (server.configfile == NULL) {
         redisLog(REDIS_WARNING,
             "Sentinel started without a config file. Exiting...");
+        exit(1);
     } else if (access(server.configfile,W_OK) == -1) {
         redisLog(REDIS_WARNING,
             "Sentinel config file %s is not writable: %s. Exiting...",
