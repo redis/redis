cd8b149dffd70cfa4b051f45de9ffc4048000e07
antirez <antirez@gmail.com>
2014-02-25 15:21:53 +0100

    Sentinel test: added TODO items in 02 unit.

--- tests/sentinel-tests/02-slaves-reconf.tcl
+++ tests/sentinel-tests/02-slaves-reconf.tcl
@@ -1,1 +1,6 @@
 # Check that slaves are reconfigured at a latter time if they are partitioned.
+#
+# Here we should test:
+# 1) That slaves point to the new master after failover.
+# 2) That partitioned slaves point to new master when they are partitioned
+#    away during failover and return at a latter time.
