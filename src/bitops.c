c1cc28f230eef129c54af7da3a1220923d9fb48f
antirez <antirez@gmail.com>
2014-02-27 13:17:23 +0100

    warnigns -> warnings in redisBitpos().

--- src/bitops.c
+++ src/bitops.c
@@ -184,7 +184,7 @@ long redisBitpos(void *s, long count, int bit) {
     /* If we reached this point, there is a bug in the algorithm, since
      * the case of no match is handled as a special case before. */
     redisPanic("End of redisBitpos() reached.");
-    return 0; /* Just to avoid warnigns. */
+    return 0; /* Just to avoid warnings. */
 }
 
 /* -----------------------------------------------------------------------------
