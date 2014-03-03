c5edd91716486b1134aa9318657e38e43445a75d
antirez <antirez@gmail.com>
2014-03-03 17:11:51 +0100

    Cluster: invalidate current transaction on redirections.

--- src/redis.c
+++ src/redis.c
@@ -2027,6 +2027,7 @@ int processCommand(redisClient *c) {
                 addReplyError(c,"Multi keys request invalid in cluster");
                 return REDIS_OK;
             } else if (n != server.cluster->myself) {
+                flagTransaction(c);
                 addReplySds(c,sdscatprintf(sdsempty(),
                     "-%s %d %s:%d\r\n", ask ? "ASK" : "MOVED",
                     hashslot,n->ip,n->port));
