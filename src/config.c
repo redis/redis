08da025f56b2b99523fc9fa9a8976ceafa69fe57
antirez <antirez@gmail.com>
2014-03-04 16:39:44 +0100

    CONFIG REWRITE should be logged at WARNING level.

--- src/config.c
+++ src/config.c
@@ -1827,6 +1827,7 @@ void configCommand(redisClient *c) {
             redisLog(REDIS_WARNING,"CONFIG REWRITE failed: %s", strerror(errno));
             addReplyErrorFormat(c,"Rewriting config file: %s", strerror(errno));
         } else {
+            redisLog(REDIS_WARNING,"CONFIG REWRITE executed with success.");
             addReply(c,shared.ok);
         }
     } else {
