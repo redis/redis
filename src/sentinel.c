a2ec9a906801c1b19ea640cf93667bb02c491cff
Jan-Erik Rediger <badboy@archlinux.us>
2014-03-05 00:41:02 +0100

    Small typo fixed

--- src/sentinel.c
+++ src/sentinel.c
@@ -1571,7 +1571,7 @@ void sentinelFlushConfig(void) {
             close(fd);
         }
     } else {
-        redisLog(REDIS_WARNING,"WARNING: Senitnel was not able to save the new configuration on disk!!!: %s", strerror(errno));
+        redisLog(REDIS_WARNING,"WARNING: Sentinel was not able to save the new configuration on disk!!!: %s", strerror(errno));
     }
     server.hz = saved_hz;
     return;
