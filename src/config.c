4d5ba5962cc178b5e79cce09e763e4af84007983
antirez <antirez@gmail.com>
2014-03-05 11:26:14 +0100

    Cast saveparams[].seconds to long for %ld format specifier.

--- src/config.c
+++ src/config.c
@@ -1461,7 +1461,7 @@ void rewriteConfigSaveOption(struct rewriteConfigState *state) {
      * resulting into no RDB persistence as expected. */
     for (j = 0; j < server.saveparamslen; j++) {
         line = sdscatprintf(sdsempty(),"save %ld %d",
-            server.saveparams[j].seconds, server.saveparams[j].changes);
+            (long) server.saveparams[j].seconds, server.saveparams[j].changes);
         rewriteConfigRewriteLine(state,"save",line,1);
     }
     /* Mark "save" as processed in case server.saveparamslen is zero. */
