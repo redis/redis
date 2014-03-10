2705306ba1304e6db46d4ebc3006b1dea40588b7
antirez <antirez@gmail.com>
2014-03-10 16:43:38 +0100

    DEBUG CMDKEYS: provide some guarantee to getKeysFromCommand().
    getKeysFromCommand() is designed to be called with the command arguments
    passing the basic arity checks described in the command table.
    
    DEBUG CMDKEYS must provide the same guarantees for calling
    getKeysFromCommand() to be safe.

--- src/debug.c
+++ src/debug.c
@@ -370,7 +370,13 @@ void debugCommand(redisClient *c) {
         if (!cmd) {
             addReplyError(c,"Invalid command specified");
             return;
+        } else if ((cmd->arity > 0 && cmd->arity != c->argc-2) ||
+                   ((c->argc-2) < -cmd->arity))
+        {
+            addReplyError(c,"Invalid number of arguments specified for command");
+            return;
         }
+
         keys = getKeysFromCommand(cmd,c->argv+2,c->argc-2,&numkeys);
         addReplyMultiBulkLen(c,numkeys);
         for (j = 0; j < numkeys; j++) addReplyBulk(c,c->argv[keys[j]+2]);
