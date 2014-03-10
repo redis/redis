db0d9f4326d09f40b38a066eed66d05472adfec8
antirez <antirez@gmail.com>
2014-03-10 16:35:58 +0100

    DEBUG CMDKEYS added for getKeysFromCommand() testing.
    Examples:
    
        redis 127.0.0.1:6379> debug cmdkeys set foo bar
        1) "foo"
        redis 127.0.0.1:6379> debug cmdkeys mget a b c
        1) "a"
        2) "b"
        3) "c"
        redis 127.0.0.1:6379> debug cmdkeys zunionstore foo 2 a b
        1) "a"
        2) "b"
        3) "foo"
        redis 127.0.0.1:6379> debug cmdkeys ping
        (empty list or set)

--- src/debug.c
+++ src/debug.c
@@ -363,6 +363,18 @@ void debugCommand(redisClient *c) {
     {
         server.active_expire_enabled = atoi(c->argv[2]->ptr);
         addReply(c,shared.ok);
+    } else if (!strcasecmp(c->argv[1]->ptr,"cmdkeys") && c->argc >= 3) {
+        struct redisCommand *cmd = lookupCommand(c->argv[2]->ptr);
+        int *keys, numkeys, j;
+
+        if (!cmd) {
+            addReplyError(c,"Invalid command specified");
+            return;
+        }
+        keys = getKeysFromCommand(cmd,c->argv+2,c->argc-2,&numkeys);
+        addReplyMultiBulkLen(c,numkeys);
+        for (j = 0; j < numkeys; j++) addReplyBulk(c,c->argv[keys[j]+2]);
+        getKeysFreeResult(keys);
     } else {
         addReplyErrorFormat(c, "Unknown DEBUG subcommand or wrong number of arguments for '%s'",
             (char*)c->argv[1]->ptr);
