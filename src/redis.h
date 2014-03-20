001775f5fb306d162c97a0dd56c364831282ec47
antirez <antirez@gmail.com>
2014-03-20 17:56:27 +0100

    Use 24 bits for the lru object field and improve resolution.
    There were 2 spare bits inside the Redis object structure that are now
    used in order to enlarge 4x the range of the LRU field.
    
    At the same time the resolution was improved from 10 to 1 second: this
    still provides 194 days before the LRU counter overflows (restarting from
    zero).
    
    This is not a problem since it only causes lack of eviction precision for
    objects not touched for a very long time, and the lack of precision is
    only temporary.

--- src/redis.h
+++ src/redis.h
@@ -373,12 +373,11 @@ typedef long long mstime_t; /* millisecond time type. */
 /* A redis object, that is a type able to hold a string / list / set */
 
 /* The actual Redis Object */
-#define REDIS_LRU_BITS 22
+#define REDIS_LRU_BITS 24
 #define REDIS_LRU_CLOCK_MAX ((1<<REDIS_LRU_BITS)-1) /* Max value of obj->lru */
-#define REDIS_LRU_CLOCK_RESOLUTION 10 /* LRU clock resolution in seconds */
+#define REDIS_LRU_CLOCK_RESOLUTION 1 /* LRU clock resolution in seconds */
 typedef struct redisObject {
     unsigned type:4;
-    unsigned notused:2;     /* Not used */
     unsigned encoding:4;
     unsigned lru:REDIS_LRU_BITS; /* lru time (relative to server.lruclock) */
     int refcount;
