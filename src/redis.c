7054f2a0291660e7b8f324de0ca68a6cdf910f39
antirez <antirez@gmail.com>
2014-03-24 10:00:08 +0100

    Cache uname() output across INFO calls.
    Uname was profiled to be a slow syscall. It produces always the same
    output in the context of a single execution of Redis, so calling it at
    every INFO output generation does not make too much sense.
    
    The uname utsname structure was modified as a static variable. At the
    same time a static integer was added to check if we need to call uname
    the first time.

--- src/redis.c
+++ src/redis.c
@@ -2236,14 +2236,21 @@ sds genRedisInfoString(char *section) {
 
     /* Server */
     if (allsections || defsections || !strcasecmp(section,"server")) {
-        struct utsname name;
+        static int call_uname = 1;
+        static struct utsname name;
         char *mode;
 
         if (server.sentinel_mode) mode = "sentinel";
         else mode = "standalone";
     
         if (sections++) info = sdscat(info,"\r\n");
-        uname(&name);
+
+        if (call_uname) {
+            /* Uname can be slow and is always the same output. Cache it. */
+            uname(&name);
+            call_uname = 0;
+        }
+
         info = sdscatprintf(info,
             "# Server\r\n"
             "redis_version:%s\r\n"
