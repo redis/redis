7a20f09647a8d460b64a88cb6acb3cc15aa0292e
antirez <antirez@gmail.com>
2014-03-24 09:38:33 +0100

    sdscatvprintf(): guess buflen using format length.
    sdscatvprintf() uses a loop where it tries to output the formatted
    string in a buffer of the initial length, if there was not enough room,
    a buffer of doubled size is tried and so forth.
    
    The initial guess for the buffer length was very poor, an hardcoded
    "16". This caused the printf to be processed multiple times without a
    good reason. Given that printf functions are already not fast, the
    overhead was significant.
    
    The new heuristic is to use a buffer 4 times the length of the format
    buffer, and 32 as minimal size. This appears to be a good balance for
    typical uses of the function inside the Redis code base.
    
    This change improved INFO command performances 3 times.

--- src/sds.c
+++ src/sds.c
@@ -293,8 +293,9 @@ sds sdscpy(sds s, const char *t) {
 sds sdscatvprintf(sds s, const char *fmt, va_list ap) {
     va_list cpy;
     char *buf, *t;
-    size_t buflen = 16;
+    size_t buflen = strlen(fmt)*4;
 
+    if (buflen < 32) buflen = 32;
     while(1) {
         buf = zmalloc(buflen);
         if (buf == NULL) return NULL;
