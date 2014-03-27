2af2c63eb66584b7fff04d948c032cdf750b35bb
Matt Stancliff <matt@genges.com>
2014-03-26 22:25:20 -0400

    Factor out duplicate signal handling code
    No options are set for SIGTERM, so we can use the simpler
    signal handling interface and remove unnecessary structure initilization.
    
    sigemptyset() is moved outside of the ifdef to prevent any potential
    unused variable warning.
    
    Closes #1637

--- src/redis.c
+++ src/redis.c
@@ -3420,17 +3420,12 @@ static void sigShutdownHandler(int sig) {
 
 void setupSignalHandlers(void) {
     struct sigaction act;
-
-    /* When the SA_SIGINFO flag is set in sa_flags then sa_sigaction is used.
-     * Otherwise, sa_handler is used. */
     sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-    act.sa_handler = sigShutdownHandler;
-    sigaction(SIGTERM, &act, NULL);
+
+    signal(SIGTERM, sigShutdownHandler);
     signal(SIGINT, sigShutdownHandler);
 
 #ifdef HAVE_BACKTRACE
-    sigemptyset(&act.sa_mask);
     act.sa_flags = SA_NODEFER | SA_RESETHAND | SA_SIGINFO;
     act.sa_sigaction = sigsegvHandler;
     sigaction(SIGSEGV, &act, NULL);
