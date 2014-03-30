2ce104a2986b8eebd23f6cb0dd7c5e05e160c14d
Matt Stancliff <matt@genges.com>
2014-03-30 16:54:55 -0400

    Fix 9 warnings about type specifiers under Linux
    Under some 64-bit Linux, 64-bit types are unsigned long.
    Under 64-bit OS X and some other Linux, 64-bit types are unsigned long long.
    
    Use the system provided C99 format type specifier abstraction so
    it decides to use %lu or %llu itself depending on requirements.

--- src/redis.c
+++ src/redis.c
@@ -1540,25 +1540,25 @@ void adjustOpenFilesLimit(void) {
                 server.maxclients = f-REDIS_MIN_RESERVED_FDS;
                 if (server.maxclients < 1) {
                     redisLog(REDIS_WARNING,"Your current 'ulimit -n' "
-                        "of %llu is not enough for Redis to start. "
+                        "of %"PRIu64" is not enough for Redis to start. "
                         "Please increase your open file limit to at least "
-                        "%llu. Exiting.", oldlimit, maxfiles);
+                        "%"PRIu64". Exiting.", oldlimit, maxfiles);
                     exit(1);
                 }
                 redisLog(REDIS_WARNING,"You requested maxclients of %d "
-                    "requiring at least %llu max file descriptors.",
+                    "requiring at least %"PRIu64" max file descriptors.",
                     old_maxclients, maxfiles);
                 redisLog(REDIS_WARNING,"Redis can't set maximum open files "
-                    "to %llu because of OS error: %s.",
+                    "to %"PRIu64" because of OS error: %s.",
                     maxfiles, strerror(setrlimit_error));
-                redisLog(REDIS_WARNING,"Current maximum open files is %llu. "
-                    "maxclients has been reduced to %d to compensate for "
-                    "low ulimit. "
+                redisLog(REDIS_WARNING,"Current maximum open files is "
+                    "%"PRIu64". maxclients has been reduced to %d to "
+                    "compensate for low ulimit. "
                     "If you need higher maxclients increase 'ulimit -n'.",
                     oldlimit, server.maxclients);
             } else {
                 redisLog(REDIS_NOTICE,"Increased maximum number of open files "
-                    "to %llu (it was originally set to %llu).",
+                    "to %"PRIu64" (it was originally set to %"PRIu64").",
                     maxfiles, oldlimit);
             }
         }
