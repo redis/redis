e2a48cd715f33fc02328f1c88b6e9fa34788c7f4
jonathan pickett <v-jonpic@microsoft.com>
2014-04-02 16:48:17 -0700

    PS scripts to automate NuGet/Chocolatey package generation. Also eliminated VC redist dependency in Chocolatey spec

---
+++ msvs/setups/CreatePackages.ps1
@@ -0,0 +1,24 @@
+$CurDir = split-path -parent $MyInvocation.MyCommand.Definition
+
+$ChocolateyDir = $CurDir + "\chocolatey"
+$NugetDir = $CurDir + "\nuget"
+$PackagesDir = $CurDir + "\packages"
+
+If (Test-Path $PackagesDir){
+	Remove-Item $PackagesDir -recurse | Out-Null
+}
+New-Item $PackagesDir -type directory  | Out-Null
+New-Item ($PackagesDir+"\Chocolatey") -type directory  | Out-Null
+New-Item ($PackagesDir+"\NuGet") -type directory  | Out-Null
+
+Set-Location $ChocolateyDir
+invoke-expression cpack
+Copy-Item *.nupkg ..\packages\Chocolatey
+
+Set-Location $NugetDir
+invoke-expression "NuGet Pack Redis.nuspec"
+Copy-Item *.nupkg ..\packages\NuGet
+
+Set-Location $CurDir
+
+Write-Host "The .nuspec files are in the 'packages' directory" -foregroundcolor black -backgroundcolor green
