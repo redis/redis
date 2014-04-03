d2ca4bb62d807291f9e21ec57c44373a03a99a5e
antirez <antirez@gmail.com>
2014-04-03 22:10:20 +0200

    Return "WRONGTYPE" error on PF* type mismatch.

--- src/hyperloglog.c
+++ src/hyperloglog.c
@@ -464,7 +464,9 @@ int isHLLObjectOrReply(redisClient *c, robj *o) {
     /* If this is a string representing an HLL, the size should match
      * exactly. */
     if (stringObjectLen(o) != REDIS_HLL_SIZE) {
-        addReplyErrorFormat(c, "Key is not a valid HyperLogLog string value.");
+        addReplySds(c,
+            sdsnew("-WRONGTYPE Key is not a valid "
+                   "HyperLogLog string value.\r\n"));
         return REDIS_ERR;
     }
     return REDIS_OK;
