86f27382d500947dcbc559ce511d7d1f32c11d61
Kashif Rasul <kashif.rasul@gmail.com>
2014-04-04 10:25:40 +0200

    fix memory leak and possible null exception

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -634,6 +634,7 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
             printf("Entering slave output mode...  (press Ctrl-C to quit)\n");
             slaveMode();
             config.slave_mode = 0;
+			free(argvlen);
             return REDIS_ERR;  /* Error = slaveMode lost connection to master */
         }
 
