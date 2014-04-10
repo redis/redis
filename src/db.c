e9d0d86dbfbbe4d8e10c761b734ac12a9b9c1a8c
Matt Stancliff <matt@genges.com>
2014-04-10 17:08:02 -0400

    Check key expiration before deleting
    Deleting an expired key should return 0, not success.
    
    Fixes #1648

--- src/db.c
+++ src/db.c
@@ -274,6 +274,7 @@ void delCommand(redisClient *c) {
     int deleted = 0, j;
 
     for (j = 1; j < c->argc; j++) {
+        expireIfNeeded(c->db,c->argv[j]);
         if (dbDelete(c->db,c->argv[j])) {
             signalModifiedKey(c->db,c->argv[j]);
             notifyKeyspaceEvent(REDIS_NOTIFY_GENERIC,
