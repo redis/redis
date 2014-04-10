b06de30dd78746263bc49fe91c97277f7766d820
Matt Stancliff <matt@genges.com>
2014-04-10 18:32:19 -0400

    Add test for deleting an expired key
    Verify proper expire-before-delete behavior.
    
    This test passes with the expire-before-delete commit and fails
    without it.

--- tests/unit/basic.tcl
+++ tests/unit/basic.tcl
@@ -261,6 +261,14 @@ start_server {tags {"basic"}} {
         assert_equal 20 [r get x]
     }
 
+    test "DEL against expired key" {
+        r debug set-active-expire 0
+        r setex keyExpire 1 valExpire
+        after 1100
+        assert_equal 0 [r del keyExpire]
+        r debug set-active-expire 1
+    }
+
     test {EXISTS} {
         set res {}
         r set newkey test
