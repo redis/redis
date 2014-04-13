261da523e860146066e052771e2217a84bc8d168
antirez <antirez@gmail.com>
2014-04-13 23:01:21 +0200

    PFDEBUG added, PFGETREG removed.
    PFDEBUG will be the interface to do debugging tasks with a key
    containing an HLL object.

--- src/hyperloglog.c
+++ src/hyperloglog.c
@@ -1214,26 +1214,42 @@ cleanup:
     sdsfree(bitcounters);
 }
 
-/* PFGETREG
- * Return the registers values of the specified HLL. */
-void pfgetregCommand(redisClient *c) {
-    robj *o = lookupKeyRead(c->db,c->argv[1]);
+/* PFDEBUG <subcommand> <key> ... args ...
+ * Different debugging related operations about the HLL implementation. */
+void pfdebugCommand(redisClient *c) {
+    char *cmd = c->argv[1]->ptr;
     struct hllhdr *hdr;
+    robj *o;
     int j;
 
+    o = lookupKeyRead(c->db,c->argv[2]);
     if (o == NULL) {
         addReplyError(c,"The specified key does not exist");
         return;
-    } else {
-        if (isHLLObjectOrReply(c,o) != REDIS_OK) return;
+    }
+    if (isHLLObjectOrReply(c,o) != REDIS_OK) return;
+    o = dbUnshareStringValue(c->db,c->argv[2],o);
+    hdr = o->ptr;
 
-        hdr = o->ptr;
+    /* PFDEBUG GETREG <key> */
+    if (!strcasecmp(cmd,"getreg")) {
+        if (c->argc != 3) goto arityerr;
+
         addReplyMultiBulkLen(c,HLL_REGISTERS);
+        hllSparseToDense(o);
         for (j = 0; j < HLL_REGISTERS; j++) {
             uint8_t val;
 
             HLL_DENSE_GET_REGISTER(val,hdr->registers,j);
             addReplyLongLong(c,val);
         }
+    } else {
+        addReplyErrorFormat(c,"Unknown PFDEBUG subcommand '%s'", cmd);
     }
+    return;
+
+arityerr:
+    addReplyErrorFormat(c,
+        "Wrong number of arguments for the '%s' subcommand",cmd);
 }
+
