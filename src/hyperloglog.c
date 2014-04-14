29c7d1974b7b3583eaa22c74910fafe872dc0e9d
antirez <antirez@gmail.com>
2014-04-14 10:25:19 +0200

    Fixed error message generation in PFDEBUG GETREG.
    Bulk length for registers was emitted too early, so if there was a bug
    the reply looked like a long array with just one element, blocking the
    client as result.

--- src/hyperloglog.c
+++ src/hyperloglog.c
@@ -1246,12 +1246,13 @@ void pfdebugCommand(redisClient *c) {
     if (!strcasecmp(cmd,"getreg")) {
         if (c->argc != 3) goto arityerr;
 
-        addReplyMultiBulkLen(c,HLL_REGISTERS);
         if (hllSparseToDense(o) == REDIS_ERR) {
             addReplyError(c,"HLL sparse encoding is corrupted");
             return;
         }
+
         hdr = o->ptr;
+        addReplyMultiBulkLen(c,HLL_REGISTERS);
         for (j = 0; j < HLL_REGISTERS; j++) {
             uint8_t val;
 
