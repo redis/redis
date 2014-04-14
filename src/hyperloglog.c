3bc35f9ce917c630c401b2ed0d56b3a51658a5e2
antirez <antirez@gmail.com>
2014-04-14 15:56:30 +0200

    Correctly replicate PFDEBUG GETREG.
    Even if it is a debugging command, make sure that when it forces a
    change in encoding, the command is propagated.

--- src/hyperloglog.c
+++ src/hyperloglog.c
@@ -1306,9 +1306,12 @@ void pfdebugCommand(redisClient *c) {
     if (!strcasecmp(cmd,"getreg")) {
         if (c->argc != 3) goto arityerr;
 
-        if (hllSparseToDense(o) == REDIS_ERR) {
-            addReplyError(c,"HLL sparse encoding is corrupted");
-            return;
+        if (hdr->encoding == HLL_SPARSE) {
+            if (hllSparseToDense(o) == REDIS_ERR) {
+                addReplyError(c,"HLL sparse encoding is corrupted");
+                return;
+            }
+            server.dirty++; /* Force propagation on encoding change. */
         }
 
         hdr = o->ptr;
