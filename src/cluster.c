a69178fdd2df5a0045a49f89ac24e4f91c2a89cf
kingsumos <kingsumos@gmail.com>
2014-04-22 11:44:53 -0400

    fix cluster node description showing wrong slot allocation

--- src/cluster.c
+++ src/cluster.c
@@ -3131,7 +3131,7 @@ sds clusterGenNodeDescription(clusterNode *node) {
             if (start == -1) start = j;
         }
         if (start != -1 && (!bit || j == REDIS_CLUSTER_SLOTS-1)) {
-            if (j == REDIS_CLUSTER_SLOTS-1) j++;
+            if (bit && j == REDIS_CLUSTER_SLOTS-1) j++;
 
             if (start == j-1) {
                 ci = sdscatprintf(ci," %d",start);
