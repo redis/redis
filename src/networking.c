35252037fdf0891b78a2a96d2d9899740ee88bbf
antirez <antirez@gmail.com>
2014-04-23 10:19:43 +0200

    Missing return REDIS_ERR added to processMultibulkBuffer().
    When we set a protocol error we should return with REDIS_ERR to let the
    caller know it should stop processing the client.
    
    Bug found in a code auditing related to issue #1699.

--- src/networking.c
+++ src/networking.c
@@ -977,8 +977,10 @@ int processMultibulkBuffer(redisClient *c) {
             newline = strchr(c->querybuf+pos,'\r');
             if (newline == NULL) {
                 if (sdslen(c->querybuf) > REDIS_INLINE_MAX_SIZE) {
-                    addReplyError(c,"Protocol error: too big bulk count string");
+                    addReplyError(c,
+                        "Protocol error: too big bulk count string");
                     setProtocolError(c,0);
+                    return REDIS_ERR;
                 }
                 break;
             }
