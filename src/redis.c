357b039fdb66fe70d83e5175c39b5cf03ce7f384
antirez <antirez@gmail.com>
2014-04-24 17:33:57 +0200

    Accept multiple clients per iteration.
    When the listening sockets readable event is fired, we have the chance
    to accept multiple clients instead of accepting a single one. This makes
    Redis more responsive when there is a mass-connect event (for example
    after the server startup), and in workloads where a connect-disconnect
    pattern is used often, so that multiple clients are waiting to be
    accepted continuously.
    
    As a side effect, this commit makes the LOADING, BUSY, and similar
    errors much faster to deliver to the client, making Redis more
    responsive when there is to return errors to inform the clients that the
    server is blocked in an not interruptible operation.

--- src/redis.c
+++ src/redis.c
@@ -1615,10 +1615,16 @@ int listenToPort(int port, int *fds, int *count) {
              * server.bindaddr_count == 0. */
             fds[*count] = anetTcp6Server(server.neterr,port,NULL,
                 server.tcp_backlog);
-            if (fds[*count] != ANET_ERR) (*count)++;
+            if (fds[*count] != ANET_ERR) {
+                anetNonBlock(NULL,fds[*count]);
+                (*count)++;
+            }
             fds[*count] = anetTcpServer(server.neterr,port,NULL,
                 server.tcp_backlog);
-            if (fds[*count] != ANET_ERR) (*count)++;
+            if (fds[*count] != ANET_ERR) {
+                anetNonBlock(NULL,fds[*count]);
+                (*count)++;
+            }
             /* Exit the loop if we were able to bind * on IPv4 or IPv6,
              * otherwise fds[*count] will be ANET_ERR and we'll print an
              * error and return to the caller with an error. */
@@ -1639,6 +1645,7 @@ int listenToPort(int port, int *fds, int *count) {
                 port, server.neterr);
             return REDIS_ERR;
         }
+        anetNonBlock(NULL,fds[*count]);
         (*count)++;
     }
     return REDIS_OK;
@@ -1708,6 +1715,7 @@ void initServer() {
             redisLog(REDIS_WARNING, "Opening socket: %s", server.neterr);
             exit(1);
         }
+        anetNonBlock(NULL,server.sofd);
     }
 
     /* Abort if there are no listening sockets at all. */
