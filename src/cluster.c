b008863edbb4f20a067498257aec390424ca2e4f
antirez <antirez@gmail.com>
2014-04-24 16:23:03 +0200

    clusterLoadConfig() REDIS_ERR retval semantics refined.
    We should return REDIS_ERR to signal we can't read the configuration
    because there is no config file only after checking errno, othewise
    we risk to rewrite an existing file that was not accessible for some
    other reason.

--- src/cluster.c
+++ src/cluster.c
@@ -103,7 +103,16 @@ int clusterLoadConfig(char *filename) {
     char *line;
     int maxline, j;
    
-    if (fp == NULL) return REDIS_ERR;
+    if (fp == NULL) {
+        if (errno == ENOENT) {
+            return REDIS_ERR;
+        } else {
+            redisLog(REDIS_WARNING,
+                "Loading the cluster node config from %s: %s",
+                filename, strerror(errno));
+            exit(1);
+        }
+    }
 
     /* Check if the file is zero-length: if so return REDIS_ERR to signal
      * we have to write the config. */
