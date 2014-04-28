52189cb9c30a9b87400929a53c640d831d337141
antirez <antirez@gmail.com>
2014-04-28 16:41:38 +0200

    Use sdscatfmt() in getClientInfoString() to make it faster.

--- src/sds.h
+++ src/sds.h
@@ -76,6 +76,7 @@ sds sdscatprintf(sds s, const char *fmt, ...)
 sds sdscatprintf(sds s, const char *fmt, ...);
 #endif
 
+sds sdscatfmt(sds s, char const *fmt, ...);
 sds sdstrim(sds s, const char *cset);
 void sdsrange(sds s, int start, int end);
 void sdsupdatelen(sds s);
