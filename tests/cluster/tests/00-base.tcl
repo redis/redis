e244bee60cf8f92e1abe1698f3f6adc36d311501
antirez <antirez@gmail.com>
2014-04-29 10:42:32 +0200

    Cluster test: check that every node has a different ID.

--- tests/cluster/tests/00-base.tcl
+++ tests/cluster/tests/00-base.tcl
@@ -8,9 +8,23 @@ if {$::simulate_error} {
     }
 }
 
-test "Cluster nodes are reachable." {
+test "Cluster nodes are reachable" {
     foreach_redis_id id {
         # Every node should just know itself.
         assert {[R $id ping] eq {PONG}}
     }
+}
+
+test "Different nodes have different IDs" {
+    set ids {}
+    set numnodes 0
+    foreach_redis_id id {
+        incr numnodes
+        # Every node should just know itself.
+        set nodeid [dict get [get_myself $id] id]
+        assert {$nodeid ne {}}
+        lappend ids $nodeid
+    }
+    set numids [llength [lsort -unique $ids]]
+    assert {$numids == $numnodes}
 }
