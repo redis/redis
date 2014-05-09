6ae4c1fcbc59768406344e3f277f51a26fb719de
antirez <antirez@gmail.com>
2014-05-09 15:02:29 +0200

    DEBUG POPULATE: call dictExpand() to avoid useless rehashing.

--- src/debug.c
+++ src/debug.c
@@ -326,6 +326,7 @@ void debugCommand(redisClient *c) {
 
         if (getLongFromObjectOrReply(c, c->argv[2], &keys, NULL) != REDIS_OK)
             return;
+        dictExpand(c->db->dict,keys);
         for (j = 0; j < keys; j++) {
             snprintf(buf,sizeof(buf),"key:%lu",j);
             key = createStringObject(buf,strlen(buf));
