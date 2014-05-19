a82c000bdb66dc0cef45c281e3cb9a0ebac5b582
antirez <antirez@gmail.com>
2014-05-19 11:24:05 +0200

    Cluster test: proper initialization at unit startup.

--- tests/cluster/tests/includes/init-tests.tcl
+++ tests/cluster/tests/includes/init-tests.tcl
@@ -11,3 +11,43 @@ test "(init) Restart killed instances" {
         }
     }
 }
+
+test "Cluster nodes are reachable" {
+    foreach_redis_id id {
+        # Every node should just know itself.
+        assert {[R $id ping] eq {PONG}}
+    }
+}
+
+test "Cluster nodes hard reset" {
+    foreach_redis_id id {
+        R $id flushall
+        R $id cluster reset hard
+    }
+}
+
+test "Cluster Join and auto-discovery test" {
+    # Join node 0 with 1, 1 with 2, ... and so forth.
+    # If auto-discovery works all nodes will know every other node
+    # eventually.
+    set ids {}
+    foreach_redis_id id {lappend ids $id}
+    for {set j 0} {$j < [expr [llength $ids]-1]} {incr j} {
+        set a [lindex $ids $j]
+        set b [lindex $ids [expr $j+1]]
+        set b_port [get_instance_attrib redis $b port]
+        R $a cluster meet 127.0.0.1 $b_port
+    }
+
+    foreach_redis_id id {
+        wait_for_condition 1000 50 {
+            [llength [get_cluster_nodes $id]] == [llength $ids]
+        } else {
+            fail "Cluster failed to join into a full mesh."
+        }
+    }
+}
+
+test "Before slots allocation, all nodes report cluster failure" {
+    assert_cluster_state fail
+}
