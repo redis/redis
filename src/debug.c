75266018eebe1633b4b083afaece793d2b005766
Matt Stancliff <matt@genges.com>
2014-05-22 15:34:02 -0400

    Disable recursive watchdog signal handler
    If we are in the signal handler, we don't want to handle
    the signal again.  In extreme cases, this can cause a stack overflow
    and segfault Redis.
    
    Fixes #1771

--- src/debug.c
+++ src/debug.c
@@ -931,7 +931,7 @@ void enableWatchdog(int period) {
         /* Watchdog was actually disabled, so we have to setup the signal
          * handler. */
         sigemptyset(&act.sa_mask);
-        act.sa_flags = SA_NODEFER | SA_ONSTACK | SA_SIGINFO;
+        act.sa_flags = SA_ONSTACK | SA_SIGINFO;
         act.sa_sigaction = watchdogSignalHandler;
         sigaction(SIGALRM, &act, NULL);
     }
