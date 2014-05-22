4d9cb1d8b3986677c0a6de9ee29e3c2624852dea
Jan-Erik Rediger <badboy@archlinux.us>
2014-05-23 00:21:05 +0200

    Fixed a small typo

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1832,7 +1832,7 @@ static void intrinsicLatencyMode(void) {
         if (end > test_end) {
             printf("\n%lld total runs (avg %lld microseconds per run).\n",
                 runs, run_time/runs);
-            printf("Worst run took %.02fx times the avarege.\n",
+            printf("Worst run took %.02f times the average.\n",
                 (double) max_latency / (run_time/runs));
             exit(0);
         }
