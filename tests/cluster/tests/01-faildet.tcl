81af783fecfd303a68a840116e79478ba2d400d1
antirez <antirez@gmail.com>
2014-05-23 11:40:25 +0200

    Cluster test: more reliable 01-faildet unit.
    Do things in a sequence that prevents failover during failure detection.

--- tests/cluster/tests/01-faildet.tcl
+++ tests/cluster/tests/01-faildet.tcl
@@ -6,19 +6,7 @@ test "Create a 5 nodes cluster" {
     create_cluster 5 5
 }
 
-test "Killing one master node" {
-    kill_instance redis 0
-}
-
-test "Cluster should be down now" {
-    assert_cluster_state fail
-}
-
-test "Restarting master node" {
-    restart_instance redis 0
-}
-
-test "Cluster should be up again" {
+test "Cluster should start ok" {
     assert_cluster_state ok
 }
 
@@ -28,5 +16,23 @@ test "Killing two slave nodes" {
 }
 
 test "Cluster should be still up" {
+    assert_cluster_state ok
+}
+
+test "Killing one master node" {
+    kill_instance redis 0
+}
+
+# Note: the only slave of instance 0 is already down so no
+# failover is possible, that would change the state back to ok.
+test "Cluster should be down now" {
+    assert_cluster_state fail
+}
+
+test "Restarting master node" {
+    restart_instance redis 0
+}
+
+test "Cluster should be up again" {
     assert_cluster_state ok
 }
