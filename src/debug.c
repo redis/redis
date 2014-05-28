b98ee12f61ce16b0ad284c0d5d00d3bc09adbef0
Jan-Erik Rediger <badboy@archlinux.us>
2014-05-28 09:46:01 +0200

    Small typo fixed

--- src/debug.c
+++ src/debug.c
@@ -848,7 +848,7 @@ void sigsegvHandler(int sig, siginfo_t *info, void *secret) {
 "\n=== REDIS BUG REPORT END. Make sure to include from START to END. ===\n\n"
 "       Please report the crash opening an issue on github:\n\n"
 "           http://github.com/antirez/redis/issues\n\n"
-"  Suspect RAM error? Use redis-server --test-memory to veryfy it.\n\n"
+"  Suspect RAM error? Use redis-server --test-memory to verify it.\n\n"
 );
     /* free(messages); Don't call free() with possibly corrupted memory. */
     if (server.daemonize) unlink(server.pidfile);
