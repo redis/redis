9ac7787838e56b630e145c35b510d1f4d28f81c6
kingsumos <kingsumos@users.noreply.github.com>
2014-06-09 15:32:27 -0300

    cluster: fix node connection memory leak
    Cluster leaks memory while connecting due to missing freeaddrinfo()
    
    (Commit modified by @antirez. The freeaddrinfo() call was misplaced so
     in case of no address was bound, the memory leak was still there).
    
    Closes #1801

--- src/anet.c
+++ src/anet.c
@@ -262,7 +262,8 @@ static int anetTcpGenericConnect(char *err, char *addr, int port,
         if (source_addr) {
             int bound = 0;
             /* Using getaddrinfo saves us from self-determining IPv4 vs IPv6 */
-            if ((rv = getaddrinfo(source_addr, NULL, &hints, &bservinfo)) != 0) {
+            if ((rv = getaddrinfo(source_addr, NULL, &hints, &bservinfo)) != 0)
+            {
                 anetSetError(err, "%s", gai_strerror(rv));
                 goto end;
             }
@@ -272,6 +273,7 @@ static int anetTcpGenericConnect(char *err, char *addr, int port,
                     break;
                 }
             }
+            freeaddrinfo(bservinfo);
             if (!bound) {
                 anetSetError(err, "bind: %s", strerror(errno));
                 goto end;
