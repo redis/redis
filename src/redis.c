d3f43db20fbcb1aef38fa6c635a7c886e68055eb
antirez <antirez@gmail.com>
2014-06-27 12:11:15 +0200

    COMMAND COUNT subcommand added.

--- src/redis.c
+++ src/redis.c
@@ -2466,8 +2466,10 @@ void commandCommand(redisClient *c) {
         for (i = 2; i < c->argc; i++) {
             addReplyCommand(c, dictFetchValue(server.commands, c->argv[i]->ptr));
         }
+    } else if (!strcasecmp(c->argv[1]->ptr, "count") && c->argc == 2) {
+        addReplyLongLong(c, dictSize(server.commands));
     } else {
-        addReplyError(c, "Unknown subcommand.");
+        addReplyError(c, "Unknown subcommand or wrong number of arguments.");
         return;
     }
 }
