551bee86b42e7c41c9cd550795685eb20b207b88
antirez <antirez@gmail.com>
2014-07-01 16:04:59 +0200

    LATENCY SAMPLES implemented.

--- src/redis.c
+++ src/redis.c
@@ -282,7 +282,8 @@ struct redisCommand redisCommandTable[] = {
     {"pfadd",pfaddCommand,-2,"wmF",0,NULL,1,1,1,0,0},
     {"pfcount",pfcountCommand,-2,"w",0,NULL,1,1,1,0,0},
     {"pfmerge",pfmergeCommand,-2,"wm",0,NULL,1,-1,1,0,0},
-    {"pfdebug",pfdebugCommand,-3,"w",0,NULL,0,0,0,0,0}
+    {"pfdebug",pfdebugCommand,-3,"w",0,NULL,0,0,0,0,0},
+    {"latency",latencyCommand,-2,"arslt",0,NULL,0,0,0,0,0}
 };
 
 struct evictionPoolEntry *evictionPoolAlloc(void);
