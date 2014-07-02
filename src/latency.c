1f665d6e82bb11454d239e676cfcb96e29651291
antirez <antirez@gmail.com>
2014-07-02 16:56:08 +0200

    Cast void* to char* to avoid waring in latencyCommand().

--- src/latency.c
+++ src/latency.c
@@ -228,6 +228,6 @@ nodataerr:
     /* Common error when the user asks for an event we have no latency
      * information about. */
     addReplyErrorFormat(c,
-        "No samples available for event '%s'", c->argv[2]->ptr);
+        "No samples available for event '%s'", (char*) c->argv[2]->ptr);
 }
 
