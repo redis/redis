bb10ad9c1397b62ec685399b3777308942cb23a9
antirez <antirez@gmail.com>
2014-07-10 16:42:43 +0200

    Test: more reliable AOF rewrite test under write load.

--- tests/unit/aofrw.tcl
+++ tests/unit/aofrw.tcl
@@ -39,6 +39,16 @@ start_server {tags {"aofrw"}} {
         stop_write_load $load_handle3
         stop_write_load $load_handle4
 
+        # Make sure that we remain the only connected client.
+        # This step is needed to make sure there are no pending writes
+        # that will be processed between the two "debug digest" calls.
+        wait_for_condition 50 100 {
+            [llength [split [string trim [r client list]] "\n"]] == 1
+        } else {
+            puts [r client list]
+            fail "Clients generating loads are not disconnecting"
+        }
+
         # Get the data set digest
         set d1 [r debug digest]
 
