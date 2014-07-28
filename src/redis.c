0f8df3df417a22ce8f34e4c21b09f8ff5eb60320
Matt Robenolt <m@robenolt.com>
2014-07-28 00:30:12 -0700

    Fix spelling of "stand alone" in ascii art
    Also unified the logic to match `genRedisInfoString`

--- src/redis.c
+++ src/redis.c
@@ -3381,10 +3381,11 @@ void usage() {
 void redisAsciiArt(void) {
 #include "asciilogo.h"
     char *buf = zmalloc(1024*16);
-    char *mode = "stand alone";
+    char *mode;
 
     if (server.cluster_enabled) mode = "cluster";
     else if (server.sentinel_mode) mode = "sentinel";
+    else mode = "standalone";
 
     snprintf(buf,1024*16,ascii_logo,
         REDIS_VERSION,
