ff58d5a380811deb2887b5eed23105de77970ec0
Yossi Gottlieb <yossigo@gmail.com>
2014-07-28 14:43:30 +0300

    Fail SYNC if background save child aborted due to a signal.

--- src/rdb.c
+++ src/rdb.c
@@ -1221,7 +1221,7 @@ void backgroundSaveDoneHandler(int exitcode, int bysignal) {
     server.rdb_save_time_start = -1;
     /* Possibly there are slaves waiting for a BGSAVE in order to be served
      * (the first stage of SYNC is a bulk transfer of dump.rdb) */
-    updateSlavesWaitingBgsave(exitcode == 0 ? REDIS_OK : REDIS_ERR);
+    updateSlavesWaitingBgsave((!bysignal && exitcode == 0) ? REDIS_OK : REDIS_ERR);
 }
 
 void saveCommand(redisClient *c) {
