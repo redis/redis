28343966a4447ccbb23323d2dd1aae406ea62dbb
Matt Stancliff <matt@genges.com>
2014-07-31 14:25:48 -0400

    Spell software correctly

--- tests/instances.tcl
+++ tests/instances.tcl
@@ -4,7 +4,7 @@
 # instances.
 #
 # Copyright (C) 2014 Salvatore Sanfilippo antirez@gmail.com
-# This softare is released under the BSD License. See the COPYING file for
+# This software is released under the BSD License. See the COPYING file for
 # more information.
 
 package require Tcl 8.5
--- tests/sentinel/run.tcl
+++ tests/sentinel/run.tcl
@@ -1,5 +1,5 @@
 # Sentinel test suite. Copyright (C) 2014 Salvatore Sanfilippo antirez@gmail.com
-# This softare is released under the BSD License. See the COPYING file for
+# This software is released under the BSD License. See the COPYING file for
 # more information.
 
 cd tests/sentinel
