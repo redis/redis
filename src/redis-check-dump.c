da0132638f15d81782c5543ed49a1d1f17594d9d
Matt Stancliff <matt@genges.com>
2014-08-01 15:42:50 -0400

    Cleanup double semicolons
    Closes #1161

--- src/redis-check-dump.c
+++ src/redis-check-dump.c
@@ -614,7 +614,7 @@ void process(void) {
             printf("RDB version >= 5 but no room for checksum.\n");
             exit(1);
         }
-        positions[0].size -= 8;;
+        positions[0].size -= 8;
     }
 
     level = 1;
