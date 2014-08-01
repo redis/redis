25a4699a9ea321062fc44e651f49fe82174dc2f1
Matt Stancliff <matt@genges.com>
2014-08-01 14:55:24 -0400

    Reject MOVE to non-integer DBs
    Previously, "MOVE key somestring" would move the key to
    DB 0 which is just unexpected and wrong.
    String as DB == error.
    
    Test added too.
    
    Modified by @antirez in order to use the getLongLongFromObject() API
    instead of strtol().
    
    Fixes #1428

--- src/db.c
+++ src/db.c
@@ -697,11 +697,16 @@ void moveCommand(redisClient *c) {
     robj *o;
     redisDb *src, *dst;
     int srcid;
+    long long dbid;
 
     /* Obtain source and target DB pointers */
     src = c->db;
     srcid = c->db->id;
-    if (selectDb(c,atoi(c->argv[2]->ptr)) == REDIS_ERR) {
+
+    if (getLongLongFromObject(c->argv[2],&dbid) == REDIS_ERR ||
+        dbid < INT_MIN || dbid > INT_MAX ||
+        selectDb(c,dbid) == REDIS_ERR)
+    {
         addReply(c,shared.outofrangeerr);
         return;
     }
