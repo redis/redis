e713dc5a43851359da78ecb8168aea6bccbdc097
Matt Stancliff <matt@genges.com>
2014-08-04 13:13:12 -0400

    Sentinel: Abort Hello quicker if not connected
    We can save a little work by aborting when we enter the function
    if we're disconnected.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2216,9 +2216,10 @@ int sentinelSendHello(sentinelRedisInstance *ri) {
     sentinelRedisInstance *master = (ri->flags & SRI_MASTER) ? ri : ri->master;
     sentinelAddr *master_addr = sentinelGetCurrentMasterAddress(master);
 
+    if (ri->flags & SRI_DISCONNECTED) return REDIS_ERR;
+
     /* Try to obtain our own IP address. */
     if (anetSockName(ri->cc->c.fd,ip,sizeof(ip),NULL) == -1) return REDIS_ERR;
-    if (ri->flags & SRI_DISCONNECTED) return REDIS_ERR;
 
     /* Format and send the Hello message. */
     snprintf(payload,sizeof(payload),
