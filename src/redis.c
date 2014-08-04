e3ab9a54f973b71011bf91f89cf3c859327b0860
Matt Stancliff <matt@genges.com>
2014-08-04 12:28:45 -0400

    Sentinel: Reject config from STDIN
    Sentinel needs to die with a more accurate error message
    when attempted to open a config from STDIN.
    
    See: https://groups.google.com/forum/#!topic/redis-db/sYx7VNMWaNM

--- src/redis.c
+++ src/redis.c
@@ -3561,6 +3561,13 @@ int main(int argc, char **argv) {
             }
             j++;
         }
+        if (server.sentinel_mode && configfile && *configfile == '-') {
+            redisLog(REDIS_WARNING,
+                "Sentinel config from STDIN not allowed.");
+            redisLog(REDIS_WARNING,
+                "Sentinel needs config file on disk to save state.  Exiting...");
+            exit(1);
+        }
         if (configfile) server.configfile = getAbsolutePath(configfile);
         resetServerSaveParams();
         loadServerConfig(configfile,options);
