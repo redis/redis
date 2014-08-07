7bb25f8a46cafee90ba927d98141ccfa57e190df
antirez <antirez@gmail.com>
2014-08-07 16:32:50 +0200

    Force quit when receiving a second SIGINT.
    Also quit ASAP when we are still loading a DB, since care is not needed
    in this special condition, especially for a SIGINT.

--- src/rdb.c
+++ src/rdb.c
@@ -766,7 +766,7 @@ int rdbSaveBackground(char *filename) {
 void rdbRemoveTempFile(pid_t childpid) {
     char tmpfile[256];
 
-    snprintf(tmpfile,256,"temp-%d.rdb", (int) childpid);
+    snprintf(tmpfile,sizeof(tmpfile),"temp-%d.rdb", (int) childpid);
     unlink(tmpfile);
 }
 
