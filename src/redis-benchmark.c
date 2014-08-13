060718a7b3aa39f98bd3f3bc22b6736ee4e5c3c0
Matt Stancliff <matt@genges.com>
2014-08-13 10:58:54 -0400

    Fix redis-benchmark abort output

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -597,7 +597,7 @@ int showThroughput(struct aeEventLoop *eventLoop, long long id, void *clientData
     REDIS_NOTUSED(clientData);
 
     if (config.liveclients == 0) {
-        fprintf(stderr,"All clients disconnected... aborting.");
+        fprintf(stderr,"All clients disconnected... aborting.\n");
         exit(1);
     }
 
