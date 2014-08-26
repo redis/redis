293348d0de8776e409551bbbd72691f66cb121cf
antirez <antirez@gmail.com>
2014-08-26 10:05:24 +0200

    Missing va_end also added in hiredis copy of sds.c.

--- deps/hiredis/sds.c
+++ deps/hiredis/sds.c
@@ -390,6 +390,7 @@ sds sdscatvprintf(sds s, const char *fmt, va_list ap) {
         buf[buflen-2] = '\0';
         va_copy(cpy,ap);
         vsnprintf(buf, buflen, fmt, cpy);
+        va_end(ap);
         if (buf[buflen-2] != '\0') {
             if (buf != staticbuf) zfree(buf);
             buflen *= 2;
