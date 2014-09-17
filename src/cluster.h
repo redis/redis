d4c3c1248f72f53fe9c52826d0a6bc92ec4d85fb
antirez <antirez@gmail.com>
2014-09-17 11:10:09 +0200

    Cluster: new option to work with partial slots coverage.

--- src/cluster.h
+++ src/cluster.h
@@ -15,6 +15,7 @@
  * multiplicators of the node timeout value (when ending with MULT). */
 #define REDIS_CLUSTER_DEFAULT_NODE_TIMEOUT 15000
 #define REDIS_CLUSTER_DEFAULT_SLAVE_VALIDITY 10 /* Slave max data age factor. */
+#define REDIS_CLUSTER_DEFAULT_REQUIRE_FULL_COVERAGE 1
 #define REDIS_CLUSTER_FAIL_REPORT_VALIDITY_MULT 2 /* Fail report validity. */
 #define REDIS_CLUSTER_FAIL_UNDO_TIME_MULT 2 /* Undo fail if master is back. */
 #define REDIS_CLUSTER_FAIL_UNDO_TIME_ADD 10 /* Some additional time. */
