56c1f8805e72cbc55d74a7fc46ff17fdc5d7882d
Matt Stancliff <matt@genges.com>
2014-09-18 14:21:12 -0400

    Fix hiredis getaddrinfo leak
    Fixed in Redis by 1a5e5b6, but since that part of code
    is largely copy/paste from Redis, the fix needs to be
    ported over too.
    
    Closes #2012

--- deps/hiredis/net.c
+++ deps/hiredis/net.c
@@ -300,6 +300,7 @@ static int _redisContextConnectTcp(redisContext *c, const char *addr, int port,
                     break;
                 }
             }
+            freeaddrinfo(bservinfo);
             if (!bound) {
                 char buf[128];
                 snprintf(buf,sizeof(buf),"Can't bind socket: %s",strerror(errno));
