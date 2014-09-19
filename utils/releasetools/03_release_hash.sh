5807558c981541e0d4087ae850d536c2dffac247
antirez <antirez@gmail.com>
2014-09-19 19:18:48 +0200

    03_release_hahs.sh: let me edit before committing.

--- utils/releasetools/03_release_hash.sh
+++ utils/releasetools/03_release_hash.sh
@@ -2,7 +2,7 @@
 SHA=$(curl -s http://download.redis.io/releases/redis-${1}.tar.gz | shasum | cut -f 1 -d' ')
 ENTRY="hash redis-${1}.tar.gz sha1 $SHA http://download.redis.io/releases/redis-${1}.tar.gz"
 echo $ENTRY >> ~/hack/redis-hashes/README
-cat ~/hack/redis-hashes/README
+vi ~/hack/redis-hashes/README
 echo "Press any key to commit, Ctrl-C to abort)."
 read yes
 (cd ~/hack/redis-hashes; git commit -a -m '${1} hash.'; git push)
