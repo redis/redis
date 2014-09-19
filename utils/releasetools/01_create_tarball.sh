f3107559bee963f1345956581f90722e7c79be81
antirez <antirez@gmail.com>
2014-09-19 19:17:52 +0200

    Quick, wrong, fix for create_tarball script.

--- utils/releasetools/01_create_tarball.sh
+++ utils/releasetools/01_create_tarball.sh
@@ -8,6 +8,7 @@ fi
 TAG=$1
 TARNAME="redis-${TAG}.tar"
 echo "Generating /tmp/${TARNAME}"
+cd ~/hack/redis
 git archive $TAG --prefix redis-${TAG}/ > /tmp/$TARNAME || exit 1
 echo "Gizipping the archive"
 rm -f /tmp/$TARNAME.gz
