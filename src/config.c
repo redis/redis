c3d252d9955ee4d9b4be296cb2cf8560c9caf00c
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2014-09-25 17:40:11 -0300

    Fix typo
    Closes #2029

--- src/config.c
+++ src/config.c
@@ -1493,7 +1493,7 @@ void rewriteConfigEnumOption(struct rewriteConfigState *state, char *option, int
     rewriteConfigRewriteLine(state,option,line,force);
 }
 
-/* Rewrite the syslog-fability option. */
+/* Rewrite the syslog-facility option. */
 void rewriteConfigSyslogfacilityOption(struct rewriteConfigState *state) {
     int value = server.syslog_facility, j;
     int force = value != LOG_LOCAL0;
