f0124c1d90e4346a52ca474e4190abe8c2b93c8d
antirez <antirez@gmail.com>
2014-10-09 11:17:27 +0200

    Fix DEBUG POPULATE warning for lack of casting.

--- src/debug.c
+++ src/debug.c
@@ -336,7 +336,7 @@ void debugCommand(redisClient *c) {
         dictExpand(c->db->dict,keys);
         for (j = 0; j < keys; j++) {
             snprintf(buf,sizeof(buf),"%s:%lu",
-                (c->argc == 3) ? "key" : c->argv[3]->ptr, j);
+                (c->argc == 3) ? "key" : (char*)c->argv[3]->ptr, j);
             key = createStringObject(buf,strlen(buf));
             if (lookupKeyRead(c->db,key) != NULL) {
                 decrRefCount(key);
