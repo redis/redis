583a762cd911a61d99166017b214b301f4d09e2b
antirez <antirez@gmail.com>
2014-10-16 10:22:02 +0200

    Diskless replication: redis.conf and CONFIG SET/GET support.

--- src/replication.c
+++ src/replication.c
@@ -1802,7 +1802,8 @@ void replicationCron(void) {
             }
         }
 
-        if (slaves_waiting && max_idle > REDIS_DEFAULT_RDB_DISKLESS_SYNC_DELAY) {
+        if (slaves_waiting && max_idle > REDIS_DEFAULT_REPL_DISKLESS_SYNC_DELAY)
+        {
             /* Let's start a BGSAVE with disk target. */
             if (startBgsaveForReplication() == REDIS_OK) {
                 /* It started! We need to change the state of slaves
