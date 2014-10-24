2882f819f468dcf14dd57cd715fbdac7a6c79a1d
antirez <antirez@gmail.com>
2014-10-24 10:38:42 +0200

    Remove duplicated log message about starting BGSAVE.

--- src/replication.c
+++ src/replication.c
@@ -531,7 +531,6 @@ void syncCommand(redisClient *c) {
             redisLog(REDIS_NOTICE,"Delay next BGSAVE for SYNC");
         } else {
             /* Ok we don't have a BGSAVE in progress, let's start one. */
-            redisLog(REDIS_NOTICE,"Starting BGSAVE for SYNC");
             if (startBgsaveForReplication() != REDIS_OK) {
                 redisLog(REDIS_NOTICE,"Replication failed, can't BGSAVE");
                 addReplyError(c,"Unable to perform background save");
