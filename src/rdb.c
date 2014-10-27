f6879766016b0827b044cf9d7dbb857eaac6f8a5
antirez <antirez@gmail.com>
2014-10-27 12:23:03 +0100

    Use new slave name function for diskless repl reporting.

--- src/rdb.c
+++ src/rdb.c
@@ -1347,15 +1347,15 @@ void backgroundSaveDoneHandlerSocket(int exitcode, int bysignal) {
             }
             if (j == ok_slaves[0] || errorcode != 0) {
                 redisLog(REDIS_WARNING,
-                "Closing slave %llu: child->slave RDB transfer failed: %s",
-                    slave->id,
+                "Closing slave %s: child->slave RDB transfer failed: %s",
+                    replicationGetSlaveName(slave),
                     (errorcode == 0) ? "RDB transfer child aborted"
                                      : strerror(errorcode));
                 freeClient(slave);
             } else {
                 redisLog(REDIS_WARNING,
-                "Slave %llu correctly received the streamed RDB file.",
-                slave->id);
+                "Slave %s correctly received the streamed RDB file.",
+                    replicationGetSlaveName(slave));
                 /* Restore the socket as non-blocking. */
                 anetNonBlock(NULL,slave->fd);
                 anetSendTimeout(NULL,slave->fd,0);
