0014966c1e39cf2dcb4ca271900b93990d823bec
Matt Stancliff <matt@genges.com>
2014-10-28 12:25:16 -0400

    Networking: add more outbound IP binding fixes
    Same as the original bind fixes (we just missed these the
    first time around).
    
    This helps Redis not automatically send
    connections from the first IP on an interface if we are bound
    to a specific IP address (e.g. with multiple IP aliases on one
    interface, you want to send from _your_ IP, not from the first IP
    on the interface).

--- src/cluster.c
+++ src/cluster.c
@@ -4307,8 +4307,8 @@ int migrateGetSocket(redisClient *c, robj *host, robj *port, long timeout) {
     }
 
     /* Create the socket */
-    fd = anetTcpNonBlockConnect(server.neterr,c->argv[1]->ptr,
-                atoi(c->argv[2]->ptr));
+    fd = anetTcpNonBlockBindConnect(server.neterr,c->argv[1]->ptr,
+                atoi(c->argv[2]->ptr),REDIS_BIND_ADDR);
     if (fd == -1) {
         sdsfree(name);
         addReplyErrorFormat(c,"Can't connect to target node: %s",
