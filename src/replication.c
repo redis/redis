f1a6f780242a804d9ae38ecfd974c2c728955d32
Matt Stancliff <matt@genges.com>
2014-10-28 12:25:16 -0400

    Networking: add more outbound IP binding fixes
    Same as the original bind fixes (we just missed these the
    first time around).
    
    This helps Redis not automatically send
    connections from the first IP on an interface if we are bound
    to a specific IP address (e.g. with multiple IP aliases on one
    interface, you want to send from _your_ IP, not from the first IP
    on the interface).

--- src/replication.c
+++ src/replication.c
@@ -1357,7 +1357,8 @@ error:
 int connectWithMaster(void) {
     int fd;
 
-    fd = anetTcpNonBlockConnect(NULL,server.masterhost,server.masterport);
+    fd = anetTcpNonBlockBindConnect(NULL,
+        server.masterhost,server.masterport,REDIS_BIND_ADDR);
     if (fd == -1) {
         redisLog(REDIS_WARNING,"Unable to connect to MASTER: %s",
             strerror(errno));
