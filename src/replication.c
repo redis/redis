ec1ab0f18b2a5344849fcf0c638309256b4b80f4
antirez <antirez@gmail.com>
2014-11-11 15:10:58 +0100

    Disconnect timedout slave: regression introduced with diskless repl.

--- src/replication.c
+++ src/replication.c
@@ -1837,8 +1837,9 @@ void replicationCron(void) {
             if (slave->flags & REDIS_PRE_PSYNC) continue;
             if ((server.unixtime - slave->repl_ack_time) > server.repl_timeout)
             {
-                    redisLog(REDIS_WARNING, "Disconnecting timedout slave: %s",
-                        replicationGetSlaveName(slave));
+                redisLog(REDIS_WARNING, "Disconnecting timedout slave: %s",
+                    replicationGetSlaveName(slave));
+                freeClient(slave);
             }
         }
     }
