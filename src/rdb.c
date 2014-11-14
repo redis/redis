d956d809acb848aec0f6524e1337d274a635980d
Matt Stancliff <matt@genges.com>
2014-11-13 23:35:10 -0500

    Fix three simple clang analyzer warnings

--- src/rdb.c
+++ src/rdb.c
@@ -720,7 +720,7 @@ int rdbSave(char *filename) {
     char tmpfile[256];
     FILE *fp;
     rio rdb;
-    int error;
+    int error = 0;
 
     snprintf(tmpfile,256,"temp-%d.rdb", (int) getpid());
     fp = fopen(tmpfile,"w");
--- src/sentinel.c
+++ src/sentinel.c
@@ -577,7 +577,7 @@ void sentinelEvent(int level, char *type, sentinelRedisInstance *ri,
     if (level == REDIS_WARNING && ri != NULL) {
         sentinelRedisInstance *master = (ri->flags & SRI_MASTER) ?
                                          ri : ri->master;
-        if (master->notification_script) {
+        if (master && master->notification_script) {
             sentinelScheduleScriptExecution(master->notification_script,
                 type,msg,NULL);
         }
