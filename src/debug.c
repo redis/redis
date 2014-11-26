4c1b3add7fe2daf2eaad7ec9d9da227426219e1a
antirez <antirez@gmail.com>
2014-11-26 16:35:46 +0100

    Fix DEBUG OBJECT lru field to report seconds.
    Because of (not so) recent Redis changes, now the LRU internally
    reported unit is milliseconds, not seconds, but the DEBUG OBJECT output
    was still claiming seconds while providing milliseconds.
    However OBJECT IDLETIME was working as expected, which is the correct
    API to use.

--- src/debug.c
+++ src/debug.c
@@ -301,7 +301,7 @@ void debugCommand(redisClient *c) {
             "lru:%d lru_seconds_idle:%llu",
             (void*)val, val->refcount,
             strenc, (long long) rdbSavedObjectLen(val),
-            val->lru, estimateObjectIdleTime(val));
+            val->lru, estimateObjectIdleTime(val)/1000);
     } else if (!strcasecmp(c->argv[1]->ptr,"sdslen") && c->argc == 3) {
         dictEntry *de;
         robj *val;
