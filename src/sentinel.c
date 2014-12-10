0422321617861a4a5b7b88d8767f6c3aa33189fa
antirez <antirez@gmail.com>
2014-12-10 11:05:37 +0100

    Sentinel: removed useless flag var from INFO-CACHE.

--- src/sentinel.c
+++ src/sentinel.c
@@ -2797,12 +2797,10 @@ void sentinelCommand(redisClient *c) {
         dictType copy_keeper = instancesDictType;
         copy_keeper.valDestructor = NULL;
         dict *masters_local = NULL;
-        int needs_cleanup = 0;
         if (c->argc == 2) {
             masters_local = sentinel.masters;
         } else {
             masters_local = dictCreate(&copy_keeper, NULL);
-            needs_cleanup = 1;
 
             for (int i = 2; i < c->argc; i++) {
                 sentinelRedisInstance *ri;
@@ -2841,7 +2839,7 @@ void sentinelCommand(redisClient *c) {
             dictReleaseIterator(sdi);
         }
         dictReleaseIterator(di);
-        if (needs_cleanup) dictRelease(masters_local);
+        if (masters_local != sentinel.masters) dictRelease(masters_local);
     } else {
         addReplyErrorFormat(c,"Unknown sentinel subcommand '%s'",
                                (char*)c->argv[1]->ptr);
