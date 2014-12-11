ff01ce55d8c71335eb5a69f71d2712013d8480ed
antirez <antirez@gmail.com>
2014-12-11 15:16:20 +0100

    Removed useless space in redis-benchmark.c.

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -602,7 +602,7 @@ int showThroughput(struct aeEventLoop *eventLoop, long long id, void *clientData
     if (config.liveclients == 0) {
         fprintf(stderr,"All clients disconnected... aborting.\n");
         exit(1);
-    } 
+    }
     if (config.csv) return 250;
     if (config.idlemode == 1) {
         printf("clients: %d\r", config.liveclients);
