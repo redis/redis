fac2035c7dd523309cdf06d008f4df56daade7fd
Sun He <sunheehnus@gmail.com>
2015-01-06 22:39:01 +0800

    rand.c: update next()

--- src/rand.c
+++ src/rand.c
@@ -44,26 +44,22 @@
 #include <stdint.h>
 
 #define N	16
-#define MASK	((1 << (N - 1)) + (1 << (N - 1)) - 1)
+#define MASK	((1 << N) - 1)
 #define LOW(x)	((unsigned)(x) & MASK)
 #define HIGH(x)	LOW((x) >> N)
-#define MUL(x, y, z)	{ int32_t l = (long)(x) * (long)(y); \
-		(z)[0] = LOW(l); (z)[1] = HIGH(l); }
-#define CARRY(x, y)	((int32_t)(x) + (long)(y) > MASK)
-#define ADDEQU(x, y, z)	(z = CARRY(x, (y)), x = LOW(x + (y)))
+
+/* Initial X: 0x1234ABCD330E */
 #define X0	0x330E
 #define X1	0xABCD
 #define X2	0x1234
+
+/* A: 0x5DEECE66D */
 #define A0	0xE66D
 #define A1	0xDEEC
 #define A2	0x5
+
+/* C: 0xB */
 #define C	0xB
-#define SET3(x, x0, x1, x2)	((x)[0] = (x0), (x)[1] = (x1), (x)[2] = (x2))
-#define SETLOW(x, y, n) SET3(x, LOW((y)[n]), LOW((y)[(n)+1]), LOW((y)[(n)+2]))
-#define SEED(x0, x1, x2) (SET3(x, x0, x1, x2), SET3(a, A0, A1, A2), c = C)
-#define REST(v)	for (i = 0; i < 3; i++) { xsubi[i] = x[i]; x[i] = temp[i]; } \
-		return (v);
-#define HI_BIT	(1L << (2 * N - 1))
 
 static uint32_t x[3] = { X0, X1, X2 }, a[3] = { A0, A1, A2 }, c = C;
 static void next(void);
@@ -74,20 +70,32 @@ int32_t redisLrand48() {
 }
 
 void redisSrand48(int32_t seedval) {
-    SEED(X0, LOW(seedval), HIGH(seedval));
+    x[0] = X0;
+    x[1] = LOW(seedval);
+    x[2] = HIGH(seedval);
 }
 
 static void next(void) {
-    uint32_t p[2], q[2], r[2], carry0, carry1;
+    /* Z = (X * A) mod 2^48
+     *
+     * Every block is 16 bits
+     *     Z: | z2 | z1 | z0 |
+     *     X: |x[2]|x[1]|x[0]|
+     *     A: |a[2]|a[1]|a[0]|
+     */
+    uint32_t y0, y1, y2;
+    y0 = x[0] * a[0];
+    y1 = x[0] * a[1] + x[1] * a[0];
+    y2 = x[1] * a[1] + x[0] * a[2] + x[2] * a[0];
+    uint32_t z0, z1, z2;
+    z0 = LOW(y0);
+    z1 = LOW(HIGH(y0) + LOW(y1));
+    z2 = LOW(HIGH(HIGH(y0) + LOW(y1)) + HIGH(y1) + y2);
 
-    MUL(a[0], x[0], p);
-    ADDEQU(p[0], c, carry0);
-    ADDEQU(p[1], carry0, carry1);
-    MUL(a[0], x[1], q);
-    ADDEQU(p[1], q[0], carry0);
-    MUL(a[1], x[0], r);
-    x[2] = LOW(carry0 + carry1 + CARRY(p[1], r[0]) + q[1] + r[1] +
-            a[0] * x[2] + a[1] * x[1] + a[2] * x[0]);
-    x[1] = LOW(p[1] + r[0]);
-    x[0] = LOW(p[0]);
+    /* X = (Z + c) mod 2^48
+     *   = (X * A + c) mod 2^48
+     */
+    x[0] = LOW(z0 + c);
+    x[1] = LOW(z1 + HIGH(z0 + c));
+    x[2] = LOW(z2 + HIGH(z1 + HIGH(z0 + c)));
 }
