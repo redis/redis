df1a7fc4fe12c4027b7dfaac39377a7644486dc9
antirez <antirez@gmail.com>
2015-01-13 10:48:49 +0100

    Cluster: fetch my IP even if msg is not MEET for the first time.
    In order to avoid that misconfigured cluster nodes at some time may
    force an IP update on other nodes, it is required that nodes update
    their own address only on MEET messages. However it does not make sense
    to do this the first time a node is contacted and yet does not have an
    IP, we just risk that myself->ip remains not assigned if there are
    messages lost or cluster creation procedures that don't make sure
    everybody is targeted by at least one incoming MEET message.
    
    Also fix the logging of the IP switch avoiding the :-1 tail.

--- src/cluster.c
+++ src/cluster.c
@@ -1547,8 +1547,12 @@ int clusterProcessPacket(clusterLink *link) {
          * later if we changed address, and those nodes will use our
          * official address to connect to us. So by obtaining this address
          * from the socket is a simple way to discover / update our own
-         * address in the cluster without it being hardcoded in the config. */
-        if (type == CLUSTERMSG_TYPE_MEET) {
+         * address in the cluster without it being hardcoded in the config.
+         *
+         * However if we don't have an address at all, we update the address
+         * even with a normal PING packet. If it's wrong it will be fixed
+         * by MEET later. */
+        if (type == CLUSTERMSG_TYPE_MEET || myself->ip[0] == '\0') {
             char ip[REDIS_IP_STR_LEN];
 
             if (anetSockName(link->fd,ip,sizeof(ip),NULL) != -1 &&
