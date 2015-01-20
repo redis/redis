1dea8b5a298aa62ff25231d6a4bdc1993b2608b8
antirez <antirez@gmail.com>
2015-01-20 23:13:47 +0100

    luaRedisGenericCommand() recursion: just return an error.
    Instead of calling redisPanic() to abort the server.
    
    Related to issue #2302.

--- src/scripting.c
+++ src/scripting.c
@@ -220,7 +220,9 @@ int luaRedisGenericCommand(lua_State *lua, int raise_error) {
      * To make this function reentrant is futile and makes it slower, but
      * we should at least detect such a misuse, and abort. */
     if (inuse) {
-        redisPanic("luaRedisGenericCommand() recursive call detected. Are you doing funny stuff with Lua debug hooks?");
+        luaPushError(lua,
+            "luaRedisGenericCommand() recursive call detected. Are you doing funny stuff with Lua debug hooks?");
+        return 1;
     }
     inuse++;
 
