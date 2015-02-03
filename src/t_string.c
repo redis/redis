1dbd8e94a7a0bed51c9c35f6c34486dad876ebc5
antirez <antirez@gmail.com>
2015-02-03 14:17:06 +0100

    More obvious indentation in setCommand().

--- src/t_string.c
+++ src/t_string.c
@@ -105,22 +105,26 @@ void setCommand(redisClient *c) {
 
         if ((a[0] == 'n' || a[0] == 'N') &&
             (a[1] == 'x' || a[1] == 'X') && a[2] == '\0' &&
-            !(flags & REDIS_SET_XX)) {
+            !(flags & REDIS_SET_XX))
+        {
             flags |= REDIS_SET_NX;
         } else if ((a[0] == 'x' || a[0] == 'X') &&
                    (a[1] == 'x' || a[1] == 'X') && a[2] == '\0' &&
-                   !(flags & REDIS_SET_NX)) {
+                   !(flags & REDIS_SET_NX))
+        {
             flags |= REDIS_SET_XX;
         } else if ((a[0] == 'e' || a[0] == 'E') &&
                    (a[1] == 'x' || a[1] == 'X') && a[2] == '\0' &&
-                   !(flags & REDIS_SET_PX) && next) {
+                   !(flags & REDIS_SET_PX) && next)
+        {
             flags |= REDIS_SET_EX;
             unit = UNIT_SECONDS;
             expire = next;
             j++;
         } else if ((a[0] == 'p' || a[0] == 'P') &&
                    (a[1] == 'x' || a[1] == 'X') && a[2] == '\0' &&
-                   !(flags & REDIS_SET_EX) && next) {
+                   !(flags & REDIS_SET_EX) && next)
+        {
             flags |= REDIS_SET_PX;
             unit = UNIT_MILLISECONDS;
             expire = next;
