eaeba1b2c85812c6b16a8ef50bf889d72804db09
Chris Lamb <chris@chris-lamb.co.uk>
2015-02-04 18:29:22 +0000

    Tidy grammar in CONFIG SET maxmemory warning.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- src/config.c
+++ src/config.c
@@ -648,7 +648,7 @@ void configSetCommand(redisClient *c) {
         server.maxmemory = ll;
         if (server.maxmemory) {
             if (server.maxmemory < zmalloc_used_memory()) {
-                redisLog(REDIS_WARNING,"WARNING: the new maxmemory value set via CONFIG SET is smaller than the current memory usage. This will result in keys eviction and/or inability to accept new write commands depending on the maxmemory-policy.");
+                redisLog(REDIS_WARNING,"WARNING: the new maxmemory value set via CONFIG SET is smaller than the current memory usage. This will result in key eviction and/or the inability to accept new write commands depending on the maxmemory-policy.");
             }
             freeMemoryIfNeeded();
         }
