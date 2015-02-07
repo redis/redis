630634a37a9b0f96e14f4ef7821275874a993b99
antirez <antirez@gmail.com>
2015-02-07 15:05:40 +0100

    Add missing latency-dest legend symbol.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1140,7 +1140,7 @@ void showLatencyDistSamples(struct distsamples *samples, long long tot) {
 /* Show the legend: different buckets values and colors meaning, so
  * that the spectrum is more easily readable. */
 void showLatencyDistLegend(void) {
-    printf(". - *            0.01 0.125 0.5  milliseconds\n");
+    printf(". - * #          .01 .125 .25 .5 milliseconds\n");
     printf("1,2,3,...,9      from 1 to 9     milliseconds\n");
     printf("A,B,C,D,E        10,20,30,40,50  milliseconds\n");
     printf("F,G,H,I,J        .1,.2,.3,.4,.5       seconds\n");
