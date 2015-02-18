acb933a747ea5ecc85d984867e8627a813ffe2c1
Sisir Koppaka <sisir.koppaka@gmail.com>
2015-02-18 08:16:41 -0500

    rehashing.c: Fix compile error originating from SPOP rewrite

--- utils/hashtable/rehashing.c
+++ utils/hashtable/rehashing.c
@@ -73,7 +73,7 @@ void stressGetKeys(dict *d, int times) {
     dictEntry **des = zmalloc(sizeof(dictEntry*)*dictSize(d));
     for (j = 0; j < times; j++) {
         int requested = rand() % (dictSize(d)+1);
-        int returned = dictGetRandomKeys(d, des, requested);
+        int returned = dictGetSomeKeys(d, des, requested);
         if (requested != returned) {
             printf("*** ERROR! Req: %d, Ret: %d\n", requested, returned);
             exit(1);
