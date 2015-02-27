c77081a45a7c350428785b6e7fafeb2b57ea4330
antirez <antirez@gmail.com>
2015-02-27 22:33:54 +0100

    Migrate: replace conditional with pre-computed value.

--- src/cluster.c
+++ src/cluster.c
@@ -4584,7 +4584,7 @@ try_again:
             /* On error assume that last_dbid is no longer valid. */
             cs->last_dbid = -1;
             addReplyErrorFormat(c,"Target instance replied with error: %s",
-                (cs->last_dbid != dbid && buf1[0] == '-') ? buf1+1 : buf2+1);
+                (select && buf1[0] == '-') ? buf1+1 : buf2+1);
         } else {
             /* Update the last_dbid in migrateCachedSocket */
             cs->last_dbid = dbid;
