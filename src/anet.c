7ab1d16c2848331b89e73b306dbf5abbf0ccebc3
Sun He <sunheehnus@gmail.com>
2015-03-06 14:44:10 +0800

    anet.c/_anetTcpServer: close fd when anetSetReuseAddr failed
    anetV6Only and anetListen close fd inside when failed,
    anetSetReuseAddr only reports error but doesn't close it.

--- src/anet.c
+++ src/anet.c
@@ -463,13 +463,15 @@ static int _anetTcpServer(char *err, int port, char *bindaddr, int af, int backl
             continue;
 
         if (af == AF_INET6 && anetV6Only(err,s) == ANET_ERR) goto error;
-        if (anetSetReuseAddr(err,s) == ANET_ERR) goto error;
+        if (anetSetReuseAddr(err,s) == ANET_ERR) {
+            close(s);
+            goto error;
+        }
         if (anetListen(err,s,p->ai_addr,p->ai_addrlen,backlog) == ANET_ERR) goto error;
         goto end;
     }
     if (p == NULL) {
         anetSetError(err, "unable to bind socket");
-        goto error;
     }
 
 error:
