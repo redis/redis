f5d9e3f7152c9d5bb5ac47592476e330f662411f
antirez <antirez@gmail.com>
2015-03-08 15:33:42 +0100

    Config: activerehashing option support in CONFIG SET.

--- src/config.c
+++ src/config.c
@@ -785,6 +785,11 @@ void configSetCommand(redisClient *c) {
 
         if (yn == -1) goto badfmt;
         server.repl_slave_ro = yn;
+    } else if (!strcasecmp(c->argv[2]->ptr,"activerehashing")) {
+        int yn = yesnotoi(o->ptr);
+
+        if (yn == -1) goto badfmt;
+        server.activerehashing = yn;
     } else if (!strcasecmp(c->argv[2]->ptr,"dir")) {
         if (chdir((char*)o->ptr) == -1) {
             addReplyErrorFormat(c,"Changing directory: %s", strerror(errno));
