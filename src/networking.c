19b1eef317b8837510426bf1cccc3a4dca28bc44
Sun He <sunheehnus@gmail.com>
2015-03-11 00:08:03 +0800

    networking.c/addReplyBulkLen: replace a code block with existing sdigits10

--- src/networking.c
+++ src/networking.c
@@ -495,17 +495,7 @@ void addReplyBulkLen(redisClient *c, robj *obj) {
     if (sdsEncodedObject(obj)) {
         len = sdslen(obj->ptr);
     } else {
-        long n = (long)obj->ptr;
-
-        /* Compute how many bytes will take this integer as a radix 10 string */
-        len = 1;
-        if (n < 0) {
-            len++;
-            n = -n;
-        }
-        while((n = n/10) != 0) {
-            len++;
-        }
+        len = sdigits10((long)obj->ptr);
     }
 
     if (len < REDIS_SHARED_BULKHDR_LEN)
