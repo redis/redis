d8236ea262f947ac27f5e2e324511796609a1752
antirez <antirez@gmail.com>
2015-03-18 12:09:31 +0100

    Cluster: more robust slave check in CLUSTER REPLICATE.
    There are rare conditions where node->slaveof may be NULL even if the
    node is a slave. To check by flag is much more robust.

--- src/cluster.c
+++ src/cluster.c
@@ -4058,7 +4058,7 @@ void clusterCommand(redisClient *c) {
         }
 
         /* Can't replicate a slave. */
-        if (n->slaveof != NULL) {
+        if (nodeIsSlave(n)) {
             addReplyError(c,"I can only replicate a master, not a slave.");
             return;
         }
