5893c89719ad5f405935499fd1e332fcb36b4e4f
Sun He <sunheehnus@gmail.com>
2015-04-08 22:26:16 +0800

    replication.c/readSyncBulkPayload(): retry is -1 when AOF fatal happen

--- src/replication.c
+++ src/replication.c
@@ -1047,7 +1047,7 @@ void readSyncBulkPayload(aeEventLoop *el, int fd, void *privdata, int mask) {
                 redisLog(REDIS_WARNING,"Failed enabling the AOF after successful master synchronization! Trying it again in one second.");
                 sleep(1);
             }
-            if (!retry) {
+            if (retry == -1) {
                 redisLog(REDIS_WARNING,"FATAL: this slave instance finished the synchronization with its master, but the AOF can't be turned on. Exiting now.");
                 exit(1);
             }
