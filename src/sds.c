239494db645c6ed87e605cc4b0a10db78b50e5cd
FuGangqiang <fu_gangqiang@163.com>
2015-04-20 21:46:48 +0800

    fix doc example

--- src/sds.c
+++ src/sds.c
@@ -557,7 +557,7 @@ sds sdscatfmt(sds s, char const *fmt, ...) {
  * Example:
  *
  * s = sdsnew("AA...AA.a.aa.aHelloWorld     :::");
- * s = sdstrim(s,"A. :");
+ * s = sdstrim(s,"Aa. :");
  * printf("%s\n", s);
  *
  * Output will be just "Hello World".
