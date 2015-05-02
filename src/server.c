2739544668c3a8948835f1803d30c30255c8aecf
SridharIyer <sridhariyer@versa-networks.com>
2015-05-01 18:04:14 -0700

    Bringup redis-server if at least 1 ip is up
    The earlier behavior was to exit out if bind to any ip failed. The new
    behavior allows redis-server to come up if even a single bind succeeds.

--- src/server.c
+++ src/server.c
@@ -1635,6 +1635,7 @@ void checkTcpBacklogSettings(void) {
  * one of the IPv4 or IPv6 protocols. */
 int listenToPort(int port, int *fds, int *count) {
     int j;
+    int working_set = 0;
 
     /* Force binding of 0.0.0.0 if no bind address is specified, always
      * entering the loop if j == 0. */
@@ -1684,10 +1685,17 @@ int listenToPort(int port, int *fds, int *count) {
                 "Creating Server TCP listening socket %s:%d: %s",
                 server.bindaddr[j] ? server.bindaddr[j] : "*",
                 port, server.neterr);
-            return C_ERR;
+        } else {
+            anetNonBlock(NULL,fds[*count]);
+            (*count)++;
+            working_set++;
         }
-        anetNonBlock(NULL,fds[*count]);
-        (*count)++;
+    }
+
+    if (working_set == 0) {
+        serverLog(LL_WARNING,
+            "Could not bind to any specied addresses");
+        return C_ERR;
     }
     return C_OK;
 }
