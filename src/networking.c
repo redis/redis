08d4df8d313ccd5147ac0ad2f92129b37cf44fa2
Jungtaek Lim <kabhwan@gmail.com>
2015-05-12 10:04:52 +0900

    protocol error log should be seen debug/verbose level

--- src/networking.c
+++ src/networking.c
@@ -974,7 +974,7 @@ int processInlineBuffer(redisClient *c) {
 /* Helper function. Trims query buffer to make the function that processes
  * multi bulk requests idempotent. */
 static void setProtocolError(redisClient *c, int pos) {
-    if (server.verbosity >= REDIS_VERBOSE) {
+    if (server.verbosity <= REDIS_VERBOSE) {
         sds client = catClientInfoString(sdsempty(),c);
         redisLog(REDIS_VERBOSE,
             "Protocol error from client: %s", client);
