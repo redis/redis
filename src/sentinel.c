4dee18cb66fe37190c619232d61f4bea43d6dfb0
antirez <antirez@gmail.com>
2015-05-14 17:44:52 +0200

    Sentinel: config-rewrite unique ID just one time

--- src/sentinel.c
+++ src/sentinel.c
@@ -1646,15 +1646,15 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
     dictEntry *de;
     sds line;
 
+    /* sentinel unique ID. */
+    line = sdscatprintf(sdsempty(), "sentinel myid %s", sentinel.myid);
+    rewriteConfigRewriteLine(state,"sentinel",line,1);
+
     /* For every master emit a "sentinel monitor" config entry. */
     di = dictGetIterator(sentinel.masters);
     while((de = dictNext(di)) != NULL) {
         sentinelRedisInstance *master, *ri;
         sentinelAddr *master_addr;
-
-        /* sentinel unique ID. */
-        line = sdscatprintf(sdsempty(), "sentinel myid %s", sentinel.myid);
-        rewriteConfigRewriteLine(state,"sentinel",line,1);
 
         /* sentinel monitor */
         master = dictGetVal(de);
