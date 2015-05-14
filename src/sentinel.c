f9e942d4ae310a9416d0f91fa0e32c799009b8bc
antirez <antirez@gmail.com>
2015-05-14 14:12:45 +0200

    Sentinel: remove debugging message from releaseInstanceLink()

--- src/sentinel.c
+++ src/sentinel.c
@@ -980,7 +980,6 @@ instanceLink *releaseInstanceLink(instanceLink *link, sentinelRedisInstance *ri)
             cb = callbacks->head;
             while(cb) {
                 if (cb->privdata == ri) {
-                    printf("HERE\n");
                     cb->fn = sentinelDiscardReplyCallback;
                     cb->privdata = NULL; /* Not strictly needed. */
                 }
