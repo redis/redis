87b6013adb750b1f5ba161202876a84ffac45d3a
antirez <antirez@gmail.com>
2015-05-14 13:40:23 +0200

    Sentinel: remove SHARED! debugging printf

--- src/sentinel.c
+++ src/sentinel.c
@@ -1031,7 +1031,6 @@ int sentinelTryConnectionSharing(sentinelRedisInstance *ri) {
         releaseInstanceLink(ri->link,NULL);
         ri->link = match->link;
         match->link->refcount++;
-        printf("SHARED!\n");
         return REDIS_OK;
     }
     dictReleaseIterator(di);
