5080f2d69908e2324311f03224a5dc7c98322569
antirez <antirez@gmail.com>
2015-05-25 10:24:27 +0200

    Sentinel: help subcommand in simulate-failure command

--- src/sentinel.c
+++ src/sentinel.c
@@ -3138,6 +3138,10 @@ void sentinelCommand(redisClient *c) {
                     SENTINEL_SIMFAILURE_CRASH_AFTER_PROMOTION;
                 redisLog(REDIS_WARNING,"Failure simulation: this Sentinel "
                     "will crash after promoting the selected slave to master");
+            } else if (!strcasecmp(c->argv[j]->ptr,"help")) {
+                addReplyMultiBulkLen(c,2);
+                addReplyBulkCString(c,"crash-after-election");
+                addReplyBulkCString(c,"crash-after-promotion");
             } else {
                 addReplyError(c,"Unknown failure simulation specified");
                 return;
