3a87d02e1d815e31a8ef3a8564fe4d8c39a521b1
Alexis Campailla <alexis@janeasystems.com>
2015-06-03 13:10:35 +0200

    Fix tabs and indentation

--- src/replication.c
+++ src/replication.c
@@ -1990,7 +1990,7 @@ void replicationCron(void) {
     }
 
     /* Timed out master when we are an already connected slave? */
-	if (server.masterhost && server.repl_state == REDIS_REPL_CONNECTED &&
+    if (server.masterhost && server.repl_state == REDIS_REPL_CONNECTED &&
         (time(NULL)-server.master->lastinteraction) > server.repl_timeout)
     {
         redisLog(REDIS_WARNING,"MASTER timeout: no data nor PING received...");
