c68970e268e476a7789c5006440b422cff9d4a78
Enrico Giordani <enrico.giordani@gmail.com>
2015-06-11 15:30:04 +0200

    Enabled dlmalloc DEBUG mode in the debug build.

--- src/Win32_Interop/Win32_dlmalloc.c
+++ src/Win32_Interop/Win32_dlmalloc.c
@@ -1466,7 +1466,8 @@ DLMALLOC_EXPORT int mspace_mallopt(int, int);
 #ifndef LACKS_ERRNO_H
 #include <errno.h>       /* for MALLOC_FAILURE_ACTION */
 #endif /* LACKS_ERRNO_H */
-#ifdef DEBUG
+#ifdef _DEBUG
+#define DEBUG 1
 #if ABORT_ON_ASSERT_FAILURE
 #undef assert
 #define assert(x) if(!(x)) ABORT
