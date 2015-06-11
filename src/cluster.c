a401a84eb2fb0b22e003cdd2c5231995f57bab8b
antirez <antirez@gmail.com>
2015-06-11 12:29:37 +0200

    Don't try to bind the source address for MIGRATE
    Related to issues #2609 and #2612.

--- src/cluster.c
+++ src/cluster.c
@@ -4486,8 +4486,8 @@ migrateCachedSocket* migrateGetSocket(redisClient *c, robj *host, robj *port, lo
     }
 
     /* Create the socket */
-    fd = anetTcpNonBlockBindConnect(server.neterr,c->argv[1]->ptr,
-                atoi(c->argv[2]->ptr),REDIS_BIND_ADDR);
+    fd = anetTcpNonBlockConnect(server.neterr,c->argv[1]->ptr,
+                                atoi(c->argv[2]->ptr));
     if (fd == -1) {
         sdsfree(name);
         addReplyErrorFormat(c,"Can't connect to target node: %s",
