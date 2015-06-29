d308cadc8a18fe8aea41898d8d881f91103617a0
antirez <antirez@gmail.com>
2015-06-29 16:34:02 +0200

    Geo: added my copyright notice in modified files.

--- src/geo.c
+++ src/geo.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014, Matt Stancliff <matt@genges.com>.
+ * Copyright (c) 2015, Salvatore Sanfilippo <antirez@gmail.com>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
