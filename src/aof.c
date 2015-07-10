a57aa8831ee8cdb36b47d501db48a0554acd8aa8
Yongyue Sun <abioy.sun@gmail.com>
2015-07-10 15:25:40 +0800

    bugfix: errno might change before logging
    Signed-off-by: Yongyue Sun <abioy.sun@gmail.com>

--- src/aof.c
+++ src/aof.c
@@ -1169,9 +1169,9 @@ int rewriteAppendOnlyFile(char *filename) {
     return REDIS_OK;
 
 werr:
+    redisLog(REDIS_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     fclose(fp);
     unlink(tmpfile);
-    redisLog(REDIS_WARNING,"Write error writing append only file on disk: %s", strerror(errno));
     if (di) dictReleaseIterator(di);
     return REDIS_ERR;
 }
--- src/rdb.c
+++ src/rdb.c
@@ -755,9 +755,9 @@ int rdbSave(char *filename) {
     return REDIS_OK;
 
 werr:
+    redisLog(REDIS_WARNING,"Write error saving DB on disk: %s", strerror(errno));
     fclose(fp);
     unlink(tmpfile);
-    redisLog(REDIS_WARNING,"Write error saving DB on disk: %s", strerror(errno));
     return REDIS_ERR;
 }
 
