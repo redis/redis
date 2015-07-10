9c7f98521e038dc7602de872c756a8644fa8cad0
Yongyue Sun <abioy.sun@gmail.com>
2015-07-10 15:25:40 +0800

    bugfix: errno might change before logging
    Signed-off-by: Yongyue Sun <abioy.sun@gmail.com>

--- src/rdb.c
+++ src/rdb.c
@@ -754,9 +754,9 @@ int rdbSave(char *filename) {
     return REDIS_OK;
 
 werr:
+    redisLog(REDIS_WARNING,"Write error saving DB on disk: %s", strerror(errno));
     fclose(fp);
     unlink(tmpfile);
-    redisLog(REDIS_WARNING,"Write error saving DB on disk: %s", strerror(errno));
     return REDIS_ERR;
 }
 
