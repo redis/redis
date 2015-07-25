9894495c5a8e37a8e1dad9698e49cf842a027b2e
antirez <antirez@gmail.com>
2015-07-25 17:08:44 +0200

    SDS: Copyright notice updated.

--- src/sds.h
+++ src/sds.h
@@ -1,6 +1,7 @@
-/* SDSLib, A C dynamic strings library
+/* SDSLib 2.0 -- A C dynamic strings library
  *
- * Copyright (c) 2006-2010, Salvatore Sanfilippo <antirez at gmail dot com>
+ * Copyright (c) 2006-2015, Salvatore Sanfilippo <antirez at gmail dot com>
+ * Copyright (c) 2015, Redis Labs, Inc
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
