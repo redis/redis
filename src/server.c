dac4fc35d9afa10878c31d2ed37df986f3fbaa58
Mingyoo, Jung <tugs@linecorp.com>
2015-08-04 10:08:15 +0900

    Fix bug in maxmemoryToString function

--- src/server.c
+++ src/server.c
@@ -2752,7 +2752,7 @@ sds genRedisInfoString(char *section) {
         char maxmemory_hmem[64];
         size_t zmalloc_used = zmalloc_used_memory();
         size_t total_system_mem = server.system_memory_size;
-        const char *evict_policy = maxmemoryToString();
+        const char *maxmemory_policy = maxmemoryPolicyToString();
         long long memory_lua = (long long)lua_gc(server.lua,LUA_GCCOUNT,0)*1024;
 
         /* Peak memory is updated from time to time by serverCron() so it
@@ -2799,7 +2799,7 @@ sds genRedisInfoString(char *section) {
             used_memory_lua_hmem,
             server.maxmemory,
             maxmemory_hmem,
-            evict_policy,
+            maxmemory_policy,
             zmalloc_get_fragmentation_ratio(server.resident_set_size),
             ZMALLOC_LIB
             );
