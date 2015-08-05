6da198cdf106b46047b07730c9633ae0d5c0ddb7
antirez <antirez@gmail.com>
2015-08-05 13:58:56 +0200

    Remove slave state change handled by replicationSetupSlaveForFullResync().

--- src/rdb.c
+++ src/rdb.c
@@ -1420,7 +1420,6 @@ int rdbSaveToSlavesSockets(void) {
         if (slave->replstate == REDIS_REPL_WAIT_BGSAVE_START) {
             clientids[numfds] = slave->id;
             fds[numfds++] = slave->fd;
-            slave->replstate = REDIS_REPL_WAIT_BGSAVE_END;
             replicationSetupSlaveForFullResync(slave,getPsyncInitialOffset());
             /* Put the socket in non-blocking mode to simplify RDB transfer.
              * We'll restore it when the children returns (since duped socket
