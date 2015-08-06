ce5761e0618b8d5788c2f6f8f6a514a77040ab24
antirez <antirez@gmail.com>
2015-08-06 09:49:38 +0200

    startBgsaveForReplication(): log what you really do.

--- src/replication.c
+++ src/replication.c
@@ -490,11 +490,12 @@ need_full_resync:
  * Returns C_OK on success or C_ERR otherwise. */
 int startBgsaveForReplication(int mincapa) {
     int retval;
+    int socket_target = server.repl_diskless_sync && (mincapa & SLAVE_CAPA_EOF);
 
     serverLog(LL_NOTICE,"Starting BGSAVE for SYNC with target: %s",
-        server.repl_diskless_sync ? "slaves sockets" : "disk");
+        socket_target ? "slaves sockets" : "disk");
 
-    if (server.repl_diskless_sync && (mincapa & SLAVE_CAPA_EOF))
+    if (socket_target)
         retval = rdbSaveToSlavesSockets();
     else
         retval = rdbSaveBackground(server.rdb_filename);
