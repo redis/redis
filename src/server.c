ff524c39f4f1dcc3384aa16f93c7c1ba62db78dd
zhangxinyu13 <zhangxinyu3@jd.com>
2015-08-19 13:26:25 +0800

    add READONLY flag to ROLE command

--- src/server.c
+++ src/server.c
@@ -250,7 +250,7 @@ struct redisCommand redisCommandTable[] = {
     {"pttl",pttlCommand,2,"rF",0,NULL,1,1,1,0,0},
     {"persist",persistCommand,2,"wF",0,NULL,1,1,1,0,0},
     {"slaveof",slaveofCommand,3,"ast",0,NULL,0,0,0,0,0},
-    {"role",roleCommand,1,"lst",0,NULL,0,0,0,0,0},
+    {"role",roleCommand,1,"rlst",0,NULL,0,0,0,0,0},
     {"debug",debugCommand,-2,"as",0,NULL,0,0,0,0,0},
     {"config",configCommand,-2,"art",0,NULL,0,0,0,0,0},
     {"subscribe",subscribeCommand,-2,"rpslt",0,NULL,0,0,0,0,0},
