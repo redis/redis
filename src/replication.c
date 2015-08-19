c84f14f151ea3cdd646d1f95e7fa8c3bc50ab373
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-08-19 09:12:48 -0400

    [WIP] Fix compile error.
    But I'm not sure if it's right... Tests pass.

--- src/replication.c
+++ src/replication.c
@@ -619,7 +619,7 @@ void syncCommand(redisClient *c) {
             /* Target is disk (or the slave is not capable of supporting
              * diskless replication) and we don't have a BGSAVE in progress,
              * let's start one. */
-            if (startBgsaveForReplication(c->slave_capa) != C_OK) {
+            if (startBgsaveForReplication(c->slave_capa) != REDIS_OK) {
                 redisLog(REDIS_NOTICE,"Replication failed, can't BGSAVE");
                 addReplyError(c,"Unable to perform background save");
                 return;
