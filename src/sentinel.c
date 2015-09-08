33769f840cd53f03caf8f4b886a7c95182492027
antirez <antirez@gmail.com>
2015-09-08 09:27:43 +0200

    Sentinel: command arity check added where missing.

--- src/sentinel.c
+++ src/sentinel.c
@@ -3023,6 +3023,7 @@ void sentinelCommand(client *c) {
             addReply(c,shared.ok);
         }
     } else if (!strcasecmp(c->argv[1]->ptr,"flushconfig")) {
+        if (c->argc != 2) goto numargserr;
         sentinelFlushConfig();
         addReply(c,shared.ok);
         return;
@@ -3030,6 +3031,7 @@ void sentinelCommand(client *c) {
         /* SENTINEL REMOVE <name> */
         sentinelRedisInstance *ri;
 
+        if (c->argc != 3) goto numargserr;
         if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2]))
             == NULL) return;
         sentinelEvent(LL_WARNING,"-monitor",ri,"%@");
