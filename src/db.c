37843be92f3d503d8dea190c2fd3795aeec096be
antirez <antirez@gmail.com>
2015-09-14 12:28:22 +0200

    MOVE now can move TTL metadata as well.
    MOVE was not able to move the TTL: when a key was moved into a different
    database number, it became persistent like if PERSIST was used.
    
    In some incredible way (I guess almost nobody uses Redis MOVE) this bug
    remained unnoticed inside Redis internals for many years.
    Finally Andy Grunwald discovered it and opened an issue.
    
    This commit fixes the bug and adds a regression test.
    
    Close #2765.

--- src/db.c
+++ src/db.c
@@ -700,7 +700,7 @@ void moveCommand(redisClient *c) {
     robj *o;
     redisDb *src, *dst;
     int srcid;
-    long long dbid;
+    long long dbid, expire;
 
     /* Obtain source and target DB pointers */
     src = c->db;
@@ -729,6 +729,7 @@ void moveCommand(redisClient *c) {
         addReply(c,shared.czero);
         return;
     }
+    expire = getExpire(c->db,c->argv[1]);
 
     /* Return zero if the key already exists in the target DB */
     if (lookupKeyWrite(dst,c->argv[1]) != NULL) {
@@ -736,6 +737,7 @@ void moveCommand(redisClient *c) {
         return;
     }
     dbAdd(dst,c->argv[1],o);
+    if (expire) setExpire(dst,c->argv[1],expire);
     incrRefCount(o);
 
     /* OK! key moved, free the entry in the source DB */
