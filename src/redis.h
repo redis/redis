7cb8481053cae4b41b71c6ac6ea2b2b5af360ba2
antirez <antirez@gmail.com>
2015-10-15 10:04:35 +0200

    Move end-comment of handshake states.
    For an error I missed the last handshake state.
    Related to issue #2813.

--- src/redis.h
+++ src/redis.h
@@ -281,8 +281,8 @@ typedef long long mstime_t; /* millisecond time type. */
 #define REDIS_REPL_SEND_CAPA 8 /* Send REPLCONF capa */
 #define REDIS_REPL_RECEIVE_CAPA 9 /* Wait for REPLCONF reply */
 #define REDIS_REPL_SEND_PSYNC 10 /* Send PSYNC */
-/* --- End of handshake states --- */
 #define REDIS_REPL_RECEIVE_PSYNC 11 /* Wait for PSYNC reply */
+/* --- End of handshake states --- */
 #define REDIS_REPL_TRANSFER 12 /* Receiving .rdb from master */
 #define REDIS_REPL_CONNECTED 13 /* Connected to master */
 
