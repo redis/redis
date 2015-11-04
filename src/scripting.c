35afefc7cbdb903fad41ce24908dfc849b6c2f5d
Jan-Erik Rediger <janerik@fnordig.de>
2015-11-04 11:48:14 +0100

    Remove printf

--- src/scripting.c
+++ src/scripting.c
@@ -1199,7 +1199,6 @@ void evalGenericCommand(client *c, int evalsha) {
             rewriteClientCommandArgument(c,0,
                 resetRefCount(createStringObject("EVAL",4)));
             rewriteClientCommandArgument(c,1,script);
-            printf("forceCommandPropagation\n");
             forceCommandPropagation(c,PROPAGATE_REPL|PROPAGATE_AOF);
         }
     }
