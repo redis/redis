111d24f2465a41ddc100a11549dab3f0cd5ddc1b
antirez <antirez@gmail.com>
2015-11-09 11:10:53 +0100

    Fix error reply in subscribed Pub/Sub mode.
    PING is now a valid command to issue in this context.

--- src/server.c
+++ src/server.c
@@ -2414,7 +2414,7 @@ int processCommand(client *c) {
         c->cmd->proc != unsubscribeCommand &&
         c->cmd->proc != psubscribeCommand &&
         c->cmd->proc != punsubscribeCommand) {
-        addReplyError(c,"only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context");
+        addReplyError(c,"only (P)SUBSCRIBE / (P)UNSUBSCRIBE / PING / QUIT allowed in this context");
         return C_OK;
     }
 
