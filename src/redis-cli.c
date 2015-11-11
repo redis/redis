129cf33edecb3a23151dcc0c6eb7c983458846bd
antirez <antirez@gmail.com>
2015-11-11 23:29:43 +0100

    Lua debugger: redis-cli, mark end of debugging session.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -380,6 +380,7 @@ static int cliConnect(int force) {
             /* Disconnection from the server signals end of EVAL
              * debugging session. */
             if (config.eval_ldb) {
+                printf("\n(Lua debugging session terminated)\n\n");
                 config.eval_ldb = 0;
                 config.output = OUTPUT_STANDARD;
                 cliRefreshPrompt();
