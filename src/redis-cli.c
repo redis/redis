85faf61b64f753bd6dba7101fe42f00e8f796b02
antirez <antirez@gmail.com>
2015-11-12 12:59:19 +0100

    Lua debugger: inform user changes are rolled back.
    When redis-cli will support the synchronous mode, this will not be
    printed.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1116,7 +1116,7 @@ static void repl(void) {
                     if (config.eval_ldb_end) {
                         config.eval_ldb_end = 0;
                         cliReadReply(0);
-                        printf("\n(Lua debugging session ended)\n\n");
+                        printf("\n(Lua debugging session ended. Dataset changes rolled back)\n\n");
                     }
 
                     elapsed = mstime()-start_time;
