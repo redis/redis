8a0258aa4374212d8bf7004fa9022f821f6c93ed
antirez <antirez@gmail.com>
2015-11-13 09:32:07 +0100

    AOF: rewriting child killed by SIGUSR1 is not an error.

--- src/aof.c
+++ src/aof.c
@@ -1455,8 +1455,10 @@ void backgroundRewriteDoneHandler(int exitcode, int bysignal) {
         serverLog(LL_VERBOSE,
             "Background AOF rewrite signal handler took %lldus", ustime()-now);
     } else if (!bysignal && exitcode != 0) {
-        server.aof_lastbgrewrite_status = C_ERR;
-
+        /* SIGUSR1 is whitelisted, so we have a way to kill a child without
+         * tirggering an error conditon. */
+        if (bysignal != SIGUSR1)
+            server.aof_lastbgrewrite_status = C_ERR;
         serverLog(LL_WARNING,
             "Background AOF rewrite terminated with error");
     } else {
