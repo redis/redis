34aadf79c333527da4a188bc3470ce6cacd7a13b
antirez <antirez@gmail.com>
2015-11-14 22:18:02 +0100

    Lua debugging: fix error message for SCRIPT DEBUG.
    "async" -> "sync".
    
    Thanks to Itamar Haber for reporting.

--- src/scripting.c
+++ src/scripting.c
@@ -1474,7 +1474,7 @@ void scriptCommand(client *c) {
             addReply(c,shared.ok);
             c->flags |= CLIENT_LUA_DEBUG_SYNC;
         } else {
-            addReplyError(c,"Use SCRIPT DEBUG yes/async/no");
+            addReplyError(c,"Use SCRIPT DEBUG yes/sync/no");
         }
     } else {
         addReplyError(c, "Unknown SCRIPT subcommand or wrong # of args.");
