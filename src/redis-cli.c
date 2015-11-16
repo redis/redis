6de2306add95460a488d76e57b5cf2b5db17d5da
antirez <antirez@gmail.com>
2015-11-16 12:07:08 +0100

    Lua debugger: redis-cli error when --ldb is without --eval.
    Otherwise the result is a messed CLI without prompt.
    Thanks to Itamar Haber for reporting this issue.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -917,6 +917,13 @@ static int parseOptions(int argc, char **argv) {
             }
         }
     }
+
+    /* --ldb requires --eval. */
+    if (config.eval_ldb && config.eval == NULL) {
+        fprintf(stderr,"Options --ldb and --ldb-sync-mode require --eval.\n");
+        fprintf(stderr,"Try %s --help for more information.\n", argv[0]);
+        exit(1);
+    }
     return i;
 }
 
