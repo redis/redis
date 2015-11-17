6604e04260b1b6b17afcec9e54d20cda041751b1
antirez <antirez@gmail.com>
2015-11-17 13:15:48 +0100

    Lua debugger: redis-cli: allow restart after end of session.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1107,7 +1107,9 @@ static void repl(void) {
                 {
                     exit(0);
                 } else if (strcasecmp(argv[0],"restart") == 0) {
-                    if (config.eval_ldb) {
+                    if (config.eval) {
+                        config.eval_ldb = 1;
+                        config.output = OUTPUT_RAW;
                         return; /* Return to evalMode to restart the session. */
                     } else {
                         printf("Use 'restart' only in Lua debugging mode.");
