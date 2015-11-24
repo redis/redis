607d6f928c1fc292bfe25b129031bc674d7e535b
Enrico Giordani <enrico.giordani@gmail.com>
2015-11-24 16:01:28 -0400

    [Setup] Updated the command to push the chocolatey package.
    [Setup] Updated the help text to set the chocolatey apikey.
    [Setup] Updated the description text for nuget and chocolatey.

--- msvs/setups/PushPackages.ps1
+++ msvs/setups/PushPackages.ps1
@@ -5,7 +5,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 Write-Host "Ensure that the api keys have been set for chocolatey and nuget" -foregroundcolor red -backgroundcolor yellow
-Write-Host "  NuGet SetApiKey <your key here> -source http://chocolatey.org/" -foregroundcolor red -backgroundcolor yellow
+Write-Host "  choco apikey -k <your key here> -s https://chocolatey.org/" -foregroundcolor red -backgroundcolor yellow
 Write-Host "  NuGet SetApiKey <your key here>" -foregroundcolor red -backgroundcolor yellow
 
 $CurDir = split-path -parent $MyInvocation.MyCommand.Definition
@@ -15,7 +15,7 @@ $ChocolateyDir = $PackagesDir + "\chocolatey"
 $NugetDir = $PackagesDir + "\nuget"
 
 Set-Location $ChocolateyDir
-$ChocolateyCommand = "chocolatey push Redis-64." + $Version + ".nupkg"
+$ChocolateyCommand = "chocolatey push Redis-64." + $Version + ".nupkg -s https://chocolatey.org/"
 invoke-expression $ChocolateyCommand
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Chocolatey package pushed successfully." -foregroundcolor black -backgroundcolor green
