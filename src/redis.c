3626699f1fd39b97f68858f9ca2d0bf0999aae53
antirez <antirez@gmail.com>
2015-11-27 16:05:52 +0100

    Handle wait3() errors.
    My guess was that wait3() with WNOHANG could never return -1 and an
    error. However issue #2897 may possibly indicate that this could happen
    under non clear conditions. While we try to understand this better,
    better to handle a return value of -1 explicitly, otherwise in the
    case a BGREWRITE is in progress but wait3() returns -1, the effect is to
    match the first branch of the if/else block since server.rdb_child_pid
    is -1, and call backgroundSaveDoneHandler() without a good reason, that
    will, in turn, crash the Redis server with an assertion.

--- src/redis.c
+++ src/redis.c
@@ -1172,7 +1172,13 @@ int serverCron(struct aeEventLoop *eventLoop, long long id, void *clientData) {
 
             if (WIFSIGNALED(statloc)) bysignal = WTERMSIG(statloc);
 
-            if (pid == server.rdb_child_pid) {
+            if (pid == -1) {
+                redisLog(LOG_WARNING,"wait3() returned an error: %s. "
+                    "rdb_child_pid = %d, aof_child_pid = %d",
+                    strerror(errno),
+                    (int) server.rdb_child_pid,
+                    (int) server.aof_child_pid);
+            } else if (pid == server.rdb_child_pid) {
                 backgroundSaveDoneHandler(exitcode,bysignal);
             } else if (pid == server.aof_child_pid) {
                 backgroundRewriteDoneHandler(exitcode,bysignal);
