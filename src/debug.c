0bc1993879c696ceef365621122d159fe8fc8509
antirez <antirez@gmail.com>
2015-11-28 09:05:41 +0100

    fix sprintf and snprintf format string
    There are some cases of printing unsigned integer with %d conversion
    specificator and vice versa (signed integer with %u specificator).
    
    Patch by Sergey Polovko. Backported to Redis from Disque.

--- src/debug.c
+++ src/debug.c
@@ -415,7 +415,7 @@ void _redisAssertPrintClientInfo(redisClient *c) {
         if (c->argv[j]->type == REDIS_STRING && sdsEncodedObject(c->argv[j])) {
             arg = (char*) c->argv[j]->ptr;
         } else {
-            snprintf(buf,sizeof(buf),"Object type: %d, encoding: %d",
+            snprintf(buf,sizeof(buf),"Object type: %u, encoding: %u",
                 c->argv[j]->type, c->argv[j]->encoding);
             arg = buf;
         }
