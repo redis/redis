d5b55bdf135d4c29232d7095c6561d2551377b5a
antirez <antirez@gmail.com>
2015-12-15 18:00:29 +0100

    Log address causing SIGSEGV.

--- src/debug.c
+++ src/debug.c
@@ -914,6 +914,10 @@ void sigsegvHandler(int sig, siginfo_t *info, void *secret) {
     bugReportStart();
     serverLog(LL_WARNING,
         "    Redis %s crashed by signal: %d", REDIS_VERSION, sig);
+    if (sig == SIGSEGV) {
+        serverLog(LL_WARNING,
+        "    SIGSEGV caused by address: %p", (void*)info->si_addr);
+    }
     serverLog(LL_WARNING,
         "    Failed assertion: %s (%s:%d)", server.assert_failed,
                         server.assert_file, server.assert_line);
