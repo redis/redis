2f3f3fd5e0cb19ea2df5712198dc40f093bd1255
Paul Kulchenko <paul@kulchenko.com>
2015-12-15 18:13:09 -0800

    Update global protection error message to fix a typo.

--- src/scripting.c
+++ src/scripting.c
@@ -865,7 +865,7 @@ void scriptingEnableGlobalsProtection(lua_State *lua) {
     s[j++]="end\n";
     s[j++]="mt.__index = function (t, n)\n";
     s[j++]="  if dbg.getinfo(2) and dbg.getinfo(2, \"S\").what ~= \"C\" then\n";
-    s[j++]="    error(\"Script attempted to access unexisting global variable '\"..tostring(n)..\"'\", 2)\n";
+    s[j++]="    error(\"Script attempted to access nonexistent global variable '\"..tostring(n)..\"'\", 2)\n";
     s[j++]="  end\n";
     s[j++]="  return rawget(t, n)\n";
     s[j++]="end\n";
