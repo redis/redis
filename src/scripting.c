db1df45494330a0e63a5a14255ac8e2a503e46ea
Paul Kulchenko <paul@kulchenko.com>
2015-12-15 20:24:41 -0800

    Update pretty printing in debugging to generate valid Lua code for userdata-like types.

--- src/scripting.c
+++ src/scripting.c
@@ -1916,11 +1916,11 @@ sds ldbCatStackValue(sds s, lua_State *lua, int idx) {
         else if (t == LUA_TUSERDATA) typename = "userdata";
         else if (t == LUA_TTHREAD) typename = "thread";
         else if (t == LUA_TLIGHTUSERDATA) typename = "light-userdata";
-        s = sdscatprintf(s,"%s@%p",typename,p);
+        s = sdscatprintf(s,"\"%s@%p\"",typename,p);
         }
         break;
     default:
-        s = sdscat(s,"<unknown-lua-type>");
+        s = sdscat(s,"\"<unknown-lua-type>\"");
         break;
     }
     return s;
