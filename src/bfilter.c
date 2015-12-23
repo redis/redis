71f98591c91d98aa772c45753055c32f5d42438d
tianzhongbo <tianzhongbo@douban.com>
2015-12-23 17:41:29 +0800

    use crc64 and murmur2

--- src/bfilter.c
+++ src/bfilter.c
@@ -1,22 +1,18 @@
 #include "server.h"
-#include "murmur3.h"
-
-unsigned int murmurHash(char* key) {
-    unsigned int hash = 0;
-    MurmurHash3_x86_32(key,sdslen(key),0,&hash);
-    return hash; 
+#include "crc64.h"
+extern uint64_t MurmurHash64A (const void * key, int len, unsigned int seed);
+static uint64_t crc64_hash(const sds key) {
+    return crc64(0, (const unsigned char*)key, sdslen(key));
 }
 
-unsigned int SuperFastHash(const char* data, int len);
-
-unsigned int superfastHash(char* key) {
-    return SuperFastHash(key,sdslen(key));
+static uint64_t murmurHash(const sds key) {
+    return MurmurHash64A(key, sdslen(key), 0xadc83b19ULL);
 }
 
 typedef struct {
-    unsigned int magic;
-    long m;
-    int k;
+    uint32_t magic;
+    uint64_t m;
+    uint32_t k;
 } __attribute__((packed)) bfHeader;
 
 void bfcreateCommand(client *c){
@@ -25,13 +21,21 @@ void bfcreateCommand(client *c){
     int byte;
     bfHeader* header;
 
-    if ((C_OK != getLongFromObjectOrReply(c, c->argv[2],&m,
-                    "filter bit is not an positive integer or out of rage")) || (m < 0))
+    if (C_OK != getLongFromObjectOrReply(c, c->argv[2],&m,
+                    "filter bits is not an positive integer or out of rage"))
         return;
+    if (m <= 0) {
+        addReplyError(c,"filter bits is not an positive integer");
+        return;
+    }
 
-    if ((C_OK != getLongFromObjectOrReply(c, c->argv[3],&k,
-                    "hash times is not an positive integer or out of rage")) || (k < 0))
+    if (C_OK != getLongFromObjectOrReply(c, c->argv[3],&k,
+                    "hash times is not an positive integer or out of rage"))
         return;
+    if (k <= 0) {
+        addReplyError(c,"hash times is not an positive integer");
+        return;
+    }
 
     byte = (m >> 3) + sizeof(bfHeader) + 1;
     if (byte > 512*1024*1024) {
@@ -50,8 +54,8 @@ void bfcreateCommand(client *c){
 
     header = (bfHeader*) o->ptr;
     header->magic=0xDEADBEEF;
-    header->m=m;
-    header->k=(int)k;
+    header->m=(uint64_t)m;
+    header->k=(uint32_t)k;
 
     signalModifiedKey(c->db,c->argv[1]);
     server.dirty++;
@@ -62,7 +66,8 @@ void bfaddCommand(client *c) {
     robj *o;
     char *err = "invalid filter format", *buf;
     size_t len, byte;
-    int i, j;
+    int i;
+    uint32_t j;
     bfHeader *header;
 
     if (c->argc > 2002) {
@@ -86,35 +91,35 @@ void bfaddCommand(client *c) {
     header = (bfHeader*)o->ptr;
     byte = (header->m >> 3) + sizeof(bfHeader) + 1;
     buf = (char*)(header + 1);
-    if ((len != byte) || (header->magic != 0xDEADBEEF) 
-            || (header->m < 0) || (header->k < 0)) {
+    if ((len != byte) || (header->magic != 0xDEADBEEF)) {
         addReplyError(c,err);
         return;
     }
 
 
     for (i = 2; i < c->argc; i++) {
-        unsigned int hash1 = superfastHash(c->argv[i]->ptr),
+        uint64_t hash1 = crc64_hash(c->argv[i]->ptr),
             hash2 = murmurHash(c->argv[i]->ptr);
         for (j = 0; j < header->k; j++) {
-            long bit = (hash1 + j * hash2) % header->m;
-            long byteoffset = bit >> 3;
-            long bitoffset = 7 - (bit & 0x7);
-            buf[byteoffset] |= (1 << bitoffset);             
+            uint64_t bit = (hash1 + j * hash2) % header->m,
+                     byteoffset = bit >> 3,
+                     bitoffset = 7 - (bit & 0x7);
+            buf[byteoffset] |= (1 << bitoffset);
         }
     }
 
     signalModifiedKey(c->db,c->argv[1]);
     server.dirty++;
     addReply(c,shared.ok);
-} 
+}
 
 void bfmatchCommand(client *c) {
     robj *o;
     char *err = "invalid filter format", *buf;
     size_t len, byte;
     bfHeader *header;
-    int i,j;
+    int i;
+    uint32_t j;
 
     if (c->argc > 2002) {
         addReplyError(c,"too many arguments");
@@ -137,21 +142,21 @@ void bfmatchCommand(client *c) {
     header = (bfHeader*)o->ptr;
     byte = (header->m >> 3) + sizeof(bfHeader) + 1;
     buf = (char*)(header + 1);
-    if ((len != byte) || (header->magic != 0xDEADBEEF) 
-            || (header->m < 0) || (header->k < 0)) {
+    if ((len != byte) || (header->magic != 0xDEADBEEF)) {
         addReplyError(c,err);
         return;
     }
 
     addReplyMultiBulkLen(c,c->argc-2);
     for (i = 2; i < c->argc; i++) {
-        unsigned int hash1 = superfastHash(c->argv[i]->ptr),
+        unsigned int hash1 = crc64_hash(c->argv[i]->ptr),
             hash2 = murmurHash(c->argv[i]->ptr);
         for (j = 0; j < header->k; j++) {
-            long bit = (hash1 + j * hash2) % header->m;
-            long byteoffset = bit >> 3;
-            long bitoffset = 7 - (bit & 0x7);
-            if (!(buf[byteoffset] & (1 << bitoffset))) break;
+            uint64_t bit = (hash1 + j * hash2) % header->m,
+                     byteoffset = bit >> 3,
+                     bitoffset = 7 - (bit & 0x7);
+            if (!(buf[byteoffset] & (1 << bitoffset)))
+                break;
         }
 
         if (j < header->k) {
