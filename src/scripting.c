f43c794b0b43b9134385a487e96bcd791bb0472a
antirez <antirez@gmail.com>
2016-01-08 15:42:43 +0100

    Scripting: handle trailing comments.
    This fix, provided by Paul Kulchenko (@pkulchenko), allows the Lua
    scripting engine to evaluate statements with a trailing comment like the
    following one:
    
        EVAL "print() --comment" 0
    
    Lua can't parse the above if the string does not end with a newline, so
    now a final newline is always added automatically. This does not change
    the SHA1 of scripts since the SHA1 is computed on the body we pass to
    EVAL, without the other code we add to register the function.
    
    Close #2951.

--- src/scripting.c
+++ src/scripting.c
@@ -1145,7 +1145,7 @@ int luaCreateFunction(client *c, lua_State *lua, char *funcname, robj *body) {
     funcdef = sdscatlen(funcdef,funcname,42);
     funcdef = sdscatlen(funcdef,"() ",3);
     funcdef = sdscatlen(funcdef,body->ptr,sdslen(body->ptr));
-    funcdef = sdscatlen(funcdef," end",4);
+    funcdef = sdscatlen(funcdef,"\nend",4);
 
     if (luaL_loadbuffer(lua,funcdef,sdslen(funcdef),"@user_script")) {
         addReplyErrorFormat(c,"Error compiling script (new function): %s\n",
