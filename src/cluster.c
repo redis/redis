8ef716d19ae0859e02199d24d5f78bb2711861cf
antirez <antirez@gmail.com>
2016-01-11 15:01:14 +0100

    Cluster: CLUSTER BUMPEPOCH introduced to help redis-trib fix.
    Sometimes during "fixes" we have to setup a new configuration and assign
    slots to nodes. With BUMPEPOCH we can make sure the new configuration of
    the node will win if there are conflicting configurations (for example
    another node is *also* claiming the same slot because the cluster is
    totally messed up).

--- src/cluster.c
+++ src/cluster.c
@@ -4027,6 +4027,13 @@ void clusterCommand(redisClient *c) {
             }
             clusterDelSlot(slot);
             clusterAddSlot(n,slot);
+        } else if (!strcasecmp(c->argv[3]->ptr,"bumpepoch") && c->argc == 2) {
+            /* CLUSTER BUMPEPOCH */
+            int retval = clusterBumpConfigEpochWithoutConsensus();
+            sds reply = sdscatprintf(sdsempty(),"%s %llu\r\n",
+                    (retval == C_OK) ? "BUMPED" : "STILL",
+                    (unsigned long long) myself->configEpoch);
+            addReplySds(c,reply);
         } else {
             addReplyError(c,
                 "Invalid CLUSTER SETSLOT action or number of arguments");
