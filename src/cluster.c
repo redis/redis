524be1e4656f53c347367fb5c7b1ac7e80b890e9
antirez <antirez@gmail.com>
2016-01-11 15:00:45 +0100

    Cluster: don't allow CLUSTER SETSLOT with slaves.

--- src/cluster.c
+++ src/cluster.c
@@ -3940,6 +3940,11 @@ void clusterCommand(client *c) {
         int slot;
         clusterNode *n;
 
+        if (nodeIsSlave(myself)) {
+            addReplyError(c,"Please use SETSLOT only with masters.");
+            return;
+        }
+
         if ((slot = getSlotOrReply(c,c->argv[2])) == -1) return;
 
         if (!strcasecmp(c->argv[3]->ptr,"migrating") && c->argc == 5) {
