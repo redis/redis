6d567d0c6fe129ce73562185329a8b2a544e848b
Jan-Erik Rediger <janerik@fnordig.de>
2016-01-13 10:22:29 -0700

    Fix nanosecond conversion
    1 microsecond = 1000 nanoseconds
    1e3 = 1000
    10e3 = 10000

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -2161,7 +2161,7 @@ static void intrinsicLatencyMode(void) {
         }
 
         double avg_us = (double)run_time/runs;
-        double avg_ns = avg_us * 10e3;
+        double avg_ns = avg_us * 1e3;
         if (force_cancel_loop || end > test_end) {
             printf("\n%lld total runs "
                 "(avg latency: "
