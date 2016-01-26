fe44a7cb609e1161a47494f3ab71d9e45973fb51
antirez <antirez@gmail.com>
2016-01-26 14:21:18 +0100

    Cluster: mismatch sender ID log put back at DEBUG level.

--- src/cluster.c
+++ src/cluster.c
@@ -1716,7 +1716,7 @@ int clusterProcessPacket(clusterLink *link) {
                 /* If the reply has a non matching node ID we
                  * disconnect this node and set it as not having an associated
                  * address. */
-                serverLog(LL_WARNING,"PONG contains mismatching sender ID. About node %.40s added %d ms ago, having flags %d",
+                serverLog(LL_DEBUG,"PONG contains mismatching sender ID. About node %.40s added %d ms ago, having flags %d",
                     link->node->name,
                     (int)(mstime()-(link->node->ctime)),
                     link->node->flags);
