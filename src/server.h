cdbe8a6ae1f5adac4513c53173124cd0e7cec8d7
antirez <antirez@gmail.com>
2016-01-29 12:08:10 +0100

    Typo ASII -> ASCII fixed in comment.

--- src/server.h
+++ src/server.h
@@ -61,7 +61,7 @@ typedef long long mstime_t; /* millisecond time type. */
 #include "version.h" /* Version macro */
 #include "util.h"    /* Misc functions useful in many places */
 #include "latency.h" /* Latency monitor API */
-#include "sparkline.h" /* ASII graphs API */
+#include "sparkline.h" /* ASCII graphs API */
 #include "quicklist.h"
 
 /* Following includes allow test functions to be called from Redis main() */
