cd9f7c6976f81c0e9ffae9e34b31eda19384a7dd
Itamar Haber <itamar@redislabs.com>
2016-02-03 11:04:09 -0800

    Fixes a typo

--- src/server.c
+++ src/server.c
@@ -3820,7 +3820,7 @@ int redisSupervisedUpstart(void) {
         return 0;
     }
 
-    serverLog(LL_NOTICE, "supervised by upstart, will stop to signal readyness");
+    serverLog(LL_NOTICE, "supervised by upstart, will stop to signal readiness");
     raise(SIGSTOP);
     unsetenv("UPSTART_JOB");
     return 1;
@@ -3844,7 +3844,7 @@ int redisSupervisedSystemd(void) {
         return 0;
     }
 
-    serverLog(LL_NOTICE, "supervised by systemd, will signal readyness");
+    serverLog(LL_NOTICE, "supervised by systemd, will signal readiness");
     if ((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
         serverLog(LL_WARNING,
                 "Can't connect to systemd socket %s", notify_socket);
