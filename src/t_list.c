d695f3e8994e37a6c96362daa29d839b72c96bf0
antirez <antirez@gmail.com>
2016-02-05 15:57:42 +0100

    Fix NOTIFY macro names after cherry pick of fix.

--- src/t_list.c
+++ src/t_list.c
@@ -680,12 +680,12 @@ void lremCommand(redisClient *c) {
 
     if (removed) {
         signalModifiedKey(c->db,c->argv[1]);
-        notifyKeyspaceEvent(NOTIFY_GENERIC,"lrem",c->argv[1],c->db->id);
+        notifyKeyspaceEvent(REDIS_NOTIFY_GENERIC,"lrem",c->argv[1],c->db->id);
     }
 
     if (listTypeLength(subject) == 0) {
         dbDelete(c->db,c->argv[1]);
-        notifyKeyspaceEvent(NOTIFY_GENERIC,"del",c->argv[1],c->db->id);
+        notifyKeyspaceEvent(REDIS_NOTIFY_GENERIC,"del",c->argv[1],c->db->id);
     }
 
     addReplyLongLong(c,removed);
