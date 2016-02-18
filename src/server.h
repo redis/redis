cf42c48adcea05c1bd4b939fcd36a01f23ec6303
antirez <antirez@gmail.com>
2016-02-18 22:08:47 +0100

    addReplyHumanLongDouble() API added.
    Send a long double or double as a bulk reply, in a human friendly
    format.

--- src/server.h
+++ src/server.h
@@ -1123,6 +1123,7 @@ void addReplyBulkSds(client *c, sds s);
 void addReplyError(client *c, const char *err);
 void addReplyStatus(client *c, const char *status);
 void addReplyDouble(client *c, double d);
+void addReplyHumanLongDouble(client *c, long double d);
 void addReplyLongLong(client *c, long long ll);
 void addReplyMultiBulkLen(client *c, long length);
 void copyClientOutputBuffer(client *dst, client *src);
