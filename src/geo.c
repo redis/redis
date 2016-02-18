41030ae2dee2d88451cb0bfe6b6a1b72ea0d84eb
Itamar Haber <itamar@redislabs.com>
2016-02-18 15:01:34 -0800

    Fixes a typo in a comment

--- src/geo.c
+++ src/geo.c
@@ -163,7 +163,7 @@ double extractDistanceOrReply(client *c, robj **argv,
     return distance * to_meters;
 }
 
-/* The defailt addReplyDouble has too much accuracy.  We use this
+/* The default addReplyDouble has too much accuracy.  We use this
  * for returning location distances. "5.2145 meters away" is nicer
  * than "5.2144992818115 meters away." We provide 4 digits after the dot
  * so that the returned value is decently accurate even when the unit is
