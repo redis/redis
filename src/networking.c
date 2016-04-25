d6864d299904fe0e53797e4c8bd3b29a32869fd0
Oran Agra <oran@monfort.co.il>
2016-04-25 14:18:40 +0300

    additional fix to issue #2948

--- src/networking.c
+++ src/networking.c
@@ -1147,6 +1147,9 @@ void processInputBuffer(redisClient *c) {
             /* Only reset the client when the command was executed. */
             if (processCommand(c) == REDIS_OK)
                 resetClient(c);
+            /* freeMemoryIfNeeded may flush slave output buffers. This may result
+             * into a slave, that may be the active client, to be freed. */
+            if (server.current_client == NULL) break;
         }
     }
 }
