7ba2cf5f8bea9cd125e773de147e3ea9b19ee2b0
antirez <antirez@gmail.com>
2016-05-04 08:59:56 +0200

    Reply with error on negative geo radius.
    Thanks to @tidwall for reporting.
    Close #3194.

--- src/geo.c
+++ src/geo.c
@@ -157,7 +157,10 @@ double extractDistanceOrReply(client *c, robj **argv,
     }
 
     double to_meters = extractUnitOrReply(c,argv[1]);
-    if (to_meters < 0) return -1;
+    if (to_meters < 0) {
+        addReplyError(c,"radius cannot be negative");
+        return -1;
+    }
 
     if (conversion) *conversion = to_meters;
     return distance * to_meters;
