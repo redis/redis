995b9ffe07ee497d0a8c7590e393a2ad2d8365da
antirez <antirez@gmail.com>
2016-05-04 15:45:38 +0200

    Allow CONFIG GET during loading.
    Thanks to @oranagra for the idea of allowing CONFIG GET during loading.

--- src/config.c
+++ src/config.c
@@ -1923,6 +1923,12 @@ int rewriteConfig(char *path) {
  *----------------------------------------------------------------------------*/
 
 void configCommand(client *c) {
+    /* Only allow CONFIG GET while loading. */
+    if (server.loading && strcasecmp(c->argv[1]->ptr,"get")) {
+        addReplyError(c,"Only CONFIG GET is allowed during loading");
+        return;
+    }
+
     if (!strcasecmp(c->argv[1]->ptr,"set")) {
         if (c->argc != 4) goto badarity;
         configSetCommand(c);
