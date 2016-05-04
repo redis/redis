5f977c3f8b5425ba57afaca728482c944b1986f8
antirez <antirez@gmail.com>
2016-05-04 16:12:37 +0200

    Modules: commandFlagsFromString() top comment back to 80 cols max.

--- src/module.c
+++ src/module.c
@@ -376,33 +376,38 @@ int commandFlagsFromString(char *s) {
  * be passed as a C string compoesd of space separated words, like for
  * example "write deny-oom". The set of flags are:
  *
- * * **"write"**:     The command may modify the data set (it may also read from it).
+ * * **"write"**:     The command may modify the data set (it may also read
+ *                    from it).
  * * **"readonly"**:  The command returns data from keys but never writes.
- * * **"admin"**:     The command is an administrative command (may change replication
- *              or perform similar tasks).
- * * **"deny-oom"**:  The command may use additional memory and should be denied during
- *              out of memory conditions.
+ * * **"admin"**:     The command is an administrative command (may change
+ *                    replication or perform similar tasks).
+ * * **"deny-oom"**:  The command may use additional memory and should be
+ *                    denied during out of memory conditions.
  * * **"deny-script"**:   Don't allow this command in Lua scripts.
- * * **"allow-loading"**: Allow this command while the server is loading data. Only
- *                  commands not interacting with the data set should be allowed
- *                  to run in this mode. If not sure don't use this flag.
+ * * **"allow-loading"**: Allow this command while the server is loading data.
+ *                        Only commands not interacting with the data set
+ *                        should be allowed to run in this mode. If not sure
+ *                        don't use this flag.
  * * **"pubsub"**:    The command publishes things on Pub/Sub channels.
- * * **"random"**:    The command may have different outputs even starting from the
- *              same input arguments and key values.
- * * **"allow-stale"**: The command is allowed to run on slaves that don't serve stale
- *              data. Don't use if you don't know what this means.
- * * **"no-monitor"**: Don't propoagate the command on monitor. Use this if the command
- *               has sensible data among the arguments.
- * * **"fast"**:      The command time complexity is not greater than O(log(N)) where
- *              N is the size of the collection or anything else representing
- *              the normal scalability issue with the command.
- * * **"getkeys-api"**: The command implements the interface to return the arguments
- *                that are keys. Used when start/stop/step is not enough because
- *                of the command syntax.
- * * **"no-cluster"**: The command should not register in Redis Cluster since is not
- *               designed to work with it because, for example, is unable to
- *               report the position of the keys, programmatically creates key
- *               names, or any other reason.
+ * * **"random"**:    The command may have different outputs even starting
+ *                    from the same input arguments and key values.
+ * * **"allow-stale"**: The command is allowed to run on slaves that don't
+ *                      serve stale data. Don't use if you don't know what
+ *                      this means.
+ * * **"no-monitor"**: Don't propoagate the command on monitor. Use this if
+ *                     the command has sensible data among the arguments.
+ * * **"fast"**:      The command time complexity is not greater
+ *                    than O(log(N)) where N is the size of the collection or
+ *                    anything else representing the normal scalability
+ *                    issue with the command.
+ * * **"getkeys-api"**: The command implements the interface to return
+ *                      the arguments that are keys. Used when start/stop/step
+ *                      is not enough because of the command syntax.
+ * * **"no-cluster"**: The command should not register in Redis Cluster
+ *                     since is not designed to work with it because, for
+ *                     example, is unable to report the position of the
+ *                     keys, programmatically creates key names, or any
+ *                     other reason.
  */
 int RM_CreateCommand(RedisModuleCtx *ctx, const char *name, RedisModuleCmdFunc cmdfunc, const char *strflags, int firstkey, int lastkey, int keystep) {
     int flags = strflags ? commandFlagsFromString((char*)strflags) : 0;
