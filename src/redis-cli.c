33c8a59e97aba09b0cf77546c60eb983068fbac0
antirez <antirez@gmail.com>
2016-05-05 13:57:57 +0200

    redis-cli: don't free historyfile, is used later.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -986,6 +986,7 @@ static void repl(void) {
         if (line[0] != '\0') {
             argv = sdssplitargs(line,&argc);
             if (history) linenoiseHistoryAdd(line);
+            printf("HISTORY: %s\n", historyfile);
             if (historyfile) linenoiseHistorySave(historyfile);
 
             if (argv == NULL) {
