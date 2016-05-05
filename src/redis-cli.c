3907b059280898c6340b53982b3b65957535da7c
antirez <antirez@gmail.com>
2016-05-05 18:05:37 +0200

    redis-cli: remove debugging message.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1231,7 +1231,6 @@ static void repl(void) {
         if (line[0] != '\0') {
             argv = cliSplitArgs(line,&argc);
             if (history) linenoiseHistoryAdd(line);
-            printf("HISTORY: %s\n", historyfile);
             if (historyfile) linenoiseHistorySave(historyfile);
 
             if (argv == NULL) {
