54706a8a99e048cfe560c94acdaa1acc14d6b16d
charsyam <charsyam@naver.com>
2016-05-08 20:30:30 +0900

    allow writable command even aof_last_write_status == C_ERR

--- src/server.c
+++ src/server.c
@@ -2427,8 +2427,8 @@ int processCommand(client *c) {
 
     /* Don't accept write commands if there are problems persisting on disk
      * and if this is a master instance. */
-    if (((server.stop_writes_on_bgsave_err &&
-          server.saveparamslen > 0 &&
+    if (server.stop_writes_on_bgsave_err &&
+          ((server.saveparamslen > 0 &&
           server.lastbgsave_status == C_ERR) ||
           server.aof_last_write_status == C_ERR) &&
         server.masterhost == NULL &&
