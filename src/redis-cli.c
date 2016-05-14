586359d3e8c9a87a1a15c3d4071f88ae7dd7ccb3
Itamar Haber <itamar@redislabs.com>
2016-05-14 23:53:44 +0300

    Removes a printf that was forgotten

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -284,7 +284,6 @@ static void cliIntegrateHelp(void) {
                 break;
         }
         if (i != helpEntriesLen) continue;
-        printf("%s\n", cmdname);
 
         helpEntriesLen++;
         helpEntries = zrealloc(helpEntries,sizeof(helpEntry)*helpEntriesLen);
