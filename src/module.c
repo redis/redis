9a02dac2e8fa209f68b8e487313892cdd11c21a0
antirez <antirez@gmail.com>
2016-06-13 09:45:53 +0200

    Free module context after loading.
    Now that modules receive RedisModuleString objects on loading, they are
    allowed to call the String API, so the context must be released
    correctly.
    
    Related to #3293.

--- src/module.c
+++ src/module.c
@@ -2946,6 +2946,7 @@ int moduleLoad(const char *path, void **module_argv, int module_argc) {
     dictAdd(modules,ctx.module->name,ctx.module);
     ctx.module->handle = handle;
     serverLog(LL_NOTICE,"Module '%s' loaded from %s",ctx.module->name,path);
+    moduleFreeContext(&ctx);
     return C_OK;
 }
 
