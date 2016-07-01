1158386bb813c262f3cb08675040694740783ff7
sskorgal <santosh.korgal@huawei.com>
2016-07-01 10:42:22 +0530

    Fix for redis_cli printing default DB when select command fails.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -916,7 +916,7 @@ static int cliSendCommand(int argc, char **argv, int repeat) {
             return REDIS_ERR;
         } else {
             /* Store database number when SELECT was successfully executed. */
-            if (!strcasecmp(command,"select") && argc == 2) {
+            if (!strcasecmp(command,"select") && argc == 2 && config.last_cmd_type != REDIS_REPLY_ERROR) {
                 config.dbnum = atoi(argv[1]);
                 cliRefreshPrompt();
             } else if (!strcasecmp(command,"auth") && argc == 2) {
