41f300473a2b0e02eaeb06be765dd34485541853
antirez <antirez@gmail.com>
2016-07-01 11:59:25 +0200

    In Redis RDB check: log decompression errors.

--- src/rdb.c
+++ src/rdb.c
@@ -49,6 +49,7 @@
 
 extern int rdbCheckMode;
 void rdbCheckError(const char *fmt, ...);
+void rdbCheckSetError(const char *fmt, ...);
 
 void rdbCheckThenExit(char *reason, int where) {
      if (!rdbCheckMode) {
@@ -307,7 +308,10 @@ void *rdbLoadLzfStringObject(rio *rdb, int flags) {
 
     /* Load the compressed representation and uncompress it to target. */
     if (rioRead(rdb,c,clen) == 0) goto err;
-    if (lzf_decompress(c,clen,val,len) == 0) goto err;
+    if (lzf_decompress(c,clen,val,len) == 0) {
+        if (rdbCheckMode) rdbCheckSetError("Invalid LZF compressed string");
+        goto err;
+    }
     zfree(c);
 
     if (plain)
