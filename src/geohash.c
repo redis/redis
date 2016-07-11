92a04d3021eb93b5a43b5a18eb03fb9e0cf0b025
charsyam <charsyam@gmail.com>
2016-07-11 23:09:00 +0900

    fix compile warning in gcc 5.3.1

--- src/geohash.c
+++ src/geohash.c
@@ -211,7 +211,7 @@ int geohashDecodeAreaToLongLat(const GeoHashArea *area, double *xy) {
 }
 
 int geohashDecodeToLongLatType(const GeoHashBits hash, double *xy) {
-    GeoHashArea area = {{0}};
+    GeoHashArea area = {0};
     if (!xy || !geohashDecodeType(hash, &area))
         return 0;
     return geohashDecodeAreaToLongLat(&area, xy);
