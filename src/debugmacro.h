4bd72ab729135a4cde76bffe0fb119d9aeb72672
Guo Xiao <guoxiao08@gmail.com>
2016-08-04 15:09:42 +0800

    Use the standard predefined identifier __func__ (since C99)
    Fix warning: ISO C does not support '__FUNCTION__' predefined identifier
    [-Wpedantic]

--- src/debugmacro.h
+++ src/debugmacro.h
@@ -34,7 +34,7 @@
 #define D(...)                                                               \
     do {                                                                     \
         FILE *fp = fopen("/tmp/log.txt","a");                                \
-        fprintf(fp,"%s:%s:%d:\t", __FILE__, __FUNCTION__, __LINE__);         \
+        fprintf(fp,"%s:%s:%d:\t", __FILE__, __func__, __LINE__);             \
         fprintf(fp,__VA_ARGS__);                                             \
         fprintf(fp,"\n");                                                    \
         fclose(fp);                                                          \
