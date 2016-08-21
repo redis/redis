a6d0698b22e453221403e6aa5733c23897c20c69
oranagra <oran@redislabs.com>
2016-08-21 20:39:15 +0300

    fix tcp binding when IPv6 is unsupported

--- src/anet.c
+++ src/anet.c
@@ -486,7 +486,7 @@ static int _anetTcpServer(char *err, int port, char *bindaddr, int af, int backl
         goto end;
     }
     if (p == NULL) {
-        anetSetError(err, "unable to bind socket");
+        anetSetError(err, "unable to bind socket, errno: %d", errno);
         goto error;
     }
 
