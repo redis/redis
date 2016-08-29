c9b467f175f43749ccb67e5811bf81566fb4b512
Dennis <mrvon@qq.com>
2016-08-29 16:57:47 +0800

    fix typo

--- src/sds.c
+++ src/sds.c
@@ -1218,7 +1218,7 @@ int sdsTest(void) {
         sdsfree(x);
         x = sdsnew("aar");
         y = sdsnew("bar");
-        test_cond("sdscmp(bar,bar)", sdscmp(x,y) < 0)
+        test_cond("sdscmp(aar,bar)", sdscmp(x,y) < 0)
 
         sdsfree(y);
         sdsfree(x);
