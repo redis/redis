e1eccf9a6be88c93b71e28f24ad7e14bef434142
antirez <antirez@gmail.com>
2016-09-19 10:34:39 +0200

    zmalloc: Make fp var non local to fix build.

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -315,13 +315,14 @@ size_t zmalloc_get_smap_bytes_by_field(char *field, long pid) {
     char line[1024];
     size_t bytes = 0;
     int flen = strlen(field);
+    FILE *fp;
 
     if (pid == -1) {
-        FILE *fp = fopen("/proc/self/smaps","r");
+        fp = fopen("/proc/self/smaps","r");
     } else {
         char filename[128];
         snprintf(filename,sizeof(filename),"/proc/%ld/smaps",pid);
-        FILE *fp = fopen(filename,"r");
+        fp = fopen(filename,"r");
     }
 
     if (!fp) return 0;
