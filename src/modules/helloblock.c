a3b3ca7c2191cbe09750e2eb45b856b8cbbaa831
antirez <antirez@gmail.com>
2016-10-13 17:00:29 +0200

    Modules: use RedisModule_AbortBlock() in the example.

--- src/modules/helloblock.c
+++ src/modules/helloblock.c
@@ -99,7 +99,7 @@ int HelloBlock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int a
     targ[1] = (void*)(unsigned long) delay;
 
     if (pthread_create(&tid,NULL,HelloBlock_ThreadMain,targ) != 0) {
-        /* RedisModule_BlockedClientAbort(bc); */
+        RedisModule_AbortBlock(bc);
         return RedisModule_ReplyWithError(ctx,"-ERR Can't start thread");
     }
     return REDISMODULE_OK;
