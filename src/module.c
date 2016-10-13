553aa0e259a5b2c5f53dbc322a94b8f6930bc537
antirez <antirez@gmail.com>
2016-10-13 12:48:36 +0200

    module.c: trim comment to 80 cols.

--- src/module.c
+++ src/module.c
@@ -752,11 +752,11 @@ RedisModuleString *RM_CreateString(RedisModuleCtx *ctx, const char *ptr, size_t 
 }
 
 
-/* Create a new module string object from a printf format and arguments. 
- * The returned string must be freed with RedisModule_FreeString(), unless automatic 
- * memory is enabled.
+/* Create a new module string object from a printf format and arguments.
+ * The returned string must be freed with RedisModule_FreeString(), unless
+ * automatic memory is enabled.
  *
- * The string is created using the sds formatter function sdscatvprintf() */ 
+ * The string is created using the sds formatter function sdscatvprintf(). */
 RedisModuleString *RM_CreateStringPrintf(RedisModuleCtx *ctx, const char *fmt, ...) {
     sds s = sdsempty();
 
