4e5e366ed265f2571124edfa9c2f9eaa0d450c45
antirez <antirez@gmail.com>
2016-11-09 15:36:25 +0100

    PSYNC2: Wrap debugging code with if(0)

--- src/replication.c
+++ src/replication.c
@@ -296,7 +296,9 @@ void replicationFeedSlavesFromMasterStream(list *slaves, char *buf, size_t bufle
     listNode *ln;
     listIter li;
 
-    {
+    /* Debugging: this is handy to see the stream sent from master
+     * to slaves. Disabled with if(0). */
+    if (0) {
         printf("%zu:",buflen);
         for (size_t j = 0; j < buflen; j++) {
             printf("%c", isprint(buf[j]) ? buf[j] : '.');
