6eb720ff2d20d3e739d6214415ca832008ba9dfb
antirez <antirez@gmail.com>
2016-11-29 10:24:46 +0100

    PSYNC2: Minor memory leak reading -NOMASTERLINK master reply fixed.

--- src/replication.c
+++ src/replication.c
@@ -1539,6 +1539,7 @@ int slaveTryPartialResynchronization(int fd, int read_reply) {
         serverLog(LL_NOTICE,
             "Master is currently unable to PSYNC "
             "but should be in the future: %s", reply);
+        sdsfree(reply);
         return PSYNC_TRY_LATER;
     }
 
