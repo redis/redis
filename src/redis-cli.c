d048f9721ceb8a90618dff3461c0cc121e90b9ff
cbgbt <sean.kelly.2992@gmail.com>
2016-12-01 02:33:01 -0800

    cli: Only print elapsed time on OUTPUT_STANDARD

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1353,7 +1353,9 @@ static void repl(void) {
                     }
 
                     elapsed = mstime()-start_time;
-                    if (elapsed >= 500) {
+                    if (elapsed >= 500 &&
+                        config.output == OUTPUT_STANDARD)
+                    {
                         printf("(%.2fs)\n",(double)elapsed/1000);
                     }
                 }
