554286dbbb98d1392fa042fdf1042abeb2a61618
Chris Lamb <chris@chris-lamb.co.uk>
2016-12-01 12:10:14 +0000

    src/rdb.c: Correct "whenver" -> "whenever" typo.

--- src/rdb.c
+++ src/rdb.c
@@ -1939,7 +1939,7 @@ void bgsaveCommand(client *c) {
         } else {
             addReplyError(c,
                 "An AOF log rewriting in progress: can't BGSAVE right now. "
-                "Use BGSAVE SCHEDULE in order to schedule a BGSAVE whenver "
+                "Use BGSAVE SCHEDULE in order to schedule a BGSAVE whenever "
                 "possible.");
         }
     } else if (rdbSaveBackground(server.rdb_filename,NULL) == C_OK) {
