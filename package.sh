2194bff626dbec71986e6f3de74e56b0f48b4afa
wenchaomeng <oytmfc@gmail.com>
2017-02-09 18:35:40 +0800

    package dist clean

--- package.sh
+++ package.sh
@@ -1,13 +1,15 @@
 APP=xredis
-Version=0.0.1
+Version=0.0.2
 Package=$APP-$Version
+
+find . -name ctrip.h -exec sed -i "s/XREDIS_VERSION \"\(.*\)\"/XREDIS_VERSION \"$Version\"/" {} \;
 
 if [ -f $package ]; then
     echo remove $Package
     rm -rf $Package
 fi
 echo make clean
-make clean
+make distclean
 mkdir $Package
 ls -a | egrep -v "Debug|^\.|$Package" | xargs -J %  cp -r %  $Package
 echo create $Package.tar.gz...
