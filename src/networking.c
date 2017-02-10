d70ac1d105ddc72afc32a13a6f1690b47d452335
minghang.zmh <minghang.zmh@alibaba-inc.com>
2017-02-10 20:13:01 +0800

    fix server.stat_net_output_bytes calc bug

--- src/networking.c
+++ src/networking.c
@@ -949,11 +949,11 @@ int writeToClient(int fd, client *c, int handler_installed) {
          *
          * However if we are over the maxmemory limit we ignore that and
          * just deliver as much data as it is possible to deliver. */
-        server.stat_net_output_bytes += totwritten;
         if (totwritten > NET_MAX_WRITES_PER_EVENT &&
             (server.maxmemory == 0 ||
              zmalloc_used_memory() < server.maxmemory)) break;
     }
+    server.stat_net_output_bytes += totwritten;
     if (nwritten == -1) {
         if (errno == EAGAIN) {
             nwritten = 0;
