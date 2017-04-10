ffefc9f92d55a45439afe80d6a35615a7cc38758
antirez <antirez@gmail.com>
2017-04-10 09:33:21 +0200

    Fix modules blocking commands awake delay.
    If a thread unblocks a client blocked in a module command, by using the
    RedisMdoule_UnblockClient() API, the event loop may not be awaken until
    the next timeout of the multiplexing API or the next unrelated I/O
    operation on other clients. We actually want the client to be served
    ASAP, so a mechanism is needed in order for the unblocking API to inform
    Redis that there is a client to serve ASAP.
    
    This commit fixes the issue using the old trick of the pipe: when a
    client needs to be unblocked, a byte is written in a pipe. When we run
    the list of clients blocked in modules, we consume all the bytes
    written in the pipe. Writes and reads are performed inside the context
    of the mutex, so no race is possible in which we consume the bytes that
    are actually related to an awake request for a client that should still
    be put into the list of clients to unblock.
    
    It was verified that after the fix the server handles the blocked
    clients with the expected short delay.
    
    Thanks to @dvirsky for understanding there was such a problem and
    reporting it.

--- src/server.c
+++ src/server.c
@@ -1870,6 +1870,16 @@ void initServer(void) {
     if (server.sofd > 0 && aeCreateFileEvent(server.el,server.sofd,AE_READABLE,
         acceptUnixHandler,NULL) == AE_ERR) serverPanic("Unrecoverable error creating server.sofd file event.");
 
+
+    /* Register a readable event for the pipe used to awake the event loop
+     * when a blocked client in a module needs attention. */
+    if (aeCreateFileEvent(server.el, server.module_blocked_pipe[0], AE_READABLE,
+        moduleBlockedClientPipeReadable,NULL) == AE_ERR) {
+            serverPanic(
+                "Error registering the readable event for the module "
+                "blocked clients subsystem.");
+    }
+
     /* Open the AOF file if needed. */
     if (server.aof_state == AOF_ON) {
         server.aof_fd = open(server.aof_filename,
