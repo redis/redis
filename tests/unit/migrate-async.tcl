851c981a2f3467c36483f5066dfaf6884ba3a6e5
spinlock <wnzheng@gmail.com>
2017-05-07 21:50:26 +0800

    MigrateAsyncTest: unittest for RESTORE-ASYNC DELETE

--- tests/unit/migrate-async.tcl
+++ tests/unit/migrate-async.tcl
@@ -1,42 +1,41 @@
 start_server {tags {"migrate-async"}} {
     test {RESTORE-ASYNC-AUTH fails if there is no password configured server side} {
-        r restore-async-auth foo
-    } {RESTORE-ASYNC-ACK 1 *}
+        assert_match {RESTORE-ASYNC-ACK 1 *} [r restore-async-auth foo]
+    }
 }
 
 start_server {tags {"migrate-async"} overrides {requirepass foobar}} {
     test {RESTORE-ASYNC-AUTH fails when a wrong password is given} {
-        r restore-async-auth wrong_passwd
-    } {RESTORE-ASYNC-ACK 1 *}
+        assert_match {RESTORE-ASYNC-ACK 1 *} [r restore-async-auth wrong_passwd]
+    }
 }
 
 start_server {tags {"migrate-async"} overrides {requirepass foobar}} {
     test {RESTORE-ASYNC-SELECT fails when password is not given} {
         catch {r restore-async-select 1} err
-        set _ $err
-    } {NOAUTH*}
+        assert_match {NOAUTH*} $err
+    }
 
     test {RESTORE-ASYNC-AUTH succeeds when the right password is given} {
-        r restore-async-auth foobar
-    } {RESTORE-ASYNC-ACK 0 *}
+        assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-auth foobar]
+    }
 
     test {Once RESTORE-ASYNC-AUTH succeeded we can actually send commands to the server} {
-        assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 9]
-        r set foo 100
-        r incr foo
-    } {101}
+        assert_equal OK [r set foo 100]
+        assert_equal {101} [r incr foo]
+    }
 }
 
 start_server {tags {"migrate-async"}} {
     test {RESTORE-ASYNC-SELECT can change database} {
-        r select 0
-        r set foo 100
-
-        r select 1
-        r set foo 200
+        assert_equal OK [r select 0]
+        assert_equal OK [r set foo 100]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 0]
         assert_equal {101} [r incr foo]
+
+        assert_equal OK [r select 1]
+        assert_equal OK [r set foo 200]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 1]
         assert_equal {201} [r incr foo]
@@ -46,5 +45,16 @@ start_server {tags {"migrate-async"}} {
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 1]
         assert_equal {202} [r incr foo]
+    }
+
+    test {RESTORE-ASYNC DELETE against a single item} {
+        assert_equal OK [r set foo "hello"]
+        assert_equal {hello} [r get foo]
+
+        assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async delete foo]
+        assert_equal {} [r get foo]
+
+        assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async delete foo]
+        assert_equal {} [r get foo]
     }
 }
