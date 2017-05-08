bee0d0809310d644e940b4655b8890ac935ffef9
spinlock <wnzheng@gmail.com>
2017-05-09 00:11:10 +0800

    MigrateAsyncTest: refactor, less braces

--- tests/unit/migrate-async.tcl
+++ tests/unit/migrate-async.tcl
@@ -22,7 +22,7 @@ start_server {tags {"migrate-async"} overrides {requirepass foobar}} {
 
     test {RESTORE-ASYNC-AUTH succeeded then we can actually send commands to the server} {
         assert_equal OK [r set foo 100]
-        assert_equal {101} [r incr foo]
+        assert_equal 101 [r incr foo]
     }
 }
 
@@ -32,24 +32,24 @@ start_server {tags {"migrate-async"}} {
         r set foo 100
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 0]
-        assert_equal {101} [r incr foo]
+        assert_equal 101 [r incr foo]
 
         r select 1
         r set foo 200
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 1]
-        assert_equal {201} [r incr foo]
+        assert_equal 201 [r incr foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 0]
-        assert_equal {102} [r incr foo]
+        assert_equal 102 [r incr foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async-select 1]
-        assert_equal {202} [r incr foo]
+        assert_equal 202 [r incr foo]
     }
 
     test {RESTORE-ASYNC DELETE against a single item} {
-        r set foo hello
-        assert_equal {hello} [r get foo]
+        r set foo "hello"
+        assert_equal "hello" [r get foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async delete foo]
         assert_equal {} [r get foo]
@@ -63,31 +63,31 @@ start_server {tags {"migrate-async"}} {
         assert_equal {} [r get foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async string foo 0 hello]
-        assert_equal {hello} [r get foo]
-        assert_equal {-1} [r pttl foo]
+        assert_equal "hello" [r get foo]
+        assert_equal -1 [r pttl foo]
 
         r del foo
         assert_equal {} [r get foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async string foo 5000 world]
-        assert_equal {world} [r get foo]
+        assert_equal "world" [r get foo]
         set ttl [r pttl foo]
         assert {$ttl >= 3000 && $ttl <= 5000}
 
         r del bar
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async string bar 0 10000]
-        assert_equal {10001} [r incr bar]
+        assert_equal 10001 [r incr bar]
     }
 
     test {RESTORE-ASYNC OBJECT against a string item} {
-        r set foo hello
+        r set foo "hello"
         set encoded [r dump foo]
 
         r del foo
         assert_equal {} [r get foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async object foo 5000 $encoded]
-        assert_equal {hello} [r get foo]
+        assert_equal "hello" [r get foo]
         set ttl [r pttl foo]
         assert {$ttl >= 3000 && $ttl <= 5000}
     }
@@ -100,73 +100,73 @@ start_server {tags {"migrate-async"}} {
         assert_equal {} [r get foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async object foo 5000 $encoded]
-        assert_equal {hello} [r get foo]
+        assert_equal "hello" [r get foo]
         set ttl [r pttl foo]
         assert {$ttl >= 3000 && $ttl <= 5000}
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async expire foo 8000]
-        assert_equal {hello} [r get foo]
+        assert_equal "hello" [r get foo]
         set ttl [r pttl foo]
         assert {$ttl >= 6000 && $ttl <= 8000}
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async expire foo 0]
-        assert_equal {hello} [r get foo]
+        assert_equal "hello" [r get foo]
         set ttl [r pttl foo]
         assert {$ttl == -1}
     }
 
     test {RESTORE-ASYNC LIST against a list item} {
         r del foo
-        assert_equal {0} [r llen foo]
+        assert_equal 0 [r llen foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async list foo 0 0 a1 a2]
-        assert_equal {2} [r llen foo]
+        assert_equal 2 [r llen foo]
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async list foo 0 0 b1 b2]
-        assert_equal {4} [r llen foo]
+        assert_equal 4 [r llen foo]
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async list foo 0 0 c1 c2]
-        assert_equal {6} [r llen foo]
-        assert_equal {-1} [r pttl foo]
+        assert_equal 6 [r llen foo]
+        assert_equal -1 [r pttl foo]
         assert_encoding quicklist foo
-        assert_equal {a1} [r lindex foo 0]
-        assert_equal {a2} [r lindex foo 1]
-        assert_equal {b1} [r lindex foo 2]
-        assert_equal {b2} [r lindex foo 3]
-        assert_equal {c1} [r lindex foo 4]
-        assert_equal {c2} [r lindex foo 5]
+        assert_equal a1 [r lindex foo 0]
+        assert_equal a2 [r lindex foo 1]
+        assert_equal b1 [r lindex foo 2]
+        assert_equal b2 [r lindex foo 3]
+        assert_equal c1 [r lindex foo 4]
+        assert_equal c2 [r lindex foo 5]
     }
 
     test {RESTORE-ASYNC HASH against a hash item} {
         r del foo
-        assert_equal {0} [r hlen foo]
+        assert_equal 0 [r hlen foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async hash foo 0 0 k1 v1 k2 v2]
-        assert_equal {2} [r hlen foo]
+        assert_equal 2 [r hlen foo]
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async hash foo 0 0 k3 v3 k1 v4]
-        assert_equal {3} [r hlen foo]
-        assert_equal {-1} [r pttl foo]
+        assert_equal 3 [r hlen foo]
+        assert_equal -1 [r pttl foo]
         assert_encoding hashtable foo
-        assert_equal {v4 v2 v3} [r hmget foo k1 k2 k3]
+        assert_equal "v4 v2 v3" [r hmget foo k1 k2 k3]
     }
 
     test {RESTORE-ASYNC DICT against a set item} {
         r del foo
-        assert_equal {0} [r scard foo]
+        assert_equal 0 [r scard foo]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async dict foo 0 0 e1 e2 e3]
-        assert_equal {3} [r scard foo]
+        assert_equal 3 [r scard foo]
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async dict foo 0 0 e1 e2 e4]
-        assert_equal {4} [r scard foo]
-        assert_equal {-1} [r pttl foo]
+        assert_equal 4 [r scard foo]
+        assert_equal -1 [r pttl foo]
         assert_encoding hashtable foo
-        assert_equal {1} [r sismember foo e1]
-        assert_equal {1} [r sismember foo e2]
-        assert_equal {1} [r sismember foo e3]
-        assert_equal {1} [r sismember foo e4]
+        assert_equal 1 [r sismember foo e1]
+        assert_equal 1 [r sismember foo e2]
+        assert_equal 1 [r sismember foo e3]
+        assert_equal 1 [r sismember foo e4]
     }
 
     test {RESTORE-ASYNC ZSET against a zset item} {
         r del foo
-        assert_equal {0} [r zcard foo]
+        assert_equal 0 [r zcard foo]
 
         # 1.0 -> 3ff0000000000000 -> \x00\x00\x00\x00\x00\x00\xf0\x3f LE
         # 2.0 -> 4000000000000000 -> \x00\x00\x00\x00\x00\x00\x00\x40 LE
@@ -174,21 +174,21 @@ start_server {tags {"migrate-async"}} {
         # 4.0 -> 4010000000000000 -> \x00\x00\x00\x00\x00\x00\x10\x40 LE
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async zset foo 0 0 e1 "\x00\x00\x00\x00\x00\x00\xf0\x3f" e2 "\x00\x00\x00\x00\x00\x00\x00\x40"]
-        assert_equal {2} [r zcard foo]
-        assert_equal {1} [r zscore foo e1]
-        assert_equal {2} [r zscore foo e2]
+        assert_equal 2 [r zcard foo]
+        assert_equal 1 [r zscore foo e1]
+        assert_equal 2 [r zscore foo e2]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async zset foo 0 0 e1 "\x00\x00\x00\x00\x00\x00\x08\x40"]
-        assert_equal {2} [r zcard foo]
-        assert_equal {3} [r zscore foo e1]
-        assert_equal {2} [r zscore foo e2]
+        assert_equal 2 [r zcard foo]
+        assert_equal 3 [r zscore foo e1]
+        assert_equal 2 [r zscore foo e2]
 
         assert_match {RESTORE-ASYNC-ACK 0 *} [r restore-async zset foo 0 0 e2 "\x00\x00\x00\x00\x00\x00\x10\x40"]
-        assert_equal {2} [r zcard foo]
-        assert_equal {3} [r zscore foo e1]
-        assert_equal {4} [r zscore foo e2]
+        assert_equal 2 [r zcard foo]
+        assert_equal 3 [r zscore foo e1]
+        assert_equal 4 [r zscore foo e2]
 
-        assert_equal {-1} [r pttl foo]
+        assert_equal -1 [r pttl foo]
         assert_encoding skiplist foo
     }
 }
@@ -205,7 +205,7 @@ start_server {tags {"migrate-async"}} {
             $first migrate-async $second_host $second_port 0 0 0 foo bar
             assert_match {host * port *} [$first migrate-async-status]
 
-            assert_match {1} [$first migrate-async-cancel]
+            assert_match 1 [$first migrate-async-cancel]
             assert_match {} [$first migrate-async-status]
 
             assert_match {} [$first migrate-async-status]
@@ -224,20 +224,20 @@ start_server {tags {"migrate-async"}} {
             set second_host [srv 0 host]
             set second_port [srv 0 port]
 
-            assert_equal {1} [$second incr key]
+            assert_equal 1 [$second incr key]
 
-            assert_equal {0} [$first exists key]
-            assert_equal {0} [$first migrate-async $second_host $second_port 0 0 0 key]
+            assert_equal 0 [$first exists key]
+            assert_equal 0 [$first migrate-async $second_host $second_port 0 0 0 key]
 
-            assert_equal {1} [$first incr key]
+            assert_equal 1 [$first incr key]
             $first pexpire key 5000
 
-            assert_equal {1} [$first migrate-async $second_host $second_port 0 0 0 key]
-            assert_equal {2} [$second incr key]
+            assert_equal 1 [$first migrate-async $second_host $second_port 0 0 0 key]
+            assert_equal 2 [$second incr key]
             set ttl [$second pttl key]
             assert {$ttl >= 3000 && $ttl <= 5000}
 
-            assert_equal {0} [$first exists key]
+            assert_equal 0 [$first exists key]
         }
     }
 
@@ -254,8 +254,8 @@ start_server {tags {"migrate-async"}} {
             }
             $first pexpire key 5000
 
-            assert_equal {1} [$first migrate-async $second_host $second_port 0 100000 100 key]
-            assert_equal {0} [$first exists key]
+            assert_equal 1 [$first migrate-async $second_host $second_port 0 100000 100 key]
+            assert_equal 0 [$first exists key]
 
             set ttl [$second pttl key]
             assert {$ttl >= 3000 && $ttl <= 5000}
@@ -280,8 +280,8 @@ start_server {tags {"migrate-async"}} {
             }
             $first pexpire key 5000
 
-            assert_equal {1} [$first migrate-async $second_host $second_port 0 100000 100 key]
-            assert_equal {0} [$first exists key]
+            assert_equal 1 [$first migrate-async $second_host $second_port 0 100000 100 key]
+            assert_equal 0 [$first exists key]
 
             set ttl [$second pttl key]
             assert {$ttl >= 3000 && $ttl <= 5000}
@@ -306,15 +306,15 @@ start_server {tags {"migrate-async"}} {
             }
             $first pexpire key 5000
 
-            assert_equal {1} [$first migrate-async $second_host $second_port 0 100000 100 key]
-            assert_equal {0} [$first exists key]
+            assert_equal 1 [$first migrate-async $second_host $second_port 0 100000 100 key]
+            assert_equal 0 [$first exists key]
 
             set ttl [$second pttl key]
             assert {$ttl >= 3000 && $ttl <= 5000}
 
             assert {[$second scard key] == 20000}
             for {set j 0} {$j < 20000} {incr j} {
-                assert_equal {1} [$second sismember key "item $j"]
+                assert_equal 1 [$second sismember key "item $j"]
             }
         }
     }
@@ -332,8 +332,8 @@ start_server {tags {"migrate-async"}} {
             }
             $first pexpire key 5000
 
-            assert_equal {1} [$first migrate-async $second_host $second_port 0 100000 100 key]
-            assert_equal {0} [$first exists key]
+            assert_equal 1 [$first migrate-async $second_host $second_port 0 100000 100 key]
+            assert_equal 0 [$first exists key]
 
             set ttl [$second pttl key]
             assert {$ttl >= 3000 && $ttl <= 5000}
