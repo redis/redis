3b46cf97de6c5d7f2602dda924703db4f87efcc3
antirez <antirez@gmail.com>
2017-05-15 11:22:28 +0200

    redis-cli --bigkeys: show error when TYPE fails.
    Close #3993.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -2022,8 +2022,13 @@ static void getKeyTypes(redisReply *keys, int *types) {
                 keys->element[i]->str, context->err, context->errstr);
             exit(1);
         } else if(reply->type != REDIS_REPLY_STATUS) {
-            fprintf(stderr, "Invalid reply type (%d) for TYPE on key '%s'!\n",
-                reply->type, keys->element[i]->str);
+            if(reply->type == REDIS_REPLY_ERROR) {
+                fprintf(stderr, "TYPE returned an error: %s\n", reply->str);
+            } else {
+                fprintf(stderr,
+                    "Invalid reply type (%d) for TYPE on key '%s'!\n",
+                    reply->type, keys->element[i]->str);
+            }
             exit(1);
         }
 
