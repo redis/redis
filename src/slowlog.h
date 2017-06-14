8da9a167e08c6cb0ec1e789f322f47ff77128295
xuchengxuan <xuchengxuan@xiaomi.com>
2017-06-14 16:42:21 +0800

    Fixed comments of slowlog duration

--- src/slowlog.h
+++ src/slowlog.h
@@ -35,7 +35,7 @@ typedef struct slowlogEntry {
     robj **argv;
     int argc;
     long long id;       /* Unique entry identifier. */
-    long long duration; /* Time spent by the query, in nanoseconds. */
+    long long duration; /* Time spent by the query, in microseconds. */
     time_t time;        /* Unix time at which the query was executed. */
 } slowlogEntry;
 
