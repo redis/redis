11182a1a58ef6d36f76f7a5e06d0598254b524d9
antirez <antirez@gmail.com>
2017-07-10 16:41:25 +0200

    redis-check-aof: tell users there is a --fix option.

--- src/redis-check-aof.c
+++ src/redis-check-aof.c
@@ -220,7 +220,8 @@ int redis_check_aof_main(int argc, char **argv) {
                 printf("Successfully truncated AOF\n");
             }
         } else {
-            printf("AOF is not valid\n");
+            printf("AOF is not valid. "
+                   "Use the --fix option to try fixing it.\n");
             exit(1);
         }
     } else {
