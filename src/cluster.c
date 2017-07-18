97d50344c4f856d024053eafded74b86d77504e8
Leon Chen <chen.bao.yi@qq.com>
2017-07-18 02:28:24 -0500

    fix mismatch argument

--- src/cluster.c
+++ src/cluster.c
@@ -4007,7 +4007,7 @@ void clusterCommand(client *c) {
             }
             if ((n = clusterLookupNode(c->argv[4]->ptr)) == NULL) {
                 addReplyErrorFormat(c,"I don't know about node %s",
-                    (char*)c->argv[3]->ptr);
+                    (char*)c->argv[4]->ptr);
                 return;
             }
             server.cluster->importing_slots_from[slot] = n;
