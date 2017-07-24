e39849808c2e91262aa35b951c24e40a0f45498f
liangsijian <liangsijian@foxmail.com>
2017-07-24 19:20:31 +0800

    Fix lua ldb command log

--- src/scripting.c
+++ src/scripting.c
@@ -443,6 +443,7 @@ int luaRedisGenericCommand(lua_State *lua, int raise_error) {
             if (j == 10) {
                 cmdlog = sdscatprintf(cmdlog," ... (%d more)",
                     c->argc-j-1);
+                break;
             } else {
                 cmdlog = sdscatlen(cmdlog," ",1);
                 cmdlog = sdscatsds(cmdlog,c->argv[j]->ptr);
