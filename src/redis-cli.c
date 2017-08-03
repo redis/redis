a8c08b9b7668f784cfd24ecd2afb2fdd2ae65312
jybaek <oops.jybaek@gmail.com>
2017-08-03 17:28:04 +0900

    Add missing fclose()

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1807,6 +1807,7 @@ static void getRDB(void) {
     }
     close(s); /* Close the file descriptor ASAP as fsync() may take time. */
     fsync(fd);
+    close(fd);
     fprintf(stderr,"Transfer finished with success.\n");
     exit(0);
 }
