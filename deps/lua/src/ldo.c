c45c00f06336b7a1d35fe10533002d8d56a8112d
zimuyang <yuanhualong666@gmail.com>
2017-09-23 15:34:58 +0800

    Remove unused variable 'c'

--- deps/lua/src/ldo.c
+++ deps/lua/src/ldo.c
@@ -493,10 +493,8 @@ static void f_parser (lua_State *L, void *ud) {
   Proto *tf;
   Closure *cl;
   struct SParser *p = cast(struct SParser *, ud);
-  int c = luaZ_lookahead(p->z);
   luaC_checkGC(L);
-  tf = (luaY_parser)(L, p->z,
-                                                             &p->buff, p->name);
+  tf = (luaY_parser)(L, p->z, &p->buff, p->name);
   cl = luaF_newLclosure(L, tf->nups, hvalue(gt(L)));
   cl->l.p = tf;
   for (i = 0; i < tf->nups; i++)  /* initialize eventual upvalues */
