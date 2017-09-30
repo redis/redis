cc44a41c7e39bcbda52d2640a371db1b3260dac5
Maciej Obarski <mobarski@gmail.com>
2017-09-30 21:50:39 +0200

    Added MUL aggregate to zunionstore and zinterstore.

--- src/help.h
+++ src/help.h
@@ -928,7 +928,7 @@ struct commandHelp {
     4,
     "1.2.0" },
     { "ZINTERSTORE",
-    "destination numkeys key [key ...] [WEIGHTS weight] [AGGREGATE SUM|MIN|MAX]",
+    "destination numkeys key [key ...] [WEIGHTS weight] [AGGREGATE SUM|MIN|MAX|MUL]",
     "Intersect multiple sorted sets and store the resulting sorted set in a new key",
     4,
     "2.0.0" },
@@ -1008,7 +1008,7 @@ struct commandHelp {
     4,
     "1.2.0" },
     { "ZUNIONSTORE",
-    "destination numkeys key [key ...] [WEIGHTS weight] [AGGREGATE SUM|MIN|MAX]",
+    "destination numkeys key [key ...] [WEIGHTS weight] [AGGREGATE SUM|MIN|MAX|MUL]",
     "Add multiple sorted sets and store the resulting sorted set in a new key",
     4,
     "2.0.0" }
