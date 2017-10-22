8832af49fa1f4fdd8f9e152f013bb71b343d26dd
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-10-22 05:20:38 +0200

    Use RELEASENOTES date instead of build date
    in order to make builds reproducible.
    See https://reproducible-builds.org/ for why this is good.

--- src/mkreleasehdr.sh
+++ src/mkreleasehdr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
 GIT_DIRTY=`git diff --no-ext-diff 2> /dev/null | wc -l`
-BUILD_ID=`uname -n`"-"`date +%s`
+BUILD_ID=`uname -n`"-"`date -r ../00-RELEASENOTES +%s`
 test -f release.h || touch release.h
 (cat release.h | grep SHA1 | grep $GIT_SHA1) && \
 (cat release.h | grep DIRTY | grep $GIT_DIRTY) && exit 0 # Already up-to-date
