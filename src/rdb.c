885c4f856e5a41e86ebf2a3233dc3ba2bae6945e
zhaozhao.zz <zhaozhao.zz@alibaba-inc.com>
2017-11-01 17:52:43 +0800

    PSYNC2 & RDB: fix the missing rdbSaveInfo for BGSAVE

--- src/rdb.c
+++ src/rdb.c
@@ -1999,6 +1999,9 @@ void bgsaveCommand(client *c) {
         }
     }
 
+    rdbSaveInfo rsi, *rsiptr;
+    rsiptr = rdbPopulateSaveInfo(&rsi);
+
     if (server.rdb_child_pid != -1) {
         addReplyError(c,"Background save already in progress");
     } else if (server.aof_child_pid != -1) {
@@ -2011,7 +2014,7 @@ void bgsaveCommand(client *c) {
                 "Use BGSAVE SCHEDULE in order to schedule a BGSAVE whenever "
                 "possible.");
         }
-    } else if (rdbSaveBackground(server.rdb_filename,NULL) == C_OK) {
+    } else if (rdbSaveBackground(server.rdb_filename,rsiptr) == C_OK) {
         addReplyStatus(c,"Background saving started");
     } else {
         addReply(c,shared.err);
