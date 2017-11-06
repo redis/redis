8b72928271cc65ecec91312ba7f298fce9abeae6
Alfred Morgan <alfred@54.org>
2017-11-06 16:24:00 -0600

    Changed help to output on stdout

--- src/server.c
+++ src/server.c
@@ -3401,19 +3401,19 @@ void version(void) {
 }
 
 void usage(void) {
-    fprintf(stderr,"Usage: ./redis-server [/path/to/redis.conf] [options]\n");
-    fprintf(stderr,"       ./redis-server - (read config from stdin)\n");
-    fprintf(stderr,"       ./redis-server -v or --version\n");
-    fprintf(stderr,"       ./redis-server -h or --help\n");
-    fprintf(stderr,"       ./redis-server --test-memory <megabytes>\n\n");
-    fprintf(stderr,"Examples:\n");
-    fprintf(stderr,"       ./redis-server (run the server with default conf)\n");
-    fprintf(stderr,"       ./redis-server /etc/redis/6379.conf\n");
-    fprintf(stderr,"       ./redis-server --port 7777\n");
-    fprintf(stderr,"       ./redis-server --port 7777 --slaveof 127.0.0.1 8888\n");
-    fprintf(stderr,"       ./redis-server /etc/myredis.conf --loglevel verbose\n\n");
-    fprintf(stderr,"Sentinel mode:\n");
-    fprintf(stderr,"       ./redis-server /etc/sentinel.conf --sentinel\n");
+    fprintf(stdout,"Usage: ./redis-server [/path/to/redis.conf] [options]\n");
+    fprintf(stdout,"       ./redis-server - (read config from stdin)\n");
+    fprintf(stdout,"       ./redis-server -v or --version\n");
+    fprintf(stdout,"       ./redis-server -h or --help\n");
+    fprintf(stdout,"       ./redis-server --test-memory <megabytes>\n\n");
+    fprintf(stdout,"Examples:\n");
+    fprintf(stdout,"       ./redis-server (run the server with default conf)\n");
+    fprintf(stdout,"       ./redis-server /etc/redis/6379.conf\n");
+    fprintf(stdout,"       ./redis-server --port 7777\n");
+    fprintf(stdout,"       ./redis-server --port 7777 --slaveof 127.0.0.1 8888\n");
+    fprintf(stdout,"       ./redis-server /etc/myredis.conf --loglevel verbose\n\n");
+    fprintf(stdout,"Sentinel mode:\n");
+    fprintf(stdout,"       ./redis-server /etc/sentinel.conf --sentinel\n");
     exit(1);
 }
 
