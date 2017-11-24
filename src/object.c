b28fb3d7530f7d682223e96ed88045f11dfef989
Itamar Haber <itamar@redislabs.com>
2017-11-24 19:58:37 +0200

    Prevents `OBJECT freq` with `noeviction`
    When maxmemory is set to noeviction, idletime is implicitly kept. This renders access frequency nonsensical.

--- src/object.c
+++ src/object.c
@@ -1035,8 +1035,8 @@ void objectCommand(client *c) {
     } else if (!strcasecmp(c->argv[1]->ptr,"freq") && c->argc == 3) {
         if ((o = objectCommandLookupOrReply(c,c->argv[2],shared.nullbulk))
                 == NULL) return;
-        if (server.maxmemory_policy & MAXMEMORY_FLAG_LRU) {
-            addReplyError(c,"An LRU maxmemory policy is selected, access frequency not tracked. Please note that when switching between policies at runtime LRU and LFU data will take some time to adjust.");
+        if (!(server.maxmemory_policy & MAXMEMORY_FLAG_LFU)) {
+            addReplyError(c,"A non-LFU maxmemory policy is selected, access frequency not tracked. Please note that when switching between policies at runtime LRU and LFU data will take some time to adjust.");
             return;
         }
         addReplyLongLong(c,o->lru&255);
