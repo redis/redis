b66fbf99de88887d40df0380f80462f6bfaaffd7
zhaozhao.zz <zhaozhao.zz@alibaba-inc.com>
2017-11-27 18:08:31 +0800

    Modules: add default timeout callback for blocked module client

--- src/module.c
+++ src/module.c
@@ -5333,6 +5333,13 @@ void unblockClientFromModule(client *c) {
     bc->client = NULL;
 }
 
+/* The default timeout callback for blocked module client. */
+int moduleBlockedClientDefaultTimeout(RedisModuleCtx *ctx, void **argv, int argc) {
+    UNUSED(argv);
+    UNUSED(argc);
+    return RM_ReplyWithError(ctx, "ERR blocked module command timed out, but timeout callback is NULL");
+}
+
 /* Block a client in the context of a module: this function implements both
  * RM_BlockClient() and RM_BlockClientOnKeys() depending on the fact the
  * keys are passed or not.
@@ -5370,7 +5377,7 @@ RedisModuleBlockedClient *moduleBlockClient(RedisModuleCtx *ctx, RedisModuleCmdF
     bc->client = (islua || ismulti) ? NULL : c;
     bc->module = ctx->module;
     bc->reply_callback = reply_callback;
-    bc->timeout_callback = timeout_callback;
+    bc->timeout_callback = timeout_callback ? timeout_callback : moduleBlockedClientDefaultTimeout;
     bc->disconnect_callback = NULL; /* Set by RM_SetDisconnectCallback() */
     bc->free_privdata = free_privdata;
     bc->privdata = privdata;
