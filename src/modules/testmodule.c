84c6f1e3ca0e759aae5bdb7bff7909dd6e9ec0ec
Dvir Volk <dvirsky@gmail.com>
2017-11-27 23:18:45 +0200

    removed hellonotify.c

--- src/modules/testmodule.c
+++ src/modules/testmodule.c
@@ -157,7 +157,7 @@ int TestUnlink(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 int NotifyCallback(RedisModuleCtx *ctx, int type, const char *event, RedisModuleString *key) {
   // Increment a counter on the notifications:
   // for each key notified we increment a counter
-  RedisModule_Log(ctx, "notice", "Got event type %d, event %s, key %s\n", type, event, RedisModule_StringPtrLen(key, NULL));
+  RedisModule_Log(ctx, "notice", "Got event type %d, event %s, key %s", type, event, RedisModule_StringPtrLen(key, NULL));
 
   RedisModule_Call(ctx, "HINCRBY", "csc", "notifications", key, "1");
   return REDISMODULE_OK;
