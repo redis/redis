cbe9db50f7dd7ca5dd72e5446f37d95191d8d378
spinlock <wnzheng@gmail.com>
2018-04-07 01:00:49 +0800

    migrate: create cached socket

--- src/migrate.c
+++ src/migrate.c
@@ -10,22 +10,23 @@ typedef struct {
     int last_dbid;
     time_t last_use_time;
     const char *name;
-    int inuse;
+    sds auth;
+    int busy;
     int error;
     int authenticated;
 } migrateCachedSocket;
 
 static sds migrateSocketName(robj *host, robj *port, robj *auth) {
     sds name = sdscatfmt(sdsempty(), "%S:%S", host->ptr, port->ptr);
-    if (auth != NULL) {
-        name = sdscat(name, "#");
+    if (auth == NULL) {
+        return name;
     }
-    return name;
+    return sdscat(name, "#");
 }
 
 static void migrateCloseSocket(migrateCachedSocket *cs) {
-    serverAssert(dictDelete(server.migrate_cached_sockets, cs->name) ==
-                 DICT_OK);
+    serverAssert(dictDelete(server.migrate_cached_sockets, cs->name) == DICT_OK);
+    sdsfree(cs->auth);
     close(cs->fd);
     zfree(cs);
 }
@@ -35,13 +36,65 @@ void migrateCloseTimedoutSockets(void) {
     dictEntry *entry;
     while ((entry = dictNext(di)) != NULL) {
         migrateCachedSocket *cs = dictGetVal(entry);
-        if (cs->inuse ||
-            server.unixtime - cs->last_use_time <= MIGRATE_SOCKET_CACHE_TTL) {
+        if (cs->busy || server.unixtime - cs->last_use_time <= MIGRATE_SOCKET_CACHE_TTL) {
             continue;
         }
         migrateCloseSocket(cs);
     }
     dictReleaseIterator(di);
+}
+
+static migrateCachedSocket *migrateGetSocketOrReply(client *c, robj *host, robj *port, robj *auth, mstime_t timeout) {
+    sds name = migrateSocketName(host, port, auth);
+    migrateCachedSocket *cs = dictFetchValue(server.migrate_cached_sockets, name);
+    if (cs != NULL) {
+        sdsfree(name);
+        if (cs->busy) {
+            addReplySds(c, sdscatfmt(sdsempty(), "-RETRYLATER target %S:%S is busy.\r\n", host->ptr, port->ptr));
+            return NULL;
+        }
+        serverAssert((cs->auth != NULL && auth != NULL) || (cs->auth == NULL && auth == NULL));
+
+        if (cs->auth != NULL && sdscmp(cs->auth, auth->ptr) != 0) {
+            sdsfree(cs->auth);
+            cs->auth = sdsdup(auth->ptr);
+            cs->authenticated = 0;
+        }
+        return cs;
+    }
+
+    if (dictSize(server.migrate_cached_sockets) == MIGRATE_SOCKET_CACHE_ITEMS) {
+        dictEntry *entry = dictGetRandomKey(server.migrate_cached_sockets);
+        migrateCloseSocket(dictGetVal(entry));
+    }
+
+    int fd = anetTcpNonBlockConnect(server.neterr, host->ptr, atoi(port->ptr));
+    if (fd == -1) {
+        sdsfree(name);
+        addReplySds(c, sdscatfmt(sdsempty(), "-IOERR Can't connect to target %S:%S: '%s'.\r\n", host->ptr, port->ptr,
+                                 server.neterr));
+        return NULL;
+    }
+    anetEnableTcpNoDelay(server.neterr, fd);
+
+    if ((aeWait(fd, AE_WRITABLE, timeout) & AE_WRITABLE) == 0) {
+        sdsfree(name);
+        close(fd);
+        addReplySds(c, sdsnew("-IOERR error or timeout connecting to the client.\r\n"));
+        return NULL;
+    }
+
+    cs = zmalloc(sizeof(*cs));
+    cs->fd = fd;
+    cs->last_dbid = -1;
+    cs->last_use_time = server.unixtime;
+    cs->name = name;
+    cs->auth = (auth != NULL ? sdsdup(auth->ptr) : NULL);
+    cs->busy = 0;
+    cs->error = 0;
+    cs->authenticated = 0;
+    dictAdd(server.migrate_cached_sockets, name, cs);
+    return cs;
 }
 
 // ---------------- BACKGROUND THREAD --------------------------------------- //
@@ -63,12 +116,9 @@ static void *migrateCommandThreadMain(void *privdata) {
 #if defined(USE_JEMALLOC)
     do {
         unsigned arena_ind = 0;
-        int ret = je_mallctl("thread.arena", NULL, NULL, &arena_ind,
-                             sizeof(arena_ind));
+        int ret = je_mallctl("thread.arena", NULL, NULL, &arena_ind, sizeof(arena_ind));
         if (ret != 0) {
-            serverLog(LL_WARNING,
-                      "Call je_mallctl to set thread.arena=%d failed: %s",
-                      (int)arena_ind, strerror(ret));
+            serverLog(LL_WARNING, "Call je_mallctl to set thread.arena=%d failed: %s", (int)arena_ind, strerror(ret));
         }
     } while (0);
 #endif
@@ -80,8 +130,7 @@ static void *migrateCommandThreadMain(void *privdata) {
     return NULL;
 }
 
-static void migrateCommandThreadReadEvent(aeEventLoop *el, int fd,
-                                          void *privdata, int mask) {
+static void migrateCommandThreadReadEvent(aeEventLoop *el, int fd, void *privdata, int mask) {
     UNUSED(el);
     UNUSED(fd);
     UNUSED(mask);
@@ -116,8 +165,7 @@ static void migrateCommandThreadInit(migrateCommandThread *p) {
         serverPanic("Fatal: call anetNonBlock '%s'.", strerror(errno));
         exit(1);
     }
-    if (aeCreateFileEvent(server.el, p->pipe_fds[0], AE_READABLE,
-                          migrateCommandThreadReadEvent, p) == AE_ERR) {
+    if (aeCreateFileEvent(server.el, p->pipe_fds[0], AE_READABLE, migrateCommandThreadReadEvent, p) == AE_ERR) {
         serverPanic("Fatal: call aeCreateFileEvent '%s'.", strerror(errno));
         exit(1);
     }
@@ -130,9 +178,7 @@ static void migrateCommandThreadInit(migrateCommandThread *p) {
 
 static migrateCommandThread migrate_command_threads[1];
 
-void migrateBackgroundThreadInit(void) {
-    migrateCommandThreadInit(&migrate_command_threads[0]);
-}
+void migrateBackgroundThreadInit(void) { migrateCommandThreadInit(&migrate_command_threads[0]); }
 
 // TODO
 
