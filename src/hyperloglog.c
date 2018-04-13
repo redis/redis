0536c1061bdbeedfa98f1fbc4c7e75e45d1a1faf
Nathan Scott <nathans@redhat.com>
2018-04-13 10:24:02 +1000

    Resolve some compilation warnings with recent gcc versions
    With default options gcc will complain about cascading case
    switch statements, annotate these to indicate this is being
    used intentionally in hyperloglog.c and siphash.c
    
    The recent streams code has some cases where void pointers
    are passed under "%s" to printf-alike error handling code;
    these can be safely cast to character pointers here.

--- src/hyperloglog.c
+++ src/hyperloglog.c
@@ -429,12 +429,12 @@ uint64_t MurmurHash64A (const void * key, int len, unsigned int seed) {
     }
 
     switch(len & 7) {
-    case 7: h ^= (uint64_t)data[6] << 48;
-    case 6: h ^= (uint64_t)data[5] << 40;
-    case 5: h ^= (uint64_t)data[4] << 32;
-    case 4: h ^= (uint64_t)data[3] << 24;
-    case 3: h ^= (uint64_t)data[2] << 16;
-    case 2: h ^= (uint64_t)data[1] << 8;
+    case 7: h ^= (uint64_t)data[6] << 48; /*FALLTHROUGH*/
+    case 6: h ^= (uint64_t)data[5] << 40; /*FALLTHROUGH*/
+    case 5: h ^= (uint64_t)data[4] << 32; /*FALLTHROUGH*/
+    case 4: h ^= (uint64_t)data[3] << 24; /*FALLTHROUGH*/
+    case 3: h ^= (uint64_t)data[2] << 16; /*FALLTHROUGH*/
+    case 2: h ^= (uint64_t)data[1] << 8;  /*FALLTHROUGH*/
     case 1: h ^= (uint64_t)data[0];
             h *= m;
     };
--- src/siphash.c
+++ src/siphash.c
@@ -142,12 +142,12 @@ uint64_t siphash(const uint8_t *in, const size_t inlen, const uint8_t *k) {
     }
 
     switch (left) {
-    case 7: b |= ((uint64_t)in[6]) << 48;
-    case 6: b |= ((uint64_t)in[5]) << 40;
-    case 5: b |= ((uint64_t)in[4]) << 32;
-    case 4: b |= ((uint64_t)in[3]) << 24;
-    case 3: b |= ((uint64_t)in[2]) << 16;
-    case 2: b |= ((uint64_t)in[1]) << 8;
+    case 7: b |= ((uint64_t)in[6]) << 48; /*FALLTHROUGH*/
+    case 6: b |= ((uint64_t)in[5]) << 40; /*FALLTHROUGH*/
+    case 5: b |= ((uint64_t)in[4]) << 32; /*FALLTHROUGH*/
+    case 4: b |= ((uint64_t)in[3]) << 24; /*FALLTHROUGH*/
+    case 3: b |= ((uint64_t)in[2]) << 16; /*FALLTHROUGH*/
+    case 2: b |= ((uint64_t)in[1]) << 8;  /*FALLTHROUGH*/
     case 1: b |= ((uint64_t)in[0]); break;
     case 0: break;
     }
@@ -202,12 +202,12 @@ uint64_t siphash_nocase(const uint8_t *in, const size_t inlen, const uint8_t *k)
     }
 
     switch (left) {
-    case 7: b |= ((uint64_t)siptlw(in[6])) << 48;
-    case 6: b |= ((uint64_t)siptlw(in[5])) << 40;
-    case 5: b |= ((uint64_t)siptlw(in[4])) << 32;
-    case 4: b |= ((uint64_t)siptlw(in[3])) << 24;
-    case 3: b |= ((uint64_t)siptlw(in[2])) << 16;
-    case 2: b |= ((uint64_t)siptlw(in[1])) << 8;
+    case 7: b |= ((uint64_t)siptlw(in[6])) << 48; /*FALLTHROUGH*/
+    case 6: b |= ((uint64_t)siptlw(in[5])) << 40; /*FALLTHROUGH*/
+    case 5: b |= ((uint64_t)siptlw(in[4])) << 32; /*FALLTHROUGH*/
+    case 4: b |= ((uint64_t)siptlw(in[3])) << 24; /*FALLTHROUGH*/
+    case 3: b |= ((uint64_t)siptlw(in[2])) << 16; /*FALLTHROUGH*/
+    case 2: b |= ((uint64_t)siptlw(in[1])) << 8;  /*FALLTHROUGH*/
     case 1: b |= ((uint64_t)siptlw(in[0])); break;
     case 0: break;
     }
