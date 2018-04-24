259f5e3eabdca014ffe379bc7f76fa40e636b88c
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-04-24 15:33:13 +0300

    :shirt: Update formatting

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1398,7 +1398,11 @@ static void repl(void) {
     while((line = linenoise(context ? config.prompt : "not connected> ")) != NULL) {
         if (line[0] != '\0') {
             argv = cliSplitArgs(line,&argc);
-            if (history && strcasecmp(argv[0], "auth")) linenoiseHistoryAdd(line);
+            if (history &&
+                strcasecmp(argv[0], "auth"))
+            {
+                linenoiseHistoryAdd(line);
+            }
             if (historyfile) linenoiseHistorySave(historyfile);
 
             if (argv == NULL) {
