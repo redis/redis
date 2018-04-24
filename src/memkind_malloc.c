586d13efa3beda2561921aff306959eaa7b241f3
Krzysztof Filipek <krzysztof.filipek@intel.com>
2018-04-24 16:45:03 +0200

    Fix printf in memkind allocator

--- src/memkind_malloc.c
+++ src/memkind_malloc.c
@@ -22,7 +22,7 @@ static size_t used_memory = 0;
 
 void *memkind_alloc_wrapper(size_t size) {
     void *ptr = memkind_malloc(server.pmem_kind1, size+PREFIX_SIZE);
-    printf("alloc size=%z, ptr=%p\n",size, ptr);
+    printf("alloc size=%zu, ptr=%p\n",size, ptr);
     if (ptr==NULL) return NULL;
     *((size_t*)ptr) = size;
     update_memkind_malloc_stat_alloc(size+PREFIX_SIZE);
@@ -31,7 +31,7 @@ void *memkind_alloc_wrapper(size_t size) {
 
 void *memkind_calloc_wrapper(size_t size) {
     void *ptr = memkind_calloc(server.pmem_kind1, 1, size+PREFIX_SIZE);
-    printf("calloc size=%z, ptr=%p\n",size, ptr);
+    printf("calloc size=%zu, ptr=%p\n",size, ptr);
     if (ptr==NULL) return NULL;
     *((size_t*)ptr) = size;
     update_memkind_malloc_stat_alloc(size+PREFIX_SIZE);
@@ -45,7 +45,7 @@ void *memkind_realloc_wrapper(void *ptr, size_t size) {
     realptr = (char*)ptr-PREFIX_SIZE;
     oldsize = *((size_t*)realptr);
     void *newptr = memkind_realloc(server.pmem_kind1, realptr, size+PREFIX_SIZE);
-    printf("realloc,size=%z, ptr=%p, newptr=%p\n", size, ptr, newptr);
+    printf("realloc,size=%zu, ptr=%p, newptr=%p\n", size, ptr, newptr);
     if (newptr==NULL) return NULL;
 
     *((size_t*)newptr) = size;
