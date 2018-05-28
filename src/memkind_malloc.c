6d60cd232c6134fd2eb54aef808f4b7e88ce05b5
pkarp <karpio.p@gmail.com>
2018-05-28 10:41:42 +0200

    memkind allocation use jemk_usable_size instead of storing size as prefix to the pointer

--- src/memkind_malloc.c
+++ src/memkind_malloc.c
@@ -4,8 +4,6 @@
 #include <memkind.h>
 #include <memkind/internal/memkind_pmem.h>
 
-#define PREFIX_SIZE (sizeof(size_t))
-
 #define update_memkind_malloc_stat_alloc(__n) do { \
     size_t _n = (__n); \
     if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
@@ -21,44 +19,37 @@
 static size_t used_memory = 0;
 
 void *memkind_alloc_wrapper(size_t size) {
-    void *ptr = memkind_malloc(server.pmem_kind1, size+PREFIX_SIZE);
+    void *ptr = memkind_malloc(server.pmem_kind1, size);
     if (ptr==NULL) return NULL;
-    *((size_t*)ptr) = size;
-    update_memkind_malloc_stat_alloc(size+PREFIX_SIZE);
-    return (char*)ptr+PREFIX_SIZE;
+    update_memkind_malloc_stat_alloc(size);
+    return (char*)ptr;
 }
 
 void *memkind_calloc_wrapper(size_t size) {
-    void *ptr = memkind_calloc(server.pmem_kind1, 1, size+PREFIX_SIZE);
+    void *ptr = memkind_calloc(server.pmem_kind1, 1, size);
     if (ptr==NULL) return NULL;
-    *((size_t*)ptr) = size;
-    update_memkind_malloc_stat_alloc(size+PREFIX_SIZE);
-    return (char*)ptr+PREFIX_SIZE;
+    update_memkind_malloc_stat_alloc(size);
+    return (char*)ptr;
 }
 
 void *memkind_realloc_wrapper(void *ptr, size_t size) {
     size_t oldsize;
-    void *realptr;
     if (ptr == NULL) return memkind_alloc_wrapper(size);
-    realptr = (char*)ptr-PREFIX_SIZE;
-    oldsize = *((size_t*)realptr);
-    void *newptr = memkind_realloc(server.pmem_kind1, realptr, size+PREFIX_SIZE);
+    oldsize = jemk_malloc_usable_size(ptr);
+    void *newptr = memkind_realloc(server.pmem_kind1, ptr, size);
     if (newptr==NULL) return NULL;
 
-    *((size_t*)newptr) = size;
     update_memkind_malloc_stat_free(oldsize);
     update_memkind_malloc_stat_alloc(size);
-    return (char*)newptr+PREFIX_SIZE;
+    return (char*)newptr;
 }
 
 void memkind_free_wrapper(void *ptr) {
     if(!ptr) return;
-    void *realptr;
     size_t oldsize;
-    realptr = (char*)ptr-PREFIX_SIZE;
-    oldsize = *((size_t*)realptr);
-    update_memkind_malloc_stat_free(oldsize+PREFIX_SIZE);
-    memkind_free(server.pmem_kind1, realptr);
+    oldsize = jemk_malloc_usable_size(ptr);
+    update_memkind_malloc_stat_free(oldsize);
+    memkind_free(server.pmem_kind1, ptr);
 }
 
 
