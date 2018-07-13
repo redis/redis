8dc08ae2154a46757adb75ee091219baa2f0cb22
andrewsensus <7727721+andrewsensus@users.noreply.github.com>
2018-07-13 09:46:30 -0600

    update leap year comment

--- src/localtime.c
+++ src/localtime.c
@@ -50,7 +50,7 @@
  * designed to work with what time(NULL) may return, and to support Redis
  * logging of the dates, it's not really a complete implementation. */
 static int is_leap_year(time_t year) {
-    if (year % 4) return 0;         /* A year divisible by 4 is not leap. */
+    if (year % 4) return 0;         /* A year not divisible by 4 is not leap. */
     else if (year % 100) return 1;  /* If div by 4 and not 100 is surely leap. */
     else if (year % 400) return 0;  /* If div by 100 *and* 400 is not leap. */
     else return 1;                  /* If div by 100 and not by 400 is leap. */
