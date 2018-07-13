d6ba4fd56d9e386abaf6926fa47ead1525ff56cb
WuYunlong <xzsyeb@126.com>
2018-07-13 17:53:55 +0800

    Fix config set slowlog-log-slower-than and condition in createLatencyReport

--- src/latency.c
+++ src/latency.c
@@ -294,7 +294,7 @@ sds createLatencyReport(void) {
 
         /* Potentially commands. */
         if (!strcasecmp(event,"command")) {
-            if (server.slowlog_log_slower_than == 0) {
+            if (server.slowlog_log_slower_than < 0) {
                 advise_slowlog_enabled = 1;
                 advices++;
             } else if (server.slowlog_log_slower_than/1000 >
