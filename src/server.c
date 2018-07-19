0cf3794e6e4807caaf1c78a62dd7843174c08f70
antirez <antirez@gmail.com>
2018-07-19 17:34:15 +0200

    Fix wrong array index variable in getExpansiveClientsInfo().

--- src/server.c
+++ src/server.c
@@ -926,7 +926,7 @@ void getExpansiveClientsInfo(size_t *in_usage, size_t *out_usage) {
     size_t i = 0, o = 0;
     for (int j = 0; j < CLIENTS_PEAK_MEM_USAGE_SLOTS; j++) {
         if (ClientsPeakMemInput[j] > i) i = ClientsPeakMemInput[j];
-        if (ClientsPeakMemOutput[j] > o) o = ClientsPeakMemOutput[o];
+        if (ClientsPeakMemOutput[j] > o) o = ClientsPeakMemOutput[j];
     }
     *in_usage = i;
     *out_usage = o;
