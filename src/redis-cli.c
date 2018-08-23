b2176a186ac187ed78faf2b0e8700678fc44c0ab
yura <funny.falcon@gmail.com>
2018-08-23 18:48:20 +0300

    Use "BUMPEPOCH BROADCAST" for better stability after rebalance/resharding.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -5425,6 +5425,11 @@ static int clusterManagerCommandReshard(int argc, char **argv) {
             goto cleanup;
         }
     }
+    // Bump epoch and broadcast for better stability
+    // Result is not checked for compatibility with previous redis versions.
+    redisReply *reply = CLUSTER_MANAGER_COMMAND(target, "CLUSTER BUMPEPOCH "
+            "BROADCAST");
+    if (reply) freeReplyObject(reply);
 cleanup:
     listRelease(sources);
     clusterManagerReleaseReshardTable(table);
@@ -5601,7 +5606,12 @@ static int clusterManagerCommandRebalance(int argc, char **argv) {
                     printf("#");
                     fflush(stdout);
                 }
-
+                // Bump epoch and broadcast for better stability
+                // Result is not checked for compatibility with previous
+                // redis versions.
+                redisReply *reply = CLUSTER_MANAGER_COMMAND(dst, "CLUSTER "
+                        "BUMPEPOCH BROADCAST");
+                if (reply) freeReplyObject(reply);
             }
             printf("\n");
 end_move:
