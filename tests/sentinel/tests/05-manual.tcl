132be8aed5c7669f3061d2861eb2570c9f41dc19
Chris Lamb <chris@chris-lamb.co.uk>
2018-08-26 14:45:39 +0200

    Correct "did not received" -> "did not receive" typos/grammar.

--- tests/sentinel/tests/05-manual.tcl
+++ tests/sentinel/tests/05-manual.tcl
@@ -12,7 +12,7 @@ test "Manual failover works" {
         wait_for_condition 1000 50 {
             [lindex [S $id SENTINEL GET-MASTER-ADDR-BY-NAME mymaster] 1] != $old_port
         } else {
-            fail "At least one Sentinel did not received failover info"
+            fail "At least one Sentinel did not receive failover info"
         }
     }
     set addr [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
