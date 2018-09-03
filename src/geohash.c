993a9ee67605615ce1bebf7df03feae3ea0222af
neos <akatsuki10086@gmail.com>
2018-09-03 15:16:28 +0800

    more efficient

--- src/geohash.c
+++ src/geohash.c
@@ -232,9 +232,9 @@ static void geohash_move_x(GeoHashBits *hash, int8_t d) {
 
     if (d > 0) {
         x = x + (zz + 1);
-    } else {
-        x = x | zz;
-        x = x - (zz + 1);
+    } 
+    else {
+        --x;
     }
 
     x &= (0xaaaaaaaaaaaaaaaaULL >> (64 - hash->step * 2));
@@ -251,9 +251,9 @@ static void geohash_move_y(GeoHashBits *hash, int8_t d) {
     uint64_t zz = 0xaaaaaaaaaaaaaaaaULL >> (64 - hash->step * 2);
     if (d > 0) {
         y = y + (zz + 1);
-    } else {
-        y = y | zz;
-        y = y - (zz + 1);
+    } 
+    else {
+        --y;
     }
     y &= (0x5555555555555555ULL >> (64 - hash->step * 2));
     hash->bits = (x | y);
