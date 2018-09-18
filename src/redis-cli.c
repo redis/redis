4bbcb3142bfb289ce735c7931da6a94aa0d58c08
tengfeng <jingcheng.ljc@alibaba-inc.com>
2018-09-18 15:05:01 +0800

    Fix #5286 issue.

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1173,6 +1173,16 @@ static int cliSendCommand(int argc, char **argv, long repeat) {
             } else if (!strcasecmp(command,"auth") && argc == 2) {
                 cliSelect();
             }
+
+
+            /*  Issue the command again if we got redirected in cluster mode  */
+            if  (config.cluster_mode  &&  config.cluster_reissue_command)  {
+                cliConnect(CC_FORCE);
+                config.cluster_reissue_command  =  0;
+                /* for a '-MOVED' or '-ASK' response, we need to issue the command again, so
+                 * add repeat by 1. */
+                repeat++;
+            }
         }
         if (config.interval) usleep(config.interval);
         fflush(stdout); /* Make it grep friendly */
@@ -1542,23 +1552,15 @@ static char **convertToSds(int count, char** args) {
 }
 
 static int issueCommandRepeat(int argc, char **argv, long repeat) {
-    while (1) {
-        config.cluster_reissue_command = 0;
-        if (cliSendCommand(argc,argv,repeat) != REDIS_OK) {
-            cliConnect(CC_FORCE);
+    config.cluster_reissue_command = 0;
+    if (cliSendCommand(argc,argv,repeat) != REDIS_OK) {
+        cliConnect(CC_FORCE);
 
-            /* If we still cannot send the command print error.
-             * We'll try to reconnect the next time. */
-            if (cliSendCommand(argc,argv,repeat) != REDIS_OK) {
-                cliPrintContextError();
-                return REDIS_ERR;
-            }
-         }
-         /* Issue the command again if we got redirected in cluster mode */
-         if (config.cluster_mode && config.cluster_reissue_command) {
-            cliConnect(CC_FORCE);
-         } else {
-             break;
+        /* If we still cannot send the command print error.
+         * We'll try to reconnect the next time. */
+        if (cliSendCommand(argc,argv,repeat) != REDIS_OK) {
+            cliPrintContextError();
+            return REDIS_ERR;
         }
     }
     return REDIS_OK;
