534c3bc21a256e113265ff1af6724af5ac4c9f9c
O(ʒ) <sarikayaziya@gmail.com>
2018-10-07 13:37:58 +0200

    Fix typo

--- src/rax.c
+++ src/rax.c
@@ -1313,7 +1313,7 @@ int raxIteratorNextStep(raxIterator *it, int noup) {
     }
 }
 
-/* Seek the grestest key in the subtree at the current node. Return 0 on
+/* Seek the greatest key in the subtree at the current node. Return 0 on
  * out of memory, otherwise 1. This is an helper function for different
  * iteration functions below. */
 int raxSeekGreatest(raxIterator *it) {
