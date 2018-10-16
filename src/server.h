dfab3cba2a9e07072751402baddb769f38e08560
antirez <antirez@gmail.com>
2018-10-16 13:17:14 +0200

    Streams: XSTREAM SETID -> XSETID.
    Keep vanilla stream commands at toplevel, see #5426.

--- src/server.h
+++ src/server.h
@@ -2111,7 +2111,7 @@ void xrevrangeCommand(client *c);
 void xlenCommand(client *c);
 void xreadCommand(client *c);
 void xgroupCommand(client *c);
-void xstreamCommand(client *c);
+void xsetidCommand(client *c);
 void xackCommand(client *c);
 void xpendingCommand(client *c);
 void xclaimCommand(client *c);
