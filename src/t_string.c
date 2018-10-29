0350f597afba7d52981fca64697dbb3e1a098024
zhaozhao.zz <zhaozhao.zz@alibaba-inc.com>
2018-10-30 00:38:20 +0800

    using proto-max-bulk-len in checkStringLength for SETRANGE and APPEND
    (cherry picked from commit 2e69bfe44d154d6739da48ff87f1c66045f68e93)

--- src/t_string.c
+++ src/t_string.c
@@ -35,8 +35,8 @@
  *----------------------------------------------------------------------------*/
 
 static int checkStringLength(client *c, long long size) {
-    if (size > 512*1024*1024) {
-        addReplyError(c,"string exceeds maximum allowed size (512MB)");
+    if (!(c->flags & CLIENT_MASTER) && size > server.proto_max_bulk_len) {
+        addReplyError(c,"string exceeds maximum allowed size (proto-max-bulk-len)");
         return C_ERR;
     }
     return C_OK;
