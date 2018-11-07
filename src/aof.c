fe0f5f2b410bd1cf758d02a86b686ba7ccf6798a
hujie <13184541+hujiecs@users.noreply.github.com>
2018-11-07 13:05:21 +0800

    fix comment mistake and some typos

--- src/aof.c
+++ src/aof.c
@@ -1755,7 +1755,7 @@ void backgroundRewriteDoneHandler(int exitcode, int bysignal) {
             "Background AOF rewrite signal handler took %lldus", ustime()-now);
     } else if (!bysignal && exitcode != 0) {
         /* SIGUSR1 is whitelisted, so we have a way to kill a child without
-         * tirggering an error condition. */
+         * triggering an error condition. */
         if (bysignal != SIGUSR1)
             server.aof_lastbgrewrite_status = C_ERR;
         serverLog(LL_WARNING,
