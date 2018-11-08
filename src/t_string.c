a577230a58800a9dc88d29b75d2ada605dd85d17
antirez <antirez@gmail.com>
2018-11-08 13:28:04 +0100

    RESP3: Use new deferred len API in t_string.c.

--- src/t_string.c
+++ src/t_string.c
@@ -285,7 +285,7 @@ void getrangeCommand(client *c) {
 void mgetCommand(client *c) {
     int j;
 
-    addReplyMultiBulkLen(c,c->argc-1);
+    addReplyArrayLen(c,c->argc-1);
     for (j = 1; j < c->argc; j++) {
         robj *o = lookupKeyRead(c->db,c->argv[j]);
         if (o == NULL) {
