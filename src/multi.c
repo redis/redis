f07f3d729fcd037885d54287fa670dc25243977e
antirez <antirez@gmail.com>
2018-11-26 16:20:01 +0100

    RESP3: Use new aggregate reply API in slowlog.c.

--- src/multi.c
+++ src/multi.c
@@ -159,7 +159,7 @@ void execCommand(client *c) {
     orig_argv = c->argv;
     orig_argc = c->argc;
     orig_cmd = c->cmd;
-    addReplyMultiBulkLen(c,c->mstate.count);
+    addReplyArrayLen(c,c->mstate.count);
     for (j = 0; j < c->mstate.count; j++) {
         c->argc = c->mstate.commands[j].argc;
         c->argv = c->mstate.commands[j].argv;
