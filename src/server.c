1a2bfe2e58e60ba4bf6d67ab95bddb92f91c07d9
guogzhao <guogzhao@gmail.com>
2018-11-28 03:25:37 +0800

    Update server.c
    fix the PID of the shell output is inconsistent with redis_6379.pid

--- src/server.c
+++ src/server.c
@@ -3703,6 +3703,7 @@ void daemonize(void) {
     int fd;
 
     if (fork() != 0) exit(0); /* parent exits */
+    serverLog(LL_WARNING, "Reids daemonize pid=%d, good luck", (int)getpid());/*ggz:out real pid*/
     setsid(); /* create a new session */
 
     /* Every output goes to /dev/null. If Redis is daemonized but
