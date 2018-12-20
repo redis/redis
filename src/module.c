500e51171ddf03b7e862cc3e2fbf6fc0f491887d
antirez <antirez@gmail.com>
2018-12-20 17:31:55 +0100

    Modules shared API: prevent unloading of used modules.

--- src/module.c
+++ src/module.c
@@ -4902,10 +4902,11 @@ int moduleUnload(sds name) {
     if (module == NULL) {
         errno = ENOENT;
         return REDISMODULE_ERR;
-    }
-
-    if (listLength(module->types)) {
+    } else if (listLength(module->types)) {
         errno = EBUSY;
+        return REDISMODULE_ERR;
+    } else if (listLength(module->usedby)) {
+        errno = EPERM;
         return REDISMODULE_ERR;
     }
 
@@ -4972,7 +4973,12 @@ NULL
                 errmsg = "no such module with that name";
                 break;
             case EBUSY:
-                errmsg = "the module exports one or more module-side data types, can't unload";
+                errmsg = "the module exports one or more module-side data "
+                         "types, can't unload";
+                break;
+            case EPERM:
+                errmsg = "the module exports APIs used by other modules. "
+                         "Please unload them first and try again";
                 break;
             default:
                 errmsg = "operation not possible.";
