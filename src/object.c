30544e7e33db37cd12f9d484fa78a338e0c5808f
inshal96 <39904558+inshal96@users.noreply.github.com>
2019-01-04 16:54:50 +0500

    remove an extra 'a' in the comments

--- src/object.c
+++ src/object.c
@@ -126,7 +126,7 @@ robj *createStringObject(const char *ptr, size_t len) {
 /* Create a string object from a long long value. When possible returns a
  * shared integer object, or at least an integer encoded one.
  *
- * If valueobj is non zero, the function avoids returning a a shared
+ * If valueobj is non zero, the function avoids returning a shared
  * integer, because the object is going to be used as value in the Redis key
  * space (for instance when the INCR command is used), so we want LFU/LRU
  * values specific for each key. */
