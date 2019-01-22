711e514ea4ac27709de822f73c8c87a7fa6ddfd2
antirez <antirez@gmail.com>
2019-01-22 19:02:50 +0100

    ACL: update comments in command flags.

--- src/server.h
+++ src/server.h
@@ -202,20 +202,22 @@ typedef long long mstime_t; /* millisecond time type. */
 
 /* Command flags. Please check the command table defined in the redis.c file
  * for more information about the meaning of every flag. */
-#define CMD_WRITE (1<<0)            /* "w" flag */
-#define CMD_READONLY (1<<1)         /* "r" flag */
-#define CMD_DENYOOM (1<<2)          /* "m" flag */
+#define CMD_WRITE (1<<0)            /* "write" flag */
+#define CMD_READONLY (1<<1)         /* "read-only" flag */
+#define CMD_DENYOOM (1<<2)          /* "use-memory" flag */
 #define CMD_MODULE (1<<3)           /* Command exported by module. */
-#define CMD_ADMIN (1<<4)            /* "a" flag */
-#define CMD_PUBSUB (1<<5)           /* "p" flag */
-#define CMD_NOSCRIPT (1<<6)         /* "s" flag */
-#define CMD_RANDOM (1<<7)           /* "R" flag */
-#define CMD_SORT_FOR_SCRIPT (1<<8)  /* "S" flag */
-#define CMD_LOADING (1<<9)          /* "l" flag */
-#define CMD_STALE (1<<10)           /* "t" flag */
-#define CMD_SKIP_MONITOR (1<<11)    /* "M" flag */
-#define CMD_ASKING (1<<12)          /* "k" flag */
-#define CMD_FAST (1<<13)            /* "F" flag */
+#define CMD_ADMIN (1<<4)            /* "admin" flag */
+#define CMD_PUBSUB (1<<5)           /* "pub-sub" flag */
+#define CMD_NOSCRIPT (1<<6)         /* "no-script" flag */
+#define CMD_RANDOM (1<<7)           /* "random" flag */
+#define CMD_SORT_FOR_SCRIPT (1<<8)  /* "to-sort" flag */
+#define CMD_LOADING (1<<9)          /* "ok-loading" flag */
+#define CMD_STALE (1<<10)           /* "ok-stale" flag */
+#define CMD_SKIP_MONITOR (1<<11)    /* "no-monitor" flag */
+#define CMD_ASKING (1<<12)          /* "cluster-asking" flag */
+#define CMD_FAST (1<<13)            /* "fast" flag */
+
+/* Command flags used by the module system. */
 #define CMD_MODULE_GETKEYS (1<<14)  /* Use the modules getkeys interface. */
 #define CMD_MODULE_NO_CLUSTER (1<<15) /* Deny on Redis Cluster. */
 
