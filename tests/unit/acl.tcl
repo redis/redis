3c67c1c3940e13351eab2310681bef09b3c3642d
antirez <antirez@gmail.com>
2019-01-28 11:44:10 +0100

    ACL: Test: enabled/disabled user.

--- tests/unit/acl.tcl
+++ tests/unit/acl.tcl
@@ -2,4 +2,19 @@ start_server {tags {"acl"}} {
     test {Connections start with the default user} {
         r ACL WHOAMI
     } {default}
+
+    test {It is possible to create new users} {
+        r ACL setuser newuser
+    }
+
+    test {New users start disabled} {
+        r ACL setuser newuser >passwd1
+        catch {r AUTH newuser passwd1} err
+        set err
+    } {*WRONGPASS*}
+
+    test {Enabling the user allows the login} {
+        r ACL setuser newuser on
+        r AUTH newuser passwd1
+    } {OK}
 }
