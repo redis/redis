5f450e49280ce01d5329798d87136f109761433a
antirez <antirez@gmail.com>
2019-07-18 18:59:38 +0200

    RDB: make sure to abort on LZF encoding error.

--- src/rdb.c
+++ src/rdb.c
@@ -386,8 +386,7 @@ void *rdbLoadLzfStringObject(rio *rdb, int flags, size_t *lenptr) {
     /* Load the compressed representation and uncompress it to target. */
     if (rioRead(rdb,c,clen) == 0) goto err;
     if (lzf_decompress(c,clen,val,len) == 0) {
-        if (rdbCheckMode) rdbCheckSetError("Invalid LZF compressed string");
-        goto err;
+        rdbExitReportCorruptRDB("Invalid LZF compressed string");
     }
     zfree(c);
 
