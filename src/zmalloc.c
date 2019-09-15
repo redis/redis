819a661be5efcb045be4d34d7269ab35dc0fcb88
David Carlier <devnexen@gmail.com>
2019-09-15 14:05:00 +0100

    Getting region date per process in Darwin

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -396,6 +396,18 @@ size_t zmalloc_get_smap_bytes_by_field(char *field, long pid) {
 }
 #else
 size_t zmalloc_get_smap_bytes_by_field(char *field, long pid) {
+#if defined(__APPLE__)
+    struct proc_regioninfo pri;
+    if (proc_pidinfo(pid, PROC_PIDREGIONINFO, 0, &pri, PROC_PIDREGIONINFO_SIZE) ==
+	PROC_PIDREGIONINFO_SIZE) {
+	if (!strcmp(field, "Private_Dirty")) {
+            return (size_t)pri.pri_pages_dirtied * 4096;
+	} else if (!strcmp(field, "Rss")) {
+            return (size_t)pri.pri_pages_resident * 4096;
+	}
+    }
+    return 0;
+#endif
     ((void) field);
     ((void) pid);
     return 0;
