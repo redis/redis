1b8b8c029fa09511bfeb01c1808456de7a28565d
antirez <antirez@gmail.com>
2019-10-03 18:44:47 +0200

    Modules: add RM_Replicate() Tcl test file & fix the module.

--- tests/modules/propagate.c
+++ tests/modules/propagate.c
@@ -61,6 +61,7 @@ void timerHandler(RedisModuleCtx *ctx, void *data) {
 void *threadMain(void *arg) {
     REDISMODULE_NOT_USED(arg);
     RedisModuleCtx *ctx = RedisModule_GetThreadSafeContext(NULL);
+    RedisModule_SelectDb(ctx,9); /* Tests ran in database number 9. */
     for (int i = 0; i < 10; i++) {
         RedisModule_ThreadSafeContextLock(ctx);
         RedisModule_Replicate(ctx,"INCR","c","thread");
