37bf3e18cbc4db5538940ac4a6006627122e1c17
antirez <antirez@gmail.com>
2019-10-31 12:31:22 +0100

    Modules: block ok keys: improve example.

--- src/modules/hellotype.c
+++ src/modules/hellotype.c
@@ -244,12 +244,20 @@ int HelloTypeBRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, 
         return RedisModule_ReplyWithError(ctx,REDISMODULE_ERRORMSG_WRONGTYPE);
     }
 
+    /* Parse the timeout before even trying to serve the client synchronously,
+     * so that we always fail ASAP on syntax errors. */
     long long timeout;
     if (RedisModule_StringToLongLong(argv[4],&timeout) != REDISMODULE_OK) {
         return RedisModule_ReplyWithError(ctx,
             "ERR invalid timeout parameter");
     }
 
+    /* Can we serve the reply synchronously? */
+    if (type != REDISMODULE_KEYTYPE_EMPTY) {
+        return HelloTypeRange_RedisCommand(ctx,argv,argc-1);
+    }
+
+    /* Otherwise let's block on the key. */
     void *privdata = RedisModule_Alloc(100);
     RedisModule_BlockClientOnKeys(ctx,HelloBlock_Reply,HelloBlock_Timeout,HelloBlock_FreeData,timeout,argv+1,1,privdata);
     return REDISMODULE_OK;
