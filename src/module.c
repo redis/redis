da2906e507d63bdb9511e3fbce83c0165500b779
Guy Benoish <guy.benoish@redislabs.com>
2019-11-06 15:48:46 +0530

    Support streams in general module API functions
    Fixes GitHub issue #6492
    Added stream support in RM_KeyType and RM_ValueLength.
    Also moduleDelKeyIfEmpty was updated, even though it has
    no effect now (It will be relevant when stream type direct
    API will be coded - i.e. RM_StreamAdd)
    
    cherry picked from commit 1833d008b3af8628835b5f082c5b4b1359557893
    * modified to avoid adding new API to 5.0 (reverting the change to
      RM_KeyType)

--- src/module.c
+++ src/module.c
@@ -471,7 +471,8 @@ int moduleDelKeyIfEmpty(RedisModuleKey *key) {
     case OBJ_LIST: isempty = listTypeLength(o) == 0; break;
     case OBJ_SET: isempty = setTypeSize(o) == 0; break;
     case OBJ_ZSET: isempty = zsetLength(o) == 0; break;
-    case OBJ_HASH : isempty = hashTypeLength(o) == 0; break;
+    case OBJ_HASH: isempty = hashTypeLength(o) == 0; break;
+    case OBJ_STREAM: isempty = streamLength(o) == 0; break;
     default: isempty = 0;
     }
 
@@ -1684,6 +1685,7 @@ int RM_KeyType(RedisModuleKey *key) {
     case OBJ_ZSET: return REDISMODULE_KEYTYPE_ZSET;
     case OBJ_HASH: return REDISMODULE_KEYTYPE_HASH;
     case OBJ_MODULE: return REDISMODULE_KEYTYPE_MODULE;
+    /* case OBJ_STREAM: return REDISMODULE_KEYTYPE_STREAM; - don't wanna add new API to 5.0 */
     default: return 0;
     }
 }
@@ -1701,6 +1703,7 @@ size_t RM_ValueLength(RedisModuleKey *key) {
     case OBJ_SET: return setTypeSize(key->value);
     case OBJ_ZSET: return zsetLength(key->value);
     case OBJ_HASH: return hashTypeLength(key->value);
+    case OBJ_STREAM: return streamLength(key->value);
     default: return 0;
     }
 }
