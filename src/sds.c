28120f4322112932fd70200c00a5526260a1383e
XiongKezhi <august.xkz@gmail.com>
2019-11-11 09:49:34 +0800

    simplify code in sds.c:sdsrange

--- src/sds.c
+++ src/sds.c
@@ -748,18 +748,15 @@ void sdsrange(sds s, ssize_t start, ssize_t end) {
         end = len+end;
         if (end < 0) end = 0;
     }
-    newlen = (start > end) ? 0 : (end-start)+1;
-    if (newlen != 0) {
-        if (start >= (ssize_t)len) {
-            newlen = 0;
-        } else if (end >= (ssize_t)len) {
-            end = len-1;
-            newlen = (start > end) ? 0 : (end-start)+1;
-        }
-    } else {
-        start = 0;
+    if (end >= (ssize_t)len) {
+        end = len - 1;
     }
-    if (start && newlen) memmove(s, s+start, newlen);
+    if (start >= (ssize_t)len) {
+        newlen = 0;
+    } else {
+        newlen = (start > end) ? 0 : (end-start)+1;
+    }
+    if (newlen) memmove(s, s+start, newlen);
     s[newlen] = 0;
     sdssetlen(s,newlen);
 }
