e69331cf11057b82db9f003576fe3823f424c170
tianli <tianli@nequal.com>
2019-12-12 10:22:02 +0800

    延时做slave的migration

--- src/cluster.c
+++ src/cluster.c
@@ -3621,7 +3621,7 @@ void clusterCron(void) {
          * the orphaned masters. Note that it does not make sense to try
          * a migration if there is no master with at least *two* working
          * slaves. */
-        if (orphaned_masters && max_slaves >= 2 && this_slaves == max_slaves)
+        if (orphaned_masters && max_slaves >= 2 && this_slaves == max_slaves && server.repl_state == REPL_STATE_CONNECTED)
             clusterHandleSlaveMigration(max_slaves);
     }
 
