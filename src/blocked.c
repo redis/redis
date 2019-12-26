1f75ce30dfdb1c8c4df8474f9580f2aff032cfb4
Guy Benoish <guy.benoish@redislabs.com>
2019-12-26 15:31:37 +0530

    Stream: Handle streamID-related edge cases
    This commit solves several edge cases that are related to
    exhausting the streamID limits: We should correctly calculate
    the succeeding streamID instead of blindly incrementing 'seq'
    This affects both XREAD and XADD.
    
    Other (unrelated) changes:
    Reply with a better error message when trying to add an entry
    to a stream that has exhausted last_id

--- src/blocked.c
+++ src/blocked.c
@@ -388,7 +388,7 @@ void serveClientsBlockedOnStreamKey(robj *o, readyList *rl) {
 
             if (streamCompareID(&s->last_id, gt) > 0) {
                 streamID start = *gt;
-                start.seq++; /* Can't overflow, it's an uint64_t */
+                streamIncrID(&start);
 
                 /* Lookup the consumer for the group, if any. */
                 streamConsumer *consumer = NULL;
