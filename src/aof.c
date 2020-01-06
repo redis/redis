8ee3bddfc7792a8805a9da9eab9bba848a2b066b
WuYunlong <xzsyeb@126.com>
2020-01-06 19:56:50 +0800

    Fix potential memory leak of rioWriteBulkStreamID().

--- src/aof.c
+++ src/aof.c
@@ -1127,7 +1127,10 @@ int rioWriteBulkStreamID(rio *r,streamID *id) {
     int retval;
 
     sds replyid = sdscatfmt(sdsempty(),"%U-%U",id->ms,id->seq);
-    if ((retval = rioWriteBulkString(r,replyid,sdslen(replyid))) == 0) return 0;
+    if ((retval = rioWriteBulkString(r,replyid,sdslen(replyid))) == 0) {
+        sdsfree(replyid);
+        return 0;
+    }
     sdsfree(replyid);
     return retval;
 }
