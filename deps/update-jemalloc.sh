2b80767fbbed99ae7fc19f680897a024f51a908f
Macide Celik <54390546+BlackAngel35@users.noreply.github.com>
2020-01-10 22:53:08 +0300

    Delete update-jemalloc.sh

--- deps/update-jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VER=$1
-URL="http://www.canonware.com/download/jemalloc/jemalloc-${VER}.tar.bz2"
-echo "Downloading $URL"
-curl $URL > /tmp/jemalloc.tar.bz2
-tar xvjf /tmp/jemalloc.tar.bz2
-rm -rf jemalloc
-mv jemalloc-${VER} jemalloc
-echo "Use git status, add all files and commit changes."
