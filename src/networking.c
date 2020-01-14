b4bc2c28c3712bd938179d2f6a82b47e99331a4e
Slogen <huanglaoxie2607@gmail.com>
2020-01-14 12:44:13 +0800

    start io thread after setting io_threads_pending
    start io thread after setting io_threads_pending avoid io thread empty loop

--- src/networking.c
+++ src/networking.c
@@ -2795,9 +2795,6 @@ int handleClientsWithPendingWritesUsingThreads(void) {
         return handleClientsWithPendingWrites();
     }
 
-    /* Start threads if needed. */
-    if (!io_threads_active) startThreadedIO();
-
     if (tio_debug) printf("%d TOTAL WRITE pending clients\n", processed);
 
     /* Distribute the clients across N different lists. */
@@ -2820,6 +2817,9 @@ int handleClientsWithPendingWritesUsingThreads(void) {
         int count = listLength(io_threads_list[j]);
         io_threads_pending[j] = count;
     }
+    
+    /* Start threads if needed. */
+    if (!io_threads_active) startThreadedIO();
 
     /* Also use the main thread to process a slice of clients. */
     listRewind(io_threads_list[0],&li);
