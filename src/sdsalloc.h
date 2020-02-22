743cfc0ad604c48bfa8c408ca36c6a5b90996aad
hwware <wen.hui.ware@gmail.com>
2020-02-22 11:38:51 -0500

    add missing file marco

--- src/sdsalloc.h
+++ src/sdsalloc.h
@@ -36,7 +36,12 @@
  * the include of your alternate allocator if needed (not needed in order
  * to use the default libc allocator). */
 
+#ifndef __SDS_ALLOC_H__
+#define __SDS_ALLOC_H__
+
 #include "zmalloc.h"
 #define s_malloc zmalloc
 #define s_realloc zrealloc
 #define s_free zfree
+
+#endif
