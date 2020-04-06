25063f75d73a4c40b9cd7b0229fc63d6cec15e9a
mymilkbottles <javaxz@163.com>
2020-04-06 19:27:06 +0800

    Judge the log level in advance

--- src/scripting.c
+++ src/scripting.c
@@ -959,6 +959,7 @@ int luaLogCommand(lua_State *lua) {
         lua_pushstring(lua, "Invalid debug level.");
         return lua_error(lua);
     }
+    if (level < server.verbosity) return 0;
 
     /* Glue together all the arguments */
     log = sdsempty();
