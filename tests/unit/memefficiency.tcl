6148f949392aa060db5d4de68966dfbc03cda212
Oran Agra <oran@redislabs.com>
2020-04-16 11:05:03 +0300

    testsuite run the defrag latency test solo
    this test is time sensitive and it sometimes fail to pass below the
    latency threshold, even on strong machines.
    
    this test was the reson we're running just 2 parallel tests in the
    github actions CI, revering this.

--- tests/unit/memefficiency.tcl
+++ tests/unit/memefficiency.tcl
@@ -36,6 +36,7 @@ start_server {tags {"memefficiency"}} {
     }
 }
 
+run_solo {defrag} {
 start_server {tags {"defrag"}} {
     if {[string match {*jemalloc*} [s mem_allocator]]} {
         test "Active defrag" {
@@ -328,3 +329,4 @@ start_server {tags {"defrag"}} {
         } {1}
     }
 }
+} ;# run_solo
