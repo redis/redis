c7db333abb0e45ff8974ef2d1fc4f9ae1e7be1e2
antirez <antirez@gmail.com>
2020-04-20 12:17:11 +0200

    Implement redis_set_thread_title for MacOS.
    Strange enough, pthread_setname_np() produces a warning for not defined
    function even if pthread is included. Moreover the MacOS documentation
    claims the return value for the function is void, but actually is int.
    
    Related to #7089.

--- src/config.h
+++ src/config.h
@@ -234,7 +234,13 @@ void setproctitle(const char *fmt, ...);
 #include <pthread_np.h>
 #define redis_set_thread_title(name) pthread_set_name_np(pthread_self(), name)
 #else
+#if (defined __APPLE__ && defined(MAC_OS_X_VERSION_10_7))
+int pthread_setname_np(const char *name);
+#include <pthread.h>
+#define redis_set_thread_title(name) pthread_setname_np(name)
+#else
 #define redis_set_thread_title(name)
+#endif
 #endif
 #endif
 
