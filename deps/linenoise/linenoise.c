96193503ee32d8911e8a50804d81f1544f943034
Theo Buehler <theo.buehler@cysec.systems>
2020-04-22 10:29:03 +0200

    Fix signed char issue on unsigned char archs
    On some architectures like powerpc or arm*, char is unsigned.
    There is an if (c < 0) check a few lines below which is always
    false on these architectures.

--- deps/linenoise/linenoise.c
+++ deps/linenoise/linenoise.c
@@ -819,7 +819,7 @@ static int linenoiseEdit(int stdin_fd, int stdout_fd, char *buf, size_t buflen, 
 
     if (write(l.ofd,prompt,l.plen) == -1) return -1;
     while(1) {
-        char c;
+        signed char c;
         int nread;
         char seq[3];
 
