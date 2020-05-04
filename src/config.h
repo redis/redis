6fe2b5236d2e7aeda2a99ae59ed3f1a7d8427a68
antirez <antirez@gmail.com>
2020-05-04 11:08:57 +0200

    Fix NetBSD build by fixing redis_set_thread_title() support.
    See #7188.

--- src/config.h
+++ src/config.h
@@ -230,9 +230,12 @@ void setproctitle(const char *fmt, ...);
 #ifdef __linux__
 #define redis_set_thread_title(name) pthread_setname_np(pthread_self(), name)
 #else
-#if (defined __NetBSD__ || defined __FreeBSD__ || defined __OpenBSD__)
+#if (defined __FreeBSD__ || defined __OpenBSD__)
 #include <pthread_np.h>
 #define redis_set_thread_title(name) pthread_set_name_np(pthread_self(), name)
+#elif defined __NetBSD__
+#include <pthread.h>
+#define redis_set_thread_title(name) pthread_setname_np(pthread_self(), name, NULL)
 #else
 #if (defined __APPLE__ && defined(MAC_OS_X_VERSION_10_7))
 int pthread_setname_np(const char *name);
