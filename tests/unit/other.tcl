a2ae463520fdd794f4458e5ed5ad2d6fe8a028ba
Oran Agra <oran@redislabs.com>
2020-05-26 11:00:48 +0300

    tests: each test client work on a distinct port range
    apparently when running tests in parallel (the default of --clients 16),
    there's a chance for two tests to use the same port.
    specifically, one test might shutdown a master and still have the
    replica up, and then another test will re-use the port number of master
    for another master, and then that replica will connect to the master of
    the other test.
    
    this can cause a master to count too many full syncs and fail a test if
    we run the tests with --single integration/psync2 --loop --stop
    
    see Probmem 2 in #7314

--- tests/unit/other.tcl
+++ tests/unit/other.tcl
@@ -167,9 +167,9 @@ start_server {tags {"other"}} {
     tags {protocol} {
         test {PIPELINING stresser (also a regression for the old epoll bug)} {
             if {$::tls} {
-                set fd2 [::tls::socket $::host $::port]
+                set fd2 [::tls::socket [srv host] [srv port]]
             } else {
-                set fd2 [socket $::host $::port]
+                set fd2 [socket [srv host] [srv port]]
             }
             fconfigure $fd2 -encoding binary -translation binary
             puts -nonewline $fd2 "SELECT 9\r\n"
