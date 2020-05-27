963bab9f682c1df69d38611e0ca9d9c751bcd753
ShooterIT <wangyuancode@163.com>
2020-05-27 15:00:15 +0800

    Fix applying zero offset to null pointer when creating moduleFreeContextReusedClient
    Before this fix we where attempting to select a db before creating db the DB, see: #7323
    
    This issue doesn't seem to have any implications, since the selected DB index is 0,
    the db pointer remains NULL, and will later be correctly set before using this dummy
    client for the first time.
    
    As we know, we call 'moduleInitModulesSystem()' before 'initServer()'. We will allocate
    memory for server.db in 'initServer', but we call 'createClient()' that will call 'selectDb()'
    in 'moduleInitModulesSystem()', before the databases where created. Instead, we should call
    'createClient()' for moduleFreeContextReusedClient after 'initServer()'.

--- src/server.c
+++ src/server.c
@@ -5137,6 +5137,7 @@ int main(int argc, char **argv) {
     #ifdef __linux__
         linuxMemoryWarnings();
     #endif
+        moduleInitModulesSystemLast();
         moduleLoadFromQueue();
         ACLLoadUsersAtStartup();
         InitServerLast();
