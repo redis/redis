05e483cbb32875a90e9bf4ae8b5d359bf16bc250
Oran Agra <oran@redislabs.com>
2020-06-11 21:09:35 +0300

    EXEC always fails with EXECABORT and multi-state is cleared
    In order to support the use of multi-exec in pipeline, it is important that
    MULTI and EXEC are never rejected and it is easy for the client to know if the
    connection is still in multi state.
    
    It was easy to make sure MULTI and DISCARD never fail (done by previous
    commits) since these only change the client state and don't do any actual
    change in the server, but EXEC is a different story.
    
    Since in the past, it was possible for clients to handle some EXEC errors and
    retry the EXEC, we now can't affort to return any error on EXEC other than
    EXECABORT, which now carries with it the real reason for the abort too.
    
    Other fixes in this commit:
    - Some checks that where performed at the time of queuing need to be re-
      validated when EXEC runs, for instance if the transaction contains writes
      commands, it needs to be aborted. there was one check that was already done
      in execCommand (-READONLY), but other checks where missing: -OOM, -MISCONF,
      -NOREPLICAS, -MASTERDOWN
    - When a command is rejected by processCommand it was rejected with addReply,
      which was not recognized as an error in case the bad command came from the
      master. this will enable to count or MONITOR these errors in the future.
    - make it easier for tests to create additional (non deferred) clients.
    - add tests for the fixes of this commit.
    
    (cherry picked from commit 65a3307bc95aadbc91d85cdf9dfbe1b3493222ca)

--- src/networking.c
+++ src/networking.c
@@ -407,19 +407,23 @@ void addReplyError(client *c, const char *err) {
     addReplyErrorLength(c,err,strlen(err));
 }
 
+/* See addReplyErrorLength.
+ * Makes sure there are no newlines in the string, otherwise invalid protocol
+ * is emitted. */
+void addReplyErrorSafe(client *c, char *s, size_t len) {
+    size_t j;
+    for (j = 0; j < len; j++) {
+        if (s[j] == '\r' || s[j] == '\n') s[j] = ' ';
+    }
+    addReplyErrorLength(c,s,sdslen(s));
+}
+
 void addReplyErrorFormat(client *c, const char *fmt, ...) {
-    size_t l, j;
     va_list ap;
     va_start(ap,fmt);
     sds s = sdscatvprintf(sdsempty(),fmt,ap);
     va_end(ap);
-    /* Make sure there are no newlines in the string, otherwise invalid protocol
-     * is emitted. */
-    l = sdslen(s);
-    for (j = 0; j < l; j++) {
-        if (s[j] == '\r' || s[j] == '\n') s[j] = ' ';
-    }
-    addReplyErrorLength(c,s,sdslen(s));
+    addReplyErrorSafe(c, s, sdslen(s));
     sdsfree(s);
 }
 
