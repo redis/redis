7526e450676164481309a5d11213c9d2ed40df1e
Tomasz Poradowski <tomasz.poradowski@generiscorp.com>
2020-06-17 22:22:49 +0200

    ensure SHUTDOWN_NOSAVE in Sentinel mode
    - enforcing of SHUTDOWN_NOSAVE flag in one place to make it consitent
      when running in Sentinel mode
    
    (cherry picked from commit 4ee011adb5f0d56085ecd3e5d643ab192ef77ce6)

--- src/db.c
+++ src/db.c
@@ -963,14 +963,6 @@ void shutdownCommand(client *c) {
             return;
         }
     }
-    /* When SHUTDOWN is called while the server is loading a dataset in
-     * memory we need to make sure no attempt is performed to save
-     * the dataset on shutdown (otherwise it could overwrite the current DB
-     * with half-read data).
-     *
-     * Also when in Sentinel mode clear the SAVE flag and force NOSAVE. */
-    if (server.loading || server.sentinel_mode)
-        flags = (flags & ~SHUTDOWN_SAVE) | SHUTDOWN_NOSAVE;
     if (prepareForShutdown(flags) == C_OK) exit(0);
     addReplyError(c,"Errors trying to SHUTDOWN. Check logs.");
 }
