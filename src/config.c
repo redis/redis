e202e725878732d569e3532de80cc753bf53cc9d
michalbiesek <michal.biesek@intel.com>
2020-06-19 12:31:17 +0200

    Fix coding style - remove unnecessary space

--- src/config.c
+++ src/config.c
@@ -1276,7 +1276,7 @@ void configGetCommand(client *c) {
         sdsfree(buf);
         matches++;
     }
-    if  (stringmatch(pattern,"dram-pmem-ratio",1)) {
+    if (stringmatch(pattern,"dram-pmem-ratio",1)) {
         char buf[32];
         snprintf(buf,sizeof(buf),"%d %d", server.dram_pmem_ratio.dram_val, server.dram_pmem_ratio.pmem_val);
         addReplyBulkCString(c,"dram-pmem-ratio");
