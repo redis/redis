796b1cc703437dd198d2e3ed12fd3fbe49093899
WuYunlong <xzsyeb@126.com>
2020-07-07 13:19:21 +0800

    Add tcl tests for XGROUP HELP, LATENCY HELP and ACL HELP with unexpected options.

--- tests/unit/latency-monitor.tcl
+++ tests/unit/latency-monitor.tcl
@@ -61,4 +61,9 @@ start_server {tags {"latency-monitor"}} {
         after 500
         assert_match {*expire-cycle*} [r latency latest]
     }
+
+    test {LATENCY HELP should not have unexpected options} {
+        catch {r LATENCY help xxx} e
+        assert_match "*Unknown subcommand or wrong number of arguments*" $e
+    }
 }
