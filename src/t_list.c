09081a2adbe5f72497a7d750321c9b28ad7026e9
Ning <iscodd@gmail.com>
2020-08-07 20:07:03 +0800

    Update t_list.c
    use checkType

--- src/t_list.c
+++ src/t_list.c
@@ -198,8 +198,7 @@ void pushGenericCommand(client *c, int where) {
     int j, pushed = 0;
     robj *lobj = lookupKeyWrite(c->db,c->argv[1]);
 
-    if (lobj && lobj->type != OBJ_LIST) {
-        addReply(c,shared.wrongtypeerr);
+    if (lobj && checkType(c,lobj,OBJ_ZSET)) {
         return;
     }
 
