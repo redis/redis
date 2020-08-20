7fa210c7dde5e3cd4cb1684f117c585d800f3383
huangzhw <huang_zhw@126.com>
2020-08-20 15:07:24 +0800

    When calculating progress in processModuleLoadingProgressEvent, we should use server.loading_loaded_bytes. Otherwise, progress will always be 1024.

--- src/module.c
+++ src/module.c
@@ -7262,7 +7262,7 @@ void processModuleLoadingProgressEvent(int is_aof) {
         /* Fire the loading progress modules end event. */
         int progress = -1;
         if (server.loading_total_bytes)
-            progress = (server.loading_total_bytes<<10) / server.loading_total_bytes;
+            progress = (server.loading_loaded_bytes<<10) / server.loading_total_bytes;
         RedisModuleLoadingProgressV1 fi = {REDISMODULE_LOADING_PROGRESS_VERSION,
                                      server.hz,
                                      progress};
