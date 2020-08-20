6f206557037e8eb2774276cf9ca97d96120d3a43
杨博东 <bodong.ybd@alibaba-inc.com>
2020-08-20 13:59:02 +0800

    Fix flock cluster config may cause failure to restart after kill -9 (#7674)
    After fork, the child process(redis-aof-rewrite) will get the fd opened
    by the parent process(redis), when redis killed by kill -9, it will not
    graceful exit(call prepareForShutdown()), so redis-aof-rewrite thread may still
    alive, the fd(lock) will still be held by redis-aof-rewrite thread, and
    redis restart will fail to get lock, means fail to start.
    
    This issue was causing failures in the cluster tests in github actions.
    
    Co-authored-by: Oran Agra <oran@redislabs.com>
    (cherry picked from commit cbaf3c5bbafd43e009a2d6b38dd0e9fc450a3e12)

--- src/server.c
+++ src/server.c
@@ -4823,13 +4823,23 @@ void setupChildSignalHandlers(void) {
     return;
 }
 
+/* After fork, the child process will inherit the resources
+ * of the parent process, e.g. fd(socket or flock) etc.
+ * should close the resources not used by the child process, so that if the
+ * parent restarts it can bind/lock despite the child possibly still running. */
+void closeClildUnusedResourceAfterFork() {
+    closeListeningSockets(0);
+    if (server.cluster_enabled && server.cluster_config_file_lock_fd != -1)
+        close(server.cluster_config_file_lock_fd);  /* don't care if this fails */
+}
+
 int redisFork() {
     int childpid;
     long long start = ustime();
     if ((childpid = fork()) == 0) {
         /* Child */
-        closeListeningSockets(0);
         setupChildSignalHandlers();
+        closeClildUnusedResourceAfterFork();
     } else {
         /* Parent */
         server.stat_fork_time = ustime()-start;
