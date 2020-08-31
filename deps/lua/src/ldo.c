faac6e07a1f25c1e7a85a01cbbf39d07e880628a
Yossi Gottlieb <yossigo@gmail.com>
2020-08-31 20:42:46 +0300

    Backport Lua 5.2.2 stack overflow fix. (#7733)
    This fixes the issue described in CVE-2014-5461. At this time we cannot
    confirm that the original issue has a real impact on Redis, but it is
    included as an extra safety measure.
    
    (cherry picked from commit d75ad774a92bd7de0b9448be3d622d7a13b7af27)
    (cherry picked from commit 941174d9c9ed438f1c70dd46cddc02468614db12)

--- deps/lua/src/ldo.c
+++ deps/lua/src/ldo.c
@@ -274,7 +274,7 @@ int luaD_precall (lua_State *L, StkId func, int nresults) {
     CallInfo *ci;
     StkId st, base;
     Proto *p = cl->p;
-    luaD_checkstack(L, p->maxstacksize);
+    luaD_checkstack(L, p->maxstacksize + p->numparams);
     func = restorestack(L, funcr);
     if (!p->is_vararg) {  /* no varargs? */
       base = func + 1;
