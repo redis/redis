6a4da4958e7d21eb892ac2a5201b4936b17e57ee
Ariel Shtul <ashtul@gmail.com>
2020-09-22 10:18:07 +0300

    Fix redis-check-rdb support for modules aux data (#7826)
    redis-check-rdb was unable to parse rdb files containing module aux data.
    
    Co-authored-by: Oran Agra <oran@redislabs.com>
    (cherry picked from commit 63a05dde462c1be4bd74c32630eca6e794ae440a)

--- src/redis-check-rdb.c
+++ src/redis-check-rdb.c
@@ -58,6 +58,7 @@ struct {
 #define RDB_CHECK_DOING_CHECK_SUM 5
 #define RDB_CHECK_DOING_READ_LEN 6
 #define RDB_CHECK_DOING_READ_AUX 7
+#define RDB_CHECK_DOING_READ_MODULE_AUX 8
 
 char *rdb_check_doing_string[] = {
     "start",
@@ -67,7 +68,8 @@ char *rdb_check_doing_string[] = {
     "read-object-value",
     "check-sum",
     "read-len",
-    "read-aux"
+    "read-aux",
+    "read-module-aux"
 };
 
 char *rdb_type_string[] = {
@@ -271,6 +273,21 @@ int redis_check_rdb(char *rdbfilename, FILE *fp) {
                 (char*)auxkey->ptr, (char*)auxval->ptr);
             decrRefCount(auxkey);
             decrRefCount(auxval);
+            continue; /* Read type again. */
+        } else if (type == RDB_OPCODE_MODULE_AUX) {
+            /* AUX: Auxiliary data for modules. */
+            uint64_t moduleid, when_opcode, when;
+            rdbstate.doing = RDB_CHECK_DOING_READ_MODULE_AUX;
+            if ((moduleid = rdbLoadLen(&rdb,NULL)) == RDB_LENERR) goto eoferr;
+            if ((when_opcode = rdbLoadLen(&rdb,NULL)) == RDB_LENERR) goto eoferr;
+            if ((when = rdbLoadLen(&rdb,NULL)) == RDB_LENERR) goto eoferr;
+
+            char name[10];
+            moduleTypeNameByID(name,moduleid);
+            rdbCheckInfo("MODULE AUX for: %s", name);
+
+            robj *o = rdbLoadCheckModuleValue(&rdb,name);
+            decrRefCount(o);
             continue; /* Read type again. */
         } else {
             if (!rdbIsObjectType(type)) {
