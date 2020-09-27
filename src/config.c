7dad3280b056a7d7da96732393bf9d1c42668545
caozhengbin <caozb@yidingyun.com>
2020-09-27 10:55:45 +0800

    skip `replicaof no one` and check masterport when loading redis.conf

--- src/config.c
+++ src/config.c
@@ -472,11 +472,16 @@ void loadServerConfigFromString(char *config) {
              server.client_max_querybuf_len = memtoll(argv[1],NULL);
         } else if ((!strcasecmp(argv[0],"slaveof") ||
                     !strcasecmp(argv[0],"replicaof")) && argc == 3) {
+            if (!strcasecmp(argv[1], "no") && !strcasecmp(argv[2], "one")) {
+                server.masterhost = NULL;
+                continue;
+            }
             slaveof_linenum = linenum;
             server.masterhost = sdsnew(argv[1]);
-            server.masterport = atoi(argv[2]);
-            if (server.masterport == 0){
-                goto loaderr;
+            char *ptr;
+            server.masterport = strtol(argv[2], &ptr, 10);
+            if (server.masterport < 0 || server.masterport > 65535 || strlen(ptr) != 0) {
+                err = "Invalid master port";goto loaderr;
             }
             server.repl_state = REPL_STATE_CONNECT;
         } else if (!strcasecmp(argv[0],"requirepass") && argc == 2) {
