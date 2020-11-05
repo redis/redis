a56bb0a92101b5b57f6e143a3e582ad56f227242
hwware <wen.hui.ware@gmail.com>
2020-11-05 11:42:11 -0500

    provide getRangeLongFromObjectOrReply and getPositiveLongFromObjectOrReply for range check
    remove empty line

--- src/debug.c
+++ src/debug.c
@@ -600,22 +600,14 @@ NULL
         robj *key, *val;
         char buf[128];
 
-        if (getLongFromObjectOrReply(c, c->argv[2], &keys, NULL) != C_OK)
+        if (getPositiveLongFromObjectOrReply(c, c->argv[2], &keys, NULL) != C_OK)
             return;
 
-        if (keys < 0) {
-            addReplyError(c,"COUNT must be larger than 0.");
-            return;
-        }
         dictExpand(c->db->dict,keys);
         long valsize = 0;
-        if ( c->argc == 5 && getLongFromObjectOrReply(c, c->argv[4], &valsize, NULL) != C_OK ) 
+        if ( c->argc == 5 && getPositiveLongFromObjectOrReply(c, c->argv[4], &valsize, NULL) != C_OK ) 
             return;
 
-        if (valsize < 0) {
-            addReplyError(c,"Value size must be larger than 0.");
-            return;
-        }
         for (j = 0; j < keys; j++) {
             snprintf(buf,sizeof(buf),"%s:%lu",
                 (c->argc == 3) ? "key" : (char*)c->argv[3]->ptr, j);
