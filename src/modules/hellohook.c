f76734b0b6ac2683e5ea2ed027fb6b71a887faf5
sundb <sundbcn@gmail.com>
2020-11-12 13:29:47 +0800

    Fix build warning in hellohook,c and testmodule.c

--- src/modules/hellohook.c
+++ src/modules/hellohook.c
@@ -44,7 +44,7 @@ void clientChangeCallback(RedisModuleCtx *ctx, RedisModuleEvent e, uint64_t sub,
     REDISMODULE_NOT_USED(e);
 
     RedisModuleClientInfo *ci = data;
-    printf("Client %s event for client #%llu %s:%d\n",
+    printf("Client %s event for client #%lu %s:%d\n",
         (sub == REDISMODULE_SUBEVENT_CLIENT_CHANGE_CONNECTED) ?
             "connection" : "disconnection",
         ci->id,ci->addr,ci->port);
