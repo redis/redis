8c42daec7081103c96a3e3ea80d8c138d3755590
sundb <sundbcn@gmail.com>
2020-11-12 19:56:16 +0800

    Avoid del notification if lrem remove nothing

--- src/t_list.c
+++ src/t_list.c
@@ -641,11 +641,11 @@ void lremCommand(client *c) {
     if (removed) {
         signalModifiedKey(c,c->db,c->argv[1]);
         notifyKeyspaceEvent(NOTIFY_LIST,"lrem",c->argv[1],c->db->id);
-    }
 
-    if (listTypeLength(subject) == 0) {
-        dbDelete(c->db,c->argv[1]);
-        notifyKeyspaceEvent(NOTIFY_GENERIC,"del",c->argv[1],c->db->id);
+        if (listTypeLength(subject) == 0) {
+            dbDelete(c->db,c->argv[1]);
+            notifyKeyspaceEvent(NOTIFY_GENERIC,"del",c->argv[1],c->db->id);
+        }
     }
 
     addReplyLongLong(c,removed);
