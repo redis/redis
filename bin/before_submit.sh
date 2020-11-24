b166dc9d527abdbcdf738fa65b620915773b5a7f
suitianxiang <tianxiang.sui@xcalibyte.com>
2020-11-24 18:09:39 +0800

    add fake task_summary.pdf, fix path problem

--- bin/before_submit.sh
+++ bin/before_submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp $HOME/build/knightley/travis_ci_xcalscan_cloud_integration/c_testcase/basic/.xcalagent/task_id1/task_summary.pdf .
+cp $HOME/build/xcalibyte-github/task_summary.pdf .
