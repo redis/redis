cf3d79d4c10c5ca4e485ce29c288825d582ca067
Yossi Gottlieb <yossigo@gmail.com>
2020-12-09 20:20:55 +0200

    Fix double close on IPv6 setup error. (#8168)
    Fixes #8165.

--- src/anet.c
+++ src/anet.c
@@ -470,7 +470,6 @@ static int anetV6Only(char *err, int s) {
     int yes = 1;
     if (setsockopt(s,IPPROTO_IPV6,IPV6_V6ONLY,&yes,sizeof(yes)) == -1) {
         anetSetError(err, "setsockopt: %s", strerror(errno));
-        close(s);
         return ANET_ERR;
     }
     return ANET_OK;
