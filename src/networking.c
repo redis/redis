3df0e146f6bd021960ebc18b7a4418863752c7a2
filipecosta90 <filipecosta.90@gmail.com>
2020-12-29 18:08:20 +0000

    [fix] Fixes per PR review: stickying * to the variable

--- src/networking.c
+++ src/networking.c
@@ -413,7 +413,7 @@ void afterErrorReply(client *c, const char *s, size_t len) {
     if (s[0] != '-') {
         incrementErrorCount("ERR", 3);
     } else {
-        char* spaceloc = memchr(s, ' ', len < 32 ? len : 32);
+        char *spaceloc = memchr(s, ' ', len < 32 ? len : 32);
         if (spaceloc) {
             const size_t errEndPos = (size_t)(spaceloc - s);
             incrementErrorCount(s+1, errEndPos-1);
