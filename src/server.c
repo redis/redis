74101dd8538dd1fc1bb43204614b5d5250ef3d70
meir@redislabs.com <meir@redislabs.com>
2021-01-04 13:23:35 +0200

    initialize blocking_op_nesting on initServer

--- src/server.c
+++ src/server.c
@@ -3008,6 +3008,7 @@ void initServer(void) {
     server.events_processed_while_blocked = 0;
     server.system_memory_size = zmalloc_get_memory_size();
     server.blocked_last_cron = 0;
+    server.blocking_op_nesting = 0;
 
     if ((server.tls_port || server.tls_replication || server.tls_cluster)
                 && tlsConfigure(&server.tls_ctx_config) == C_ERR) {
