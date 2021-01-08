e5d3cb7a85ca48a7302a96daef8a012d4eb2bd95
Gnanesh <gnaneshkunal@outlook.com>
2021-01-08 15:43:20 +0530

    Add additional negative value check for overflow detection

--- src/t_string.c
+++ src/t_string.c
@@ -75,6 +75,10 @@ void setGenericCommand(client *c, int flags, robj *key, robj *val, robj *expire,
     if (expire) {
         if (getLongLongFromObjectOrReply(c, expire, &milliseconds, NULL) != C_OK)
             return;
+        if (milliseconds <= 0) {
+            addReplyErrorFormat(c,"invalid expire time in %s",c->cmd->name);
+            return;
+        }
         if (unit == UNIT_SECONDS) milliseconds *= 1000;
         if (milliseconds <= 0) {
             addReplyErrorFormat(c,"invalid expire time in %s",c->cmd->name);
