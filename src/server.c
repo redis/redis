b24b49039348f7570a3e5e3673384080c093060d
Madelyn Olson <34459052+madolson@users.noreply.github.com>
2021-01-11 23:46:24 -0800

    Fix issues in wait test (#8310)
    This fixes three issues:
    1.  Using debug SLEEP was impacting the subsequent test, and causing it to pass reliably even though it should have failed. There was exactly 5 seconds of artificial pause (after 1000, wait 3000, wait 1000) between the debug sleep 5 and when we needed to unblock the client in the subsequent test. Now the test properly makes sure the client is unblocked, and the subsequent test is fixed.
    2. Minor, the client pause types were using & comparisons instead of ==, since it was previously a flag.
    3. Test is faster now that some of the hand wavy time is removed.

--- src/server.c
+++ src/server.c
@@ -4043,8 +4043,8 @@ int processCommand(client *c) {
     /* If the server is paused, block the client until
      * the pause has ended. Replicas are never paused. */
     if (!(c->flags & CLIENT_SLAVE) && 
-        ((server.client_pause_type & CLIENT_PAUSE_ALL) ||
-        (server.client_pause_type & CLIENT_PAUSE_WRITE && is_may_replicate_command)))
+        ((server.client_pause_type == CLIENT_PAUSE_ALL) ||
+        (server.client_pause_type == CLIENT_PAUSE_WRITE && is_may_replicate_command)))
     {
         c->bpop.timeout = 0;
         blockClient(c,BLOCKED_PAUSE);
