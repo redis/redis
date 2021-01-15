708b7bb2ad9a028308b87c0ec3ea35a7aa8ee560
charsyam <charsyam@naver.com>
2021-01-15 22:01:23 +0900

    change expression for port

--- src/cluster.c
+++ src/cluster.c
@@ -509,8 +509,7 @@ void clusterInit(void) {
         serverLog(LL_WARNING, "Redis port number too high. "
                    "Cluster communication port is 10,000 port "
                    "numbers higher than your Redis port. "
-                   "Your Redis port number must be "
-                   "lower than 55535.");
+                   "Your Redis port number must be 55535 or less.");
         exit(1);
     }
     if (listenToPort(port+CLUSTER_PORT_INCR,
