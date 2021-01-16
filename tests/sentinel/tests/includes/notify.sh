72294b524e09c954ebe026c40fb0f2171aaedeb3
Andy Pan <panjf2000@gmail.com>
2021-01-16 16:51:53 +0800

    Update the shell scripts

--- tests/sentinel/tests/includes/notify.sh
+++ tests/sentinel/tests/includes/notify.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-ls -l /proc/self/fd
+
+leaked_fd_count=`ls /proc/self/fd | grep -vE '0|1|2' | wc -l`
+
+if [ $leaked_fd_count -gt 0 ]
+then
+    echo -e '\nthere are leaked fds in sentinel, please fix it:'
+    echo 'Process ID:' $$
+    ls -l /proc/self/fd
+    lsof -p $$
+    exit 1
+else
+    echo 'fd leaks test passes.'
+fi
+    
