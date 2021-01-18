30561b4fa2d1c72f60418b46f4bf98fc07dac49b
Axlgrep <axlgrep@gmail.com>
2021-01-18 21:31:55 +0800

    redis cluster mode, when new master is specified during slave load rdb, should stop ASAP

--- src/rdb.c
+++ src/rdb.c
@@ -2362,6 +2362,13 @@ int rdbLoadRio(rio *rdb, int rdbflags, rdbSaveInfo *rsi) {
         sds key;
         robj *val;
 
+        /* When a new master is specified during slave rdbLoad, we should stop load
+         * the old RDB and sync new Master ASAP. */
+        if (rdbflags & RDBFLAGS_REPLICATION && server.repl_state != REPL_STATE_TRANSFER) {
+          serverLog(LL_WARNING, "Repl state changed, Stop load the RDB");
+          return C_ERR;
+        }
+
         /* Read type. */
         if ((type = rdbLoadType(rdb)) == -1) goto eoferr;
 
