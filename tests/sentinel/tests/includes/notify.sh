56dae0c5574bc6086d94da56a2181745d71bc26f
Andy Pan <panjf2000@gmail.com>
2021-01-20 00:31:52 +0800

    Make the test shell play to the score on different OS

--- tests/sentinel/tests/includes/notify.sh
+++ tests/sentinel/tests/includes/notify.sh
@@ -1,17 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # fd 3 is meant to catch the actual access to /proc/pid/fd, 
 # in case there's an fd leak by the sentinel,
 # it can take 3, but then the access to /proc will take another fd, and we'll catch that.
-leaked_fd_count=`ls /proc/self/fd | grep -vE '^[0|1|2|3]$' | wc -l`
+
+OS=`uname -s`
 
 sentinel_fd_leaks_file="../sentinel_fd_leaks"
 
-if [ $leaked_fd_count -gt 0 ]
+if [ ${OS} == "Linux" ]
 then
-    if [ ! -f $sentinel_fd_leaks_file ]
+    leaked_fd_count=`ls /proc/self/fd | grep -vE '^[0|1|2|3]$' | wc -l`
+    if [ $leaked_fd_count -gt 0 ]
     then
-        ls -l /proc/self/fd | cat >> $sentinel_fd_leaks_file
+        if [ ! -f $sentinel_fd_leaks_file ]
+        then
+            ls -l /proc/self/fd | cat >> $sentinel_fd_leaks_file
+        fi
     fi
+else
+    echo "OS: expected Linux, but got ${OS}."
 fi
+
+
     
