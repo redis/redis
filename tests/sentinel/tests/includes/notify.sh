a3ce93ce6653842abb9c311672c60b22dcad75a4
Oran Agra <oran@redislabs.com>
2021-01-19 21:23:56 +0200

    use early exit if not on Linux rather than indentation

--- tests/sentinel/tests/includes/notify.sh
+++ tests/sentinel/tests/includes/notify.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
+OS=`uname -s`
+if [ ${OS} == "Linux" ]
+then
+    exit 0
+fi
+
 # fd 3 is meant to catch the actual access to /proc/pid/fd, 
 # in case there's an fd leak by the sentinel,
 # it can take 3, but then the access to /proc will take another fd, and we'll catch that.
-
-OS=`uname -s`
-
-sentinel_fd_leaks_file="../sentinel_fd_leaks"
-
-if [ ${OS} == "Linux" ]
+leaked_fd_count=`ls /proc/self/fd | grep -vE '^[0|1|2|3]$' | wc -l`
+if [ $leaked_fd_count -gt 0 ]
 then
-    leaked_fd_count=`ls /proc/self/fd | grep -vE '^[0|1|2|3]$' | wc -l`
-    if [ $leaked_fd_count -gt 0 ]
+    sentinel_fd_leaks_file="../sentinel_fd_leaks"
+    if [ ! -f $sentinel_fd_leaks_file ]
     then
-        if [ ! -f $sentinel_fd_leaks_file ]
-        then
-            ls -l /proc/self/fd | cat >> $sentinel_fd_leaks_file
-        fi
+        ls -l /proc/self/fd | cat >> $sentinel_fd_leaks_file
     fi
-else
-    echo "OS: expected Linux, but got ${OS}."
 fi
