8d492904c55812f728c3d3d2a7a7368b27f53468
Raghav Muddur <r@nmvk.com>
2021-01-20 09:46:39 -0800

    Refactoring updates to getEXCommand
    Removed getexGenericCommand, update notifyKeyspaceEvent
    to single line.

--- src/t_string.c
+++ src/t_string.c
@@ -109,8 +109,7 @@ void setGenericCommand(client *c, int flags, robj *key, robj *val, robj *expire,
         } else {
             setExpire(c,c->db,key,milliseconds);
         }
-        notifyKeyspaceEvent(NOTIFY_GENERIC,
-                            "expire", key, c->db->id);
+        notifyKeyspaceEvent(NOTIFY_GENERIC,"expire",key,c->db->id);
 
         /* Propagate as SET Key Value PXAT millisecond-timestamp if there is EXAT/PXAT or
          * propagate as SET Key Value PX millisecond if there is EX/PX flag.
@@ -303,81 +302,6 @@ int getGenericCommand(client *c) {
     return C_OK;
 }
 
-int getexGenericCommand(client *c, int flags, robj *expire, int unit) {
-    robj *o;
-
-    if ((o = lookupKeyReadOrReply(c,c->argv[1],shared.null[c->resp])) == NULL)
-        return C_OK;
-
-    if (checkType(c,o,OBJ_STRING)) {
-        return C_ERR;
-    }
-
-    long long milliseconds = 0;
-
-    /* Validate the expiration time value first */
-    if (expire) {
-        if (getLongLongFromObjectOrReply(c, expire, &milliseconds, NULL) != C_OK)
-            return C_ERR;
-        if (milliseconds <= 0) {
-            addReplyErrorFormat(c,"invalid expire time in %s",c->cmd->name);
-            return C_ERR;
-        }
-        if (unit == UNIT_SECONDS) milliseconds *= 1000;
-    }
-
-    /* We need to do this before we expire the key or delete it */
-    addReplyBulk(c,o);
-
-    /* When PXAT/EXAT absolute timestamp is specified, there can be a chance that timestamp
-     * has already elapsed so delete the key in that case. */
-    if ((flags & OBJ_PXAT) || (flags & OBJ_EXAT)) {
-        if (checkAlreadyExpired(milliseconds)) {
-            flags = OBJ_DEL;
-        }
-    }
-
-    /* This command is never propagated as is. It is either propagated as PEXPIRE[AT], DEL, or PERSIST.
-     * This why it doesn't need special handling in feedAppendOnlyFile to convert relative expire time to absolute one. */
-    if (flags & OBJ_DEL) {
-        int deleted = server.lazyfree_lazy_user_del ? dbAsyncDelete(c->db, c->argv[1]) :
-                      dbSyncDelete(c->db, c->argv[1]);
-        serverAssert(deleted);
-
-        rewriteClientCommandVector(c, 2, shared.del, c->argv[1]);
-        signalModifiedKey(c, c->db, c->argv[1]);
-        notifyKeyspaceEvent(NOTIFY_GENERIC,
-                            "del", c->argv[1], c->db->id);
-        server.dirty++;
-    } else if (expire) {
-        robj *exp = shared.pexpireat;
-        if ((flags & OBJ_PX) || (flags & OBJ_EX)) {
-            setExpire(c,c->db,c->argv[1],milliseconds + mstime());
-            exp = shared.pexpire;
-        } else {
-            setExpire(c,c->db,c->argv[1],milliseconds);
-        }
-
-        robj* millisecondObj = createStringObjectFromLongLong(milliseconds);
-        rewriteClientCommandVector(c,3,exp,c->argv[1],millisecondObj);
-        decrRefCount(millisecondObj);
-        signalModifiedKey(c, c->db, c->argv[1]);
-        notifyKeyspaceEvent(NOTIFY_GENERIC,
-                            "expire", c->argv[1], c->db->id);
-        server.dirty++;
-    } else if (flags & OBJ_PERSIST) {
-        if (removeExpire(c->db, c->argv[1])) {
-            signalModifiedKey(c, c->db, c->argv[1]);
-            rewriteClientCommandVector(c, 2, shared.persist, c->argv[1]);
-            notifyKeyspaceEvent(NOTIFY_GENERIC,
-                                "persist", c->argv[1], c->db->id);
-            server.dirty++;
-        }
-    }
-
-    return C_OK;
-}
-
 void getCommand(client *c) {
     getGenericCommand(c);
 }
@@ -413,7 +337,73 @@ void getexCommand(client *c) {
         return;
     }
 
-    getexGenericCommand(c,flags,expire,unit);
+    robj *o;
+
+    if ((o = lookupKeyReadOrReply(c,c->argv[1],shared.null[c->resp])) == NULL)
+        return;
+
+    if (checkType(c,o,OBJ_STRING)) {
+        return;
+    }
+
+    long long milliseconds = 0;
+
+    /* Validate the expiration time value first */
+    if (expire) {
+        if (getLongLongFromObjectOrReply(c, expire, &milliseconds, NULL) != C_OK)
+            return;
+        if (milliseconds <= 0) {
+            addReplyErrorFormat(c,"invalid expire time in %s",c->cmd->name);
+            return;
+        }
+        if (unit == UNIT_SECONDS) milliseconds *= 1000;
+    }
+
+    /* We need to do this before we expire the key or delete it */
+    addReplyBulk(c,o);
+
+    /* When PXAT/EXAT absolute timestamp is specified, there can be a chance that timestamp
+     * has already elapsed so delete the key in that case. */
+    if ((flags & OBJ_PXAT) || (flags & OBJ_EXAT)) {
+        if (checkAlreadyExpired(milliseconds)) {
+            flags = OBJ_DEL;
+        }
+    }
+
+    /* This command is never propagated as is. It is either propagated as PEXPIRE[AT], DEL, or PERSIST.
+     * This why it doesn't need special handling in feedAppendOnlyFile to convert relative expire time to absolute one. */
+    if (flags & OBJ_DEL) {
+        int deleted = server.lazyfree_lazy_user_del ? dbAsyncDelete(c->db, c->argv[1]) :
+                      dbSyncDelete(c->db, c->argv[1]);
+        serverAssert(deleted);
+
+        rewriteClientCommandVector(c, 2, shared.del, c->argv[1]);
+        signalModifiedKey(c, c->db, c->argv[1]);
+        notifyKeyspaceEvent(NOTIFY_GENERIC,"del",c->argv[1],c->db->id);
+        server.dirty++;
+    } else if (expire) {
+        robj *exp = shared.pexpireat;
+        if ((flags & OBJ_PX) || (flags & OBJ_EX)) {
+            setExpire(c,c->db,c->argv[1],milliseconds + mstime());
+            exp = shared.pexpire;
+        } else {
+            setExpire(c,c->db,c->argv[1],milliseconds);
+        }
+
+        robj* millisecondObj = createStringObjectFromLongLong(milliseconds);
+        rewriteClientCommandVector(c,3,exp,c->argv[1],millisecondObj);
+        decrRefCount(millisecondObj);
+        signalModifiedKey(c, c->db, c->argv[1]);
+        notifyKeyspaceEvent(NOTIFY_GENERIC,"expire",c->argv[1],c->db->id);
+        server.dirty++;
+    } else if (flags & OBJ_PERSIST) {
+        if (removeExpire(c->db, c->argv[1])) {
+            signalModifiedKey(c, c->db, c->argv[1]);
+            rewriteClientCommandVector(c, 2, shared.persist, c->argv[1]);
+            notifyKeyspaceEvent(NOTIFY_GENERIC,"persist",c->argv[1],c->db->id);
+            server.dirty++;
+        }
+    }
 }
 
 void getsetCommand(client *c) {
