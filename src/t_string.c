8c7abe89c3ec8ed4f20976019914ba3cb513876e
Raghav Muddur <r@nmvk.com>
2021-01-25 21:23:10 -0800

    Propagate as Unlink
    Propagate as UNLINK

--- src/t_string.c
+++ src/t_string.c
@@ -394,8 +394,9 @@ void getdelCommand(client *c) {
     int deleted = server.lazyfree_lazy_user_del ? dbAsyncDelete(c->db, c->argv[1]) :
                   dbSyncDelete(c->db, c->argv[1]);
     if (deleted) {
-        /* Propagate as DEL command */
-        rewriteClientCommandVector(c, 2, shared.del, c->argv[1]);
+        /* Propagate as DEL/UNLINK command */
+        robj *aux = server.lazyfree_lazy_user_del ? shared.unlink : shared.del;
+        rewriteClientCommandVector(c,2,aux,c->argv[1]);
         signalModifiedKey(c, c->db, c->argv[1]);
         notifyKeyspaceEvent(NOTIFY_GENERIC, "del", c->argv[1], c->db->id);
         server.dirty++;
