8c2f840265b5450afb40c6cbe3c62fa91a651e0e
sundb <sundbcn@gmail.com>
2021-02-01 11:04:34 +0800

    Change type convert in randPickCompareByIndex

--- src/ziplist.c
+++ src/ziplist.c
@@ -1529,8 +1529,8 @@ typedef struct {
 
 /* rand_pick compare by index for qsort */
 int randPickCompareByIndex(const void *a, const void *b) {
-    rand_pick *rp1 = (rand_pick *)a;
-    rand_pick *rp2 = (rand_pick *)b;
+    const rand_pick *rp1 = a;
+    const rand_pick *rp2 = b;
     return rp1->index - rp2->index;
 }
 
