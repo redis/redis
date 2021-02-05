aea6e71ef82701e07177744e600e1ef20d60b7d0
Viktor Söderqvist <viktor.soderqvist@est.tech>
2021-02-05 18:54:01 +0100

    RM_ZsetRem: Delete key if empty (#8453)
    Without this fix, RM_ZsetRem can leave empty sorted sets which are
    not allowed to exist.
    
    Removing from a sorted set while iterating seems to work (while
    inserting causes failed assetions). RM_ZsetRangeEndReached is
    modified to return 1 if the key doesn't exist, to terminate
    iteration when the last element has been removed.

--- src/module.c
+++ src/module.c
@@ -2612,6 +2612,7 @@ int RM_ZsetRem(RedisModuleKey *key, RedisModuleString *ele, int *deleted) {
     if (key->value && key->value->type != OBJ_ZSET) return REDISMODULE_ERR;
     if (key->value != NULL && zsetDel(key->value,ele->ptr)) {
         if (deleted) *deleted = 1;
+        moduleDelKeyIfEmpty(key);
     } else {
         if (deleted) *deleted = 0;
     }
@@ -2657,6 +2658,7 @@ void RM_ZsetRangeStop(RedisModuleKey *key) {
 
 /* Return the "End of range" flag value to signal the end of the iteration. */
 int RM_ZsetRangeEndReached(RedisModuleKey *key) {
+    if (!key->value || key->value->type != OBJ_ZSET) return 1;
     return key->u.zset.er;
 }
 
