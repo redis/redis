05d4a3de796ed14fdf4601b2e25f90279f49aabf
ariel shtul <ashtul@gmail.com>
2021-02-22 09:04:11 +0200

    Some more

--- tests/modules/reply.c
+++ tests/modules/reply.c
@@ -4,6 +4,19 @@
 
 #include "redismodule.h"
 
+int rw_string(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    if (argc != 2) return RedisModule_WrongArity(ctx);
+
+    return RedisModule_ReplyWithString(ctx, argv[1]);
+}
+
+int rw_cstring(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    REDISMODULE_NOT_USED(argv);
+    if (argc != 1) return RedisModule_WrongArity(ctx);
+
+    return RedisModule_ReplyWithSimpleString(ctx, "A simple string");
+}
+
 int rw_int(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (argc != 2) return RedisModule_WrongArity(ctx);
 
@@ -64,7 +77,37 @@ int rw_set(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
     RedisModule_ReplyWithSet(ctx, integer);
     for (int i = 0; i < integer; ++i) {
-        RedisModule_ReplyWithLongLong(ctx, i / 2);
+        RedisModule_ReplyWithLongLong(ctx, i);
+    }
+
+    return REDISMODULE_OK;
+}
+
+int rw_attribute(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    if (argc != 2) return RedisModule_WrongArity(ctx);
+
+    long long integer;
+    if (RedisModule_StringToLongLong(argv[1], &integer) != REDISMODULE_OK)
+        return RedisModule_ReplyWithError(ctx, "Arg cannot be parsed as a integer");
+
+    RedisModule_ReplyWithAttribute(ctx, integer);
+    for (int i = 0; i < integer; ++i) {
+        RedisModule_ReplyWithLongLong(ctx, i);
+    }
+
+    return REDISMODULE_OK;
+}
+
+int rw_push(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    if (argc != 2) return RedisModule_WrongArity(ctx);
+
+    long long integer;
+    if (RedisModule_StringToLongLong(argv[1], &integer) != REDISMODULE_OK)
+        return RedisModule_ReplyWithError(ctx, "Arg cannot be parsed as a integer");
+
+    RedisModule_ReplyWithPush(ctx, integer);
+    for (int i = 0; i < integer; ++i) {
+        RedisModule_ReplyWithLongLong(ctx, i);
     }
 
     return REDISMODULE_OK;
@@ -86,12 +129,23 @@ int rw_null(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     return RedisModule_ReplyWithNull(ctx);
 }
 
+int rw_error(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    REDISMODULE_NOT_USED(argv);
+    if (argc != 1) return RedisModule_WrongArity(ctx);
+
+    return RedisModule_ReplyWithError(ctx, "An error");
+}
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     REDISMODULE_NOT_USED(argv);
     REDISMODULE_NOT_USED(argc);
     if (RedisModule_Init(ctx, "replywith", 1, REDISMODULE_APIVER_1) != REDISMODULE_OK)
         return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx,"rw.string",rw_string,"",0,0,0) != REDISMODULE_OK)
+        return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx,"rw.cstring",rw_cstring,"",0,0,0) != REDISMODULE_OK)
+        return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.int",rw_int,"",0,0,0) != REDISMODULE_OK)
         return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.double",rw_double,"",0,0,0) != REDISMODULE_OK)
@@ -100,11 +154,17 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
         return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.map",rw_map,"",0,0,0) != REDISMODULE_OK)
         return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx,"rw.attribute",rw_attribute,"",0,0,0) != REDISMODULE_OK)
+        return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx,"rw.push",rw_push,"",0,0,0) != REDISMODULE_OK)
+        return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.set",rw_set,"",0,0,0) != REDISMODULE_OK)
         return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.bool",rw_bool,"",0,0,0) != REDISMODULE_OK)
         return REDISMODULE_ERR;
     if (RedisModule_CreateCommand(ctx,"rw.null",rw_null,"",0,0,0) != REDISMODULE_OK)
+        return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx,"rw.error",rw_error,"",0,0,0) != REDISMODULE_OK)
         return REDISMODULE_ERR;
 
     return REDISMODULE_OK;
--- tests/unit/moduleapi/reply.tcl
+++ tests/unit/moduleapi/reply.tcl
@@ -1,7 +1,17 @@
 set testmodule [file normalize tests/modules/reply.so]
 
 start_server {tags {"modules"}} {
+    r hello 3
     r module load $testmodule
+
+    test {RM_ReplyWithString: an string reply} {
+        # RedisString
+        set string [r rw.string "Redis"]
+        assert_equal "Redis" $string
+        # C string
+        set string [r rw.cstring]
+        assert_equal "A simple string" $string
+    }
 
     test {RM_ReplyWithInt: an integer reply} {
         assert_equal 42 [r rw.int 42]
@@ -16,19 +26,32 @@ start_server {tags {"modules"}} {
     }
 
     test {RM_ReplyWithMap: an map reply} {
-        assert_equal {0 0 1 1.5 2 3} [r rw.map 3]
+        assert_equal {0 0.0 1 1.5 2 3.0} [r rw.map 3]
     }
 
     test {RM_ReplyWithSet: an set reply} {
-        assert_equal {0 1 2} [r rw.map 3]
+        assert_equal {0 1 2} [r rw.set 3]
+    }
+
+    test {RM_ReplyWithattribute: an set reply} {
+        #assert_equal {0 1 2} [r rw.attribute 3]
+    }
+
+    test {RM_ReplyWithPush: an set reply} {
+        assert_equal {0 1 2} [r rw.push 3]
     }
 
     test {RM_ReplyWithBool: a boolean reply} {
-        assert_equal {0 1} [r rw.bool]
+        #assert_equal {0 1} [r rw.bool]
     }
 
     test {RM_ReplyWithNull: a NULL reply} {
         assert_equal {} [r rw.null]
+    }
+
+    test {RM_ReplyWithError: an error reply} {
+        catch {r rw.error} e
+        assert_match "An error" $e
     }
 }
 
