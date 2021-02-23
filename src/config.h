0be90eace2d12e60f1f2cc0b38617b178f7508bd
Yossi Gottlieb <yossigo@gmail.com>
2021-02-23 10:34:58 +0200

    Fix setproctitle() on libmusl.
    The previous ifdef checks were a bit too strict for no apparent
    reason.

--- src/config.h
+++ src/config.h
@@ -130,7 +130,7 @@
 #define ESOCKTNOSUPPORT 0
 #endif
 
-#if ((defined __linux && defined(__GLIBC__)) || defined __APPLE__)
+#if (defined __linux || defined __APPLE__)
 #define USE_SETPROCTITLE
 #define INIT_SETPROCTITLE_REPLACEMENT
 void spt_init(int argc, char *argv[]);
