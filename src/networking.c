8d3cdae2aed68d724ab9f9a486068ed75ed3ac23
Theo Buehler <tb@openbsd.org>
2021-03-17 07:20:00 +0100

    use llabs to avoid truncation with 64 bit time

--- src/networking.c
+++ src/networking.c
@@ -3008,7 +3008,7 @@ void securityWarningCommand(client *c) {
     static time_t logged_time;
     time_t now = time(NULL);
 
-    if (labs(now-logged_time) > 60) {
+    if (llabs(now-logged_time) > 60) {
         serverLog(LL_WARNING,"Possible SECURITY ATTACK detected. It looks like somebody is sending POST or Host: commands to Redis. This is likely due to an attacker attempting to use Cross Protocol Scripting to compromise your Redis instance. Connection aborted.");
         logged_time = now;
     }
