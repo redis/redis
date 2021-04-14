dd6b13eea3efecf8bbe09f7082f8234f2f396263
hwware <wen.hui.ware@gmail.com>
2021-04-14 11:13:09 -0400

    fix invalid master_link_down_since_seconds in info repication

--- src/server.c
+++ src/server.c
@@ -5120,7 +5120,8 @@ sds genRedisInfoString(const char *section) {
             if (server.repl_state != REPL_STATE_CONNECTED) {
                 info = sdscatprintf(info,
                     "master_link_down_since_seconds:%jd\r\n",
-                    (intmax_t)(server.unixtime-server.repl_down_since));
+                    server.repl_down_since ?
+                    (intmax_t)(server.unixtime-server.repl_down_since) : -1);
             }
             info = sdscatprintf(info,
                 "slave_priority:%d\r\n"
