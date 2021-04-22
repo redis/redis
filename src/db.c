63acfe4b00b9d3e34a53559f965c0bc44c03db61
Wang Yuan <wangyuan21@baidu.com>
2021-04-22 13:32:43 +0800

    Expire key firstly and then notify keyspace event (#8830)
    Modules event subscribers may get wrong things in notifyKeyspaceEvent callback,
    such as wrong number of keys, or be able to lookup this key.
    This commit changes the order to be like the one in evict.c.
    
    Cleanup:
    Since we know the key exists (it expires now), db*Delete is sure to return 1,
    so there's no need to check it's output (misleading).

--- src/db.c
+++ src/db.c
@@ -1541,14 +1541,17 @@ int expireIfNeeded(redisDb *db, robj *key) {
     if (checkClientPauseTimeoutAndReturnIfPaused()) return 1;
 
     /* Delete the key */
+    if (server.lazyfree_lazy_expire) {
+        dbAsyncDelete(db,key);
+    } else {
+        dbSyncDelete(db,key);
+    }
     server.stat_expiredkeys++;
     propagateExpire(db,key,server.lazyfree_lazy_expire);
     notifyKeyspaceEvent(NOTIFY_EXPIRED,
         "expired",key,db->id);
-    int retval = server.lazyfree_lazy_expire ? dbAsyncDelete(db,key) :
-                                               dbSyncDelete(db,key);
-    if (retval) signalModifiedKey(NULL,db,key);
-    return retval;
+    signalModifiedKey(NULL,db,key);
+    return 1;
 }
 
 /* -----------------------------------------------------------------------------
