e9c950c277f8e60f1a11d3fb2b34658612475837
sundb <sundbcn@gmail.com>
2021-05-30 17:20:01 +0800

    Fix comment

--- tests/unit/querybuf.tcl
+++ tests/unit/querybuf.tcl
@@ -2,26 +2,28 @@ start_server {tags {"querybuf"}} {
     test "querybuf will never be resized when querybuf size less than 64k" {
         set orig_used [s used_memory]
 
-        # memory will increase by more than 32k due to querybuf of new client.
+        # Memory will increase by more than 32k due to querybuf of new client.
         set rd [redis_deferring_client]
         assert_morethan [expr [s used_memory] - $orig_used] 32768
 
         after 1000
 
-        # check querybuf of new client has not been resized.
+        # Check querybuf of new client has not been resized.
         assert_morethan [expr [s used_memory] - $orig_used] 32768
     }
 
     test "querybuf will be resized when querybuf size more than 64k" {
         set rd1 [redis_deferring_client]
-        $rd1 set bigstring [string repeat A 65535] ;# create a 64k bigstring in advance
+        $rd1 set bigstring [string repeat A 65535] ;# Create a 64k bigstring in advance.
         $rd1 close
 
         set rd2 [redis_deferring_client]
         set orig_used [s used_memory]
         $rd2 set bigstring [string repeat A 65535]
+
+        # Wait for querybuf to be resized.
+        # Memory will be reduced 32k with glibc and 40k with jemalloc.
         wait_for_condition 500 10 {
-            # memory will be reduced 32k with glibc and 40k with jemalloc
             [expr $orig_used - [s used_memory]] > 30000
         } else {
             fail "querybuf expected to be resized"
