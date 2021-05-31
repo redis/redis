5d2acc2ddd230ccc12eb72a23a9d173fcf62d15c
Oran Agra <oran@redislabs.com>
2021-05-31 16:59:46 +0300

    Update t_string.c

--- src/t_string.c
+++ src/t_string.c
@@ -800,17 +800,16 @@ void stralgoLCS(client *c) {
         goto cleanup;
     }
 
+    /* Detect string truncation or later overflows. */
+    if (sdslen(a) >= UINT32_MAX-1 || sdslen(b) >= UINT32_MAX-1) {
+        addReplyError(c, "String too long for LCS");
+        goto cleanup;
+    }
+
     /* Compute the LCS using the vanilla dynamic programming technique of
      * building a table of LCS(x,y) substrings. */
     uint32_t alen = sdslen(a);
     uint32_t blen = sdslen(b);
-
-    /* The above casting to 32 bit length could have resulted in string truncation.
-     * Note: if we ever handle bigger lengths, we need a better overflow check before malloc. */
-    if (alen != sdslen(a) || blen != sdslen(b)) {
-        addReplyError(c, "String too long for LCS");
-        goto cleanup;
-    }
 
     /* Setup an uint32_t array to store at LCS[i,j] the length of the
      * LCS A0..i-1, B0..j-1. Note that we have a linear array here, so
@@ -818,10 +817,11 @@ void stralgoLCS(client *c) {
     #define LCS(A,B) lcs[(B)+((A)*(blen+1))]
 
     /* Try to allocate the LCS table, and abort on overflow or insufficient memory. */
-    unsigned long long lcssize = (unsigned long long)(alen+1)*(blen+1)*sizeof(uint32_t);
+    unsigned long long lcssize = (unsigned long long)(alen+1)*(blen+1); /* Can't overflow due to the size limits above. */
+    unsigned long long lcsalloc = lcssize * sizeof(uint32_t);
     uint32_t *lcs = NULL;
-    if (lcssize < SIZE_MAX)
-        lcs = ztrymalloc(lcssize);
+    if (lcsalloc < SIZE_MAX && lcsalloc / lcssize == sizeof(uint32_t))
+        lcs = ztrymalloc(lcsalloc);
     if (!lcs) {
         addReplyError(c, "Insufficient memory");
         goto cleanup;
