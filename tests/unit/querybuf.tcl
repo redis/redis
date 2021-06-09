7884d82368e87271e0a7285380d4dfbf6500da75
sundb <sundbcn@gmail.com>
2021-06-09 08:13:04 -0700

    Change slow tag and temp change ci

--- tests/unit/querybuf.tcl
+++ tests/unit/querybuf.tcl
@@ -17,34 +17,32 @@ proc client_query_buffer {name} {
     return 0
 }
 
-start_server {tags {"querybuf"}} {
-    tags {slow} {
-        # The test will run at least 2s to check if client query
-        # buffer will be resized when client idle 2s.
-        test "query buffer resized correctly" {
-            # Memory will increase by more than 32k due to client query buffer.
-            set rd [redis_deferring_client]
-            $rd client setname test_client
-            set orig_test_client_qbuf [client_query_buffer test_client]
-            assert {$orig_test_client_qbuf > 16384 && $orig_test_client_qbuf < 32768}
+start_server {tags {"querybuf slow"}} {
+    # The test will run at least 2s to check if client query
+    # buffer will be resized when client idle 2s.
+    test "query buffer resized correctly" {
+        # Memory will increase by more than 32k due to client query buffer.
+        set rd [redis_deferring_client]
+        $rd client setname test_client
+        set orig_test_client_qbuf [client_query_buffer test_client]
+        assert {$orig_test_client_qbuf > 16384 && $orig_test_client_qbuf < 32768}
 
-            # Check that the initial query buffer is not resized if it is idle for more than 2s
-            wait_for_condition 1000 10 {
-                [client_idle_sec test_client] > 3 && [client_query_buffer test_client] == $orig_test_client_qbuf
-            } else {
-                fail "query buffer was resized"
-            }
+        # Check that the initial query buffer is not resized if it is idle for more than 2s
+        wait_for_condition 1000 10 {
+            [client_idle_sec test_client] > 3 && [client_query_buffer test_client] == $orig_test_client_qbuf
+        } else {
+            fail "query buffer was resized"
+        }
 
-            # Fill query buffer to more than 32k
-            $rd set bigstring v ;# create bigstring in advance to avoid adding extra memory
-            $rd set bigstring [string repeat A 32768] nx
+        # Fill query buffer to more than 32k
+        $rd set bigstring v ;# create bigstring in advance to avoid adding extra memory
+        $rd set bigstring [string repeat A 32768] nx
 
-            # Wait for query buffer to be resized to 0.
-            wait_for_condition 1000 10 {
-                [client_query_buffer test_client] == 0
-            } else {
-                fail "querybuf expected to be resized"
-            }
+        # Wait for query buffer to be resized to 0.
+        wait_for_condition 1000 10 {
+            [client_query_buffer test_client] == 0
+        } else {
+            fail "querybuf expected to be resized"
         }
     }
 }
