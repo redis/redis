e925ae3d905bcc86cfa550ea976bbcb6a99cee4d
sundb <sundbcn@gmail.com>
2021-06-09 19:21:32 -0700

    Remove redundant space in comment

--- src/networking.c
+++ src/networking.c
@@ -2164,7 +2164,7 @@ void readQueryFromClient(connection *conn) {
     if (c->querybuf_peak < qblen) c->querybuf_peak = qblen;
     if (big_arg || sdsalloc(c->querybuf) < PROTO_IOBUF_LEN) {
         /* When BIG_ARG is detected or when query buffer does not exceed its
-         * initial size,  We will make room for query buffer non-greedily. */
+         * initial size, We will make room for query buffer non-greedily. */
         c->querybuf = sdsMakeRoomForNonGreedy(c->querybuf, readlen);
     } else {
         c->querybuf = sdsMakeRoomFor(c->querybuf, readlen);
