1ce14d6a339441adbbc81533d013afd426fe089c
enjoy-binbin <binloveplay1314@qq.com>
2021-06-19 21:55:29 +0800

    Use loaderr label to handle error.
    So that we can easily see the lines of the config.
    Also unified with other error handling.

--- src/config.c
+++ src/config.c
@@ -487,10 +487,9 @@ void loadServerConfigFromString(char *config) {
             }
         } else if (!strcasecmp(argv[0],"dir") && argc == 2) {
             if (chdir(argv[1]) == -1) {
-                serverLog(LL_WARNING,"Can't chdir to '%s': %s",
-                    argv[1], strerror(errno));
-                exit(1);
-            }
+                err = sdscatprintf(sdsempty(), "Can't chdir to '%s': %s",
+                                   argv[1], strerror(errno));
+                goto loaderr;
         } else if (!strcasecmp(argv[0],"logfile") && argc == 2) {
             FILE *logfp;
 
