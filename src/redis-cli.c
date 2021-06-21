c9ac6714305e82d4d38a0a31c4a5be3ea31574e4
Maxim Galushka <max_from_sumy@mail.ru>
2021-06-21 09:01:31 +0100

    redis-cli: support for REDIS_REPLY_SET in CSV and RAW output. (#7338)
    Fixes #6792. Added support of REDIS_REPLY_SET in raw and csv output of `./redis-cli`
    
    Test:
    
    run commands to test:
      ./redis-cli -3 --csv COMMAND
      ./redis-cli -3 --raw COMMAND
    
    Now they are returning resuts, were failing with: "Unknown reply type: 10" before the change.
    
    (cherry picked from commit 96bb078577ce2b0d093c873faae5d3ecca26a1de)

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -1129,6 +1129,7 @@ static sds cliFormatReplyRaw(redisReply *r) {
     case REDIS_REPLY_DOUBLE:
         out = sdscatprintf(out,"%s",r->str);
         break;
+    case REDIS_REPLY_SET:
     case REDIS_REPLY_ARRAY:
     case REDIS_REPLY_PUSH:
         for (i = 0; i < r->elements; i++) {
@@ -1187,6 +1188,7 @@ static sds cliFormatReplyCSV(redisReply *r) {
         out = sdscat(out,r->integer ? "true" : "false");
     break;
     case REDIS_REPLY_ARRAY:
+    case REDIS_REPLY_SET:
     case REDIS_REPLY_PUSH:
     case REDIS_REPLY_MAP: /* CSV has no map type, just output flat list. */
         for (i = 0; i < r->elements; i++) {
