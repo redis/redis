81d5f05b6e25f32476a1de3f264d45d386310ef0
Wen Hui <wen.hui.ware@gmail.com>
2021-06-22 00:37:46 -0400

    add tilt mode since in sentinel info (#9000)
    Sentinel shows tilt mode boolean in info, now it'll show an indication of how long ago it started.

--- src/sentinel.c
+++ src/sentinel.c
@@ -3919,11 +3919,13 @@ void sentinelInfoCommand(client *c) {
             "# Sentinel\r\n"
             "sentinel_masters:%lu\r\n"
             "sentinel_tilt:%d\r\n"
+            "sentinel_tilt_since_seconds:%jd\r\n"
             "sentinel_running_scripts:%d\r\n"
             "sentinel_scripts_queue_length:%ld\r\n"
             "sentinel_simulate_failure_flags:%lu\r\n",
             dictSize(sentinel.masters),
             sentinel.tilt,
+            sentinel.tilt ? (intmax_t)((mstime()-sentinel.tilt_start_time)/1000) : -1,
             sentinel.running_scripts,
             listLength(sentinel.scripts_queue),
             sentinel.simfailure_flags);
