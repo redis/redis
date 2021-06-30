a5c82a881785ce36bd8525eaf0c5e95a0937bca0
lizhaolong.lzl <lizhaolong.lzl@B-54MPMD6R-0221.lan>
2021-06-30 15:29:24 +0800

    Limit args->limit when stream_node_max_entries is zero or too big

--- src/t_stream.c
+++ src/t_stream.c
@@ -982,7 +982,6 @@ static int streamParseAddOrTrimArgsOrReply(client *c, streamAddTrimArgs *args, i
             }
         } else {
             /* User didn't provide LIMIT, we must set it. */
-
             if (args->approx_trim) {
                 /* In order to prevent from trimming to do too much work and 
                  * cause latency spikes we limit the amount of work it can do.
@@ -990,7 +989,7 @@ static int streamParseAddOrTrimArgsOrReply(client *c, streamAddTrimArgs *args, i
                  * stream_node_max_entries is 0 or too big (could cause overflow)
                  */
                 args->limit = 100 * server.stream_node_max_entries; /* Maximum 100 rax nodes. */
-                if (args->limit < 1) args->limit = 1;
+                if (args->limit <= 0) args->limit = 10000;
                 if (args->limit > 1000000) args->limit = 1000000;
             } else {
                 /* No LIMIT for exact trimming */
