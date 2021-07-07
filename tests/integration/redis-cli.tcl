c6b0b8f524d0bfcec500062adbd0c2b01465162b
Mikhail Fesenko <proggga@gmail.com>
2021-07-07 08:26:26 +0300

    Direct redis-cli repl prints to stderr, because --rdb can print to stdout. fflush stdout after responses  (#9136)
    1. redis-cli can output --rdb data to stdout
       but redis-cli also write some messages to stdout which will mess up the rdb.
    
    2. Make redis-cli flush stdout when printing a reply
      This was needed in order to fix a hung in redis-cli test that uses
      --replica.
       Note that printf does flush when there's a newline, but fwrite does not.
    
    3. fix the redis-cli --replica test which used to pass previously
       because it didn't really care what it read, and because redis-cli
       used printf to print these other things to stdout.
    
    4. improve redis-cli --replica test to run with both diskless and disk-based.
    
    Co-authored-by: Oran Agra <oran@redislabs.com>
    Co-authored-by: Viktor Söderqvist <viktor@zuiderkwast.se>

--- tests/integration/redis-cli.tcl
+++ tests/integration/redis-cli.tcl
@@ -294,9 +294,9 @@ start_server {tags {"cli"}} {
         assert_equal {key:2} [run_cli --scan --quoted-pattern {"*:\x32"}]
     }
 
-    test "Connecting as a replica" {
+    proc test_redis_cli_repl {} {
         set fd [open_cli "--replica"]
-        wait_for_condition 500 500 {
+        wait_for_condition 500 100 {
             [string match {*slave0:*state=online*} [r info]]
         } else {
             fail "redis-cli --replica did not connect"
@@ -305,12 +305,28 @@ start_server {tags {"cli"}} {
         for {set i 0} {$i < 100} {incr i} {
            r set test-key test-value-$i
         }
-        r client kill type slave
-        catch {
-            assert_match {*SET*key-a*} [read_cli $fd]
+
+        wait_for_condition 500 100 {
+            [string match {*test-value-99*} [read_cli $fd]]
+        } else {
+            fail "redis-cli --replica didn't read commands"
         }
 
-        close_cli $fd
+        fconfigure $fd -blocking true
+        r client kill type slave
+        catch { close_cli $fd } err
+        assert_match {*Server closed the connection*} $err
+    }
+
+    test "Connecting as a replica" {
+        # Disk-based master
+        assert_match "OK" [r config set repl-diskless-sync no]
+        test_redis_cli_repl
+
+        # Disk-less master
+        assert_match "OK" [r config set repl-diskless-sync yes]
+        assert_match "OK" [r config set repl-diskless-sync-delay 0]
+        test_redis_cli_repl
     } {} {needs:repl}
 
     test "Piping raw protocol" {
