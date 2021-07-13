e0b09f339f7ff52a6dff07e7c3dd6a15eaf820b6
meir@redislabs.com <meir@redislabs.com>
2021-07-13 18:01:23 +0300

    using resp3 on big number test

--- tests/unit/protocol.tcl
+++ tests/unit/protocol.tcl
@@ -138,6 +138,7 @@ start_server {tags {"protocol network"}} {
     assert_equal [r ping] {PONG}
 
     test "test big number parsing" {
+        r hello 3
         r debug protocol bignum
     } {1234567999999999999999999999999999999}
 }
