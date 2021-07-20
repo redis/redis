08d37f06d5de22a3776b2f66e7715bd191ae5398
yoav-steinberg <yoav@monfort.co.il>
2021-07-20 09:47:21 +0300

    Update src/dict.h
    comments
    
    Co-authored-by: Oran Agra <oran@redislabs.com>

--- src/dict.h
+++ src/dict.h
@@ -78,7 +78,7 @@ typedef struct dict {
 
     /* Keep small vars at end for optimal (minimal) struct padding */
     int16_t pauserehash; /* If >0 rehashing is paused (<0 indicates coding error) */
-    char ht_size_exp[2];
+    char ht_size_exp[2]; /* exponent of size. (size = 1<<exp) */
 } dict;
 
 /* If safe is set to 1 this is a safe iterator, that means, you can call
