6675713e5776d7559358091e44764af0e7c905b0
meir@redislabs.com <meir@redislabs.com>
2021-07-22 14:03:45 +0300

    copyrights date fixes

--- src/resp_parser.c
+++ src/resp_parser.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Redis Labs, Inc
+ * Copyright (c) 2009-2021, Redis Labs, Inc
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
