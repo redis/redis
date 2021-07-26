17511df59b96bfeab8b46d474c19ec929e605bb9
Huang Zhw <huang_zhw@126.com>
2021-07-26 15:07:20 +0800

    Add INFO stat total_eviction_exceeded_time and current_eviction_exceeded_time (#9031)
    Add two INFO metrics:
    ```
    total_eviction_exceeded_time:69734
    current_eviction_exceeded_time:10230
    ```
    `current_eviction_exceeded_time` if greater than 0, means how much time current used memory is greater than `maxmemory`. And we are still over the maxmemory. If used memory is below `maxmemory`, this metric is reset to 0.
    `total_eviction_exceeded_time` means total time used memory is greater than `maxmemory` since server startup. 
    The units of these two metrics are ms.
    
    Co-authored-by: Oran Agra <oran@redislabs.com>

--- src/server.h
+++ src/server.h
@@ -1312,6 +1312,8 @@ struct redisServer {
     long long stat_expired_time_cap_reached_count; /* Early expire cycle stops.*/
     long long stat_expire_cycle_time_used; /* Cumulative microseconds used. */
     long long stat_evictedkeys;     /* Number of evicted keys (maxmemory) */
+    long long stat_total_eviction_exceeded_time;  /* Total time over the memory limit, unit us */
+    monotime stat_last_eviction_exceeded_time;  /* Timestamp of current eviction start, unit us */
     long long stat_keyspace_hits;   /* Number of successful lookups of keys */
     long long stat_keyspace_misses; /* Number of failed lookups of keys */
     long long stat_active_defrag_hits;      /* number of allocations moved */
