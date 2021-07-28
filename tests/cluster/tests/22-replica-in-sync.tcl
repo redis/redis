2e8eb62caf72c6f20e50ac8172c2e729e480d5cc
Maria Gutovsky <mggutovs@amazon.com>
2021-07-28 12:52:21 +0000

    Improve unit test

--- tests/cluster/tests/22-replica-in-sync.tcl
+++ tests/cluster/tests/22-replica-in-sync.tcl
@@ -12,8 +12,9 @@ test "Cluster is writable" {
     cluster_write_test 0
 }
 
-proc is_in_slots {id slots} {
-    set found_position [string first $id $slots]
+proc is_in_slots {master_id replica} {
+    set slots [R $master_id cluster slots]
+    set found_position [string first $replica $slots]
     set result [expr {$found_position != -1}]
     return $result
 }
@@ -35,20 +36,19 @@ test "Add new node as replica" {
     set master_myself [get_myself $master_id]
     set replica_myself [get_myself $replica_id]
     set replica [dict get $replica_myself id]
-    R $replica_id clusteradmin replicate [dict get $master_myself id]
+    R $replica_id cluster replicate [dict get $master_myself id]
 }
 
 test "Check digest and replica state" {
     R 1 readonly
-    while (1) {
-        set slots [R $master_id cluster slots]
-        if ([is_in_slots $replica $slots]) {
-            set master_digest [R $master_id debug digest]
-            set replica_digest [R $replica_id debug digest]
-            set info_repl [R $master_id info replication]
-            assert {$master_digest eq $replica_digest}
-            assert {[is_replica_online $info_repl]}
-            break
-        }
+    wait_for_condition 100 50 {
+        [is_in_slots $master_id $replica]
+    } else {
+        fail "New replica didn't appear in the slots"
     }
+    set master_digest [R $master_id debug digest]
+    set replica_digest [R $replica_id debug digest]
+    set info_repl [R $master_id info replication]
+    assert {$master_digest eq $replica_digest}
+    assert {[is_replica_online $info_repl]}
 }
