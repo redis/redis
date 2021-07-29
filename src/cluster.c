e311a58f4215fb9df35fb1bc8ce6c583c3d22168
qetu3790 <qetu3790@163.com>
2021-07-29 15:53:23 +0800

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -6021,7 +6021,7 @@ int clusterRedirectBlockedClientIfNeeded(client *c) {
         (c->btype == BLOCKED_LIST ||
          c->btype == BLOCKED_ZSET ||
          c->btype == BLOCKED_STREAM||
-	 c->btype == BLOCKED_MODULE))
+         c->btype == BLOCKED_MODULE))
     {
         dictEntry *de;
         dictIterator *di;
