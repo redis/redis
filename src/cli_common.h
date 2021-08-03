0b85b79c5e56aab450c56eb8ac5f2ad7c5ac63cc
filipecosta90 <filipecosta.90@gmail.com>
2021-08-04 00:29:29 +0100

    [add] Added URI support to redis-benchmark (cli and benchmark share the same uri-parsing methods)

--- src/cli_common.h
+++ src/cli_common.h
@@ -23,6 +23,16 @@ typedef struct cliSSLconfig {
     char* ciphersuites;
 } cliSSLconfig;
 
+
+/* server connection information object, used to describe an ip:port pair, db num user input, and user:pass. */
+typedef struct cliConnInfo {
+    char *hostip;
+    int hostport;
+    int input_dbnum;
+    char *auth;
+    char *user;
+} cliConnInfo;
+
 int cliSecureConnection(redisContext *c, cliSSLconfig config, const char **err);
 
 ssize_t cliWriteConn(redisContext *c, const char *buf, size_t buf_len);
@@ -34,5 +44,7 @@ sds readArgFromStdin(void);
 sds *getSdsArrayFromArgv(int argc,char **argv, int quoted);
 
 sds unquoteCString(char *str);
+
+void parseRedisUri(const char *uri, const char* tool_name, cliConnInfo *connInfo);
 
 #endif /* __CLICOMMON_H */
