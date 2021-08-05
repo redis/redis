c48599ff24ddeb21d890e0e52632f912ca758cf2
Yoav Steinberg <yoav@monfort.co.il>
2021-08-05 08:53:28 +0300

    fix dict access broken by d4bca53cd9879e0296bfa0a7c17df79dd52496ae

--- src/object.c
+++ src/object.c
@@ -427,8 +427,8 @@ void dismissSetObject(robj *o, size_t size_hint) {
         }
 
         /* Dismiss hash table memory. */
-        dismissMemory(set->ht[0].table, set->ht[0].size*sizeof(dictEntry*));
-        dismissMemory(set->ht[1].table, set->ht[1].size*sizeof(dictEntry*));
+        dismissMemory(set->ht_table[0], DICTHT_SIZE(set->ht_size_exp[0])*sizeof(dictEntry*));
+        dismissMemory(set->ht_table[1], DICTHT_SIZE(set->ht_size_exp[1])*sizeof(dictEntry*));
     } else if (o->encoding == OBJ_ENCODING_INTSET) {
         dismissMemory(o->ptr, intsetBlobLen((intset*)o->ptr));
     }
@@ -452,8 +452,8 @@ void dismissZsetObject(robj *o, size_t size_hint) {
 
         /* Dismiss hash table memory. */
         dict *d = zs->dict;
-        dismissMemory(d->ht[0].table, d->ht[0].size*sizeof(dictEntry*));
-        dismissMemory(d->ht[1].table, d->ht[1].size*sizeof(dictEntry*));
+        dismissMemory(d->ht_table[0], DICTHT_SIZE(d->ht_size_exp[0])*sizeof(dictEntry*));
+        dismissMemory(d->ht_table[1], DICTHT_SIZE(d->ht_size_exp[1])*sizeof(dictEntry*));
     } else if (o->encoding == OBJ_ENCODING_ZIPLIST) {
         dismissMemory(o->ptr, ziplistBlobLen((unsigned char*)o->ptr));
     }
@@ -478,8 +478,8 @@ void dismissHashObject(robj *o, size_t size_hint) {
         }
 
         /* Dismiss hash table memory. */
-        dismissMemory(d->ht[0].table, d->ht[0].size*sizeof(dictEntry*));
-        dismissMemory(d->ht[1].table, d->ht[1].size*sizeof(dictEntry*));
+        dismissMemory(d->ht_table[0], DICTHT_SIZE(d->ht_size_exp[0])*sizeof(dictEntry*));
+        dismissMemory(d->ht_table[1], DICTHT_SIZE(d->ht_size_exp[1])*sizeof(dictEntry*));
     } else if (o->encoding == OBJ_ENCODING_ZIPLIST) {
         dismissMemory(o->ptr, ziplistBlobLen((unsigned char*)o->ptr));
     }
