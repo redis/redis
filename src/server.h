251d60724937d64a2ace6eb0ed6e783b41d6c278
Yoav Steinberg <yoav@monfort.co.il>
2021-08-05 17:31:51 +0300

    cleanup dead code

--- src/server.h
+++ src/server.h
@@ -1911,8 +1911,6 @@ size_t sdsZmallocSize(sds s);
 size_t getStringObjectSdsUsedMemory(robj *o);
 void freeClientReplyValue(void *o);
 void *dupClientReplyValue(void *o);
-void getClientsMaxBuffers(unsigned long *longest_output_list,
-                          unsigned long *biggest_input_buffer);
 char *getClientPeerId(client *client);
 char *getClientSockName(client *client);
 sds catClientInfoString(sds s, client *client);
