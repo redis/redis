d3356bf614155f2a4352a0630065d715ce073b8a
Eduardo Semprebon <eduardobr@gmail.com>
2021-08-09 08:40:29 +0200

    Add SORT_RO command (#9299)
    Add a readonly variant of the STORE command, so it can be used on
    read-only workloads (replica, ACL, etc)

--- src/server.h
+++ src/server.h
@@ -2606,6 +2606,7 @@ void syncCommand(client *c);
 void flushdbCommand(client *c);
 void flushallCommand(client *c);
 void sortCommand(client *c);
+void sortroCommand(client *c);
 void lremCommand(client *c);
 void lposCommand(client *c);
 void rpoplpushCommand(client *c);
