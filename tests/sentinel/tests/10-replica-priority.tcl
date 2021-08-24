9af0cd0239e960865a8b79cd93d0bba116b674df
Wen Hui <wen.hui.ware@gmail.com>
2021-08-24 14:41:57 -0400

    Update 10-replica-priority.tcl

--- tests/sentinel/tests/10-replica-priority.tcl
+++ tests/sentinel/tests/10-replica-priority.tcl
@@ -33,12 +33,11 @@ proc 10_test_number_of_replicas {n_replicas_expected} {
     test "Check sentinel replies with $n_replicas_expected replicas" {
         # ensure sentinels replies with the right number of replicas
         foreach_sentinel_id id {
-            # retries 40 x 500ms = 20s as SENTINEL_INFO_PERIOD = 10s
             S $id sentinel debug info-period 100
             S $id sentinel debug default-down-after 1000
             S $id sentinel debug publish-period 100
             set len [llength [S $id SENTINEL REPLICAS mymaster]]
-            wait_for_condition 40 500 {
+            wait_for_condition 200 100 {
                 [llength [S $id SENTINEL REPLICAS mymaster]] == $n_replicas_expected
             } else {
                 fail "Sentinel replies with a wrong number of replicas with replica-announced=yes (expected $n_replicas_expected but got $len) on sentinel $id"
