redis/redis

commit f24c63a292e045d4b14b82b25981f00a95c1767a

Slot-to-keys using dict entry metadata (#9356)
* Enhance dict to support arbitrary metadata carried in dictEntry

Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

* Rewrite slot-to-keys mapping to linked lists using dict entry metadata

This is a memory enhancement for Redis Cluster.

The radix tree slots_to_keys (which duplicates all key names prefixed with their
slot number) is replaced with a linked list for each slot. The dict entries of
the same cluster slot form a linked list and the pointers are stored as metadata
in each dict entry of the main DB dict.

This commit also moves the slot-to-key API from db.c to cluster.c.

Co-authored-by: Jim Brunner <brunnerj@amazon.com>

src/db.c
@@ -38,8 +38,7 @@
 /* Database backup. */
 struct dbBackup {
     redisDb *dbarray;
-    rax *slots_to_keys;
-    uint64_t slots_keys_count[CLUSTER_SLOTS];
+    clusterSlotsToKeysData slots_to_keys;
 };
 
 /*-----------------------------------------------------------------------------
@@ -184,11 +183,11 @@ robj *lookupKeyWriteOrReply(client *c, robj *key, robj *reply) {
  * The program is aborted if the key already exists. */
 void dbAdd(redisDb *db, robj *key, robj *val) {
     sds copy = sdsdup(key->ptr);
-    int retval = dictAdd(db->dict, copy, val);
-
-    serverAssertWithInfo(NULL,key,retval == DICT_OK);
+    dictEntry *de = dictAddRaw(db->dict, copy, NULL);
+    serverAssertWithInfo(NULL, key, de != NULL);
+    dictSetVal(db->dict, de, val);
     signalKeyAsReady(db, key, val->type);
-    if (server.cluster_enabled) slotToKeyAdd(key->ptr);
+    if (server.cluster_enabled) slotToKeyAddEntry(de);
 }
 
 /* This is a special version of dbAdd() that is used only when loading
@@ -203,9 +202,10 @@ void dbAdd(redisDb *db, robj *key, robj *val) {
  * ownership of the SDS string, otherwise 0 is returned, and is up to the
  * caller to free the SDS string. */
 int dbAddRDBLoad(redisDb *db, sds key, robj *val) {
-    int retval = dictAdd(db->dict, key, val);
-    if (retval != DICT_OK) return 0;
-    if (server.cluster_enabled) slotToKeyAdd(key);
+    dictEntry *de = dictAddRaw(db->dict, key, NULL);
+    if (de == NULL) return 0;
+    dictSetVal(db->dict, de, val);
+    if (server.cluster_enabled) slotToKeyAddEntry(de);
     return 1;
 }
 
@@ -313,8 +313,8 @@ int dbSyncDelete(redisDb *db, robj *key) {
         robj *val = dictGetVal(de);
         /* Tells the module that the key has been unlinked from the database. */
         moduleNotifyKeyUnlink(key,val,db->id);
+        if (server.cluster_enabled) slotToKeyDelEntry(de);
         dictFreeUnlinkedEntry(db->dict,de);
-        if (server.cluster_enabled) slotToKeyDel(key->ptr);
         return 1;
     } else {
         return 0;
@@ -441,7 +441,7 @@ long long emptyDb(int dbnum, int flags, void(callback)(dict*)) {
     /* Flush slots to keys map if enable cluster, we can flush entire
      * slots to keys map whatever dbnum because only support one DB
      * in cluster mode. */
-    if (server.cluster_enabled) slotToKeyFlush(async);
+    if (server.cluster_enabled) slotToKeyFlush();
 
     if (dbnum == -1) flushSlaveKeysWithExpireList();
 
@@ -469,12 +469,8 @@ dbBackup *backupDb(void) {
 
     /* Backup cluster slots to keys map if enable cluster. */
     if (server.cluster_enabled) {
-        backup->slots_to_keys = server.cluster->slots_to_keys;
-        memcpy(backup->slots_keys_count, server.cluster->slots_keys_count,
-            sizeof(server.cluster->slots_keys_count));
-        server.cluster->slots_to_keys = raxNew();
-        memset(server.cluster->slots_keys_count, 0,
-            sizeof(server.cluster->slots_keys_count));
+        slotToKeyCopyToBackup(&backup->slots_to_keys);
+        slotToKeyFlush();
     }
 
     moduleFireServerEvent(REDISMODULE_EVENT_REPL_BACKUP,
@@ -495,9 +491,6 @@ void discardDbBackup(dbBackup *backup, int flags, void(callback)(dict*)) {
         dictRelease(backup->dbarray[i].dict);
         dictRelease(backup->dbarray[i].expires);
     }
-
-    /* Release slots to keys map backup if enable cluster. */
-    if (server.cluster_enabled) freeSlotsToKeysMap(backup->slots_to_keys, async);
 
     /* Release backup. */
     zfree(backup->dbarray);
@@ -523,13 +516,7 @@ void restoreDbBackup(dbBackup *backup) {
     }
 
     /* Restore slots to keys map backup if enable cluster. */
-    if (server.cluster_enabled) {
-        serverAssert(server.cluster->slots_to_keys->numele == 0);
-        raxFree(server.cluster->slots_to_keys);
-        server.cluster->slots_to_keys = backup->slots_to_keys;
-        memcpy(server.cluster->slots_keys_count, backup->slots_keys_count,
-                sizeof(server.cluster->slots_keys_count));
-    }
+    if (server.cluster_enabled) slotToKeyRestoreBackup(&backup->slots_to_keys);
 
     /* Release backup. */
     zfree(backup->dbarray);
@@ -1912,103 +1899,4 @@ int xreadGetKeys(struct redisCommand *cmd, robj **argv, int argc, getKeysResult 
     for (i = streams_pos+1; i < argc-num; i++) keys[i-streams_pos-1] = i;
     result->numkeys = num;
     return num;
-}
-
-/* Slot to Key API. This is used by Redis Cluster in order to obtain in
- * a fast way a key that belongs to a specified hash slot. This is useful
- * while rehashing the cluster and in other conditions when we need to
- * understand if we have keys for a given hash slot. */
-void slotToKeyUpdateKey(sds key, int add) {
-    size_t keylen = sdslen(key);
-    unsigned int hashslot = keyHashSlot(key,keylen);
-    unsigned char buf[64];
-    unsigned char *indexed = buf;
-
-    server.cluster->slots_keys_count[hashslot] += add ? 1 : -1;
-    if (keylen+2 > 64) indexed = zmalloc(keylen+2);
-    indexed[0] = (hashslot >> 8) & 0xff;
-    indexed[1] = hashslot & 0xff;
-    memcpy(indexed+2,key,keylen);
-    if (add) {
-        raxInsert(server.cluster->slots_to_keys,indexed,keylen+2,NULL,NULL);
-    } else {
-        raxRemove(server.cluster->slots_to_keys,indexed,keylen+2,NULL);
-    }
-    if (indexed != buf) zfree(indexed);
-}
-
-void slotToKeyAdd(sds key) {
-    slotToKeyUpdateKey(key,1);
-}
-
-void slotToKeyDel(sds key) {
-    slotToKeyUpdateKey(key,0);
-}
-
-/* Release the radix tree mapping Redis Cluster keys to slots. If 'async'
- * is true, we release it asynchronously. */
-void freeSlotsToKeysMap(rax *rt, int async) {
-    if (async) {
-        freeSlotsToKeysMapAsync(rt);
-    } else {
-        raxFree(rt);
-    }
-}
-
-/* Empty the slots-keys map of Redis CLuster by creating a new empty one and
- * freeing the old one. */
-void slotToKeyFlush(int async) {
-    rax *old = server.cluster->slots_to_keys;
-
-    server.cluster->slots_to_keys = raxNew();
-    memset(server.cluster->slots_keys_count,0,
-           sizeof(server.cluster->slots_keys_count));
-    freeSlotsToKeysMap(old, async);
-}
-
-/* Populate the specified array of objects with keys in the specified slot.
- * New objects are returned to represent keys, it's up to the caller to
- * decrement the reference count to release the keys names. */
-unsigned int getKeysInSlot(unsigned int hashslot, robj **keys, unsigned int count) {
-    raxIterator iter;
-    int j = 0;
-    unsigned char indexed[2];
-
-    indexed[0] = (hashslot >> 8) & 0xff;
-    indexed[1] = hashslot & 0xff;
-    raxStart(&iter,server.cluster->slots_to_keys);
-    raxSeek(&iter,">=",indexed,2);
-    while(count-- && raxNext(&iter)) {
-        if (iter.key[0] != indexed[0] || iter.key[1] != indexed[1]) break;
-        keys[j++] = createStringObject((char*)iter.key+2,iter.key_len-2);
-    }
-    raxStop(&iter);
-    return j;
-}
-
-/* Remove all the keys in the specified hash slot.
- * The number of removed items is returned. */
-unsigned int delKeysInSlot(unsigned int hashslot) {
-    raxIterator iter;
-    int j = 0;
-    unsigned char indexed[2];
-
-    indexed[0] = (hashslot >> 8) & 0xff;
-    indexed[1] = hashslot & 0xff;
-    raxStart(&iter,server.cluster->slots_to_keys);
-    while(server.cluster->slots_keys_count[hashslot]) {
-        raxSeek(&iter,">=",indexed,2);
-        raxNext(&iter);
-
-        robj *key = createStringObject((char*)iter.key+2,iter.key_len-2);
-        dbDelete(&server.db[0],key);
-        decrRefCount(key);
-        j++;
-    }
-    raxStop(&iter);
-    return j;
-}
-
-unsigned int countKeysInSlot(unsigned int hashslot) {
-    return server.cluster->slots_keys_count[hashslot];
 }

src/defrag.c
@@ -34,6 +34,7 @@
  */
 
 #include "server.h"
+#include "cluster.h"
 #include <time.h>
 #include <assert.h>
 #include <stddef.h>
@@ -45,7 +46,7 @@
 int je_get_defrag_hint(void* ptr);
 
 /* forward declarations*/
-void defragDictBucketCallback(void *privdata, dictEntry **bucketref);
+void defragDictBucketCallback(dict *d, dictEntry **bucketref);
 dictEntry* replaceSatelliteDictKeyPtrAndOrDefragDictEntry(dict *d, sds oldkey, sds newkey, uint64_t hash, long *defragged);
 
 /* Defrag helper for generic allocations.
@@ -895,12 +896,15 @@ void defragScanCallback(void *privdata, const dictEntry *de) {
 
 /* Defrag scan callback for each hash table bucket,
  * used in order to defrag the dictEntry allocations. */
-void defragDictBucketCallback(void *privdata, dictEntry **bucketref) {
-    UNUSED(privdata); /* NOTE: this function is also used by both activeDefragCycle and scanLaterHash, etc. don't use privdata */
+void defragDictBucketCallback(dict *d, dictEntry **bucketref) {
     while(*bucketref) {
         dictEntry *de = *bucketref, *newde;
         if ((newde = activeDefragAlloc(de))) {
             *bucketref = newde;
+            if (server.cluster_enabled && d == server.db[0].dict) {
+                /* Cluster keyspace dict. Update slot-to-entries mapping. */
+                slotToKeyReplaceEntry(newde);
+            }
         }
         bucketref = &(*bucketref)->next;
     }

src/dict.c
@@ -338,7 +338,11 @@ dictEntry *dictAddRaw(dict *d, void *key, dictEntry **existing)
      * system it is more likely that recently added entries are accessed
      * more frequently. */
     htidx = dictIsRehashing(d) ? 1 : 0;
-    entry = zmalloc(sizeof(*entry));
+    size_t metasize = dictMetadataSize(d);
+    entry = zmalloc(sizeof(*entry) + metasize);
+    if (metasize > 0) {
+        memset(dictMetadata(entry), 0, metasize);
+    }
     entry->next = d->ht_table[htidx][index];
     d->ht_table[htidx][index] = entry;
     d->ht_used[htidx]++;
@@ -906,7 +910,7 @@ unsigned long dictScan(dict *d,
         m0 = DICTHT_SIZE_MASK(d->ht_size_exp[htidx0]);
 
         /* Emit entries at cursor */
-        if (bucketfn) bucketfn(privdata, &d->ht_table[htidx0][v & m0]);
+        if (bucketfn) bucketfn(d, &d->ht_table[htidx0][v & m0]);
         de = d->ht_table[htidx0][v & m0];
         while (de) {
             next = de->next;
@@ -937,7 +941,7 @@ unsigned long dictScan(dict *d,
         m1 = DICTHT_SIZE_MASK(d->ht_size_exp[htidx1]);
 
         /* Emit entries at cursor */
-        if (bucketfn) bucketfn(privdata, &d->ht_table[htidx0][v & m0]);
+        if (bucketfn) bucketfn(d, &d->ht_table[htidx0][v & m0]);
         de = d->ht_table[htidx0][v & m0];
         while (de) {
             next = de->next;
@@ -949,7 +953,7 @@ unsigned long dictScan(dict *d,
          * of the index pointed to by the cursor in the smaller table */
         do {
             /* Emit entries at cursor */
-            if (bucketfn) bucketfn(privdata, &d->ht_table[htidx1][v & m1]);
+            if (bucketfn) bucketfn(d, &d->ht_table[htidx1][v & m1]);
             de = d->ht_table[htidx1][v & m1];
             while (de) {
                 next = de->next;

src/dict.h
@@ -52,7 +52,10 @@ typedef struct dictEntry {
         int64_t s64;
         double d;
     } v;
-    struct dictEntry *next;
+    struct dictEntry *next;     /* Next entry in the same hash bucket. */
+    void *metadata[];           /* An arbitrary number of bytes (starting at a
+                                 * pointer-aligned address) of size as returned
+                                 * by dictType's dictEntryMetadataBytes(). */
 } dictEntry;
 
 typedef struct dict dict;
@@ -65,6 +68,9 @@ typedef struct dictType {
     void (*keyDestructor)(dict *d, void *key);
     void (*valDestructor)(dict *d, void *obj);
     int (*expandAllowed)(size_t moreMem, double usedRatio);
+    /* Allow a dictEntry to carry extra caller-defined metadata.  The
+     * extra memory is initialized to 0 when a dictEntry is allocated. */
+    size_t (*dictEntryMetadataBytes)(dict *d);
 } dictType;
 
 #define DICTHT_SIZE(exp) ((exp) == -1 ? 0 : (unsigned long)1<<(exp))
@@ -97,7 +103,7 @@ typedef struct dictIterator {
 } dictIterator;
 
 typedef void (dictScanFunction)(void *privdata, const dictEntry *de);
-typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
+typedef void (dictScanBucketFunction)(dict *d, dictEntry **bucketref);
 
 /* This is the initial size of every hash table */
 #define DICT_HT_INITIAL_EXP      2
@@ -139,6 +145,10 @@ typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
     (((d)->type->keyCompare) ? \
         (d)->type->keyCompare((d), key1, key2) : \
         (key1) == (key2))
+
+#define dictMetadata(entry) (&(entry)->metadata)
+#define dictMetadataSize(d) ((d)->type->dictEntryMetadataBytes \
+                             ? (d)->type->dictEntryMetadataBytes(d) : 0)
 
 #define dictHashKey(d, key) (d)->type->hashFunction(key)
 #define dictGetKey(he) ((he)->key)

commit a78c54c50fdad6d9c30e660a4a648b9a71e55fdb

Key embedded in dictEntry
Whenever a key is looked up, the dictEntry (hashtable bucket) and the key
are accessed together. This PR embeds the key in the same allocation as the
dictEntry, which makes sure they are in the same cache line, thus saving
one slow memory access.

This compensates for the potential regression from #9356 for key lengths
15-22 in cluster mode. Furthermore, latency is expected to improve for
other key lengths in cluster mode as well as in plain Redis. Benchmarks
will show.

The callbacks in dictType are changed in the following ways:

* dictEntryMetadataBytes is replaced by createEntry, which (if present)
  takes care of allocating the entry and setting the key.
* keyDestructor and valueDestructor are passed the dictEntry as an extra
  argument, so they can check if the key is embedded in the dictEntry and
  then not try to free it.
* repairEntry is added, which is called from active defrag code when an
  entry has been reallocated, moving custom logic from the active defrag
  code to the dict type.

TODO:

* [x] Working implamentation. Tests passing.
* [ ] Benchmarks for cluster/plain Redis, key lengths of different
      allocation sizes.
* [ ] Memory usage comparison.

Open questions:

* What's the maxumum key length we want to embed in a dictEntry?
* Do we want a similar optimization for other dicts, e.g. hashes?

src/config.c
@@ -1103,7 +1103,7 @@ void configGetCommand(client *c) {
 /* We use the following dictionary type to store where a configuration
  * option is mentioned in the old configuration file, so it's
  * like "maxmemory" -> list of line numbers (first line is zero). */
-void dictListDestructor(dict *d, void *val);
+void dictListDestructor(dict *d, dictEntry *de, void *val);
 
 /* Sentinel config rewriting is implemented inside sentinel.c by
  * rewriteConfigSentinelOption(). */

src/db.c
@@ -182,12 +182,13 @@ robj *lookupKeyWriteOrReply(client *c, robj *key, robj *reply) {
  *
  * The program is aborted if the key already exists. */
 void dbAdd(redisDb *db, robj *key, robj *val) {
-    sds copy = sdsdup(key->ptr);
-    dictEntry *de = dictAddRaw(db->dict, copy, NULL);
+    dictEntry *de = dictAddRaw(db->dict, key->ptr, NULL);
     serverAssertWithInfo(NULL, key, de != NULL);
+    /* If the key was not already copied (and embedded in the dict) by
+     * dictAddRaw, we need to copy it here. */
+    if (de->key == key->ptr) de->key = sdsdup(key->ptr);
     dictSetVal(db->dict, de, val);
     signalKeyAsReady(db, key, val->type);
-    if (server.cluster_enabled) slotToKeyAddEntry(de);
 }
 
 /* This is a special version of dbAdd() that is used only when loading
@@ -204,8 +205,8 @@ void dbAdd(redisDb *db, robj *key, robj *val) {
 int dbAddRDBLoad(redisDb *db, sds key, robj *val) {
     dictEntry *de = dictAddRaw(db->dict, key, NULL);
     if (de == NULL) return 0;
+    if (de->key != key) sdsfree(key); /* Free it if the dict copied it. */
     dictSetVal(db->dict, de, val);
-    if (server.cluster_enabled) slotToKeyAddEntry(de);
     return 1;
 }
 
@@ -313,7 +314,6 @@ int dbSyncDelete(redisDb *db, robj *key) {
         robj *val = dictGetVal(de);
         /* Tells the module that the key has been unlinked from the database. */
         moduleNotifyKeyUnlink(key,val,db->id);
-        if (server.cluster_enabled) slotToKeyDelEntry(de);
         dictFreeUnlinkedEntry(db->dict,de);
         return 1;
     } else {

src/defrag.c
@@ -816,12 +816,15 @@ long defragKey(redisDb *db, dictEntry *de) {
     robj *newob, *ob;
     unsigned char *newzl;
     long defragged = 0;
-    sds newsds;
+    sds newsds = NULL;
 
-    /* Try to defrag the key name. */
-    newsds = activeDefragSds(keysds);
-    if (newsds)
-        defragged++, de->key = newsds;
+    /* Try to defrag the key, if it's not embedded in the dict entry. */
+    if ((uintptr_t)de->key < (uintptr_t)de ||
+        (uintptr_t)de->key >= (uintptr_t)de + zmalloc_usable_size(de)) {
+        newsds = activeDefragSds(keysds);
+        if (newsds)
+            defragged++, de->key = newsds;
+    }
     if (dictSize(db->expires)) {
          /* Dirty code:
           * I can't search in db->expires for that key after i already released
@@ -900,11 +903,8 @@ void defragDictBucketCallback(dict *d, dictEntry **bucketref) {
     while(*bucketref) {
         dictEntry *de = *bucketref, *newde;
         if ((newde = activeDefragAlloc(de))) {
+            dictRepairEntry(d, newde, de);
             *bucketref = newde;
-            if (server.cluster_enabled && d == server.db[0].dict) {
-                /* Cluster keyspace dict. Update slot-to-entries mapping. */
-                slotToKeyReplaceEntry(newde);
-            }
         }
         bucketref = &(*bucketref)->next;
     }

src/dict.c
@@ -338,17 +338,16 @@ dictEntry *dictAddRaw(dict *d, void *key, dictEntry **existing)
      * system it is more likely that recently added entries are accessed
      * more frequently. */
     htidx = dictIsRehashing(d) ? 1 : 0;
-    size_t metasize = dictMetadataSize(d);
-    entry = zmalloc(sizeof(*entry) + metasize);
-    if (metasize > 0) {
-        memset(dictMetadata(entry), 0, metasize);
+    if (d->type->createEntry) {
+        entry = d->type->createEntry(d, key);
+    } else {
+        entry = zmalloc(sizeof(*entry));
+        dictSetKey(d, entry, key);
     }
     entry->next = d->ht_table[htidx][index];
     d->ht_table[htidx][index] = entry;
     d->ht_used[htidx]++;
 
-    /* Set the hash entry fields. */
-    dictSetKey(d, entry, key);
     return entry;
 }
 
@@ -1200,9 +1199,9 @@ int compareCallback(dict *d, const void *key1, const void *key2) {
     return memcmp(key1, key2, l1) == 0;
 }
 
-void freeCallback(dict *d, void *val) {
+void freeCallback(dict *d, dictEntry *de, void *val) {
     UNUSED(d);
-
+    UNUSED(de);
     zfree(val);
 }
 

src/dict.h
@@ -65,12 +65,15 @@ typedef struct dictType {
     void *(*keyDup)(dict *d, const void *key);
     void *(*valDup)(dict *d, const void *obj);
     int (*keyCompare)(dict *d, const void *key1, const void *key2);
-    void (*keyDestructor)(dict *d, void *key);
-    void (*valDestructor)(dict *d, void *obj);
+    void (*keyDestructor)(dict *d, dictEntry* de, void *key);
+    void (*valDestructor)(dict *d, dictEntry* de, void *obj);
     int (*expandAllowed)(size_t moreMem, double usedRatio);
-    /* Allow a dictEntry to carry extra caller-defined metadata.  The
-     * extra memory is initialized to 0 when a dictEntry is allocated. */
-    size_t (*dictEntryMetadataBytes)(dict *d);
+    /* Override creation of dictEntry. If defined, it must allocate an entry and
+     * set the key. */
+    dictEntry *(*createEntry)(dict *d, void *key);
+    /* Repair is called after a dictEntry has been reallocated. The pointer
+     * 'oldde' has already been freed so it must not be dereferenced. */
+    void (*repairEntry)(dict *d, dictEntry *newde, dictEntry *oldde);
 } dictType;
 
 #define DICTHT_SIZE(exp) ((exp) == -1 ? 0 : (unsigned long)1<<(exp))
@@ -112,7 +115,7 @@ typedef void (dictScanBucketFunction)(dict *d, dictEntry **bucketref);
 /* ------------------------------- Macros ------------------------------------*/
 #define dictFreeVal(d, entry) \
     if ((d)->type->valDestructor) \
-        (d)->type->valDestructor((d), (entry)->v.val)
+        (d)->type->valDestructor((d), (entry), (entry)->v.val)
 
 #define dictSetVal(d, entry, _val_) do { \
     if ((d)->type->valDup) \
@@ -132,7 +135,7 @@ typedef void (dictScanBucketFunction)(dict *d, dictEntry **bucketref);
 
 #define dictFreeKey(d, entry) \
     if ((d)->type->keyDestructor) \
-        (d)->type->keyDestructor((d), (entry)->key)
+        (d)->type->keyDestructor((d), (entry), (entry)->key)
 
 #define dictSetKey(d, entry, _key_) do { \
     if ((d)->type->keyDup) \
@@ -146,9 +149,10 @@ typedef void (dictScanBucketFunction)(dict *d, dictEntry **bucketref);
         (d)->type->keyCompare((d), key1, key2) : \
         (key1) == (key2))
 
+#define dictRepairEntry(d, newde, oldde) \
+    if ((d)->type->repairEntry) \
+        (d)->type->repairEntry((d), (newde), (oldde))
 #define dictMetadata(entry) (&(entry)->metadata)
-#define dictMetadataSize(d) ((d)->type->dictEntryMetadataBytes \
-                             ? (d)->type->dictEntryMetadataBytes(d) : 0)
 
 #define dictHashKey(d, key) (d)->type->hashFunction(key)
 #define dictGetKey(he) ((he)->key)

src/latency.c
@@ -45,7 +45,7 @@ uint64_t dictStringHash(const void *key) {
     return dictGenHashFunction(key, strlen(key));
 }
 
-void dictVanillaFree(dict *d, void *val);
+void dictVanillaFree(dict *d, dictEntry *de, void *val);
 
 dictType latencyTimeSeriesDictType = {
     dictStringHash,             /* hash function */

src/redis-cli.c
@@ -170,8 +170,8 @@ int spectrum_palette_size;
 static uint64_t dictSdsHash(const void *key);
 static int dictSdsKeyCompare(dict *d, const void *key1,
     const void *key2);
-static void dictSdsDestructor(dict *d, void *val);
-static void dictListDestructor(dict *d, void *val);
+static void dictSdsDestructor(dict *d, dictEntry *de, void *val);
+static void dictListDestructor(dict *d, dictEntry *de, void *val);
 
 /* Cluster Manager Command Info */
 typedef struct clusterManagerCommand {
@@ -466,15 +466,17 @@ static int dictSdsKeyCompare(dict *d, const void *key1, const void *key2)
     return memcmp(key1, key2, l1) == 0;
 }
 
-static void dictSdsDestructor(dict *d, void *val)
+static void dictSdsDestructor(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     sdsfree(val);
 }
 
-void dictListDestructor(dict *d, void *val)
+void dictListDestructor(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     listRelease((list*)val);
 }
 
@@ -7540,9 +7542,10 @@ static typeinfo* typeinfo_add(dict *types, char* name, typeinfo* type_template) 
     return info;
 }
 
-void type_free(dict *d, void* val) {
+void type_free(dict *d, dictEntry *de, void* val) {
     typeinfo *info = val;
     UNUSED(d);
+    UNUSED(de);
     if (info->biggest_key)
         sdsfree(info->biggest_key);
     sdsfree(info->name);

src/sentinel.c
@@ -410,8 +410,9 @@ void sentinelSimFailureCrash(void);
 
 void releaseSentinelRedisInstance(sentinelRedisInstance *ri);
 
-void dictInstancesValDestructor (dict *d, void *obj) {
+void dictInstancesValDestructor (dict *d, dictEntry *de, void *obj) {
     UNUSED(d);
+    UNUSED(de);
     releaseSentinelRedisInstance(obj);
 }
 

src/server.c
@@ -1267,15 +1267,17 @@ void exitFromChild(int retcode) {
  * keys and redis objects as values (objects can hold SDS strings,
  * lists, sets). */
 
-void dictVanillaFree(dict *d, void *val)
+void dictVanillaFree(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     zfree(val);
 }
 
-void dictListDestructor(dict *d, void *val)
+void dictListDestructor(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     listRelease((list*)val);
 }
 
@@ -1300,16 +1302,18 @@ int dictSdsKeyCaseCompare(dict *d, const void *key1,
     return strcasecmp(key1, key2) == 0;
 }
 
-void dictObjectDestructor(dict *d, void *val)
+void dictObjectDestructor(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     if (val == NULL) return; /* Lazy freeing will set value to NULL. */
     decrRefCount(val);
 }
 
-void dictSdsDestructor(dict *d, void *val)
+void dictSdsDestructor(dict *d, dictEntry *de, void *val)
 {
     UNUSED(d);
+    UNUSED(de);
     sdsfree(val);
 }
 
@@ -1384,12 +1388,67 @@ int dictExpandAllowed(size_t moreMem, double usedRatio) {
     }
 }
 
-/* Returns the size of the DB dict entry metadata in bytes. In cluster mode, the
- * metadata is used for constructing a doubly linked list of the dict entries
- * belonging to the same cluster slot. See the Slot to Key API in cluster.c. */
-size_t dictEntryMetadataSize(dict *d) {
+/* Allocates an entry for the DB dict and sets the key and cluster metadata.
+ *
+ * For small keys, a copy of the key is embedded in the entry, but otherwise the
+ * dict takes a pointer to the key without copying it, so the caller needs to
+ * check if entry->key is the same as the supplied key to know if it was copied
+ * or not.
+ *
+ * In cluster mode, extra memory is allocated for constructing a doubly linked
+ * list of the dict entries belonging to the same cluster slot. See the Slot to
+ * Key API in cluster.c. */
+dictEntry *dictDbCreateEntry(dict *d, void *key) {
     UNUSED(d);
-    return server.cluster_enabled ? sizeof(clusterDictEntryMetadata) : 0;
+    /* Compute sizes of metadata. */
+    size_t meta_sz = 0;
+    if (server.cluster_enabled) meta_sz += sizeof(clusterDictEntryMetadata);
+    size_t total_sz = sizeof(dictEntry) + meta_sz;
+
+    /* Embed the key if it can be represented as SDS_TYPE_5 and the dictEntry
+     * still fits within 64 bytes. */
+    sds sdskey = (sds)key;
+    size_t keylen = sdslen(sdskey);
+    int use_embedded_key = (keylen < (1<<5) &&
+                            total_sz + keylen + 2 <= 64);
+    if (use_embedded_key) total_sz += keylen + 2;
+
+    dictEntry *entry = zmalloc(total_sz);
+    if (meta_sz) memset(entry->metadata, 0, meta_sz);
+    if (use_embedded_key) {
+        char *embedded_key = (char *)entry->metadata + meta_sz + 1;
+        embedded_key[-1] = SDS_TYPE_5 | (keylen << SDS_TYPE_BITS);
+        memcpy(embedded_key, sdskey, keylen);
+        embedded_key[keylen] = '\0';
+        entry->key = embedded_key;
+    } else {
+        entry->key = sdskey;
+    }
+    if (server.cluster_enabled) slotToKeyAddEntry(entry);
+    return entry;
+}
+
+/* The key destructor also cleans up dictEntry metadata. */
+void dictDbKeyDestructor(dict *d, dictEntry *de, void *key) {
+    UNUSED(d);
+    if (server.cluster_enabled) slotToKeyDelEntry(de);
+    /* Only free key if it's not embedded within the entry. */
+    if ((uintptr_t)key < (uintptr_t)de ||
+        (uintptr_t)key >= (uintptr_t)de + zmalloc_usable_size(de)) sdsfree(key);
+}
+
+/* Called after reallocation of a dict entry. The pointer oldde is already
+ * freed, so we can only compare its address but not dereference it. */
+void dictDbRepairEntry(dict *d, dictEntry *newde, dictEntry *oldde) {
+    UNUSED(d);
+    if (server.cluster_enabled) slotToKeyReplaceEntry(newde);
+    if ((uintptr_t)newde->key > (uintptr_t)oldde &&
+        (uintptr_t)newde->key < (uintptr_t)oldde + zmalloc_usable_size(newde)) {
+        /* The key is embedded in the entry, but the key pointer points inside
+         * oldde. Update it to the corresponding offset within newde. */
+        uintptr_t k = (uintptr_t)newde->key + (uintptr_t)newde - (uintptr_t)oldde;
+        newde->key = (void *)k;
+    }
 }
 
 /* Generic hash table type where keys are Redis Objects, Values
@@ -1443,10 +1502,11 @@ dictType dbDictType = {
     NULL,                       /* key dup */
     NULL,                       /* val dup */
     dictSdsKeyCompare,          /* key compare */
-    dictSdsDestructor,          /* key destructor */
+    dictDbKeyDestructor,        /* key destructor */
     dictObjectDestructor,       /* val destructor */
     dictExpandAllowed,          /* allow to expand */
-    dictEntryMetadataSize       /* size of entry metadata in bytes */
+    dictDbCreateEntry,          /* special alloc+init entry */
+    dictDbRepairEntry           /* repair entry after realloc */
 };
 
 /* server.lua_scripts sha (as sds string) -> scripts (as robj) cache. */

src/server.h
@@ -2518,7 +2518,7 @@ uint64_t dictSdsHash(const void *key);
 uint64_t dictSdsCaseHash(const void *key);
 int dictSdsKeyCompare(dict *d, const void *key1, const void *key2);
 int dictSdsKeyCaseCompare(dict *d, const void *key1, const void *key2);
-void dictSdsDestructor(dict *d, void *val);
+void dictSdsDestructor(dict *d, dictEntry *de, void *val);
 
 /* Git SHA1 */
 char *redisGitSHA1(void);