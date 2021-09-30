8c25307ef7a9937c9d93fb6be300d62f11018eed
Wen Hui <wen.hui.ware@gmail.com>
2021-09-30 11:43:35 -0400

    First commit

--- src/cluster.c
+++ src/cluster.c
@@ -31,6 +31,7 @@
 #include "server.h"
 #include "cluster.h"
 #include "endianconv.h"
+#include "custom_hash.c"
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -783,24 +784,30 @@ unsigned long getClusterConnectionsCount(void) {
  * { and } is hashed. This may be useful in the future to force certain
  * keys to be in the same node (assuming no resharding is in progress). */
 unsigned int keyHashSlot(char *key, int keylen) {
-    int s, e; /* start-end indexes of { and } */
+    unsigned int custom_hash = CustomkeyHashSlot(key,keylen);
+    if (custom_hash == 0xFFFF){
+        int s, e; /* start-end indexes of { and } */
 
-    for (s = 0; s < keylen; s++)
-        if (key[s] == '{') break;
+        for (s = 0; s < keylen; s++)
+            if (key[s] == '{') break;
 
-    /* No '{' ? Hash the whole key. This is the base case. */
-    if (s == keylen) return crc16(key,keylen) & 0x3FFF;
+        /* No '{' ? Hash the whole key. This is the base case. */
+        if (s == keylen) return crc16(key,keylen) & 0x3FFF;
 
-    /* '{' found? Check if we have the corresponding '}'. */
-    for (e = s+1; e < keylen; e++)
-        if (key[e] == '}') break;
+        /* '{' found? Check if we have the corresponding '}'. */
+        for (e = s+1; e < keylen; e++)
+            if (key[e] == '}') break;
 
-    /* No '}' or nothing between {} ? Hash the whole key. */
-    if (e == keylen || e == s+1) return crc16(key,keylen) & 0x3FFF;
+        /* No '}' or nothing between {} ? Hash the whole key. */
+        if (e == keylen || e == s+1) return crc16(key,keylen) & 0x3FFF;
 
-    /* If we are here there is both a { and a } on its right. Hash
-     * what is in the middle between { and }. */
-    return crc16(key+s+1,e-s-1) & 0x3FFF;
+        /* If we are here there is both a { and a } on its right. Hash
+         * what is in the middle between { and }. */
+        return crc16(key+s+1,e-s-1) & 0x3FFF;
+    }
+    else {
+        return custom_hash;
+    }
 }
 
 /* -----------------------------------------------------------------------------
---
+++ src/custom_hash.c
@@ -0,0 +1,4 @@
+unsigned int CustomkeyHashSlot(char *key, int keylen) {
+	/* Add the custom hash function here */
+	return 0xFFFF;
+}
