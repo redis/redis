a656198f969b76fbca6e5e91ff10eae1625a4071
Maria Gutovsky <mggutovs@amazon.com>
2021-09-30 08:55:44 +0000

    Add 2 more tests

--- tests/cluster/tests/22-replica-in-sync.tcl
+++ tests/cluster/tests/22-replica-in-sync.tcl
@@ -28,7 +28,7 @@ proc is_replica_online {info_repl} {
 set master_id 0
 
 test "Fill up" {
-    R $master_id debug populate 1000000 key 100
+    R $master_id debug populate 10000000 key 100
 }
 
 test "Add new node as replica" {
@@ -41,14 +41,63 @@ test "Add new node as replica" {
 
 test "Check digest and replica state" {
     R 1 readonly
-    wait_for_condition 100 50 {
+    wait_for_condition 1000 50 {
         [is_in_slots $master_id $replica]
     } else {
         fail "New replica didn't appear in the slots"
     }
-    set master_digest [R $master_id debug digest]
+    wait_for_condition 1000 50 {
+        [is_replica_online [R $master_id info replication]]
+    } else {
+        fail "Replica is down for too long"
+    }
     set replica_digest [R $replica_id debug digest]
-    set info_repl [R $master_id info replication]
-    assert {$master_digest eq $replica_digest}
-    assert {[is_replica_online $info_repl]}
+    assert {$replica_digest ne 0}
+}
+
+test "Replica in loading state is hidden" {
+    # Kill replica client for master and load new data to the primary
+    R $master_id multi
+    R $master_id config set repl-backlog-size 100
+    R $master_id client kill type replica
+    set num 10000
+    set value [string repeat A 1024]
+    for {set j 0} {$j < $num} {incr j} {
+        set key "{0}"
+        append key $j
+        R $master_id set key $value
+    }
+    R $master_id exec
+
+    # Check that replica started loading
+    wait_for_condition 1000 50 {
+        [s $replica_id loading] eq 1
+    } else {
+        fail "Replica didn't enter loading state"
+    }
+    # Check that replica is not in cluster slots
+    assert {![is_in_slots $master_id $replica]}
+
+    # Wait for sync to finish
+    wait_for_condition 1000 50 {
+        [s $replica_id loading] eq 0
+    } else {
+        fail "Replica is in loading state for too long"
+    }
+
+    # Check replica is back to cluster slots
+    wait_for_condition 1000 50 {
+        [is_in_slots $master_id $replica] 
+    } else {
+        fail "Replica is not back to slots"
+    }
+}
+
+test "Check disconnected replica not hidden from slots" {
+    # Disconnect replica from primary
+    R $master_id client kill type replica
+    # Check master to have no replicas
+    assert {[s $master_id connected_slaves] == 0}
+    # Check that replica is still in the cluster slots
+    assert {[is_in_slots $master_id $replica]}
 }
