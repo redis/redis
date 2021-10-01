893bec6a8114014dd23961b742fcd5b703eb3847
Wen Hui <wen.hui.ware@gmail.com>
2021-10-01 12:33:19 -0400

    Update custom_hash.c

--- src/custom_hash.c
+++ src/custom_hash.c
@@ -1,4 +1,12 @@
 unsigned int CustomkeyHashSlot(char *key, int keylen) {
 	/* Add the custom hash function here */
-	return 0xFFFF;
+    unsigned int hash = 0;
+    for(int i = 0; i < keylen; i++) {
+        char c = key[i];
+        int a = c - '0';
+        hash = (hash * 10) + a;     
+    } 
+
+    return hash % 0x3FFF;
+	// return 0xFFFF;
 }
