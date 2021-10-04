fba15850e5c31666e4c3560a3be7fd034fa7e2b6
Oran Agra <oran@redislabs.com>
2021-10-04 12:10:31 +0300

    Prevent unauthenticated client from easily consuming lots of memory (CVE-2021-32675) (#9588)
    This change sets a low limit for multibulk and bulk length in the
    protocol for unauthenticated connections, so that they can't easily
    cause redis to allocate massive amounts of memory by sending just a few
    characters on the network.
    The new limits are 10 arguments of 16kb each (instead of 1m of 512mb)

--- src/server.h
+++ src/server.h
@@ -2091,6 +2091,7 @@ void protectClient(client *c);
 void unprotectClient(client *c);
 void initThreadedIO(void);
 client *lookupClientByID(uint64_t id);
+int authRequired(client *c);
 
 #ifdef __GNUC__
 void addReplyErrorFormat(client *c, const char *fmt, ...)
