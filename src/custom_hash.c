a048b1976f1fc7e8f4e1b49ca93f5d075835a4c6
Wen Hui <wen.hui.ware@gmail.com>
2021-10-05 14:56:12 -0400

    Update custom_hash.c

--- src/custom_hash.c
+++ src/custom_hash.c
@@ -1,9 +1,9 @@
-#define UNUSED(x) (void)(x)
+#define UNUSEDVAR(x) (void)(x) /* To avoid the unused variable warning */
 
 unsigned int CustomkeyHashSlot(char *key, int keylen) {
 	/* Add the custom hash function here 
 	   Please remove the following code and replace it with your custom hash function. */
-	UNUSED(key);
-	UNUSED(keylen);
+	UNUSEDVAR(key);
+	UNUSEDVAR(keylen);
 	return 0;
 }
