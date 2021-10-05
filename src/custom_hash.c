a4e3f1043448a5bf8bb3b99fcaa127957a4e626b
Wen Hui <wen.hui.ware@gmail.com>
2021-10-05 14:54:58 -0400

    Update custom_hash.c

--- src/custom_hash.c
+++ src/custom_hash.c
@@ -1,8 +1,9 @@
+#define UNUSED(x) (void)(x)
+
 unsigned int CustomkeyHashSlot(char *key, int keylen) {
 	/* Add the custom hash function here 
 	   Please remove the following code and replace it with your custom hash function. */
-	if (key && keylen >=0) {
-		return 0;
-	}
+	USUSED(key);
+	USUSED(keylen);
 	return 0;
 }
