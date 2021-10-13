d0e9f70d12180c134d04028f1745e9d245547c4f
Guy Benoish <guy.benoish@redislabs.com>
2021-10-13 07:45:11 -0700

    extend module test

--- tests/modules/container.c
+++ tests/modules/container.c
@@ -2,14 +2,14 @@
 
 #define UNUSED(V) ((void) V)
 
-int cmd_config_set(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+int cmd_set(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     UNUSED(argv);
     UNUSED(argc);
     RedisModule_ReplyWithSimpleString(ctx, "OK");
     return REDISMODULE_OK;
 }
 
-int cmd_config_get(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+int cmd_get(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     UNUSED(argv);
     UNUSED(argc);
     RedisModule_ReplyWithSimpleString(ctx, "OK");
@@ -20,22 +20,36 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     REDISMODULE_NOT_USED(argv);
     REDISMODULE_NOT_USED(argc);
 
+    int spec_id;
+
     if (RedisModule_Init(ctx, "container", 1, REDISMODULE_APIVER_1)== REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
-    if (RedisModule_CreateCommand(ctx,"container.config",NULL,"",0,0,0) == REDISMODULE_ERR)
+    if (RedisModule_CreateCommand(ctx,"container.bitarray",NULL,"",0,0,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
-    if (RedisModule_CreateSubcommand(ctx,"set",cmd_config_set,"",0,0,0,"container.config") == REDISMODULE_ERR)
+    if (RedisModule_CreateSubcommand(ctx,"set",cmd_set,"",0,0,0,"container.bitarray") == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_AddCommandKeySpec(ctx,"container.bitarray|set","write",&spec_id) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_SetCommandKeySpecBeginSearchIndex(ctx,"container.bitarray|set",spec_id,1) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_SetCommandKeySpecFindKeysRange(ctx,"container.bitarray|set",spec_id,0,1,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
-    if (RedisModule_CreateSubcommand(ctx,"get",cmd_config_get,"",0,0,0,"container.config") == REDISMODULE_ERR)
+    if (RedisModule_CreateSubcommand(ctx,"get",cmd_get,"",0,0,0,"container.bitarray") == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_AddCommandKeySpec(ctx,"container.bitarray|get","read",&spec_id) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_SetCommandKeySpecBeginSearchIndex(ctx,"container.bitarray|get",spec_id,1) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+    if (RedisModule_SetCommandKeySpecFindKeysRange(ctx,"container.bitarray|get",spec_id,0,1,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
     /* Sanity */
-    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"config") == REDISMODULE_ERR);
-    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"container.config") == REDISMODULE_ERR);
-    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"container.config|get") == REDISMODULE_ERR);
+    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"bitarray") == REDISMODULE_ERR);
+    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"container.bitarray") == REDISMODULE_ERR);
+    RedisModule_Assert(RedisModule_CreateSubcommand(ctx,"get",NULL,"",0,0,0,"container.bitarray|get") == REDISMODULE_ERR);
 
     return REDISMODULE_OK;
 }
--- tests/unit/moduleapi/container.tcl
+++ tests/unit/moduleapi/container.tcl
@@ -4,13 +4,13 @@ start_server {tags {"modules"}} {
     r module load $testmodule
 
     test "Module subcommands via COMMAND" {
-        set reply [r command info container.config]
+        set reply [r command info container.bitarray]
         set subcmds [lindex [lindex $reply 0] 8]
-        assert_equal [lsort $subcmds] {{get -2 {} 0 0 0 {} {} {}} {set -2 {} 0 0 0 {} {} {}}}
+        assert_equal [lsort $subcmds] {{get -2 {} 1 1 1 {} {{flags read begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}} {}} {set -2 {} 1 1 1 {} {{flags write begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}} {}}}
     }
 
     test "Module pure-container command fails on arity error" {
-        catch {r container.config} e
+        catch {r container.bitarray} e
         set e
     } {*wrong number of arguments *}
 }
