3df85d5335ca7b80e9655091115f0e9b36c52d70
qetu3790 <qetu3790@163.com>
2021-10-18 13:51:10 +0800

    Update cluster.tcl

--- tests/unit/moduleapi/cluster.tcl
+++ tests/unit/moduleapi/cluster.tcl
@@ -19,6 +19,7 @@ proc csi {args} {
 }
 
 set testmodule [file normalize tests/modules/blockonkeys.so]
+set testmodule_nokey [file normalize tests/modules/blockonbackground.so]
 
 # make sure the test infra won't use SELECT
 set ::singledb 1
@@ -36,6 +37,10 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
     $node2 module load $testmodule
     $node3 module load $testmodule
 
+    $node1 module load $testmodule_nokey
+    $node2 module load $testmodule_nokey
+    $node3 module load $testmodule_nokey
+
     test {Create 3 node cluster} {
         exec src/redis-cli --cluster-yes --cluster create \
                            127.0.0.1:[srv 0 port] \
@@ -51,7 +56,7 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
         }
     }
 
-    test "Run blocking command on cluster node3" {
+    test "Run blocking command (including keys) on cluster node3" {
         # key9184688 is mapped to slot 10923 (first slot of node 3)
         set node3_rd [redis_deferring_client -2]
         $node3_rd fsl.bpop key9184688 0
@@ -60,9 +65,23 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
         wait_for_condition 50 100 {
             [s -2 blocked_clients] eq {1}
         } else {
-            fail "Client not blocked"
+            fail "Client executing blocking command (including keys) not blocked"
         }
     }
+
+    test "Run blocking command (no keys) on cluster node2" {
+
+        set node2_rd [redis_deferring_client -1]
+        $node2_rd block.block 0
+        $node2_rd flush
+
+        wait_for_condition 50 100 {
+            [s -1 blocked_clients] eq {1}
+        } else {
+            fail "Client executing blocking command (no keys) not blocked"
+        }
+    }
+
 
     test "Perform a Resharding" {
         exec src/redis-cli --cluster-yes --cluster reshard 127.0.0.1:[srv -2 port] \
@@ -71,7 +90,16 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
                            --cluster-slots 1
     }
 
-    test "Verify command got unblocked after resharding" {
+    test "Verify command (no keys) is unaffected after resharding" {
+
+        # verify there are blocked clients on node2
+        assert_equal [s -1 blocked_clients]  {1}
+
+        #release client 
+        $node2 block.release 0
+    }
+
+    test "Verify command (including keys) got unblocked after resharding" {
         # this (read) will wait for the node3 to realize the new topology
         assert_error {*MOVED*} {$node3_rd read}
 
@@ -83,9 +111,9 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
 
     test "wait for cluster to be stable" {
         wait_for_condition 1000 50 {
-            [csi 0 cluster_state] eq {ok} &&
-            [csi -1 cluster_state] eq {ok} &&
-            [csi -2 cluster_state] eq {ok}
+            [catch {exec src/redis-cli --cluster \
+            check 127.0.0.1:[srv 0 port] \
+            }] == 0
         } else {
             fail "Cluster doesn't stabilize"
         }
@@ -111,7 +139,65 @@ start_server {overrides {cluster-enabled yes} tags {"external:skip cluster modul
     }
 
     $node1_rd close
+    $node2_rd close
     $node3_rd close
+
+    test "Run blocking command (including keys) again on cluster node1" {
+        $node1 del key9184688
+        # key9184688 is mapped to slot 10923 which has been moved to node1
+        set node1_rd [redis_deferring_client 0]
+        $node1_rd fsl.bpop key9184688 0
+        $node1_rd flush
+
+        wait_for_condition 50 100 {
+            [s 0 blocked_clients] eq {1}
+        } else {
+            fail "Client executing blocking command (including keys) again not blocked"
+        }
+    }
+
+    test "Run blocking command (no keys) again on cluster node2" {
+
+        set node2_rd [redis_deferring_client -1]
+
+        $node2_rd block.block 0
+        $node2_rd flush
+
+        wait_for_condition 50 100 {
+            [s -1 blocked_clients] eq {1}
+        } else {
+            fail "Client executing blocking command (no keys) again not blocked"
+        }
+    }
+
+    test "Waiting for cluster to be failed" {
+
+        #kill node3 in cluster 
+        exec kill -9 [srv -2 pid]
+
+        wait_for_condition 1000 50 {
+            [csi 0 cluster_state] eq {fail} &&
+            [csi -1 cluster_state] eq {fail}
+        } else {
+            fail "Cluster doesn't fail"
+        }
+    }
+
+    test "Verify command (including keys) got unblocked after cluster failure" {
+        assert_error {*CLUSTERDOWN*} {$node1_rd read}
+    }
+
+    test "Verify command (no keys) got unblocked after cluster failure" {
+        assert_error {*CLUSTERDOWN*} {$node2_rd read}
+
+        # verify there are no blocked clients
+        assert_equal [s 0 blocked_clients]  {0}
+        assert_equal [s -1 blocked_clients]  {0}
+    }
+
+
+    $node1_rd close
+    $node2_rd close
 
 # stop three servers
 }
