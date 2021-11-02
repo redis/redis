bf0e1b7054d946974f3e311208abe1b89dc8e046
perryitay <85821686+perryitay@users.noreply.github.com>
2021-11-02 14:31:06 +0200

    Update src/rdb.c
    Co-authored-by: sundb <sundbcn@gmail.com>

--- src/rdb.c
+++ src/rdb.c
@@ -1972,6 +1972,7 @@ robj *rdbLoadObject(int rdbtype, rio *rdb, sds key, int dbid, int *error) {
 
             if (container == QUICKLIST_NODE_CONTAINER_PLAIN) {
                 quicklistAppendPlainNode(o->ptr, data, encoded_len);
+                zfree(data);
                 continue;
             }
 
