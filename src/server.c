fdf486973ca7183794650c37c31e394c9eb6d20f
yancz2000 <yanchaozhong_yewu@cmss.chinamobile.com>
2021-11-05 17:58:35 +0800

    Fix the version display of sentinel
    [root@node redis-unstable]# src/redis-sentinel -v
    Redis server v=255.255.255 sha=00000000:0 malloc=jemalloc-5.1.0 bits=64 build=d788edfc546625c9

--- src/server.c
+++ src/server.c
@@ -7136,13 +7136,24 @@ void daemonize(void) {
 }
 
 void version(void) {
-    printf("Redis server v=%s sha=%s:%d malloc=%s bits=%d build=%llx\n",
-        REDIS_VERSION,
-        redisGitSHA1(),
-        atoi(redisGitDirty()) > 0,
-        ZMALLOC_LIB,
-        sizeof(long) == 4 ? 32 : 64,
-        (unsigned long long) redisBuildId());
+    if (server.sentinel_mode) {
+        printf("Redis sentinel v=%s sha=%s:%d malloc=%s bits=%d build=%llx\n",
+            REDIS_VERSION,
+            redisGitSHA1(),
+            atoi(redisGitDirty()) > 0,
+            ZMALLOC_LIB,
+            sizeof(long) == 4 ? 32 : 64,
+            (unsigned long long) redisBuildId());
+    }
+    else {
+        printf("Redis server v=%s sha=%s:%d malloc=%s bits=%d build=%llx\n",
+            REDIS_VERSION,
+            redisGitSHA1(),
+            atoi(redisGitDirty()) > 0,
+            ZMALLOC_LIB,
+            sizeof(long) == 4 ? 32 : 64,
+            (unsigned long long) redisBuildId());
+    }	
     exit(0);
 }
 
