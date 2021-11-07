3141f6a7cd93395b6a1d5a487f019003f9166d09
yancz2000 <yanchaozhong_yewu@cmss.chinamobile.com>
2021-11-07 11:45:51 +0800

    Update server.c

--- src/server.c
+++ src/server.c
@@ -7144,23 +7144,6 @@ void version(void) {
         ZMALLOC_LIB,
         sizeof(long) == 4 ? 32 : 64,
         (unsigned long long) redisBuildId());
-        printf("Redis sentinel v=%s sha=%s:%d malloc=%s bits=%d build=%llx\n",
-            REDIS_VERSION,
-            redisGitSHA1(),
-            atoi(redisGitDirty()) > 0,
-            ZMALLOC_LIB,
-            sizeof(long) == 4 ? 32 : 64,
-            (unsigned long long) redisBuildId());
-    }
-    else {
-        printf("Redis server v=%s sha=%s:%d malloc=%s bits=%d build=%llx\n",
-            REDIS_VERSION,
-            redisGitSHA1(),
-            atoi(redisGitDirty()) > 0,
-            ZMALLOC_LIB,
-            sizeof(long) == 4 ? 32 : 64,
-            (unsigned long long) redisBuildId());
-    }	
     exit(0);
 }
 
