60218fe1f1413acbd9cbdb643c26d2494e33aa69
sundb <sundbcn@gmail.com>
2021-11-15 23:21:58 -0800

    Fix comment

--- src/lzfP.h
+++ src/lzfP.h
@@ -129,7 +129,7 @@
  * Whether to store pointers or offsets inside the hash table. On
  * 64 bit architectures, pointers take up twice as much space,
  * and might also be slower. Default is to autodetect.
- * Notice: Avoid to set this value to 1, this will result in `LZF_HSLOT`
+ * Notice: Don't set this value to 1, it will result in 'LZF_HSLOT'
  * not being able to store offset above UINT32_MAX in 64bit. */
 #define LZF_USE_OFFSETS 0
 
