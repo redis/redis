28defd3cd60d242f21b71f16ec37b0412e220c31
Oran Agra <oran@redislabs.com>
2021-11-21 18:30:18 +0200

    flush stdout

--- src/debug.c
+++ src/debug.c
@@ -1976,8 +1976,11 @@ void bugReportEnd(int killViaSignal, int sig) {
 
     if (!killViaSignal) {
         /* To avoid issues with valgrind, we may wanna exit rahter than generate a signal */
-        if (server.use_exit_on_panic)
-            _exit(1); /* Using _exit to bypass false leak reports by gcc ASAN */
+        if (server.use_exit_on_panic) {
+             /* Using _exit to bypass false leak reports by gcc ASAN */
+             fflush(stdout);
+            _exit(1);
+        }
         abort();
     }
 
