8af9a3d40f0b71091b5098b6e0ad92b8a0a0b1e1
hwware <wen.hui.ware@gmail.com>
2021-11-26 14:44:55 -0500

    update test case for PR

--- tests/sentinel/tests/12-master-reboot.tcl
+++ tests/sentinel/tests/12-master-reboot.tcl
@@ -1,5 +1,4 @@
 # Check the basic monitoring and failover capabilities.
-#source "../tests/includes/start-init-tests.tcl"
 source "../tests/includes/init-tests.tcl"
 
 
@@ -10,11 +9,8 @@ if {$::simulate_error} {
 }
 
 
-# Restart an instance previously killed by kill_instance
-proc restart_instance_local {type id} {
-
-    puts "run restart locally"
-
+# Reboot an instance previously in very short time but do not check if it is loading
+proc reboot_instance {type id} {
     set dirname "${type}_${id}"
     set cfgfile [file join $dirname $type.conf]
     set port [get_instance_attrib $type $id port]
@@ -36,44 +32,20 @@ proc restart_instance_local {type id} {
     set link [redis 127.0.0.1 $port 0 $::tls]
     $link reconnect 1
     set_instance_attrib $type $id link $link
-
-    # Make sure the instance is not loading the dataset when this
-    # function returns.
-    # while 1 {
-    #    catch {[$link ping]} retval
-    #    if {[string match {*LOADING*} $retval]} {
-    #        after 100
-    #        continue
-    #    } else {
-    #        break
-    #    }
-    # }
-   
 }
 
 
-test "Before failover output all ids" {
-   puts ""
-   puts "current master id is $master_id"
-   set new_port [RPort $master_id]
-   puts "current port is $new_port"
-}
-
-test "Restart master test" {
+test "Master reboot in very short time" {
     set old_port [RPort $master_id]
     set addr [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
     assert {[lindex $addr 1] == $old_port}
-
-    puts "populate 10000 keys"
+    
     R $master_id debug populate 10000
     R $master_id bgsave
-    R $master_id config set key-load-delay 10000
+    R $master_id config set key-load-delay 1500
     R $master_id config set loading-process-events-interval-bytes 1024
     R $master_id config rewrite
 
-    set size_b [R $master_id dbsize]
-    puts "now dbsize is $size_b"
-    
     foreach_sentinel_id id {
         S $id SENTINEL SET mymaster master-reboot-down-after-period 5000
         S $id sentinel debug ping-period 500
@@ -81,9 +53,7 @@ test "Restart master test" {
     }
 
     kill_instance redis $master_id
-    set previous_master_id $master_id
-   
-    restart_instance_local redis $master_id
+    reboot_instance redis $master_id
     
     foreach_sentinel_id id {        
         wait_for_condition 1000 100 {
@@ -92,21 +62,42 @@ test "Restart master test" {
             fail "At least one Sentinel did not receive failover info"
         }
     }
-    after 20000   
-    puts "current port AAA ---------------------------------------"
+
     set addr [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
     set master_id [get_instance_id_by_port redis [lindex $addr 1]]
-    puts "previous_master_id = $previous_master_id"
 
+    # Make sure the instance load all the dataset
+    while 1 {
+        catch {[$link ping]} retval
+        if {[string match {*LOADING*} $retval]} {
+            after 100
+            continue
+        } else {
+            break
+        }
+    }
 }
 
 test "New master [join $addr {:}] role matches" {
     assert {[RI $master_id role] eq {master}}
 }
 
-test "after failover output all ids" {
-   puts ""
-   puts "current master id is $master_id"
-   set new_port [RPort $master_id]
-   puts "current port is $new_port"
+test "All the other slaves now point to the new master" {
+    foreach_redis_id id {
+        if {$id != $master_id && $id != 0} {
+            wait_for_condition 1000 50 {
+                [RI $id master_port] == [lindex $addr 1]
+            } else {
+                fail "Redis ID $id not configured to replicate with new master"
+            }
+        }
+    }
+}
+
+test "The old master eventually gets reconfigured as a slave" {
+    wait_for_condition 1000 50 {
+        [RI 0 master_port] == [lindex $addr 1]
+    } else {
+        fail "Old master not reconfigured as slave of new master"
+    }
 }
