37ea859e83f12493a684d744be130cd6af80efa1
zhugezy <zhugezy@outlook.com>
2021-12-08 11:08:48 +0800

    2 test cases changed

--- tests/integration/replication-3.tcl
+++ tests/integration/replication-3.tcl
@@ -140,58 +140,9 @@ start_server {tags {"repl external:skip"}} {
             }
         }
 
-        set numops 20000 ;# Enough to trigger the Script Cache LRU eviction.
-
         # While we are at it, enable AOF to test it will be consistent as well
         # after the test.
         r config set appendonly yes
-
-        test {MASTER and SLAVE consistency with EVALSHA replication} {
-            array set oldsha {}
-            for {set j 0} {$j < $numops} {incr j} {
-                set key "key:$j"
-                # Make sure to create scripts that have different SHA1s
-                set script "return redis.call('incr','$key')"
-                set sha1 [r eval "return redis.sha1hex(\"$script\")" 0]
-                set oldsha($j) $sha1
-                r eval $script 0
-                set res [r evalsha $sha1 0]
-                assert {$res == 2}
-                # Additionally call one of the old scripts as well, at random.
-                set res [r evalsha $oldsha([randomInt $j]) 0]
-                assert {$res > 2}
-
-                # Trigger an AOF rewrite while we are half-way, this also
-                # forces the flush of the script cache, and we will cover
-                # more code as a result.
-                if {$j == $numops / 2} {
-                    catch {r bgrewriteaof}
-                }
-            }
-
-            wait_for_condition 50 100 {
-                [r dbsize] == $numops &&
-                [r -1 dbsize] == $numops &&
-                [r debug digest] eq [r -1 debug digest]
-            } else {
-                set csv1 [csvdump r]
-                set csv2 [csvdump {r -1}]
-                set fd [open /tmp/repldump1.txt w]
-                puts -nonewline $fd $csv1
-                close $fd
-                set fd [open /tmp/repldump2.txt w]
-                puts -nonewline $fd $csv2
-                close $fd
-                puts "Master - Replica inconsistency"
-                puts "Run diff -u against /tmp/repldump*.txt for more info"
-            }
-
-            set old_digest [r debug digest]
-            r config set appendonly no
-            r debug loadaof
-            set new_digest [r debug digest]
-            assert {$old_digest eq $new_digest}
-        }
 
         test {SLAVE can reload "lua" AUX RDB fields of duplicated scripts} {
             # Force a Slave full resynchronization
