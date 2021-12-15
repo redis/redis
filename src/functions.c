4400e6c43cca20a95502042b6e28f1177adc8360
meir <meir@redis.com>
2021-12-15 18:57:59 +0200

    Review fixes
    Use `server.dirty++` instead of `forceCommandPropagation` to make sure
    the command will be counted as changing the data (for persistence configuration).

--- src/functions.c
+++ src/functions.c
@@ -394,7 +394,9 @@ void functionsFlushCommand(client *c) {
     } else {
         functionsCtxClear(functions_ctx);
     }
-    forceCommandPropagation(c, PROPAGATE_REPL | PROPAGATE_AOF);
+    /* Indicate that the command changed the data so it will be replicated and
+     * counted as a data change (for persistence configuration) */
+    server.dirty++;
     addReply(c,shared.ok);
 }
 
