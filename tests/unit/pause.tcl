da1c45a82acadffb3713bb7ac83209dd07fef31b
zhugezy <zhugezy@outlook.com>
2021-12-22 16:24:57 +0800

    undo previous changes, shortening pause duration instead.

--- tests/unit/pause.tcl
+++ tests/unit/pause.tcl
@@ -1,6 +1,6 @@
 start_server {tags {"pause network"}} {
     test "Test read commands are not blocked by client pause" {
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 10000 WRITE
         set rd [redis_deferring_client]
         $rd GET FOO
         $rd PING
@@ -11,11 +11,11 @@ start_server {tags {"pause network"}} {
     }
 
     test "Test write commands are paused by RO" {
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 6000 WRITE
 
         set rd [redis_deferring_client]
         $rd SET FOO BAR
-        wait_for_paused_clients_count 1 50 100
+        wait_for_blocked_clients_count 1 50 100
 
         r client unpause
         assert_match "OK" [$rd read]
@@ -24,18 +24,18 @@ start_server {tags {"pause network"}} {
 
     test "Test special commands are paused by RO" {
         r PFADD pause-hll test
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 11000 WRITE
 
         # Test that pfcount, which can replicate, is also blocked
         set rd [redis_deferring_client]
         $rd PFCOUNT pause-hll
-        wait_for_paused_clients_count 1 50 100
+        wait_for_blocked_clients_count 1 50 100
 
         # Test that publish, which adds the message to the replication
         # stream is blocked.
         set rd2 [redis_deferring_client]
         $rd2 publish foo bar
-        wait_for_paused_clients_count 2 50 100
+        wait_for_blocked_clients_count 2 50 100
 
         r client unpause 
         assert_match "1" [$rd read]
@@ -46,7 +46,7 @@ start_server {tags {"pause network"}} {
 
     test "Test read/admin mutli-execs are not blocked by pause RO" {
         r SET FOO BAR
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 10000 WRITE
         set rd [redis_deferring_client]
         $rd MULTI
         assert_equal [$rd read] "OK"
@@ -67,33 +67,33 @@ start_server {tags {"pause network"}} {
         assert_equal [$rd read] "OK"
         $rd SET FOO BAR
         assert_equal [$rd read] "QUEUED"
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 6000 WRITE
         $rd EXEC
-        wait_for_paused_clients_count 1 50 100
+        wait_for_blocked_clients_count 1 50 100
         r client unpause 
         assert_match "OK" [$rd read]
         $rd close
     }
 
     test "Test scripts are blocked by pause RO" {
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 6000 WRITE
         set rd [redis_deferring_client]
         $rd EVAL "return 1" 0
 
-        wait_for_paused_clients_count 1 50 100
+        wait_for_blocked_clients_count 1 50 100
         r client unpause 
         assert_match "1" [$rd read]
         $rd close
     }
 
     test "Test multiple clients can be queued up and unblocked" {
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 6000 WRITE
         set clients [list [redis_deferring_client] [redis_deferring_client] [redis_deferring_client]]
         foreach client $clients {
             $client SET FOO BAR
         }
 
-        wait_for_paused_clients_count 3 50 100
+        wait_for_blocked_clients_count 3 50 100
         r client unpause
         foreach client $clients {
             assert_match "OK" [$client read]
@@ -102,7 +102,7 @@ start_server {tags {"pause network"}} {
     }
 
     test "Test clients with syntax errors will get responses immediately" {
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 10000 WRITE
         catch {r set FOO} err
         assert_match "ERR wrong number of arguments for *" $err
         r client unpause
@@ -113,7 +113,7 @@ start_server {tags {"pause network"}} {
         r multi
         r set foo{t} bar{t} PX 10
         r set bar{t} foo{t} PX 10
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 5000 WRITE
         r exec
 
         wait_for_condition 10 100 {
@@ -138,14 +138,14 @@ start_server {tags {"pause network"}} {
     test "Test that client pause starts at the end of a transaction" {
         r MULTI
         r SET FOO1{t} BAR
-        r client PAUSE 100000000 WRITE
+        r client PAUSE 6000 WRITE
         r SET FOO2{t} BAR
         r exec
 
         set rd [redis_deferring_client]
         $rd SET FOO3{t} BAR
 
-        wait_for_paused_clients_count 1 50 100
+        wait_for_blocked_clients_count 1 50 100
 
         assert_match "BAR" [r GET FOO1{t}]
         assert_match "BAR" [r GET FOO2{t}]
