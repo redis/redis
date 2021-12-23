a040a44263d6c895876ef1070eaa5c641913c73f
Itamar Haber <itamar@redis.com>
2021-12-23 20:39:44 +0200

    Second iteration

--- utils/generate-commands-json.py
+++ utils/generate-commands-json.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 '''
-Generate a commands.json file from the output of `redis-cli --jspn COMMAND`
+Transform the output from `redis-cli --json COMMAND` to commands.json format.
 '''
+from collections import OrderedDict
+from sys import stdin
 import argparse
 import json
-import sys
 
 def convert_flags_to_boolean_dict(flags):
     ''' Return a dict with a key set to `True` per element in the flags list. '''
     return {f: True for f in flags} # TODO: remove after guy's PR
 
+def set_if_not_none_or_empty(dst, key, value):
+    ''' Set 'key' in 'dst' if 'value' is not `None` or an empty list. '''
+    if value is not None and (type(value) is not list or len(value)):
+        dst[key] = value
+
 def convert_argument(arg):
     ''' Transform an argument. '''
     arg.update(convert_flags_to_boolean_dict(arg.pop('flags', [])))
-    arg['arguments'] = [convert_argument(x) for x in arg.pop('arguments',[])]
+    set_if_not_none_or_empty(arg, 'arguments', [convert_argument(x) for x in arg.pop('arguments',[])])
     return arg
 
 def convert_keyspec(spec):
@@ -35,62 +41,67 @@ def convert_entry_to_objects_array(container, cmd):
 
     This returns a list with a dict for the command and per each of its
     subcommands. Each dict contains one key, the command's full name, with a
-    value of a dict that's set with the command's properties.
+    value of a dict that's set with the command's properties and meta
+    information.
     '''
+    assert len(cmd) >= 8
     obj = {}
     rep = [obj]
     name = cmd[0].upper()
+    meta = cmd[7]
     key = f'{container} {name}' if container else name
 
-    meta = cmd[7]
-    meta['arity'] = cmd[1]
-    meta['command_flags'] = cmd[2]
-    meta['acl_categories'] = cmd[6]
-    meta['key_specs'] = [convert_keyspec(x) for x in meta.pop('key_specs',[])]
-    meta['arguments'] = [convert_argument(x) for x in meta.pop('arguments', [])]
-    meta['doc_flags'] = [convert_flags_to_boolean_dict(x) for x in meta.pop('doc_flags', [])]
     rep.extend([convert_entry_to_objects_array(name, x)[0] for x in meta.pop('subcommands', [])])
 
-    FIELDS = [
-        'summary',
-        'since',
-        'group',
-        'complexity',
-        'deprecated_since',
-        'replaced_by',
-        'history',
-        'acl_categories',
-        'arity',
-        'key_specs',
-        'arguments',
-        'command_flags',
-        'doc_flags',
-    ]
-    obj[key] = {}
-    for field in FIELDS:
-        if field in meta and (type(meta[field]) is not list or len(meta[field]) > 0):
-            obj[key][field] = meta[field]
+    # The command's value is ordered so the interesting stuff that we care about
+    # is at the start. Optional `None` and empty list values are filtered out.
+    value = OrderedDict()
+    value['summary'] = meta.pop('summary')
+    value['since'] = meta.pop('since')
+    value['group'] = meta.pop('group')
+    set_if_not_none_or_empty(value, 'complexity', meta.pop('complexity', None))
+    set_if_not_none_or_empty(value, 'deprecated_since', meta.pop('deprecated_since', None))
+    set_if_not_none_or_empty(value, 'replaced_by', meta.pop('replaced_by', None))
+    set_if_not_none_or_empty(value, 'history', meta.pop('history', []))
+    set_if_not_none_or_empty(value, 'acl_categories', meta.pop('acl_categories', []))
+    value['arity'] = cmd[1]
+    set_if_not_none_or_empty(value, 'key_specs', [convert_keyspec(x) for x in meta.pop('key_specs',[])])
+    set_if_not_none_or_empty(value, 'arguments', [convert_argument(x) for x in meta.pop('arguments', [])])
+    set_if_not_none_or_empty(value, 'command_flags', [convert_flags_to_boolean_dict(x) for x in meta.pop('command_flags', [])])
+    set_if_not_none_or_empty(value, 'doc_flags', [convert_flags_to_boolean_dict(x) for x in meta.pop('doc_flags', [])])
+    set_if_not_none_or_empty(value, 'hints', meta.pop('hints', []))
 
+    # All remaining meta key-value tuples, if any, are appended to the command
+    # to be future-proof.
+    while len(meta) > 0:
+        (k, v) = meta.popitem()
+        value[k] = v
+
+    obj[key] = value
     return rep
 
 # MAIN
-parser = argparse.ArgumentParser()
-# parser.add_argument('input', nargs='?', type=argparse.FileType(), default=sys.stdin)
-parser.add_argument('input', nargs='?', type=argparse.FileType(), default='/Users/itamar/commands.json')
-args = parser.parse_args()
+if __name__ == '__main__':
+    opts = {
+        'description': 'Transform the output from `redis-cli --json COMMAND` to commands.json format.',
+        'epilog': ''
+    }
+    parser = argparse.ArgumentParser(**opts)
+    parser.add_argument('input', help='JSON-formatted input file (default: stdin)', nargs='?', type=argparse.FileType(), default=stdin)
+    args = parser.parse_args()
 
-payload = {}
-commands = []
-data = json.load(args.input)
+    payload = OrderedDict()
+    commands = []
+    data = json.load(args.input)
 
-for entry in data:
-    cmds = convert_entry_to_objects_array(None, entry)
-    commands.extend(cmds)
+    for entry in data:
+        cmds = convert_entry_to_objects_array(None, entry)
+        commands.extend(cmds)
 
-# The final output is a dict of all commands, ordered by name.
-commands.sort(key=lambda x: list(x.keys())[0])
-for cmd in commands:
-    name = list(cmd.keys())[0]
-    payload[name] = cmd[name]
+    # The final output is a dict of all commands, ordered by name.
+    commands.sort(key=lambda x: list(x.keys())[0])
+    for cmd in commands:
+        name = list(cmd.keys())[0]
+        payload[name] = cmd[name]
 
-print(json.dumps(payload, indent=4))
+    print(json.dumps(payload, indent=4))
