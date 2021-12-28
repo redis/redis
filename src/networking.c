59775898912440e76d68ec00d89a2829af0a6142
Yoav Steinberg <yoav@monfort.co.il>
2021-12-28 12:17:47 +0200

    Make sure replicas don't write their own replies to the replication link

--- src/networking.c
+++ src/networking.c
@@ -355,6 +355,15 @@ void _addReplyProtoToList(client *c, const char *s, size_t len) {
 void _addReplyToBufferOrList(client *c, const char *s, size_t len) {
     if (c->flags & CLIENT_CLOSE_AFTER_REPLY) return;
 
+    /* Replicas should normally not cause any writes to the reply buffer. In case a rouge replica sent a command on the
+     * replication link that caused a reply to be generated we'll simply disconnect it. */
+    if (getClientType(c) == CLIENT_TYPE_SLAVE) {
+        serverLog(LL_WARNING, "Replica generated a reply to command %s, disconnecting it",
+                  c->lastcmd ? c->lastcmd->name : "<unknown>");
+        freeClientAsync(c);
+        return;
+    }
+
     size_t reply_len = _addReplyToBuffer(c,s,len);
     if (len > reply_len) _addReplyProtoToList(c,s+reply_len,len-reply_len);
 }
