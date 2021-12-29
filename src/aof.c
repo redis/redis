88460ee6a9362d9e3275bd1c4028231fba69a548
yoav-steinberg <yoav@monfort.co.il>
2021-12-29 12:38:45 +0200

    CR cleanup
    Co-authored-by: sundb <sundbcn@gmail.com>

--- src/aof.c
+++ src/aof.c
@@ -1552,7 +1552,7 @@ int rewriteAppendOnlyFile(char *filename) {
 
     if (server.aof_use_rdb_preamble) {
         int error;
-        if (rdbSaveRio(SLAVE_REQ_NONE, &aof,&error,RDBFLAGS_AOF_PREAMBLE,NULL) == C_ERR) {
+        if (rdbSaveRio(SLAVE_REQ_NONE,&aof,&error,RDBFLAGS_AOF_PREAMBLE,NULL) == C_ERR) {
             errno = error;
             goto werr;
         }
