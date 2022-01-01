f3fc08c25d244f69be3f9ff0093f07b8b8e4423f
enjoy-binbin <binloveplay1314@qq.com>
2022-01-01 18:09:59 +0800

    Fix timing issue in sentinel CKQUORUM test
    A test failure was reported in Daily CI (test-centos7-tls).
    `CKQUORUM detects failover authorization cannot be reached`.
    
    ```
    CKQUORUM detects failover authorization cannot be reached: FAILED:
    Expected 'invalid command name "OK 4 usable Sentinels. Quorum and failover authorization can be reached"' to match '*NOQUORUM*'
    ```
    
    It seems that current sentinel does not confirm that the other
    sentinels are actually `down`, and then check the quorum.
    It at least take 3 seconds on my machine, and we can see there
    will be a timing issue with the hard code `after 5000`.
    
    In this commit, we check the response of `SENTINEL SENTINELS mymaster`
    to ensure that other sentinels are actually `down` in the view the
    current sentinel. Solve the timing issue due to sentinel monitor mechanism.

--- tests/sentinel/tests/06-ckquorum.tcl
+++ tests/sentinel/tests/06-ckquorum.tcl
@@ -3,6 +3,20 @@
 source "../tests/includes/init-tests.tcl"
 set num_sentinels [llength $::sentinel_instances]
 
+# According to the SENTINELS MASTER response, get the number of sentinels
+# that in the `down` state (s_down or o_down).
+proc get_sentinel_sentinels_down_count {sentinel_id master_name} {
+    set down_sentinel_count 0
+    set lines [S $sentinel_id SENTINEL SENTINELS $master_name]
+
+    foreach line $lines {
+        set flags [dict get $line flags]
+        if {[string match *down* $flags]} { incr down_sentinel_count 1 }
+    }
+
+    return $down_sentinel_count
+}
+
 test "CKQUORUM reports OK and the right amount of Sentinels" {
     foreach_sentinel_id id {
         assert_match "*OK $num_sentinels usable*" [S $id SENTINEL CKQUORUM mymaster]
@@ -24,9 +38,15 @@ test "CKQUORUM detects failover authorization cannot be reached" {
         kill_instance sentinel [expr {$i + 1}]
     }
 
-    after 5000
-    catch {[S 0 SENTINEL CKQUORUM mymaster]} err
-    assert_match "*NOQUORUM*" $err
+    # Wait for the current sentinel to monitor that other sentinels are
+    # `down`, so that we can check the quorum directly later.
+    wait_for_condition 1000 50 {
+        [get_sentinel_sentinels_down_count 0 mymaster] == $orig_quorum
+    } else {
+        fail "At least $orig_quorum sentinels did not enter the down state."
+    }
+
+    assert_error "*NOQUORUM*" {S 0 SENTINEL CKQUORUM mymaster}
     S 0 SENTINEL SET mymaster quorum $orig_quorum
     for {set i 0} {$i < $orig_quorum} {incr i} {
         restart_instance sentinel [expr {$i + 1}]
