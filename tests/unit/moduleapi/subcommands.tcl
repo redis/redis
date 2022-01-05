82de9184077a0fc586a72a60ec58248220868b1d
Oran Agra <oran@redislabs.com>
2022-01-05 12:16:04 +0200

    fix module tests

--- tests/unit/moduleapi/subcommands.tcl
+++ tests/unit/moduleapi/subcommands.tcl
@@ -5,15 +5,18 @@ start_server {tags {"modules"}} {
 
     test "Module subcommands via COMMAND" {
         # Verify that module subcommands are displayed correctly in COMMAND
-        set reply [r command info subcommands.bitarray]
-        # create a dict for easy lookup
-        unset -nocomplain mydict
-        foreach {k v} [lindex [lindex $reply 0] 7] {
-            dict append mydict $k $v
-        }
-        set subcmds [lsort [dict get $mydict subcommands]]
-        assert_equal [lindex $subcmds 0] {get -2 module 1 1 1 {} {summary {} since {} group module key_specs {{flags read begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}}}}
-        assert_equal [lindex $subcmds 1] {set -2 module 1 1 1 {} {summary {} since {} group module key_specs {{flags write begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}}}}
+        set command_reply [r command info subcommands.bitarray]
+        set first_cmd [lindex $command_reply 0]
+        set subcmds_in_command [lsort [lindex $first_cmd 9]]
+        assert_equal [lindex $subcmds_in_command 0] {get -2 module 1 1 1 {} {} {{flags read begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}}}
+        assert_equal [lindex $subcmds_in_command 1] {set -2 module 1 1 1 {} {} {{flags write begin_search {type index spec {index 1}} find_keys {type range spec {lastkey 0 keystep 1 limit 0}}}}}
+
+        # Verify that module subcommands are displayed correctly in COMMAND DETAILS
+        set details_reply [r command details subcommands.bitarray]
+        set details [dict create {*}[lindex $details_reply 1]]
+        set subcmds_in_cmd_detail [dict create {*}[dict get $details subcommands]]
+        assert_equal [dict get $subcmds_in_cmd_detail "get"] {summary {} since {} group module}
+        assert_equal [dict get $subcmds_in_cmd_detail "set"] {summary {} since {} group module}
     }
 
     test "Module pure-container command fails on arity error" {
