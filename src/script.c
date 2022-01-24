15241ee1648f0c1d471d99dfcf822a0113f54df5
Yoav Steinberg <yoav@monfort.co.il>
2022-01-24 09:52:49 +0200

    CR cleanup.

--- src/script.c
+++ src/script.c
@@ -150,12 +150,12 @@ int scriptPrepareForRun(scriptRunCtx *run_ctx, client *engine_client, client *ca
             if (deny_write_type != DISK_ERROR_TYPE_NONE && server.masterhost == NULL) {
                 if (deny_write_type == DISK_ERROR_TYPE_RDB)
                     addReplyError(caller, "-MISCONF Redis is configured to save RDB snapshots, "
-                                     "but it is currently not able to persist on disk. "
-                                     "So its impossible to run scripts that have 'write' flag on.");
+                                     "but it's currently unable to persist to disk. "
+                                     "Writable scripts are blocked. Use 'no-writes' flag for read only scripts.");
                 else
                     addReplyErrorFormat(caller, "-MISCONF Redis is configured to persist data to AOF, "
-                                           "but it is currently not able to persist on disk. "
-                                           "So its impossible to run scripts that have 'write' flag on. "
+                                           "but it's currently unable to persist to disk. "
+                                           "Writable scripts are blocked. Use 'no-writes' flag for read only scripts. "
                                            "AOF error: %s", strerror(server.aof_last_write_errno));
                 return C_ERR;
             }
