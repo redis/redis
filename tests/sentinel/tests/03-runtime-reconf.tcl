f98cc2edb01ac18a1d244ec6c8c7523298aed776
Yossi Gottlieb <yossigo@gmail.com>
2022-02-01 22:16:51 +0200

    One fix and some cleanups.
    If SENTINEL SET fails input validation *and* fails to flush other
    changes, we need to choose as we can't produce two errors. For now, keep
    reporting the user error - but ultimately this should probably be
    atomic.
    
    Other minor changes:
    * Shorten error message
    * Use a helper function to avoid repeating the check/error reply.

--- tests/sentinel/tests/03-runtime-reconf.tcl
+++ tests/sentinel/tests/03-runtime-reconf.tcl
@@ -55,7 +55,6 @@ test "Sentinel Set with other error situations" {
 
    catch {[S 0 SENTINEL SET mymaster quorum $update_quorum]} err
    exec chmod 644 $configfilename
-   assert_equal "ERR Failed to save Sentinel new configuration on disk" $err
-   
+   assert_equal "ERR failed to save config file" $err
 }
 
