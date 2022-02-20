6697c41598a90d2fd234e4049aff92077a91c6f5
Ran Shidlansik <ranshid@amazon.com>
2022-02-20 11:05:20 +0200

    make test replybufsize::verify reply buffer limits to run if --accurate

--- tests/unit/replybufsize.tcl
+++ tests/unit/replybufsize.tcl
@@ -12,29 +12,30 @@ proc get_reply_buffer_size {cname} {
 start_server {tags {"replybufsize"}} {
     
     test {verify reply buffer limits} {
-        
-        # Create a simple idle test client
-        variable tc [redis_client]
-        $tc client setname test_client
-        
-        # make sure the client is idle for 5 seconds to make it shrink the reply buffer
-        wait_for_condition 5 1000 {
-            [get_reply_buffer_size test_client] >= 1024 && [get_reply_buffer_size test_client] < 2046
-        } else {
-            set rbs [get_reply_buffer_size test_client]
-            fail "reply buffer of idle client is $rbs after 5 seconds"
-        }
-        
-        r set bigval [string repeat x 32768]
-        
-        wait_for_condition 50 100 {
-            [$tc get bigval ; get_reply_buffer_size test_client] >= 16384 && [get_reply_buffer_size test_client] < 32768
-        } else {
-            set rbs [get_reply_buffer_size test_client]
-            fail "reply buffer of busy client is $rbs after 5 seconds"
-        }
-        
-        $tc close
+        if {$::accurate} {
+            # Create a simple idle test client
+            variable tc [redis_client]
+            $tc client setname test_client
+            
+            # make sure the client is idle for 5 seconds to make it shrink the reply buffer
+            wait_for_condition 5 1000 {
+                [get_reply_buffer_size test_client] >= 1024 && [get_reply_buffer_size test_client] < 2046
+            } else {
+                set rbs [get_reply_buffer_size test_client]
+                fail "reply buffer of idle client is $rbs after 5 seconds"
+            }
+            
+            r set bigval [string repeat x 32768]
+            
+            wait_for_condition 50 100 {
+                [$tc get bigval ; get_reply_buffer_size test_client] >= 16384 && [get_reply_buffer_size test_client] < 32768
+            } else {
+                set rbs [get_reply_buffer_size test_client]
+                fail "reply buffer of busy client is $rbs after 5 seconds"
+            }
+            
+            $tc close
+        } 
     }
 }
     
