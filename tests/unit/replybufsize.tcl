0fd1407be77c7e2763f2616bbfc2c72d8fe6e3b3
Ran Shidlansik <ranshid@amazon.com>
2022-02-20 18:25:33 +0200

    remove accurate from test replybufrsize and mark as needs::debug

--- tests/unit/replybufsize.tcl
+++ tests/unit/replybufsize.tcl
@@ -11,36 +11,34 @@ proc get_reply_buffer_size {cname} {
 start_server {tags {"replybufsize"}} {
     
     test {verify reply buffer limits} {
-        if {$::accurate} {
-            # In order to reduce test time we can set the peak reset time very low
-            r debug replybuffer-peak-reset-time 0
-            
-            # Create a simple idle test client
-            variable tc [redis_client]
-            $tc client setname test_client
-            
-            # make sure the client is idle for 5 seconds to make it shrink the reply buffer
-            wait_for_condition 10 100 {
-                [get_reply_buffer_size test_client] >= 1024 && [get_reply_buffer_size test_client] < 2046
-            } else {
-                set rbs [get_reply_buffer_size test_client]
-                fail "reply buffer of idle client is $rbs after 1 seconds"
-            }
-            
-            r set bigval [string repeat x 32768]
-            
-            # In order to reduce test time we can set the peak reset time very low
-            r debug replybuffer-peak-reset-time never
-            
-            wait_for_condition 10 100 {
-                [$tc get bigval ; get_reply_buffer_size test_client] >= 16384 && [get_reply_buffer_size test_client] < 32768
-            } else {
-                set rbs [get_reply_buffer_size test_client]
-                fail "reply buffer of busy client is $rbs after 1 seconds"
-            }
-            
-            $tc close
-        } 
-    }
+        # In order to reduce test time we can set the peak reset time very low
+        r debug replybuffer-peak-reset-time 0
+        
+        # Create a simple idle test client
+        variable tc [redis_client]
+        $tc client setname test_client
+        
+        # make sure the client is idle for 5 seconds to make it shrink the reply buffer
+        wait_for_condition 10 100 {
+            [get_reply_buffer_size test_client] >= 1024 && [get_reply_buffer_size test_client] < 2046
+        } else {
+            set rbs [get_reply_buffer_size test_client]
+            fail "reply buffer of idle client is $rbs after 1 seconds"
+        }
+        
+        r set bigval [string repeat x 32768]
+        
+        # In order to reduce test time we can set the peak reset time very low
+        r debug replybuffer-peak-reset-time never
+        
+        wait_for_condition 10 100 {
+            [$tc get bigval ; get_reply_buffer_size test_client] >= 16384 && [get_reply_buffer_size test_client] < 32768
+        } else {
+            set rbs [get_reply_buffer_size test_client]
+            fail "reply buffer of busy client is $rbs after 1 seconds"
+        }
+        
+        $tc close
+    } {} {needs:debug}
 }
     
