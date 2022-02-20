55a3cb31c5116c968b240ac91d32f58351ed5353
Ran Shidlansik <ranshid@amazon.com>
2022-02-20 13:47:03 +0200

    adjust test replybuffer to complete faster

--- tests/unit/replybufsize.tcl
+++ tests/unit/replybufsize.tcl
@@ -5,7 +5,6 @@ proc get_reply_buffer_size {cname} {
     if {![regexp rbs=(\[a-zA-Z0-9-\]+) $c - rbufsize]} {
         error "field rbus not found in $c"
     }
-    puts $c
     return $rbufsize
 }
 
@@ -13,25 +12,31 @@ start_server {tags {"replybufsize"}} {
     
     test {verify reply buffer limits} {
         if {$::accurate} {
+            # In order to reduce test time we can set the peak reset time very low
+            r debug replybuffer-peak-reset-time 0
+            
             # Create a simple idle test client
             variable tc [redis_client]
             $tc client setname test_client
             
             # make sure the client is idle for 5 seconds to make it shrink the reply buffer
-            wait_for_condition 6 1000 {
+            wait_for_condition 10 100 {
                 [get_reply_buffer_size test_client] >= 1024 && [get_reply_buffer_size test_client] < 2046
             } else {
                 set rbs [get_reply_buffer_size test_client]
-                fail "reply buffer of idle client is $rbs after 5 seconds"
+                fail "reply buffer of idle client is $rbs after 1 seconds"
             }
             
             r set bigval [string repeat x 32768]
             
-            wait_for_condition 50 100 {
+            # In order to reduce test time we can set the peak reset time very low
+            r debug replybuffer-peak-reset-time never
+            
+            wait_for_condition 10 100 {
                 [$tc get bigval ; get_reply_buffer_size test_client] >= 16384 && [get_reply_buffer_size test_client] < 32768
             } else {
                 set rbs [get_reply_buffer_size test_client]
-                fail "reply buffer of busy client is $rbs after 5 seconds"
+                fail "reply buffer of busy client is $rbs after 1 seconds"
             }
             
             $tc close
