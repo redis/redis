a1141fc01144327f43f4032a91226cd479a88747
Ran Shidlansik <ranshid@amazon.com>
2022-02-20 09:57:26 +0200

    fix comment

--- src/server.h
+++ src/server.h
@@ -1584,7 +1584,7 @@ struct redisServer {
         long long samples[STATS_METRIC_SAMPLES];
         int idx;
     } inst_metric[STATS_METRIC_COUNT];
-    long long stat_reply_buffer_shrinks; /* Total number of buffer output buffer shrinks */
+    long long stat_reply_buffer_shrinks; /* Total number of output buffer shrinks */
     long long stat_reply_buffer_expends; /* Total number of output buffer expends */
 
     /* Configuration */
