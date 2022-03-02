58143f8b9788eb452389d8c73f042ed6d4d408ca
Yoav Steinberg <yoav@monfort.co.il>
2022-03-02 09:35:34 +0200

    Make defrag logging visible in notice level.

--- src/defrag.c
+++ src/defrag.c
@@ -1166,7 +1166,7 @@ void activeDefragCycle(void) {
                 long long now = ustime();
                 size_t frag_bytes;
                 float frag_pct = getAllocatorFragmentation(&frag_bytes);
-                serverLog(LL_VERBOSE,
+                serverLog(LL_NOTICE,
                     "Active defrag done in %dms, reallocated=%d, frag=%.0f%%, frag_bytes=%zu",
                     (int)((now - start_scan)/1000), (int)(server.stat_active_defrag_hits - start_stat), frag_pct, frag_bytes);
 
