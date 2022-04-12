hanzoai/kv

fix new cluster tests issues (#9657)
Following #9483 the daily CI exposed a few problems.

* The cluster creation code (uses redis-cli) is complicated to test with TLS enabled.
  for now i'm just skipping them since the tests we run there don't really need that kind of coverage
* cluster port binding failures
  note that `find_available_port` already looks for a free cluster port
  but the code in `wait_server_started` couldn't detect the failure of binding
  (the text it greps for wasn't found in the log)

src/cluster.c
@@ -584,6 +584,8 @@ void clusterInit(void) {
     }
     int cport = server.cluster_port ? server.cluster_port : port + CLUSTER_PORT_INCR;
     if (listenToPort(cport, &server.cfd) == C_ERR ) {
+        /* Note: the following log text is matched by the test suite. */
+        serverLog(LL_WARNING, "Failed listening on port %u (cluster), aborting.", cport);
         exit(1);
     }
     

redis/redis

(cherry picked from commit 7d6744c739b25ae27a42f6fcf235e0d6bfbb3e61)

src/cluster.c
@@ -536,6 +536,8 @@ void clusterInit(void) {
         exit(1);
     }
     if (listenToPort(port+CLUSTER_PORT_INCR, &server.cfd) == C_ERR) {
+        /* Note: the following log text is matched by the test suite. */
+        serverLog(LL_WARNING, "Failed listening on port %u (cluster), aborting.", port);
         exit(1);
     }
     if (createSocketAcceptHandler(&server.cfd, clusterAcceptHandler) != C_OK) {