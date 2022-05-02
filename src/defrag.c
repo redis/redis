3e1100145c7e51dc0f9bd12fd13add8a68ab9eab
Yoav Steinberg <yoav@redislabs.com>
2022-05-02 15:06:59 +0300

    cleanup

--- src/defrag.c
+++ src/defrag.c
@@ -1170,7 +1170,7 @@ void activeDefragCycle(void) {
                 long long now = ustime();
                 size_t frag_bytes;
                 float frag_pct = getAllocatorFragmentation(&frag_bytes);
-                serverLog(LL_NOTICE,
+                serverLog(LL_VERBOSE,
                     "Active defrag done in %dms, reallocated=%d, frag=%.3f%%, frag_bytes=%zu",
                     (int)((now - start_scan)/1000), (int)(server.stat_active_defrag_hits - start_stat), frag_pct, frag_bytes);
 
