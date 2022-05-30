8495cdf9f061e14c3ddf704b1f5cbab46d6ca08f
tian <skylypig@gmail.com>
2022-05-30 05:26:30 +0000

    format the code

--- src/util.c
+++ src/util.c
@@ -953,7 +953,7 @@ int fsyncFileDir(const char *filename) {
     dir_fd = open(dname, O_RDONLY);
     if (dir_fd == -1) {
         /* Some OSs don't allow us to open directories at all (Windows returns
-	     * EACCES), just ignore the error in that case */
+         * EACCES), just ignore the error in that case */
         if (errno == EISDIR || errno == EACCES) {
 		    return 0;
         }
@@ -962,7 +962,7 @@ int fsyncFileDir(const char *filename) {
     /* Some OSs don't allow us to fsync directories at all, so we can ignore
 	 * those errors. */
     if (redis_fsync(dir_fd) == -1 && !(errno == EBADF || errno == EINVAL)) {
-        int	save_errno = errno;
+        int save_errno = errno;
         close(dir_fd);
         errno = save_errno;
         return -1;
