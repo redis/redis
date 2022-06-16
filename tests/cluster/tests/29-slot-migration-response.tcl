daf78078ee47be095530322938cde0a906968f44
Ubuntu <lucas.guang.yang1@huawei.com>
2022-06-16 20:31:24 +0000

    update codes for cluster UNSTABLE status

--- tests/cluster/tests/29-slot-migration-response.tcl
+++ tests/cluster/tests/29-slot-migration-response.tcl
@@ -3,10 +3,8 @@
 source "../tests/includes/init-tests.tcl"
 source "../tests/includes/utils.tcl"
 
-# TODO: This test currently tests the ASK and MOVED replies, need to add a test to check for the UNSTABLE reply
-
-test "Create a 10 nodes cluster" {
-    create_cluster 10 0
+test "Create a 2 nodes cluster" {
+    create_cluster 2 0
     config_set_all_nodes cluster-allow-replica-migration no
 }
 
@@ -21,23 +19,22 @@ catch {unset nodeto}
 $cluster refresh_nodes_map
 
 test "Set many keys" {
-    for {set i 0} {$i < 40000} {incr i} {
+    for {set i 0} {$i < 30000} {incr i} {
         $cluster set $i $i
     }
 }
 
 test "Keys are accessible" {
-    for {set i 0} {$i < 40000} {incr i} {
+    for {set i 0} {$i < 30000} {incr i} {
         assert { [$cluster get $i] eq $i }
     }
 }
 
 test "Migration of slot x" {
 
-    set slot 10
+    set slot 200
     array set nodefrom [$cluster masternode_for_slot $slot]
     array set nodeto [$cluster masternode_notfor_slot $slot]
-
 
     $nodeto(link) cluster setslot $slot importing $nodefrom(id)
     $nodefrom(link) cluster setslot $slot migrating $nodeto(id)
@@ -57,10 +54,9 @@ test "Migration of slot x" {
         fail "Should contain ASK when migrating"
     }
 
-    # TODO UNSTABLE REPLY
-    # catch {[$nodefrom(link) mset "a{$key}" "newVal" "abc{$key}" "newVal2"]} f
-    # puts $f
-    # if {![string match "*UNSTABLE*" $f]} {
-    #     fail "Should contain ASK when migrating"
-    # }
+    # UNSTABLE REPLY
+    catch {[$nodefrom(link) mset "a{$key}" "newVal" $key "newVal2"]} f
+    if {![string match "*UNSTABLE*" $f]} {
+        fail "Should contain UNSTABLE when migrating"
+    }
 }
