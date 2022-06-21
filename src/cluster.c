091701f363da953695b52cd7b4d7668d47f0258e
Bar Shaul <88437685+barshaul@users.noreply.github.com>
2022-06-21 07:40:48 +0300

    Set replicas' configEpoch to 0 when loaded from cluster configuration file (#10798)
    * Changed clusterLoadConfig to set the config epoch of replica nodes to 0 when loaded.

--- src/cluster.c
+++ src/cluster.c
@@ -308,8 +308,10 @@ int clusterLoadConfig(char *filename) {
         if (atoi(argv[4])) n->ping_sent = mstime();
         if (atoi(argv[5])) n->pong_received = mstime();
 
-        /* Set configEpoch for this node. */
-        n->configEpoch = strtoull(argv[6],NULL,10);
+        /* Set configEpoch for this node.
+         * If the node is a replica, set its config epoch to 0.
+         * If it's a primary, load the config epoch from the configuration file. */
+        n->configEpoch = (nodeIsSlave(n) && n->slaveof) ? 0 : strtoull(argv[6],NULL,10);
 
         /* Populate hash slots served by this instance. */
         for (j = 8; j < argc; j++) {
