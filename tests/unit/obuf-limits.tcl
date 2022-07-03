26d0f12b29a701ea3946a5cd7caaac03d1191666
Yossi Gottlieb <yossigo@gmail.com>
2022-07-03 13:34:14 +0300

    Fix TLS tests on newer tcl-tls/OpenSSL. (#10910)
    Before this commit, TLS tests on Ubuntu 22.04 would fail as dropped
    connections result with an ECONNABORTED error thrown instead of an empty
    read.
    
    (cherry picked from commit 69d5576832a02af43f2b1bdc7e94abfc92a27907)

--- tests/unit/obuf-limits.tcl
+++ tests/unit/obuf-limits.tcl
@@ -96,7 +96,7 @@ start_server {tags {"obuf-limits"}} {
 
         # Read nothing
         set fd [$rd channel]
-        assert_equal {} [read $fd]
+        assert_equal {} [$rd rawread]
     }
 
     # Note: This test assumes that what's written with one write, will be read by redis in one read.
@@ -136,8 +136,7 @@ start_server {tags {"obuf-limits"}} {
         assert_equal "PONG" [r ping]
         set clients [r client list]
         assert_no_match "*name=multicommands*" $clients
-        set fd [$rd2 channel]
-        assert_equal {} [read $fd]
+        assert_equal {} [$rd2 rawread]
     }
 
     test {Execute transactions completely even if client output buffer limit is enforced} {
