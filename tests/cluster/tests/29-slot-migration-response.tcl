d3256dd6c9de51665fb423a4aaa9406e17243107
Madelyn Olson <madelyneolson@gmail.com>
2022-07-04 10:07:10 -0500

    Review comments

--- tests/cluster/tests/29-slot-migration-response.tcl
+++ tests/cluster/tests/29-slot-migration-response.tcl
@@ -18,19 +18,14 @@ catch {unset nodeto}
 
 $cluster refresh_nodes_map
 
-test "Set many keys" {
+test "Set many keys in the cluster" {
     for {set i 0} {$i < 5000} {incr i} {
         $cluster set $i $i
-    }
-}
-
-test "Keys are accessible" {
-    for {set i 0} {$i < 5000} {incr i} {
         assert { [$cluster get $i] eq $i }
     }
 }
 
-test "Migration of slot x" {
+test "Test cluster responses during migration of slot x" {
 
     set slot 10
     array set nodefrom [$cluster masternode_for_slot $slot]
@@ -43,20 +38,13 @@ test "Migration of slot x" {
     set key [$nodefrom(link) cluster GETKEYSINSLOT $slot "1"]
 
     # MOVED REPLY
-    catch {[$nodeto(link) set $key "newVal"]} f
-    if {![string match "*MOVED*" $f]} {
-        fail "Should contain MOVED when migrating"
-    }
+    assert_error "*MOVED*" {$nodeto(link) set $key "newVal"}
 
     # ASK REPLY
-    catch {[$nodefrom(link) set "abc{$key}" "newVal"]} g
-    if {![string match "*ASK*" $g]} {
-        fail "Should contain ASK when migrating"
-    }
+    assert_error "*ASK*" {$nodefrom(link) set "abc{$key}" "newVal"}
 
     # UNSTABLE REPLY
-    catch {[$nodefrom(link) mset "a{$key}" "newVal" $key "newVal2"]} f
-    if {![string match "*TRYAGAIN*" $f]} {
-        fail "It is in UNSTABLE status, client should see TRYAGAIN  error message when migrating"
-    }
+    assert_error "*TRYAGAIN*" {$nodefrom(link) mset "a{$key}" "newVal" $key "newVal2"}
 }
+
+config_set_all_nodes cluster-allow-replica-migration no
