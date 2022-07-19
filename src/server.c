925decdfc33c2e720ce3cdf7616c2baa9b665607
filipecosta90 <filipecosta.90@gmail.com>
2022-07-19 11:15:11 +0100

    Removed shared.crlf from sharedObjects

--- src/server.c
+++ src/server.c
@@ -1680,7 +1680,6 @@ void createSharedObjects(void) {
     int j;
 
     /* Shared command responses */
-    shared.crlf = createObject(OBJ_STRING,sdsnew("\r\n"));
     shared.ok = createObject(OBJ_STRING,sdsnew("+OK\r\n"));
     shared.emptybulk = createObject(OBJ_STRING,sdsnew("$0\r\n\r\n"));
     shared.czero = createObject(OBJ_STRING,sdsnew(":0\r\n"));
