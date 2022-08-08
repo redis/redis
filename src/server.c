bbf2927c5787fe407d23eaf350b447fb970e512b
Oran Agra <oran@redislabs.com>
2022-08-08 08:21:40 +0300

    exit if setlocale fails on startup, and revert unneeded change

--- src/server.c
+++ src/server.c
@@ -2439,16 +2439,14 @@ void initServer(void) {
     server.cluster_drop_packet_filter = -1;
     server.reply_buffer_peak_reset_time = REPLY_BUFFER_DEFAULT_PEAK_RESET_TIME;
     server.reply_buffer_resizing_enabled = 1;
-
-    /* Make sure the locale is set on startup based on the config file.*/
-    if (setlocale(LC_COLLATE,server.locale) == NULL) {
-        serverLog(LL_WARNING, "Failed to configure LOCALE. Applying default empty string.");
-        server.locale = "";
-        setlocale(LC_COLLATE, server.locale);
-    }
-    
     resetReplicationBuffer();
-    
+
+    /* Make sure the locale is set on startup based on the config file. */
+    if (setlocale(LC_COLLATE,server.locale) == NULL) {
+        serverLog(LL_WARNING, "Failed to configure LOCALE. %s", strerror(errno));
+        exit(1);
+    }
+
     if ((server.tls_port || server.tls_replication || server.tls_cluster)
                 && tlsConfigure(&server.tls_ctx_config) == C_ERR) {
         serverLog(LL_WARNING, "Failed to configure TLS. Check logs for more info.");
