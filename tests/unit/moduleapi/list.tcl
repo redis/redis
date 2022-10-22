9fc20f4f9db1f79ce6ea8d09b82f974d7e3bc1fa
sundb <sundbcn@gmail.com>
2022-10-23 01:36:50 +0800

    Fix crash due to to reuse iterator entry after list deletion in module (#11383)
    In the module, we will reuse the list iterator entry for RM_ListDelete, but `listTypeDelete` will only update
    `quicklistEntry->zi` but not `quicklistEntry->node`, which will result in `quicklistEntry->node` pointing to
    a freed memory address if the quicklist node is deleted.
    
    This PR sync `key->u.list.index` and `key->u.list.entry` to list iterator after `RM_ListDelete`.
    
    This PR also optimizes the release code of the original list iterator.
    
    Co-authored-by: Viktor Söderqvist <viktor@zuiderkwast.se>
    (cherry picked from commit 6dd213558b0f77e2233743718e23556fb06a557e)

--- tests/unit/moduleapi/list.tcl
+++ tests/unit/moduleapi/list.tcl
@@ -1,5 +1,17 @@
 set testmodule [file normalize tests/modules/list.so]
 
+# The following arguments can be passed to args:
+#   i -- the number of inserts
+#   d -- the number of deletes
+#   r -- the number of replaces
+#   index -- the last index
+#   entry -- The entry pointed to by index
+proc verify_list_edit_reply {reply argv} {
+    foreach {k v} $argv {
+        assert_equal [dict get $reply $k] $v
+    }
+}
+
 start_server {tags {"modules"}} {
     r module load $testmodule
 
@@ -39,30 +51,72 @@ start_server {tags {"modules"}} {
     test {Module list insert & delete} {
         r del k
         r rpush k x y z
-        r list.edit k ikikdi foo bar baz
+        verify_list_edit_reply [r list.edit k ikikdi foo bar baz] {i 3 index 5}
         r list.getall k
     } {foo x bar y baz}
 
     test {Module list insert & delete, neg index} {
         r del k
         r rpush k x y z
-        r list.edit k REVERSE ikikdi foo bar baz
+        verify_list_edit_reply [r list.edit k REVERSE ikikdi foo bar baz] {i 3 index -6}
         r list.getall k
     } {baz y bar z foo}
 
     test {Module list set while iterating} {
         r del k
         r rpush k x y z
-        r list.edit k rkr foo bar
+        verify_list_edit_reply [r list.edit k rkr foo bar] {r 2 index 3}
         r list.getall k
     } {foo y bar}
 
     test {Module list set while iterating, neg index} {
         r del k
         r rpush k x y z
-        r list.edit k reverse rkr foo bar
+        verify_list_edit_reply [r list.edit k reverse rkr foo bar] {r 2 index -4}
         r list.getall k
     } {bar y foo}
+
+    test {Module list - list entry and index should be updated when deletion} {
+        set original_config [config_get_set list-max-listpack-size 1]
+
+        # delete from start (index 0)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l dd] {d 2 index 0 entry z}
+        assert_equal [r list.getall l] {z}
+
+        # delete from start (index -3)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l reverse kkd] {d 1 index -3}
+        assert_equal [r list.getall l] {y z}
+
+        # # delete from tail (index 2)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l kkd] {d 1 index 2}
+        assert_equal [r list.getall l] {x y}
+
+        # # delete from tail (index -1)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l reverse dd] {d 2 index -1 entry x}
+        assert_equal [r list.getall l] {x}
+
+        # # delete from middle (index 1)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l kdd] {d 2 index 1}
+        assert_equal [r list.getall l] {x}
+
+        # # delete from middle (index -2)
+        r del l
+        r rpush l x y z
+        verify_list_edit_reply [r list.edit l reverse kdd] {d 2 index -2}
+        assert_equal [r list.getall l] {z}
+
+        config_set list-max-listpack-size $original_config
+    }
 
     test "Unload the module - list" {
         assert_equal {OK} [r module unload list]
