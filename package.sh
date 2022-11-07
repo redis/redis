8802edf10cd6c2a9d82a0d5eeb54851a7a975629
sr_zhao <sr_zhao@trip.com>
2022-11-07 17:19:46 +0800

    add package.sh to build & package xredis tarball.

--- package.sh
+++ package.sh
@@ -1,17 +1,54 @@
-APP=xredis
-Version=2.0.1
-Package=$APP-$Version
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_DIR=${SCRIPT_DIR}
+SRC_DIR="${BUILD_DIR}/src"
+REPACK_DIR="${BUILD_DIR}/repack"
 
-find . -name ctrip.h -exec sed -i "s/XREDIS_VERSION \"\(.*\)\"/XREDIS_VERSION \"$Version\"/" {} \;
+XREDIS_NAME=xredis
+REDIS_VERSION=$(cat ${SCRIPT_DIR}/src/version.h| grep -w REDIS_VERSION | awk -F \" '{print $2}')
+XREDIS_VERSION=$(cat ${SCRIPT_DIR}/src/ctrip.h| grep -w XREDIS_VERSION | awk -F \" '{print $2}')
+PKG_COMPILE_COUNT="0"
+PKG_VERSION="$REDIS_VERSION-$XREDIS_VERSION-$PKG_COMPILE_COUNT"
 
-if [ -f $package ]; then
-    echo remove $Package
-    rm -rf $Package
+OSRELEASE=""
+OSBITS=`uname -p`
+
+ARCH=$(echo `uname -s` | awk '{print toupper($1)}')
+if [[ "$ARCH" == "LINUX" ]]; then
+    os_release_file=/etc/os-release
+    if [[ -s ${os_release_file} ]]; then
+        . ${os_release_file}
+        OSRELEASE="${ID}${VERSION_ID}"
+    else
+		echo "/etc/os-release file not found!"
+		exit 1
+    fi
+else
+    echo "The system $ARCH does not supported!"
+    exit 1
 fi
-echo make clean
-make distclean
-mkdir $Package
-ls -a | egrep -v "Debug|^\.|$APP" | xargs -J %  cp -r %  $Package
-echo create $Package.tar.gz...
-tar -czf $Package.tar.gz $Package
-rm -rf $Package
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+cecho() {
+    local text="$RED[== $1 ==]$NC" 
+    printf "$text\n"
+}
+
+# build & install
+XREDIS_TAR_NAME=${XREDIS_NAME}"-"${PKG_VERSION}"-"${OSRELEASE}"-"${OSBITS}
+XREDIS_TAR_BALL=${XREDIS_TAR_NAME}.tar.gz
+XREDIS_INSTALL_DIR=${REPACK_DIR}/${XREDIS_TAR_NAME}
+
+cecho "Build & install xredis to ${XREDIS_INSTALL_DIR}"
+
+cd ${BUILD_DIR} && make distclean
+cd ${BUILD_DIR} && make -j >/dev/null
+
+mkdir -p ${XREDIS_INSTALL_DIR}
+
+cp ${SRC_DIR}/{redis-server,redis-benchmark,redis-cli,redis-sentinel,redis-check-aof,redis-check-rdb,../redis-ctrip.conf} ${XREDIS_INSTALL_DIR}
+
+#  package
+cecho "Package ${XREDIS_TAR_BALL}"
+cd ${REPACK_DIR} && tar -czvf ${XREDIS_TAR_BALL} ${XREDIS_TAR_NAME}
+
