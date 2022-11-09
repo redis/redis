57ad908feed0dfa119ba19578f383725b1bbbbb1
Oran Agra <oran@redislabs.com>
2022-11-09 20:09:01 +0200

    hickup

--- src/redis-check-rdb.c
+++ src/redis-check-rdb.c
@@ -98,7 +98,7 @@ char *rdb_type_string[] = {
     "hash-listpack",
     "zset-listpack",
     "quicklist-v2",
-    "set-listpack"
+    "set-listpack",
 };
 
 /* Show a few stats collected into 'rdbstate' */
