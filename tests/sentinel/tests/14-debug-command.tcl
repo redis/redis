044975ff1402955843e82df179f807fa8655a2fc
hwware <wen.hui.ware@gmail.com>
2022-11-15 20:58:37 +0000

    update json file and add test case

---
+++ tests/sentinel/tests/14-debug-command.tcl
@@ -0,0 +1,9 @@
+source "../tests/includes/init-tests.tcl"
+
+test "sentinel debug test with arguments and without argument" {
+   set current_info_period [lindex [S 0 SENTINEL DEBUG] 1]
+   S 0 SENTINEL DEBUG info-period 8888
+   assert { [lindex [S 0 SENTINEL DEBUG] 1] == {8888} }
+   S 0 SENTINEL DEBUG info-period $current_info_period
+   assert { [lindex [S 0 SENTINEL DEBUG] 1] == $current_info_period }
+}
