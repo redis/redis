f357a8f8dfb24e86ecb78bea977ba17116531c4e
hwware <wen.hui.ware@gmail.com>
2022-11-16 19:02:48 +0000

    address Moti comment

--- tests/sentinel/tests/14-debug-command.tcl
+++ tests/sentinel/tests/14-debug-command.tcl
@@ -1,9 +1,9 @@
 source "../tests/includes/init-tests.tcl"
 
-test "sentinel debug test with arguments and without argument" {
+test "Sentinel debug test with arguments and without argument" {
    set current_info_period [lindex [S 0 SENTINEL DEBUG] 1]
    S 0 SENTINEL DEBUG info-period 8888
-   assert { [lindex [S 0 SENTINEL DEBUG] 1] == {8888} }
+   assert_equal [string first [S 0 SENTINEL DEBUG] 8888] -1
    S 0 SENTINEL DEBUG info-period $current_info_period
-   assert { [lindex [S 0 SENTINEL DEBUG] 1] == $current_info_period }
+   assert_equal [string first [S 0 SENTINEL DEBUG] $current_info_period] -1
 }
