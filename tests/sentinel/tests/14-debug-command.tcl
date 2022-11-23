38ca1920452d00be839856884ba3382573f03a0a
hwware <wen.hui.ware@gmail.com>
2022-11-23 21:36:54 +0000

    update to assert_equal in test case

--- tests/sentinel/tests/14-debug-command.tcl
+++ tests/sentinel/tests/14-debug-command.tcl
@@ -3,7 +3,7 @@ source "../tests/includes/init-tests.tcl"
 test "Sentinel debug test with arguments and without argument" {
    set current_info_period [lindex [S 0 SENTINEL DEBUG] 1]
    S 0 SENTINEL DEBUG info-period 8888
-   assert { [lindex [S 0 SENTINEL DEBUG] 1] == {8888} }
+   assert_equal {8888} [lindex [S 0 SENTINEL DEBUG] 1] 
    S 0 SENTINEL DEBUG info-period $current_info_period
-   assert { [lindex [S 0 SENTINEL DEBUG] 1] == $current_info_period }
+   assert_equal $current_info_period [lindex [S 0 SENTINEL DEBUG] 1]
 }
