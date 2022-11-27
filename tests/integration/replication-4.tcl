dcdd5764615d59643ef4c7ae2ee03efe53901717
Binbin <binloveplay1314@qq.com>
2022-11-27 15:11:04 +0800

    Fix replication on expired key test timing issue, give it more chances
    In replica, the key expired before master's `INCR` was arrived, so INCR
    creates a new key in the replica and the test failed.
    ```
    *** [err]: Replication of an expired key does not delete the expired key in tests/integration/replication-4.tcl
    Expected '0' to be equal to '1' (context: type eval line 13 cmd {assert_equal 0 [$slave exists k]} proc ::test)
    ```
    
    This test is very likely to do a false positive if the `wait_for_ofs_sync`
    takes longer than the expiration time, so give it a few more chances.
    
    The test was introduced in #9572.

--- tests/integration/replication-4.tcl
+++ tests/integration/replication-4.tcl
@@ -122,6 +122,11 @@ start_server {tags {"repl external:skip"}} {
         }
 
         test {Replication of an expired key does not delete the expired key} {
+            # This test is very likely to do a false positive if the wait_for_ofs_sync
+            # takes longer than the expiration time, so give it a few more chances.
+            for {set i 0} {$i < 10} {incr i} {
+
+            wait_for_ofs_sync $master $slave
             $master debug set-active-expire 0
             $master set k 1 ex 1
             wait_for_ofs_sync $master $slave
@@ -133,8 +138,15 @@ start_server {tags {"repl external:skip"}} {
             exec kill -SIGCONT [srv 0 pid]
             wait_for_ofs_sync $master $slave
             # Check that k is logically expired but is present in the replica.
-            assert_equal 0 [$slave exists k]
-            $slave debug object k ; # Raises exception if k is gone.
+            set res [$slave exists k]
+            set errcode [catch {$slave debug object k} err] ; # Raises exception if k is gone.
+            if {$res == 0 && $errcode == 0} { break }
+
+            } ;# for
+
+            if {$::verbose} { puts "Replication of an expired key does not delete the expired key test attempts: $i" }
+            assert_equal $res 0
+            assert_equal $errcode 0
         }
     }
 }
