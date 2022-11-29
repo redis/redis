41d23efe42447dde9a77898a1bf26539f669d06f
filipe oliveira <filipecosta.90@gmail.com>
2022-11-29 12:20:22 +0000

    Reduce eval related overhead introduced in v7.0 by evalCalcFunctionName (#11521)
    As being discussed in #10981 we see a degradation in performance
    between v6.2 and v7.0 of Redis on the EVAL command.
    
    After profiling the current unstable branch we can see that we call the
    expensive function evalCalcFunctionName twice.
    
    The current "fix" is to basically avoid calling evalCalcFunctionName and
    even dictFind(lua_scripts) twice for the same command.
    Instead we cache the current script's dictEntry (for both Eval and Functions)
    in the current client so we don't have to repeat these calls.
    The exception would be when doing an EVAL on a new script that's not yet
    in the script cache. in that case we will call evalCalcFunctionName (and even
    evalExtractShebangFlags) twice.
    
    Co-authored-by: Oran Agra <oran@redislabs.com>
    (cherry picked from commit 7dfd7b9197bbe216912049eebecbda3f1684925e)

--- src/eval.c
+++ src/eval.c
@@ -385,17 +385,17 @@ uint64_t evalGetCommandFlags(client *c, uint64_t cmd_flags) {
     int evalsha = c->cmd->proc == evalShaCommand || c->cmd->proc == evalShaRoCommand;
     if (evalsha && sdslen(c->argv[1]->ptr) != 40)
         return cmd_flags;
+    uint64_t script_flags;
     evalCalcFunctionName(evalsha, c->argv[1]->ptr, funcname);
     char *lua_cur_script = funcname + 2;
-    dictEntry *de = dictFind(lctx.lua_scripts, lua_cur_script);
-    uint64_t script_flags;
-    if (!de) {
+    c->cur_script = dictFind(lctx.lua_scripts, lua_cur_script);
+    if (!c->cur_script) {
         if (evalsha)
             return cmd_flags;
         if (evalExtractShebangFlags(c->argv[1]->ptr, &script_flags, NULL, NULL) == C_ERR)
             return cmd_flags;
     } else {
-        luaScript *l = dictGetVal(de);
+        luaScript *l = dictGetVal(c->cur_script);
         script_flags = l->flags;
     }
     if (script_flags & SCRIPT_FLAG_EVAL_COMPAT_MODE)
@@ -502,7 +502,12 @@ void evalGenericCommand(client *c, int evalsha) {
         return;
     }
 
-    evalCalcFunctionName(evalsha, c->argv[1]->ptr, funcname);
+    if (c->cur_script) {
+        funcname[0] = 'f', funcname[1] = '_';
+        memcpy(funcname+2, dictGetKey(c->cur_script), 40);
+        funcname[42] = '\0';
+    } else
+        evalCalcFunctionName(evalsha, c->argv[1]->ptr, funcname);
 
     /* Push the pcall error handler function on the stack. */
     lua_getglobal(lua, "__redis__err__handler");
@@ -531,7 +536,9 @@ void evalGenericCommand(client *c, int evalsha) {
     }
 
     char *lua_cur_script = funcname + 2;
-    dictEntry *de = dictFind(lctx.lua_scripts, lua_cur_script);
+    dictEntry *de = c->cur_script;
+    if (!de)
+        de = dictFind(lctx.lua_scripts, lua_cur_script);
     luaScript *l = dictGetVal(de);
     int ro = c->cmd->proc == evalRoCommand || c->cmd->proc == evalShaRoCommand;
 
