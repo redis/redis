3bf1d01ebd289050199fafbf51fe14814b68b861
filipecosta90 <filipecosta.90@gmail.com>
2022-11-29 22:32:58 +0000

    Using addReplySimpleString() within luaReplyToRedisReply

--- src/script_lua.c
+++ src/script_lua.c
@@ -653,7 +653,7 @@ static void luaReplyToRedisReply(client *c, client* script_client, lua_State *lu
         if (t == LUA_TSTRING) {
             sds ok = sdsnew(lua_tostring(lua,-1));
             sdsmapchars(ok,"\r\n","  ",2);
-            addReplyStatusLength(ok, strlen(ok));
+            addReplySimpleString(c, ok, strlen(ok));
             sdsfree(ok);
             lua_pop(lua,2);
             return;
