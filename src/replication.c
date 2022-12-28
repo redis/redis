bb2f73984bd016622ed1b7afd1ee5037ccd8f622
sundb <sundbcn@gmail.com>
2022-12-28 21:20:39 +0800

    Optimize comment

--- src/replication.c
+++ src/replication.c
@@ -593,8 +593,6 @@ void replicationFeedMonitors(client *c, list *monitors, int dictid, robj **argv,
     while((ln = listNext(&li))) {
         client *monitor = ln->value;
         addReply(monitor,cmdobj);
-        /* Note: If someday we wanna update the monitors' memory usage accurately
-         * that we need to re-add an `updateClientMemoryUsage()` call here. */
     }
     decrRefCount(cmdobj);
 }
