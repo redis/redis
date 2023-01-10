25dc3b07571469a5fb004aa817a3852ade2f840f
Krunoslav Husak <kruno@husak.me>
2023-01-10 15:06:24 +0100

    Fixes typo (double word) in memory overcommit message (#11675)
    Fixes small typo in memory overcommit message in syscheck.c (double word can).

--- src/syscheck.c
+++ src/syscheck.c
@@ -154,7 +154,7 @@ int checkOvercommit(sds *error_msg) {
         *error_msg = sdsnew(
             "Memory overcommit must be enabled! Without it, a background save or replication may fail under low memory condition. "
 #if defined(USE_JEMALLOC)
-            "Being disabled, it can can also cause failures without low memory condition, see https://github.com/jemalloc/jemalloc/issues/1328. "
+            "Being disabled, it can also cause failures without low memory condition, see https://github.com/jemalloc/jemalloc/issues/1328. "
 #endif
             "To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the "
             "command 'sysctl vm.overcommit_memory=1' for this to take effect.");
