4938369199a79d36f55098fb4867972d90ac843a
Oran Agra <oran@redislabs.com>
2023-01-12 08:29:20 +0200

    Obuf limit, exit during loop in *RAND* commands and KEYS
    Related to the hang reported in #11671
    Currently, redis can disconnect a client due to reaching output buffer limit,
    it'll also avoid feeding that output buffer with more data, but it will keep
    running the loop in the command (despite the client already being marked for
    disconnection)
    
    This PR is an attempt to mitigate the problem, specifically for commands that
    are easy to abuse, specifically: KEYS, HRANDFIELD, SRANDMEMBER, ZRANDMEMBER.
    The RAND family of commands can take a negative COUNT argument (which is not
    bound to the number of elements in the key), so it's enough to create a key
    with one field, and then these commands can be used to hang redis.
    For KEYS the caller can use the existing keyspace in redis (if big enough).

--- src/t_set.c
+++ src/t_set.c
@@ -701,6 +701,8 @@ void srandmemberWithCountCommand(client *c) {
             } else {
                 addReplyBulkCBuffer(c,ele,sdslen(ele));
             }
+            if (c->flags & CLIENT_CLOSE_ASAP)
+                break;
         }
         return;
     }
