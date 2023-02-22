d844c1d3298523870af2ca4517569f2e7d5c7559
Neniel <11655196+Neniel@users.noreply.github.com>
2023-02-21 23:37:48 -0300

    remove extra spaces

--- src/t_string.c
+++ src/t_string.c
@@ -643,18 +643,18 @@ void incrDecrCommand(client *c, long long incr, const char *event) {
 }
 
 void incrCommand(client *c) {
-    incrDecrCommand(c,1, "incr");
+    incrDecrCommand(c,1,"incr");
 }
 
 void decrCommand(client *c) {
-    incrDecrCommand(c,-1, "decr");
+    incrDecrCommand(c,-1,"decr");
 }
 
 void incrbyCommand(client *c) {
     long long incr;
 
     if (getLongLongFromObjectOrReply(c, c->argv[2], &incr, NULL) != C_OK) return;
-    incrDecrCommand(c,incr, "incrby");
+    incrDecrCommand(c,incr,"incrby");
 }
 
 void decrbyCommand(client *c) {
@@ -666,7 +666,7 @@ void decrbyCommand(client *c) {
         addReplyError(c, "decrement would overflow");
         return;
     }
-    incrDecrCommand(c,-incr, "decrby");
+    incrDecrCommand(c,-incr,"decrby");
 }
 
 void incrbyfloatCommand(client *c) {
