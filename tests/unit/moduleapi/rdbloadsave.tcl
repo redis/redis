75a4efddc556bbdc395037024fac95075f5e7e63
Ozan Tezcan <ozantezcan@gmail.com>
2023-03-07 15:39:15 +0300

    faster tests

--- tests/unit/moduleapi/rdbloadsave.tcl
+++ tests/unit/moduleapi/rdbloadsave.tcl
@@ -23,9 +23,9 @@ start_server {tags {"modules"}} {
         r config set key-load-delay 50
         r flushdb
 
-        populate 20000 a 256
+        populate 3000 a 1024
         r set x 111
-        assert_equal [r dbsize] 20001
+        assert_equal [r dbsize] 3001
 
         assert_equal OK [r test.rdbsave blabla.rdb]
         r flushdb
@@ -51,8 +51,9 @@ start_server {tags {"modules"}} {
 
         assert_equal OK [$rd1 read]
         assert_equal 111 [$rd1 read]
-        assert_equal 20001 [$rd1 read]
+        assert_equal 3001 [$rd1 read]
         r flushdb
+        r config set key-load-delay 0
     }
 
     test "Module rdbloadsave with aof" {
@@ -95,7 +96,7 @@ start_server {tags {"modules"}} {
         assert_equal OK [r test.rdbsave bgsave.rdb]
 
         r set k v2
-        r config set rdb-key-save-delay 10000000
+        r config set rdb-key-save-delay 500000
         r bgsave
 
         # RM_RdbLoad() should kill RDB fork
@@ -116,7 +117,7 @@ start_server {tags {"modules"}} {
     test "Module rdbloadsave calls rdbsave in a module fork" {
         r flushdb
         r config set save ""
-        r config set rdb-key-save-delay 3000000
+        r config set rdb-key-save-delay 500000
 
         r set k v1
 
