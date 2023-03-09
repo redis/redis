01543cc06f9916a5c0781ad25e8eb62992832923
Hanna Fadida <hanna.fadida@redislabs.com>
2023-03-09 13:42:15 +0200

    revert coverage test for LATENCY GRAPH

--- tests/unit/latency-monitor.tcl
+++ tests/unit/latency-monitor.tcl
@@ -145,10 +145,4 @@ start_server {tags {"latency-monitor needs:latency"}} {
         catch {r LATENCY help xxx} e
         assert_match "*wrong number of arguments for 'latency|help' command" $e
     }
-
-    test {Coverage: LATENCY GRAPH} {
-        r config resetstat
-        r debug sleep .2
-        assert_match {*command*} [r LATENCY GRAPH command]
-    } {} {needs:debug}
 }
