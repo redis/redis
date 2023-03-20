1b4f8117c04f917f384e0ed4b9ba94f7778cad5d
dong <810776886@qq.com>
2023-03-20 20:55:22 +0800

    fix swap/unit/list.tcl  case

--- tests/swap/unit/list.tcl
+++ tests/swap/unit/list.tcl
@@ -145,6 +145,7 @@ start_server {tags {"target"}} {
 start_server {tags {"repl"}} {
     r config set swap-debug-evict-keys 0
     r config set swap-debug-rio-delay-micro 100000
+    r config set swap-absent-cache-enabled no
     test " lpush + (swap start) + del + (swap end) + lpush " {
         r rpush target a 
         set rd1 [redis_deferring_client]
