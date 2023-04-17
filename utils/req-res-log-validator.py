e410a3547a1d5451101c7a729bd6da7374408f87
Wen Hui <wen.hui.ware@gmail.com>
2023-04-17 14:43:04 -0400

    Update req-res-log-validator.py
    Removing sentinel command filter.

--- utils/req-res-log-validator.py
+++ utils/req-res-log-validator.py
@@ -335,8 +335,9 @@ if __name__ == '__main__':
     for k, v in sorted(command_counter.items()):
         print(f"  {k}: {v}")
     # We don't care about SENTINEL commands
-    not_hit = set(filter(lambda x: not x.startswith("sentinel"),
-                  set(docs.keys()) - set(command_counter.keys()) - set(IGNORED_COMMANDS)))
+    #not_hit = set(filter(lambda x: not x.startswith("sentinel"),
+    #              set(docs.keys()) - set(command_counter.keys()) - set(IGNORED_COMMANDS)))
+    not_hit = set(filter(set(docs.keys()) - set(command_counter.keys()) - set(IGNORED_COMMANDS)))
     if not_hit:
         if args.verbose:
             print("WARNING! The following commands were not hit at all:")
