7d2044adebfc8701c1b94301b6524d00630d38d2
Binbin <binloveplay1314@qq.com>
2023-04-18 16:06:51 +0800

    handle sentinel / cluster disable save

--- tests/sentinel/run.tcl
+++ tests/sentinel/run.tcl
@@ -17,11 +17,13 @@ proc main {} {
         "sentinel deny-scripts-reconfig no"
         "enable-protected-configs yes"
         "enable-debug-command yes"
+        "save ''"
     } "../tests/includes/sentinel.conf"
 
     spawn_instance redis $::redis_base_port $::instances_count {
         "enable-protected-configs yes"
         "enable-debug-command yes"
+        "save ''"
     }
     run_tests
     cleanup
