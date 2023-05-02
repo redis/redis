51f558cb7a8be5c0925dbd4160448ca115c2bc95
Madelyn Olson <madelyneolson@gmail.com>
2023-05-01 21:17:36 -0700

    SMOVE isn't variadic, so it only has one item

--- src/t_set.c
+++ src/t_set.c
@@ -677,7 +677,7 @@ void smoveCommand(client *c) {
 
     /* Create the destination set when it doesn't exist */
     if (!dstset) {
-        dstset = setTypeCreate(ele->ptr, c->argc - 2);
+        dstset = setTypeCreate(ele->ptr, 1);
         dbAdd(c->db,c->argv[2],dstset);
     }
 
