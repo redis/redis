48757934ffb8e3d84b3e4457bf2ded5ea0a733a1
Binbin <binloveplay1314@qq.com>
2023-05-18 20:24:46 +0800

    Performance improvement to ZADD and ZRANGESTORE, convert to skiplist and expand dict in advance (#12185)
    For zsets that will eventually be stored as the skiplist encoding (has a dict),
    we can convert it to skiplist ahead of time. This change checks the number
    of arguments in the ZADD command, and converts the data-structure
    if the number of new entries exceeds the listpack-max-entries configuration.
    This can cause us to over-allocate memory if there are duplicate entries in the
    input, which is unexpected.
    
    For ZRANGESTORE, we know the size of the zset, so we can expand
    the dict in advance, to avoid the temporary dict from being rehashed
    while it grows.
    
    Simple benchmarks shows it provides some 4% improvement in ZADD and 20% in ZRANGESTORE

--- tests/unit/type/zset.tcl
+++ tests/unit/type/zset.tcl
@@ -2285,7 +2285,9 @@ start_server {tags {"zset"}} {
         r config set zset-max-listpack-entries 0
         r del z1{t} z2{t}
         r zadd z1{t} 1 a
+        assert_encoding skiplist z1{t}
         assert_equal 1 [r zrangestore z2{t} z1{t} 0 -1]
+        assert_encoding skiplist z2{t}
         r config set zset-max-listpack-entries $original_max
     }
 
@@ -2615,5 +2617,38 @@ start_server {tags {"zset"}} {
         r config set set-max-intset-entries 512
         r config set set-max-listpack-entries 128
         r config set zset-max-listpack-entries 128
+    }
+
+    foreach type {single multiple single_multiple} {
+        test "ZADD overflows the maximum allowed elements in a listpack - $type" {
+            r del myzset
+
+            set max_entries 64
+            set original_max [lindex [r config get zset-max-listpack-entries] 1]
+            r config set zset-max-listpack-entries $max_entries
+
+            if {$type == "single"} {
+                # All are single zadd commands.
+                for {set i 0} {$i < $max_entries} {incr i} { r zadd myzset $i $i }
+            } elseif {$type == "multiple"} {
+                # One zadd command to add all elements.
+                set args {}
+                for {set i 0} {$i < $max_entries * 2} {incr i} { lappend args $i }
+                r zadd myzset {*}$args
+            } elseif {$type == "single_multiple"} {
+                # First one zadd adds an element (creates a key) and then one zadd adds all elements.
+                r zadd myzset 1 1
+                set args {}
+                for {set i 0} {$i < $max_entries * 2} {incr i} { lappend args $i }
+                r zadd myzset {*}$args
+            }
+
+            assert_encoding listpack myzset
+            assert_equal $max_entries [r zcard myzset]
+            assert_equal 1 [r zadd myzset 1 b]
+            assert_encoding skiplist myzset
+
+            r config set zset-max-listpack-entries $original_max
+        }
     }
 }
