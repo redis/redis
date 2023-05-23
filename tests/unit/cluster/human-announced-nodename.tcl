b1d6436f414928c5334f8b60d289931fb6a85341
hwware <wen.hui.ware@gmail.com>
2023-05-23 21:49:28 +0000

    updating testcase as we are currently not displaing aux parameter in config node output

--- tests/unit/cluster/human-announced-nodename.tcl
+++ tests/unit/cluster/human-announced-nodename.tcl
@@ -1,4 +1,5 @@
 # Check if cluster's view of human announced nodename is consistent
+#Exclude aux fields from "cluster nodes" We may decide to re-introduce them in some form or another in the #future, but not in v7.2
 proc are_nodenames_propagated {match_string} {
     for {set j 0} {$j < [llength $::servers]} {incr j} {
         foreach n [get_cluster_nodes $j] {
@@ -12,76 +13,31 @@ proc are_nodenames_propagated {match_string} {
 
 # Start a cluster with 3 masters.
 start_cluster 3 0 {tags {external:skip cluster}} {
-test "Set cluster human announced nodename and verify they are propagated" {
+test "Set cluster human announced nodename and verify they are excluded from cluster node" {
     for {set j 0} {$j < [llength $::servers]} {incr j} {
         R $j config set cluster-announce-human-nodename "nodename-$j.com"
     }
     wait_for_condition 50 100 {
-        [are_nodenames_propagated "nodename-*.com"] eq 1
+        [are_nodenames_propagated "nodename-*.com"] eq 0
     } else {
-        fail "cluster human announced nodename were not propagated"
+        fail "cluster human announced nodename were propagated"
     }
 
     # Now that everything is propagated, assert everyone agrees
     wait_for_cluster_propagation
 }
 
-test "Update human announced nodename and make sure they are all eventually propagated" {
+test "Update human announced nodename and make sure they are excluded from cluster node" {
     for {set j 0} {$j < [llength $::servers]} {incr j} {
         R $j config set cluster-announce-human-nodename "nodename-updated-$j.com"
     }
     wait_for_condition 50 100 {
-        [are_nodenames_propagated "nodename-updated-*.com"] eq 1
+        [are_nodenames_propagated "nodename-updated-*.com"] eq 0
     } else {
-        fail "cluster human announced nodename were not propagated"
+        fail "cluster human announced nodename were propagated"
     }
 
     # Now that everything is propagated, assert everyone agrees
     wait_for_cluster_propagation
-}
-
-test "Remove human announced nodename and make sure they are all eventually propagated" {
-    for {set j 0} {$j < [llength $::servers]} {incr j} {
-        R $j config set cluster-announce-human-nodename ""
-    }
-
-    wait_for_condition 50 100 {
-        [are_nodenames_propagated ""] eq 1
-    } else {
-        fail "cluster human announced nodename were not propagated"
-    }
-
-    # Now that everything is propagated, assert everyone agrees
-    wait_for_cluster_propagation
-}
-
-test "Test restart will keep nodename information" {
-    # Set a new nodename, reboot and make sure it sticks
-    R 0 config set cluster-announce-human-nodename "restart-1.com"
-
-    # Store the nodename in the config
-    R 0 config rewrite
-
-    restart_server 0 true false
-    foreach n [get_cluster_nodes 0] {
-        if { [string match [R 0 CLUSTER MYID] [dict get $n id]] } {
-            set node $n
-        }
-    }
-    wait_for_condition 50 100 {
-        [dict get $node nodename] eq "restart-1.com"
-    } else {
-        fail "cluster human announced nodename were not propagated after restart"
-    }
-    # As a sanity check, make sure everyone eventually agrees
-    wait_for_cluster_propagation
-}
-
-test "Test human announced nodename validation" {
-    catch {R 0 config set cluster-announce-human-nodename "?.com"} err
-    assert_match "* may only contain alphanumeric characters, hyphens, dots or underscore*" $err
-
-    # Note this isn't a valid nodename, but it passes our internal validation
-    R 0 config set cluster-announce-human-nodename "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-."
 }
 }
