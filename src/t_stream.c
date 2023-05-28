0de44a2f47b9caa4dd0e95a948f1603cd00fa7e1
Oran Agra <oran@redislabs.com>
2023-05-28 08:15:36 +0300

    Update src/t_stream.c

--- src/t_stream.c
+++ src/t_stream.c
@@ -2208,7 +2208,7 @@ void xreadCommand(client *c) {
             streams_arg = i+1;
             streams_count = (c->argc-streams_arg);
             if ((streams_count % 2) != 0) {
-                char symbol = (strcasecmp(c->cmd->fullname,"XREAD") == 0) ? '$' :'>';
+                char symbol = xreadgroup ? '>' : '$';
                 addReplyErrorFormat(c,"Unbalanced '%s' list of streams: "
                                       "for each stream key an ID or '%c' must be "
                                       "specified.", c->cmd->fullname,symbol);
