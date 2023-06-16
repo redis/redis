a770bff17b329fd263e2c71fb37e5da961f48b93
judeng <abc3844@126.com>
2023-06-16 16:14:33 +0800

    use ll2string for sds

--- src/util.h
+++ src/util.h
@@ -67,7 +67,6 @@ uint32_t digits10(uint64_t v);
 uint32_t sdigits10(int64_t v);
 int ll2string(char *s, size_t len, long long value);
 int ull2string(char *s, size_t len, unsigned long long value);
-sds sdsfromlonglong(long long value);
 int string2ll(const char *s, size_t slen, long long *value);
 int string2ull(const char *s, unsigned long long *value);
 int string2l(const char *s, size_t slen, long *value);
