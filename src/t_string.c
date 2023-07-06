db8ba99f548a06d5003e0a94ad44151dd8a8cc7c
hwware <wen.hui.ware@gmail.com>
2023-07-06 14:34:39 +0000

    minor indentation

--- src/t_string.c
+++ src/t_string.c
@@ -738,7 +738,7 @@ void lcsCommand(client *c) {
     uint32_t i, j;
     long long minmatchlen = 0;
     sds a = NULL, b = NULL;
-    int getlen = 0, getidx = 0, withmatchlen = 0, minmatchlenflag=0;
+    int getlen = 0, getidx = 0, withmatchlen = 0, minmatchlenflag = 0;
     robj *obja = NULL, *objb = NULL;
 
     obja = lookupKeyRead(c->db,c->argv[1]);
@@ -773,7 +773,7 @@ void lcsCommand(client *c) {
             if (getLongLongFromObjectOrReply(c,c->argv[j+1],&minmatchlen,NULL)
                 != C_OK) goto cleanup;
             if (minmatchlen < 0) minmatchlen = 0;
-            minmatchlenflag=1;
+            minmatchlenflag = 1;
             j++;
         } else {
             addReplyErrorObject(c,shared.syntaxerr);
@@ -787,7 +787,7 @@ void lcsCommand(client *c) {
             "If you want both the length and indexes, please just use IDX.");
         goto cleanup;
     }
-    if((withmatchlen || minmatchlenflag) && !getidx){
+    if ((withmatchlen || minmatchlenflag) && !getidx) {
         addReplyError(c,
             "WITHMATCHLEN and MINMATCHLEN can only be used with IDX.");
         goto cleanup;
