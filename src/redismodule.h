002e3a1caaaac4c203d98125886f2a36a2503360
weedge <weege007@gmail.com>
2023-08-13 16:09:00 +0800

    reuse RedisModuleCronLoopV1 add activeRehashing hasActiveChildProcess
    Signed-off-by: weedge <weege007@gmail.com>

--- src/redismodule.h
+++ src/redismodule.h
@@ -779,17 +779,9 @@ typedef struct RedisModuleCronLoopInfo {
                                from the module to the core right now. Here
                                for future compatibility. */
     int32_t hz;             /* Approximate number of events per second. */
-} RedisModuleCronLoopV1;
-
-#define REDISMODULE_CRON_LOOP_VERSION_2 2
-typedef struct RedisModuleCronLoopInfoV2 {
-    uint64_t version; /* Not used since this structure is never passed
-                         from the module to the core right now. Here
-                         for future compatibility. */
-    int32_t hz;       /* Approximate number of events per second. */
     int activeRehashing;
     int hasActiveChildProcess;
-} RedisModuleCronLoopV2;
+} RedisModuleCronLoopV1;
 
 #define RedisModuleCronLoop RedisModuleCronLoopV1
 
