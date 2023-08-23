66e605805f54605c99dbbe4b103bb085f44a8ff9
Dolphin von Chips <td2505052@gmail.com>
2023-08-23 23:22:34 +0500

    Fix typo in function_lua.c
    luaEngineGetUsedMemoy -> luaEngineGetUsedMemory

--- src/function_lua.c
+++ src/function_lua.c
@@ -182,7 +182,7 @@ static void luaEngineCall(scriptRunCtx *run_ctx,
     lua_pop(lua, 1); /* Pop error handler */
 }
 
-static size_t luaEngineGetUsedMemoy(void *engine_ctx) {
+static size_t luaEngineGetUsedMemory(void *engine_ctx) {
     luaEngineCtx *lua_engine_ctx = engine_ctx;
     return luaMemory(lua_engine_ctx->lua);
 }
@@ -497,7 +497,7 @@ int luaEngineInitEngine(void) {
         .engine_ctx = lua_engine_ctx,
         .create = luaEngineCreate,
         .call = luaEngineCall,
-        .get_used_memory = luaEngineGetUsedMemoy,
+        .get_used_memory = luaEngineGetUsedMemory,
         .get_function_memory_overhead = luaEngineFunctionMemoryOverhead,
         .get_engine_memory_overhead = luaEngineMemoryOverhead,
         .free_function = luaEngineFreeFunction,
