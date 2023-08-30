964d87740652644624e8511a0369c7eb0cb7e6c0
alon <alonreshef24@gmail.com>
2023-08-30 12:42:21 +0300

    added test for reading from stdin

--- tests/integration/redis-benchmark.tcl
+++ tests/integration/redis-benchmark.tcl
@@ -1,5 +1,5 @@
 source tests/support/benchmark.tcl
-
+source tests/support/cli.tcl
 
 proc cmdstat {cmd} {
     return [cmdrstat $cmd r]
@@ -124,6 +124,14 @@ start_server {tags {"benchmark network external:skip logreqres:skip"}} {
             catch { exec {*}$cmd } error
             assert_match "*Error*" $error
             r config set maxclients $original_maxclients
+        }
+
+        test {benchmark: read last argument from stdin} {
+            set base_cmd [redisbenchmark $master_host $master_port "-x -n 10 set key"]
+            set cmd "sh -c \"printf \\\"arg\\\" | $base_cmd\""
+            common_bench_setup $cmd
+            set cli_cmd [rediscli $master_host $master_port {get key}]
+            assert_equal "arg" [exec {*}$cli_cmd]
         }
 
         # tls specific tests
