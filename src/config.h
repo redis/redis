c0b852b8ad0ddf2baea40af187e86b03801ccdaf
jachin <ygcaicn@gmail.com>
2023-09-28 17:21:15 +0800

    Fix MAC_OS_X_VERSION_10_7.

--- src/config.h
+++ src/config.h
@@ -294,7 +294,7 @@ void setproctitle(const char *fmt, ...);
 #include <kernel/OS.h>
 #define redis_set_thread_title(name) rename_thread(find_thread(0), name)
 #else
-#if (defined __APPLE__ && defined(MAC_OS_X_VERSION_10_7))
+#if (defined __APPLE__ && defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1070)
 int pthread_setname_np(const char *name);
 #include <pthread.h>
 #define redis_set_thread_title(name) pthread_setname_np(name)
