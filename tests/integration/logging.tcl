162645cae6d3a144757e98b22c7956881b404697
meiravgri <meirav.grimberg@redis.com>
2023-10-02 14:38:39 +0000

    some fixes to looging.tcl
    check_log_backtrace_for_debug(): added log_pattern
    
    added check_crash_log() to be called when backtrace is no supported
    
    watchdog test: make sure redis is still alive
    
    SIGABRT test: added a `debug sleep` call to look it the stacktrace printed in the log.
    
    added SIGALRM test

--- tests/integration/logging.tcl
+++ tests/integration/logging.tcl
@@ -19,16 +19,23 @@ if {$system_name eq {darwin}} {
     }
 }
 
-proc check_log_backtrace {} {
-    # If the stacktrace is printed more than once, it means we crashed.
+proc check_log_backtrace_for_debug {log_pattern} {
+    set res [wait_for_log_messages 0 \"$log_pattern\" 0 100 100]
+    if {$::verbose} { puts $res}          
+   
+    # If the stacktrace is printed more than once, it means redis crashed during crash report generation
     assert_equal [count_log_message 0 "STACK TRACE"] 1
 
-    set pattern "*redis-server *main*"
-    set res [wait_for_log_messages 0 \"$pattern\" 0 100 100]
-    if {$::verbose} { puts $res }
     set pattern "*debugCommand*"
     set res [wait_for_log_messages 0 \"$pattern\" 0 100 100]
     if {$::verbose} { puts $res}    
+    
+}
+
+# used when backtrace_supported == 0
+proc check_crash_log {log_pattern} {
+    set res [wait_for_log_messages 0 \"$log_pattern\" 0 50 100]
+    if {$::verbose} { puts $res }
 }
 
 if {$backtrace_supported} {
@@ -44,37 +51,31 @@ if {$backtrace_supported} {
                 assert_equal [count_log_message 0 "threads_mngr: waiting for threads' output timed out"] 0
                 assert_equal [count_log_message 0 "bioProcessBackgroundJobs"] 3
             }
-            
-            check_log_backtrace
+            check_log_backtrace_for_debug "*WATCHDOG TIMER EXPIRED*"
+            # make sure redis is still alive
+            assert_equal "PONG" [r ping]
         }
-    }
-
-    set server_path [tmpdir server3.log]
-    # Use exit() instead of abort() upon assertion so Valgrind tests won't fail.
-    start_server [list overrides [list dir $server_path use-exit-on-panic yes crash-memcheck-enabled no]] {
-        test "Generate stacktrace on assertion" {
-            catch {r debug assert}
-            check_log_backtrace
-        }
-            
     }
 }
 
 # Valgrind will complain that the process terminated by a signal, skip it.
 if {!$::valgrind} {
     if {$backtrace_supported} {
-        set crash_pattern "*STACK TRACE*"
-    } else {
-        set crash_pattern "*crashed by signal*"
+        set check_cb check_log_backtrace_for_debug
+    } else {  
+        set check_cb check_crash_log
     }
 
     set server_path [tmpdir server1.log]
     start_server [list overrides [list dir $server_path crash-memcheck-enabled no]] {
         test "Crash report generated on SIGABRT" {
             set pid [s process_id]
+            r deferred 1
+            r debug sleep 10
+            r flush
+            after 100 # wait for redis to get into the SLEEP
             exec kill -SIGABRT $pid
-            set res [wait_for_log_messages 0 \"$crash_pattern\" 0 50 100]
-            if {$::verbose} { puts $res }
+            $check_cb "*crashed by signal*"
         }
     }
 
@@ -82,13 +83,37 @@ if {!$::valgrind} {
     start_server [list overrides [list dir $server_path crash-memcheck-enabled no]] {
         test "Crash report generated on DEBUG SEGFAULT" {
             catch {r debug segfault}
-            if {$backtrace_supported} {
-                check_log_backtrace
-            } else {
-                set res [wait_for_log_messages 0 \"$crash_pattern\" 0 50 100]
-                if {$::verbose} { puts $res }
-            }
+            $check_cb "*crashed by signal*"
         }
+    }
+
+    set server_path [tmpdir server3.log]
+    start_server [list overrides [list dir $server_path]] {
+        test "Stacktraces generated on SIGALRM" {
+            set pid [s process_id]
+            r deferred 1
+            r debug sleep 10
+            r flush
+            after 100 # wait for redis to get into the SLEEP
+            exec kill -SIGALRM $pid
+            $check_cb "*Received SIGALRM*"
+            r read
+            r deferred 0
+            # make sure redis is still alive
+            assert_equal "PONG" [r ping]
+        }
+    }
+}
+
+if {$backtrace_supported} {
+    set server_path [tmpdir server4.log]
+    # Use exit() instead of abort() upon assertion so Valgrind tests won't fail.
+    start_server [list overrides [list dir $server_path use-exit-on-panic yes crash-memcheck-enabled no]] {
+        test "Generate stacktrace on assertion" {
+            catch {r debug assert}
+            check_log_backtrace_for_debug "*ASSERTION FAILED*"
+        }
+            
     }
 }
 
