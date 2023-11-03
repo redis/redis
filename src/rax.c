2e724c9c732e6b19d910b31221e88b4d7294c6ab
Absoler-in-seclab_zkb-csmith <1045607979@qq.com>
2023-11-03 12:29:06 +0800

    remove unnecessary memcpy

--- src/rax.c
+++ src/rax.c
@@ -933,10 +933,8 @@ void *raxFind(rax *rax, unsigned char *s, size_t len) {
  * memory without any bound checking). */
 raxNode **raxFindParentLink(raxNode *parent, raxNode *child) {
     raxNode **cp = raxNodeFirstChildPtr(parent);
-    raxNode *c;
     while(1) {
-        memcpy(&c,cp,sizeof(c));
-        if (c == child) break;
+        if (*cp == child) break;
         cp++;
     }
     return cp;
