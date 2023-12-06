c4bb6a8418bf2789e5f0e53e6e20cb3205f0d14c
hwware <wen.hui.ware@gmail.com>
2023-12-06 04:03:19 +0000

    update unhappy path INCR tests

--- tests/unit/type/incr.tcl
+++ tests/unit/type/incr.tcl
@@ -183,6 +183,16 @@ start_server {tags {"incr"}} {
         r get foo
     } {0}
 
+    test {INCRBY INCRBYFLOAT DECRBY against unhappy path} {
+        r del mykeyincr
+        r set mykeyincr 10
+        assert_error "*value is not an integer or out of range*" {r incrby mykeyincr v}
+        assert_error "*value is not an integer or out of range*" {r incrby mykeyincr 1.5}
+        assert_error "*value is not an integer or out of range*" {r decrby mykeyincr v}
+        assert_error "*value is not an integer or out of range*" {r decrby mykeyincr 1.5}
+        assert_error "*value is not a valid float*" {r incrbyfloat mykeyincr v}
+    }
+
     foreach cmd {"incr" "decr" "incrby" "decrby"} {
         test "$cmd operation should update encoding from raw to int" {
             set res {}
