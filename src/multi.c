edbedef14494b38dec9a8afa34e0d6eb49983469
scottli <scottli@didiglobal.com>
2023-12-06 17:03:32 +0800

    fix:mem leak

--- src/multi.c
+++ src/multi.c
@@ -80,8 +80,9 @@ void queueMultiCommand(client *c, uint64_t cmd_flags) {
     mc->cmd = c->cmd;
     mc->argc = c->argc;
     if(c->argv == c-> argv_static) {
-        mc->argv = zmalloc(sizeof(robj*)*c->argc);
-        mc->argv =  memcpy(mc->argv, c->argv, sizeof(robj*)*c->argc);
+        c->argv = zmalloc(sizeof(robj*)*c->argc);
+        c->argv = memcpy(c->argv, c->argv_static, sizeof(robj*)*c->argc);
+        mc->argv = c->argv;
         mc->argv_len = c->argc;
     } else {
         mc->argv = c->argv;
