28d6aa4f2447632b5cbbd36409ca43805e459a23
debing.sun <debing.sun@redis.com>
2024-01-04 21:30:27 +0800

    Processing events in the main thread instead of module thread
    the reason why moving the events processing into main thread:
    
    The main thread has prepared the eventloop's io events and the events number
    before it enters the acquiring GIL state, and when the module thread enters the event
    loop, it will rebuild this data and process these events data.
    
    Note that at this point the data for the main thread and the module thread may be in two caches,
    and we don't have a memory barrier to protect them.
    
    When the main thread comes back, the events it was processing may have already been processed or invalid.

--- src/module.c
+++ src/module.c
@@ -2394,25 +2394,6 @@ void RM_Yield(RedisModuleCtx *ctx, int flags, const char *busy_reply) {
             /* Let redis process events */
             processEventsWhileBlocked();
         } else {
-            /* If we are not in the main thread, we need to process events after the
-             * main thread enters acquiring GIL state in order to protect the event
-             * loop (ae.c) and avoid potential race conditions. */
-            if (!pthread_equal(server.main_thread_id, pthread_self())) {
-                int acquiring;
-                atomicGet(server.module_gil_acquring, acquiring);
-                if (!acquiring) {
-                    /* If the main thread has not yet entered the acquiring GIL state,
-                     * we attempt to wake it up and exit without waiting for it to
-                     * acquire the GIL. This avoids blocking the caller, allowing them to
-                     * continue with unfinished tasks before the next yield.
-                     * We assume the caller keeps the GIL locked. */
-                    if (write(server.module_pipe[1],"A",1) != 1) {
-                        /* Ignore the error, this is best-effort. */
-                    }
-                    goto end;
-                }
-            }
-
             const char *prev_busy_module_yield_reply = server.busy_module_yield_reply;
             server.busy_module_yield_reply = busy_reply;
             /* start the blocking operation if not already started. */
@@ -2426,7 +2407,34 @@ void RM_Yield(RedisModuleCtx *ctx, int flags, const char *busy_reply) {
                 server.busy_module_yield_flags |= BUSY_MODULE_YIELD_CLIENTS;
 
             /* Let redis process events */
-            processEventsWhileBlocked();
+            if (!pthread_equal(server.main_thread_id, pthread_self())) {
+                /* If we are not in the main thread, we defer event loop processing to the main thread
+                 * after the main thread enters acquiring GIL state in order to protect the event
+                 * loop (ae.c) and avoid potential race conditions. */
+
+                int acquiring;
+                atomicGet(server.module_gil_acquring, acquiring);
+                if (!acquiring) {
+                    /* If the main thread has not yet entered the acquiring GIL state,
+                     * we attempt to wake it up and exit without waiting for it to
+                     * acquire the GIL. This avoids blocking the caller, allowing them to
+                     * continue with unfinished tasks before the next yield.
+                     * We assume the caller keeps the GIL locked. */
+                    if (write(server.module_pipe[1],"A",1) != 1) {
+                        /* Ignore the error, this is best-effort. */
+                    }
+                    goto end;
+                }
+
+                /* Release the GIL, yielding CPU to give the main thread an opportunity to start
+                 * event processing, and then acquire the GIL again until the main thread releases it. */
+                moduleReleaseGIL();
+                sched_yield();
+                moduleAcquireGIL();
+            } else {
+                /* If we are in the main thread, we can safely process events. */
+                processEventsWhileBlocked();
+            }
 
             server.busy_module_yield_reply = prev_busy_module_yield_reply;
             /* Possibly restore the previous flags in case of two nested contexts
