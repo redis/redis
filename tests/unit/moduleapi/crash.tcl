4c099e626c169d54c134b591079dfe89ae708370
Madelyn Olson <madelyneolson@gmail.com>
2024-01-05 20:14:52 -0800

    Address some failures with new segfault tests

--- tests/unit/moduleapi/crash.tcl
+++ tests/unit/moduleapi/crash.tcl
@@ -1,6 +1,7 @@
 # This file is used to test certain crash edge cases to make sure they produce
 # correct stack traces for debugging.
 set testmodule [file normalize tests/modules/crash.so]
+set backtrace_supported [system_backtrace_supported]
 
 # Valgrind will complain that the process terminated by a signal, skip it.
 if {!$::valgrind} {
@@ -20,8 +21,11 @@ if {!$::valgrind} {
             wait_for_log_messages 0 {"*=== REDIS BUG REPORT END. Make sure to include from START to END. ===*"} $loglines 10 1000
             assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT END. Make sure to include from START to END. ==="]
             assert_equal 2 [count_log_message 0 "ASSERTION FAILED"]
-            # There will be 3 crash assertions, 1 in the first stack trace and 2 in the second
-            assert_equal 3 [count_log_message 0 "assertCrash"]
+            if {$backtrace_supported} {
+                # Make sure the crash trace is printed twice. There will be 3 instances of,
+                # assertCrash 1 in the first stack trace and 2 in the second.
+                assert_equal 3 [count_log_message 0 "assertCrash"]
+            }
             assert_equal 1 [count_log_message 0 "RECURSIVE ASSERTION FAILED"]
             assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT START: Cut & paste starting from here ==="]
         }
@@ -34,18 +38,25 @@ if {!$::valgrind} {
             set res [wait_for_log_messages 0 {"*=== REDIS BUG REPORT START: Cut & paste starting from here ===*"} 0 10 1000]
             set loglines [lindex $res 1]
 
-            set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
-            set loglines [lindex $res 1]
+            if {$backtrace_supported} {
+                set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
 
-            set res [wait_for_log_messages 0 {"*Crashed running signal handler. Providing reduced version of recursive crash report*"} $loglines 10 1000]
-            set loglines [lindex $res 1]
-            set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
-            set loglines [lindex $res 1]
+                set res [wait_for_log_messages 0 {"*Crashed running signal handler. Providing reduced version of recursive crash report*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
+                set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
+            }
 
             wait_for_log_messages 0 {"*=== REDIS BUG REPORT END. Make sure to include from START to END. ===*"} $loglines 10 1000
             assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT END. Make sure to include from START to END. ==="]
             assert_equal 1 [count_log_message 0 "Crashed running signal handler. Providing reduced version of recursive crash report"]
-            assert_equal 2 [count_log_message 0 "Crashed running the instruction at"]
+            if {$backtrace_supported} {
+                assert_equal 2 [count_log_message 0 "Crashed running the instruction at"]
+                # Make sure the crash trace is printed twice. There will be 3 instances of 
+                # modulesCollectInfo, 1 in the first stack trace and 2 in the second.
+                assert_equal 3 [count_log_message 0 "modulesCollectInfo"]
+            }
             assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT START: Cut & paste starting from here ==="]
         }
     }
