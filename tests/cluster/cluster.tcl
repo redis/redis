28e86d7b300e845f39b1b55459d4da2a7488b895
Ben Totten <btotten@amazon.com>
2024-01-10 20:08:24 +0000

    Remove artifact function from cluster.tcl

--- tests/cluster/cluster.tcl
+++ tests/cluster/cluster.tcl
@@ -206,17 +206,6 @@ proc wait_for_cluster_propagation {} {
     }
 }
 
-proc wait_for_node_to_be_marked_fail {id_to_check} {
-    foreach_redis_id id {
-        if {[instance_is_killed redis $id]} continue
-        wait_for_condition 1000 50 {
-            [has_flag [get_node_by_id $id $id_to_check] fail]
-        } else {
-            fail "Node $id_to_check was never marked as failed."
-        }
-    }
-}
-
 # Check if cluster's view of hostnames is consistent
 proc are_hostnames_propagated {match_string} {
     for {set j 0} {$j < $::cluster_master_nodes + $::cluster_replica_nodes} {incr j} {
