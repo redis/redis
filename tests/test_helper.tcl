7701d04063f0f7a15628fcf1cd369f7a9c2c1116
Ben Totten <btotten@amazon.com>
2024-01-10 22:36:50 +0000

    Add test to test helper

--- tests/test_helper.tcl
+++ tests/test_helper.tcl
@@ -103,6 +103,7 @@ set ::all_tests {
     unit/cluster/slot-ownership
     unit/cluster/links
     unit/cluster/cluster-response-tls
+    unit/cluster/failure-marking
 }
 # Index to the next test to run in the ::all_tests list.
 set ::next_test 0
