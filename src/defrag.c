29991364f6aa26bdcb8dfaf38fbbab38046c43a7
debing.sun <debing.sun@redis.com>
2024-01-30 17:59:29 +0800

    Increase threshold for defrag tests under 32-bit

--- src/defrag.c
+++ src/defrag.c
@@ -779,7 +779,7 @@ float getAllocatorFragmentation(size_t *out_frag_bytes) {
     if(out_frag_bytes)
         *out_frag_bytes = frag_bytes;
     serverLog(LL_DEBUG,
-        "allocated=%zu, active=%zu, resident=%zu, frag=%.0f%% (%.0f%% rss), frag_bytes=%zu (%zu rss)",
+        "allocated=%zu, active=%zu, resident=%zu, frag=%.1f%% (%.1f%% rss), frag_bytes=%zu (%zu rss)",
         allocated, active, resident, frag_pct, rss_pct, frag_bytes, rss_bytes);
     return frag_pct;
 }
