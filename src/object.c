debf894d93d594bbecea8b058b405cdb0dd9b3e6
Binbin <binloveplay1314@qq.com>
2024-02-08 17:50:38 +0800

    code review cleanup and fix test

--- src/object.c
+++ src/object.c
@@ -232,11 +232,10 @@ robj *dupStringObject(const robj *o) {
     }
 }
 
-robj *createQuicklistObject(int fill, int depth) {
-    quicklist *l = quicklistCreate();
+robj *createQuicklistObject(int fill, int compress) {
+    quicklist *l = quicklistNew(fill, compress);
     robj *o = createObject(OBJ_LIST,l);
     o->encoding = OBJ_ENCODING_QUICKLIST;
-    quicklistSetOptions(o->ptr, fill, depth);
     return o;
 }
 
