514a6fa7cfe6ef334f60303eb59333d9db8d091e
Binbin <binloveplay1314@qq.com>
2024-02-18 17:54:53 +0800

    Update src/server.h
    Co-authored-by: Oran Agra <oran@redislabs.com>

--- src/server.h
+++ src/server.h
@@ -1778,8 +1778,8 @@ struct redisServer {
     sds aof_buf;      /* AOF buffer, written before entering the event loop */
     int aof_fd;       /* File descriptor of currently selected AOF file */
     int aof_selected_db; /* Currently selected DB in AOF */
-    time_t aof_flush_postponed_start; /* mstime of postponed AOF flush */
-    time_t aof_last_fsync;            /* mstime of last fsync() */
+    mstime_t aof_flush_postponed_start; /* mstime of postponed AOF flush */
+    mstime_t aof_last_fsync;            /* mstime of last fsync() */
     time_t aof_rewrite_time_last;   /* Time used by last AOF rewrite run. */
     time_t aof_rewrite_time_start;  /* Current AOF rewrite start time. */
     time_t aof_cur_timestamp;       /* Current record timestamp in AOF */
