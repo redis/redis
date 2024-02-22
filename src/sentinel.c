52317749240992cf5c42079f1a2ce7aa5d2f685f
hany_li <hany_li@trip.com>
2024-02-22 19:50:31 +0800

    pub channel

--- src/sentinel.c
+++ src/sentinel.c
@@ -5156,9 +5156,10 @@ int sentinelTest(int argc, char *argv[], int accurate) {
     initSentinel();
 
     robj *channel;
-    // channel = zcalloc(sizeof(*o));
+    channel = zcalloc(sizeof(*o));
 
     list *clients = listCreate();
+    server.pubsub_channels = dictCreate(&keylistDictType,NULL);
     dictAdd(server.pubsub_channels,channel,clients);
 
     TEST("test elect abort") {
