0e1de78fca849c135fd00cd85b5b87920e46e50d
guybe7 <guy.benoish@redislabs.com>
2024-05-06 16:55:42 +0800

    XREADGROUP from PEL should not affect server.dirty (#13251)
    Because it does not cause any propagation (arguably it should, see the
    comment in the tcl file)
    
    The motivation for this fix is that in 6.2 if dirty changed without
    propagation inside MULTI/EXEC it would cause propagation of EXEC only,
    which would result in the replica sending errors to its master

--- src/stream.h
+++ src/stream.h
@@ -116,7 +116,7 @@ struct client;
 stream *streamNew(void);
 void freeStream(stream *s);
 unsigned long streamLength(const robj *subject);
-size_t streamReplyWithRange(client *c, stream *s, streamID *start, streamID *end, size_t count, int rev, streamCG *group, streamConsumer *consumer, int flags, streamPropInfo *spi);
+size_t streamReplyWithRange(client *c, stream *s, streamID *start, streamID *end, size_t count, int rev, streamCG *group, streamConsumer *consumer, int flags, streamPropInfo *spi, unsigned long *propCount);
 void streamIteratorStart(streamIterator *si, stream *s, streamID *start, streamID *end, int rev);
 int streamIteratorGetID(streamIterator *si, streamID *id, int64_t *numfields);
 void streamIteratorGetField(streamIterator *si, unsigned char **fieldptr, unsigned char **valueptr, int64_t *fieldlen, int64_t *valuelen);
