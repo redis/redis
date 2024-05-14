bb828d11b34287c8a2042ce024efc44e23e3b68c
debing.sun <debing.sun@redis.com>
2024-05-14 19:40:29 +0800

    Verify xreadgroup in advance rather than in MULTI
    Co-authored-by: guybe7 <guy.benoish@redislabs.com>

--- tests/unit/type/stream-cgroups.tcl
+++ tests/unit/type/stream-cgroups.tcl
@@ -1398,23 +1398,10 @@ start_server {
             $master del mystream
             $master xadd mystream 1-0 a b c d e f
             $master xgroup create mystream mygroup 0
-            $master xreadgroup group mygroup ryan count 1 streams mystream >
+            assert_equal [$master xreadgroup group mygroup ryan count 1 streams mystream >] {{mystream {{1-0 {a b c d e f}}}}}
             $master multi
             $master xreadgroup group mygroup ryan count 1 streams mystream 0
-            set reply [lindex [$master exec] 0]
-            # If using RESP3, we transform a map response into array of tuples
-            # to ensure consistency with RESP2 response.
-            if {[is_dict $reply]} {
-                set tuparray {}
-                foreach {key val} $reply {
-                    set tmp {}
-                    lappend tmp $key
-                    lappend tmp $val
-                    lappend tuparray $tmp
-                }
-                set reply $tuparray
-            }
-            assert_equal $reply {{mystream {{1-0 {a b c d e f}}}}}
+            $master exec
         }
     }
 
