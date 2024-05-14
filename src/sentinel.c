f17b8cd68320387780ffb051365749583a1dc4e7
nayutah <dullboy@apecloud.com>
2024-05-14 14:20:03 +0800

    fix spell check erros

--- src/sentinel.c
+++ src/sentinel.c
@@ -3109,7 +3109,7 @@ void sentinelProcessHelloMessage(char *hello, int hello_len) {
         /* Update scaled slave info if received scale epoch is newer. */
         if (master->scale_epoch < scale_epoch) {
             serverLog(LL_NOTICE, "Current scale epoch:%llu, received scale epoch:%llu", 
-                master->scale_epoch, scale_epoch);
+                (unsigned long long)master->scale_epoch, (unsigned long long)scale_epoch);
             /* Release scale_addr for previous scaling operation. */
             if (master->scale_addr)
                 releaseSentinelAddr(master->scale_addr);
