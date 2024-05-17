20821479e976d9c89a6e8d859687a2da01adc06e
debing.sun <debing.sun@redis.com>
2024-05-17 09:29:12 +0800

    Update the license

--- tests/unit/cluster/sharded-pubsub.tcl
+++ tests/unit/cluster/sharded-pubsub.tcl
@@ -1,3 +1,13 @@
+#
+# Copyright (c) 2009-Present, Redis Ltd.
+# All rights reserved.
+#
+# Licensed under your choice of the Redis Source Available License 2.0
+# (RSALv2) or the Server Side Public License v1 (SSPLv1).
+#
+# Portions of this file are available under BSD3 terms; see REDISCONTRIBUTIONS for more information.
+#
+
 start_cluster 1 1 {tags {external:skip cluster}} {
         set primary_id 0
         set replica1_id 1
