6c6b1035d546586aaca618669aaac35f3107f618
debing.sun <debing.sun@redis.com>
2024-06-05 11:36:50 +0800

    Update license
    Co-authored-by: judeng <abc3844@126.com>

--- tests/unit/scan.tcl
+++ tests/unit/scan.tcl
@@ -1,3 +1,13 @@
+#
+# Copyright (c) 2009-Present, Redis Ltd.
+# All rights reserved.
+#
+# Licensed under your choice of the Redis Source Available License 2.0
+# (RSALv2) or the Server Side Public License v1 (SSPLv1).
+#
+# Portions of this file are available under BSD3 terms; see REDISCONTRIBUTIONS for more information.
+#
+
 proc test_scan {type} {
     test "{$type} SCAN basic" {
         r flushdb
