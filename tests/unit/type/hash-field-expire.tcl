d0f191fc179edbd500ebc0b2d10594f89fad5990
Moti Cohen <moticless@gmail.com>
2024-06-23 14:26:35 +0300

    Add test for HRANDFIELD that deletes expired fields

--- tests/unit/type/hash-field-expire.tcl
+++ tests/unit/type/hash-field-expire.tcl
@@ -1012,7 +1012,13 @@ start_server {tags {"external:skip needs:debug"}} {
                 r hpexpireat h1 [expr [clock seconds]*1000+100000] NX FIELDS 1 f2
                 r hpexpire h1 100000 NX FIELDS 3 f3 f4 f5
                 r hexpire h1 100000 FIELDS 1 f6
-                r hset h5 f1 v1
+
+                # Verify HRANDFIELD deletes expired fields and propagates it
+                r hset h2 f1 v1 f2 v2
+                r hpexpire h2 1 FIELDS 1 f1
+                r hpexpire h2 50 FIELDS 1 f2
+                after 100
+                assert_equal [r hrandfield h4 2] ""
 
                 assert_aof_content $aof {
                     {select *}
@@ -1021,7 +1027,11 @@ start_server {tags {"external:skip needs:debug"}} {
                     {hpexpireat h1 * FIELDS 1 f2}
                     {hpexpireat h1 * NX FIELDS 3 f3 f4 f5}
                     {hpexpireat h1 * FIELDS 1 f6}
-                    {hset h5 f1 v1}
+                    {hset h2 f1 v1 f2 v2}
+                    {hpexpireat h2 * FIELDS 1 f1}
+                    {hpexpireat h2 * FIELDS 1 f2}
+                    {hdel h2 f1}
+                    {hdel h2 f2}
                 }
 
                 array set keyAndFields1 [dumpAllHashes r]
@@ -1044,6 +1054,7 @@ start_server {tags {"external:skip needs:debug"}} {
             r flushall ; # Clean up keyspace to avoid interference by keys from other tests
             set repl [attach_to_replication_stream]
 
+            # HEXPIRE/HPEXPIRE should be translated into HPEXPIREAT
             r hset h1 f1 v1
             r hexpireat h1 [expr [clock seconds]+100] NX FIELDS 1 f1
             r hset h2 f2 v2
@@ -1063,6 +1074,28 @@ start_server {tags {"external:skip needs:debug"}} {
                 {hset h3 f3 v3 f4 v4 f5 v5}
                 {hpexpireat h3 * FIELDS 3 f3 f4 non_exists_field}
                 {hpersist h3 FIELDS 1 f3}
+            }
+            close_replication_stream $repl
+        } {} {needs:repl}
+
+        test {HRANDFIELD delete expired fields and propagate DELs to replica} {
+            r flushall
+            set repl [attach_to_replication_stream]
+
+            r hset h4 f1 v1 f2 v2
+            r hpexpire h4 1 FIELDS 1 f1
+            r hpexpire h4 2 FIELDS 1 f2
+            after 100
+            assert_equal [r hrandfield h4 2] ""
+
+
+            assert_replication_stream $repl {
+                {select *}
+                {hset h4 f1 v1 f2 v2}
+                {hpexpireat h4 * FIELDS 1 f1}
+                {hpexpireat h4 * FIELDS 1 f2}
+                {hdel h4 f1}
+                {hdel h4 f2}
             }
             close_replication_stream $repl
         } {} {needs:repl}
