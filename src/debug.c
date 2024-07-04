46d002953d71fc75234335a6f5767dcadfb045a5
debing.sun <debing.sun@redis.com>
2024-07-04 14:56:52 +0800

    Update license

--- src/debug.c
+++ src/debug.c
@@ -4,6 +4,8 @@
  *
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2) or the Server Side Public License v1 (SSPLv1).
+ *
+ * Portions of this file are available under BSD3 terms; see REDISCONTRIBUTIONS for more information.
  */
 
 #include "server.h"
