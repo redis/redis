c04f4f6bbca54e640829b86f37b3676f50ff268e
debing.sun <debing.sun@redis.com>
2024-09-20 08:48:17 +0800

    Update src/bitops.c

--- src/bitops.c
+++ src/bitops.c
@@ -29,7 +29,7 @@ long long redisPopcount(void *s, long count) {
         count--;
     }
 
-    /* Count bits 32 bytes at a time */ */
+    /* Count bits 32 bytes at a time */
     while (i + 4*8 <= count) {
         bits += __builtin_popcountll(*(uint64_t*)(p + i));
         bits += __builtin_popcountll(*(uint64_t*)(p + i + 8));
