8233cf6adad673eaa39914750f55934246acffd0
debing.sun <debing.sun@redis.com>
2024-09-22 19:05:14 +0800

    Fix complaint on macos

--- src/config.h
+++ src/config.h
@@ -307,4 +307,10 @@ void setcpuaffinity(const char *cpulist);
 #define HAVE_FADVISE
 #endif
 
+#if __has_builtin(__builtin_cpu_supports)
+    #define HAS_BUILTIN_CPU_SUPPORTS 1
+#else
+    #define HAS_BUILTIN_CPU_SUPPORTS 0
+#endif
+
 #endif
