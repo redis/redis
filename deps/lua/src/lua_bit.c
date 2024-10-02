944aff7fb6fcffa363237c4bafc33cdd805d05c9
Oran Agra <oran@redislabs.com>
2024-10-02 19:54:06 +0300

    Fix lua bit.tohex (CVE-2024-31449)
    INT_MIN value must be explicitly checked, and cannot be negated.

--- deps/lua/src/lua_bit.c
+++ deps/lua/src/lua_bit.c
@@ -132,6 +132,7 @@ static int bit_tohex(lua_State *L)
   const char *hexdigits = "0123456789abcdef";
   char buf[8];
   int i;
+  if (n == INT32_MIN) n = INT32_MIN+1;
   if (n < 0) { n = -n; hexdigits = "0123456789ABCDEF"; }
   if (n > 8) n = 8;
   for (i = (int)n; --i >= 0; ) { buf[i] = hexdigits[b & 15]; b >>= 4; }
