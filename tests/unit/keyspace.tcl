9317bf64659b33166a943ec03d5d9b954e86afb0
Oran Agra <oran@redislabs.com>
2024-10-02 20:11:01 +0300

    Prevent pattern matching abuse (CVE-2024-31228)

--- tests/unit/keyspace.tcl
+++ tests/unit/keyspace.tcl
@@ -485,4 +485,10 @@ start_server {tags {"keyspace"}} {
         r SET aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 1
         r KEYS "a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*b"
     } {}
+
+    test {Regression for pattern matching very long nested loops} {
+        r flushdb
+        r SET [string repeat "a" 50000] 1
+        r KEYS [string repeat "*?" 50000]
+    } {}
 }
