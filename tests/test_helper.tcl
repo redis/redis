5f7d7ce8b05c99725ef90651a977fe768fd0c305
chx9 <cheng.huan@icloud.com>
2024-10-08 14:15:48 +0800

    fix typo in test_helper.tcl (#13576)
    fix typo in test_helper.tcl: even driven => event driven

--- tests/test_helper.tcl
+++ tests/test_helper.tcl
@@ -506,7 +506,7 @@ proc the_end {} {
     }
 }
 
-# The client is not even driven (the test server is instead) as we just need
+# The client is not event driven (the test server is instead) as we just need
 # to read the command, execute, reply... all this in a loop.
 proc test_client_main server_port {
     set ::test_server_fd [socket localhost $server_port]
