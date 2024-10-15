71af93087039b03efd1702b920ec7da52b144a2c
YaacovHazan <yaacov.hazan@redislabs.com>
2024-10-15 09:33:50 +0300

    Skip experimental tests by default
    - Move the SFLUSH tests from the old framework to the new one

--- tests/unit/cluster/multi-slot-operations.tcl
+++ tests/unit/cluster/multi-slot-operations.tcl
@@ -107,3 +107,76 @@ test "DELSLOTSRANGE command with several boundary conditions test suite" {
     assert_match "*9829 11000*12001 12100*12201 13104*" [$master4 CLUSTER SLOTS]
 }
 } cluster_allocate_with_continuous_slots_local
+
+start_cluster 2 0 {tags {external:skip cluster}} {
+
+set master1 [srv 0 "client"]
+set master2 [srv -1 "client"]
+
+test "SFLUSH - Errors and output validation" {
+    assert_match "* 0-8191*" [$master1 CLUSTER NODES]
+    assert_match "* 8192-16383*" [$master2 CLUSTER NODES]
+    assert_match "*0 8191*" [$master1 CLUSTER SLOTS]
+    assert_match "*8192 16383*" [$master2 CLUSTER SLOTS]
+
+    # make master1 non-continuous slots
+    $master1 cluster DELSLOTSRANGE 1000 2000
+
+    # Test SFLUSH errors validation
+    assert_error {ERR wrong number of arguments*}           {$master1 SFLUSH 4}
+    assert_error {ERR wrong number of arguments*}           {$master1 SFLUSH 4 SYNC}
+    assert_error {ERR Invalid or out of range slot}         {$master1 SFLUSH x 4}
+    assert_error {ERR Invalid or out of range slot}         {$master1 SFLUSH 0 12x}
+    assert_error {ERR Slot 3 specified multiple times}      {$master1 SFLUSH 2 4 3 5}
+    assert_error {ERR start slot number 8 is greater than*} {$master1 SFLUSH 8 4}
+    assert_error {ERR wrong number of arguments*}           {$master1 SFLUSH 4 8 10}
+    assert_error {ERR wrong number of arguments*}           {$master1 SFLUSH 0 999 2001 8191 ASYNCX}
+
+    # Test SFLUSH output validation
+    assert_match "" [$master1 SFLUSH 2 4]
+    assert_match "" [$master1 SFLUSH 0 4]
+    assert_match "" [$master2 SFLUSH 0 4]
+    assert_match "" [$master1 SFLUSH 1 8191]
+    assert_match "" [$master1 SFLUSH 0 8190]
+    assert_match "" [$master1 SFLUSH 0 998 2001 8191]
+    assert_match "" [$master1 SFLUSH 1 999 2001 8191]
+    assert_match "" [$master1 SFLUSH 0 999 2001 8190]
+    assert_match "" [$master1 SFLUSH 0 999 2002 8191]
+    assert_match "{0 999} {2001 8191}" [$master1 SFLUSH 0 999 2001 8191]
+    assert_match "{0 999} {2001 8191}" [$master1 SFLUSH 0 8191]
+    assert_match "{0 999} {2001 8191}" [$master1 SFLUSH 0 4000 4001 8191]
+    assert_match "" [$master2 SFLUSH 8193 16383]
+    assert_match "" [$master2 SFLUSH 8192 16382]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 16383]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 16383 SYNC]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 16383 ASYNC]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 9000 9001 16383]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 9000 9001 16383 SYNC]
+    assert_match "{8192 16383}" [$master2 SFLUSH 8192 9000 9001 16383 ASYNC]
+
+    # restore master1 continuous slots
+    $master1 cluster ADDSLOTSRANGE 1000 2000
+} {OK} {experimental}
+
+test "SFLUSH - Deletes the keys with argument <NONE>/SYNC/ASYNC" {
+    foreach op {"" "SYNC" "ASYNC"} {
+        for {set i 0} {$i < 100} {incr i} {
+            catch {$master1 SET key$i val$i}
+            catch {$master2 SET key$i val$i}
+        }
+
+        assert {[$master1 DBSIZE] > 0}
+        assert {[$master2 DBSIZE] > 0}
+        if {$op eq ""} {
+            assert_match "{0 8191}" [ $master1 SFLUSH 0 8191]
+        } else {
+            assert_match "{0 8191}" [ $master1 SFLUSH 0 8191 $op]
+        }
+        assert {[$master1 DBSIZE] == 0}
+        assert {[$master2 DBSIZE] > 0}
+        assert_match "{8192 16383}" [ $master2 SFLUSH 8192 16383]
+        assert {[$master2 DBSIZE] == 0}
+    }
+} {} {experimental}
+
+}
