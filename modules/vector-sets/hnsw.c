bb654eb0f858ab0f4f36a252ffe991399d15cbcf
fcostaoliveira <filipe@redis.com>
2025-04-21 20:11:10 +0100

    Replace manual popcount with __builtin_popcountll for binary vector distance

--- modules/vector-sets/hnsw.c
+++ modules/vector-sets/hnsw.c
@@ -278,23 +278,13 @@ float vectors_distance_q8(const int8_t *x, const int8_t *y, uint32_t dim,
     return distance;
 }
 
-static inline int popcount64(uint64_t x) {
-    x = (x & 0x5555555555555555) + ((x >> 1) & 0x5555555555555555);
-    x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333);
-    x = (x & 0x0F0F0F0F0F0F0F0F) + ((x >> 4) & 0x0F0F0F0F0F0F0F0F);
-    x = (x & 0x00FF00FF00FF00FF) + ((x >> 8) & 0x00FF00FF00FF00FF);
-    x = (x & 0x0000FFFF0000FFFF) + ((x >> 16) & 0x0000FFFF0000FFFF);
-    x = (x & 0x00000000FFFFFFFF) + ((x >> 32) & 0x00000000FFFFFFFF);
-    return x;
-}
-
 /* Binary vectors distance. */
 float vectors_distance_bin(const uint64_t *x, const uint64_t *y, uint32_t dim) {
     uint32_t len = (dim+63)/64;
     uint32_t opposite = 0;
     for (uint32_t j = 0; j < len; j++) {
         uint64_t xor = x[j]^y[j];
-        opposite += popcount64(xor);
+        opposite += __builtin_popcountll(xor);
     }
     return (float)opposite*2/dim;
 }
