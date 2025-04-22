b97cce43cf3f6b9630d0f5a9d51191a0ecab4daf
vitah <vitahlin@gmail.com>
2025-04-22 21:44:46 +0800

    Fix tls port update not reflected in CLUSTER SLOTS

--- tests/unit/cluster/announced-endpoints.tcl
+++ tests/unit/cluster/announced-endpoints.tcl
@@ -49,12 +49,21 @@ start_cluster 2 2 {tags {external:skip cluster}} {
     }
 
     test "CONFIG SET port updates cluster-announced port" {
+        set count [expr [llength $::servers] + 1]
         # Get the original port and change to new_port
-        set orig_port [lindex [R 0 config get port] 1]
+        if {$::tls} {
+            set orig_port [lindex [R 0 config get tls-port] 1]
+        } else {
+            set orig_port [lindex [R 0 config get port] 1]
+        }
         assert {$orig_port != ""}
+        set new_port [find_available_port $orig_port $count]
 
-        set new_port [find_available_port $baseport $count]
-        R 0 config set port $new_port
+        if {$::tls} {
+            R 0 config set tls-port $new_port
+        } else {
+            R 0 config set port $new_port
+        }
 
         # Verify that the new port appears in the output of cluster slots
         wait_for_condition 50 100 {
