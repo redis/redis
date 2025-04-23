a387455f0b082bacc32d6e4c28f8d3fa4c09f992
fcostaoliveira <filipe@redis.com>
2025-04-23 11:20:09 +0100

    Removed spurious blank space change.

--- src/config.h
+++ src/config.h
@@ -210,7 +210,6 @@ void setproctitle(const char *fmt, ...);
 #define	BIG_ENDIAN	4321	/* most-significant byte first (IBM, net) */
 #define	PDP_ENDIAN	3412	/* LSB first in word, MSW first in long (pdp)*/
 
-
 #if defined(__i386__) || defined(__x86_64__) || defined(__amd64__) || \
    defined(vax) || defined(ns32000) || defined(sun386) || \
    defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
