84343bb1ee11c05c21f44de5b41819e3353c259a
Ozan Tezcan <ozantezcan@gmail.com>
2025-05-14 11:02:30 +0300

    Retry accept() even if accepted connection reports an error (CVE-2025-48367)
    In case of accept4() returns an error, we should check errno value and decide if we should retry accept4() without waiting next event loop iteration.

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -1253,6 +1253,8 @@ void clusterAcceptHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
     while(max--) {
         cfd = anetTcpAccept(server.neterr, fd, cip, sizeof(cip), &cport);
         if (cfd == ANET_ERR) {
+            if (anetAcceptFailureNeedsRetry(errno))
+                continue;
             if (errno != EWOULDBLOCK)
                 serverLog(LL_VERBOSE,
                     "Error accepting cluster node: %s", server.neterr);
