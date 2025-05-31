38d0141ff834408983be7d7499111f0bd347574b
Thalia Archibald <thalia@archibald.dev>
2025-05-31 09:09:59 -0700

    Match .c licenses in .h
    Some portions of Redis have remained licensed under BSD-3-Clause after
    the relicense (see REDISCONTRIBUTIONS.txt) and this is indicated by the
    license headers, so it is important that this information be accurate.
    
    These C header files either have no license header or have a different
    license than their corresponding .c files. As I understand it, that
    would mean that further changes to BSD-licensed .c files can be
    cherry-picked by other projects, but not corresponding changes to their
    unmarked or tri-licensed .h headers. Since the business value to Redis
    would be in the implementations, not the interfaces, it makes sense to
    keep them consistent. Copy the license headers correspondingly and note
    the copyright year as when the .h was created.

--- src/cli_common.h
+++ src/cli_common.h
@@ -1,3 +1,13 @@
+/* CLI (command line interface) common methods
+ *
+ * Copyright (c) 2020-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of (a) the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ */
+
 #ifndef __CLICOMMON_H
 #define __CLICOMMON_H
 
--- src/cluster.h
+++ src/cluster.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2013-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of (a) the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ */
+
 #ifndef __CLUSTER_H
 #define __CLUSTER_H
 
