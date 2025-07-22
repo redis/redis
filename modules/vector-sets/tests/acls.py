1a1b8bec1819a3b3dd0fa55b862cfa1c363aead9
Ali-Akber Saifee <ali@indydevs.org>
2025-07-22 16:40:23 -0700

    Add tests for vector set acls

---
+++ modules/vector-sets/tests/acls.py
@@ -0,0 +1,47 @@
+from test import TestCase
+
+
+class AclCategories(TestCase):
+    VECTOR_SET_COMMANDS = {
+        b"VADD",
+        b"VCARD",
+        b"VDIM",
+        b"VEMB",
+        b"VGETATTR",
+        b"VINFO",
+        b"VISMEMBER",
+        b"VLINKS",
+        b"VRANDMEMBER",
+        b"VREM",
+        b"VSETATTR",
+        b"VSIM",
+    }
+
+    def getname(self):
+        return "Vector set command ACL categories"
+
+    def test(self):
+        vector_set_commands = set(self.redis.execute_command("ACL CAT VECTORSET"))
+        assert AclCategories.VECTOR_SET_COMMANDS == vector_set_commands, (
+            f"Expected {AclCategories.VECTOR_SET_COMMANDS}, got {vector_set_commands}"
+        )
+
+        readonly_commands = set(self.redis.execute_command("ACL CAT READ"))
+        assert AclCategories.VECTOR_SET_COMMANDS & readonly_commands == {
+            b"VGETATTR",
+            b"VSIM",
+            b"VCARD",
+            b"VISMEMBER",
+            b"VDIM",
+            b"VRANDMEMBER",
+            b"VINFO",
+            b"VLINKS",
+            b"VEMB",
+        }
+
+        write_commands = set(self.redis.execute_command("ACL CAT WRITE"))
+        assert AclCategories.VECTOR_SET_COMMANDS & write_commands == {
+            b"VADD",
+            b"VREM",
+            b"VSETATTR",
+        }
