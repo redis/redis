5a0dc9d111ef4ba222de7627815dc009befdc7ee
debing.sun <debing.sun@redis.com>
2025-08-08 19:06:00 +0800

    Remove unnecessary change

--- src/quicklist.c
+++ src/quicklist.c
@@ -496,9 +496,9 @@ void quicklistNodeLimit(int fill, size_t *size, unsigned int *count) {
     }
 }
 
-/* Determine whether the quicklist node has reached its limit before performing
- * insertions, merges, or other operations that would increase the node size.
- * Return 1 if exceeds the limit, otherwise 0. */
+/* Check if the limit of the quicklist node has been reached to determine if
+ * insertions, merges or other operations that would increase the size of
+ * the node can be performed.
 int quicklistNodeExceedsLimit(const quicklist *ql, size_t new_sz, unsigned int new_count) {
     if (ql->limit_type == QUICKLIST_NODE_LIMIT_SIZE) {
         return new_sz > ql->limit;
