459e98ec2e507ea521bd87a629e9dfad4fc3b6cd
debing.sun <debing.sun@redis.com>
2025-09-05 13:11:50 +0800

    Add license

--- tests/support/server.tcl
+++ tests/support/server.tcl
@@ -1,3 +1,17 @@
+#
+# Copyright (c) 2009-Present, Redis Ltd.
+# All rights reserved.
+#
+# Copyright (c) 2024-present, Valkey contributors.
+# All rights reserved.
+#
+# Licensed under your choice of (a) the Redis Source Available License 2.0
+# (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+# GNU Affero General Public License v3 (AGPLv3).
+#
+# Portions of this file are available under BSD3 terms; see REDISCONTRIBUTIONS for more information.
+#
+
 set ::global_overrides {}
 set ::tags {}
 set ::valgrind_errors {}
@@ -223,7 +237,7 @@ proc tags_acceptable {tags err_return} {
     }
 
     if {$::debug_defrag && [lsearch $tags "debug_defrag:skip"] >= 0} {
-        set err "Not supported when debug_defrag is enabled"
+        set err "Not supported on server compiled with DEBUG_FORCE_DEFRAG option"
         return 0
     }
 
