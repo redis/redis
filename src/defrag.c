c4f4209c38783b186ce693060acdd8b5a04621c6
debing.sun <debing.sun@redis.com>
2025-09-07 23:54:07 +0800

    Fix DDEBUG_DEFRAG_FULLY define

--- src/defrag.c
+++ src/defrag.c
@@ -1597,7 +1597,7 @@ static int activeDefragTimeProc(struct aeEventLoop *eventLoop, long long id, voi
 
     monotime starttime = getMonotonicUs();
     int dutyCycleUs = computeDefragCycleUs();
-#if defined(DDEBUG_DEFRAG_FULLY)
+#if defined(DEBUG_DEFRAG_FULLY)
     dutyCycleUs = 30*1000*1000LL; /* 30 seconds */
 #endif
     monotime endtime = starttime + dutyCycleUs;
