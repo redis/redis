e21c7899b09fa18590fc5d5550a1ccab962b0e95
debing.sun <debing.sun@redis.com>
2025-09-08 11:55:31 +0800

    Skip the timeout test

--- tests/integration/replication-rdbchannel.tcl
+++ tests/integration/replication-rdbchannel.tcl
@@ -401,7 +401,7 @@ start_server {tags {"repl external:skip"}} {
     }
 }
 
-start_server {tags {"repl external:skip"}} {
+start_server {tags {"repl external:skip debug_defrag:skip"}} {
     set master [srv 0 client]
     set master_host [srv 0 host]
     set master_port [srv 0 port]
