ecf03e0e676563af9128708bf62d2f0d862f5c95
debing.sun <debing.sun@redis.com>
2025-09-10 15:15:19 +0800

    Make more data for large_ebrax test

--- tests/unit/memefficiency.tcl
+++ tests/unit/memefficiency.tcl
@@ -527,14 +527,14 @@ run_solo {defrag} {
             $rd_pubsub close
         }
 
-        foreach {eb_container fields n} {eblist 16 3000 ebrax 30 1600 large_ebrax 1600 30} {
+        foreach {eb_container fields n} {eblist 16 3000 ebrax 30 1600 large_ebrax 500 100} {
         test "Active Defrag HFE with $eb_container: $type" {
             r flushdb
             r config set hz 100
             r config set activedefrag no
             wait_for_defrag_stop 500 100
             r config resetstat
-            r config set active-defrag-threshold-lower 8
+            r config set active-defrag-threshold-lower 6
             r config set active-defrag-cycle-min 65
             r config set active-defrag-cycle-max 75
             r config set active-defrag-ignore-bytes 1000kb
@@ -577,7 +577,7 @@ run_solo {defrag} {
                 puts "frag [s allocator_frag_ratio]"
                 puts "frag_bytes [s allocator_frag_bytes]"
             }
-            assert_lessthan [s allocator_frag_ratio] 1.08
+            assert_lessthan [s allocator_frag_ratio] 1.06
 
             # Delete all the keys to create fragmentation
             for {set i 0} {$i < $n} {incr i} {
@@ -608,7 +608,7 @@ run_solo {defrag} {
                 }
 
                 # wait for the active defrag to stop working
-                wait_for_defrag_stop 500 100 1.08
+                wait_for_defrag_stop 500 100 1.06
 
                 # test the fragmentation is lower
                 after 120 ;# serverCron only updates the info once in 100ms
