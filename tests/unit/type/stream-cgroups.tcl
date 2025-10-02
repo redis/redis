e6ec6fa6076c92b401404d252c581e16c1d286f3
Sergey Georgiev <s_ggeorgiev@yahoo.com>
2025-10-02 15:44:24 +0300

    Add tests for XREADGROUP CLAIM functionality

--- tests/unit/type/stream-cgroups.tcl
+++ tests/unit/type/stream-cgroups.tcl
@@ -49,7 +49,7 @@ start_server {
         r XGROUP CREATE mystream mygroup $
     }
 
-    test {XREADGROUP: basic argument count validation} {
+    test {XREADGROUP basic argument count validation} {
         # Too few arguments
         assert_error "*wrong number of arguments*" {r XREADGROUP}
         assert_error "*wrong number of arguments*" {r XREADGROUP GROUP}
@@ -58,7 +58,7 @@ start_server {
         assert_error "*wrong number of arguments*" {r XREADGROUP GROUP mygroup consumer STREAMS}
     }
 
-    test {XREADGROUP: GROUP keyword validation} {
+    test {XREADGROUP GROUP keyword validation} {
         # Missing GROUP keyword entirely - wrong syntax
         assert_error "*wrong number of arguments*" {r XREADGROUP mygroup consumer STREAMS mystream >}
         
@@ -66,12 +66,12 @@ start_server {
         assert_error "*syntax error*" {r XREADGROUP GROUPS mygroup consumer STREAMS mystream >}
     }
 
-    test {XREADGROUP: empty group name handling} {
+    test {XREADGROUP empty group name handling} {
         # Empty group name should give NOGROUP error
         assert_error "*NOGROUP*" {r XREADGROUP GROUP "" consumer STREAMS mystream >}
     }
 
-    test {XREADGROUP: STREAMS keyword validation} {
+    test {XREADGROUP STREAMS keyword validation} {
         # Missing STREAMS keyword
         assert_error "*wrong number of arguments*" {r XREADGROUP GROUP mygroup consumer mystream >}
         
@@ -79,7 +79,7 @@ start_server {
         assert_error "*syntax error*" {r XREADGROUP GROUP mygroup consumer STREAM mystream >}
     }
 
-    test {XREADGROUP: stream and ID pairing} {
+    test {XREADGROUP stream and ID pairing} {
         # Missing stream ID
         assert_error "*wrong number of arguments*" {r XREADGROUP GROUP mygroup consumer STREAMS mystream}
         
@@ -94,13 +94,13 @@ start_server {
         r DEL stream2
     }
 
-    test {XREADGROUP: COUNT parameter validation} {
+    test {XREADGROUP COUNT parameter validation} {
         # Non-numeric count
         assert_error "*not an integer*" {r XREADGROUP GROUP mygroup consumer COUNT abc STREAMS mystream >}
         assert_error "*not an integer*" {r XREADGROUP GROUP mygroup consumer COUNT 1.5 STREAMS mystream >}
     }
 
-    test {XREADGROUP: BLOCK parameter validation} {
+    test {XREADGROUP BLOCK parameter validation} {
         # Non-numeric block timeout
         assert_error "*not an integer*" {r XREADGROUP GROUP mygroup consumer BLOCK abc STREAMS mystream >}
         assert_error "*not an integer*" {r XREADGROUP GROUP mygroup consumer BLOCK 1.5 STREAMS mystream >}
@@ -112,7 +112,7 @@ start_server {
         assert_error "*ERR timeout is negative*" {r XREADGROUP GROUP mygroup consumer BLOCK -1 STREAMS mystream >}
     }
 
-    test {XREADGROUP: stream ID format validation} {
+    test {XREADGROUP stream ID format validation} {
         # Invalid ID formats should error
         assert_error "*Invalid stream ID*" {r XREADGROUP GROUP mygroup consumer STREAMS mystream invalid-id}
         assert_error "*Invalid stream ID*" {r XREADGROUP GROUP mygroup consumer STREAMS mystream 123-}
@@ -122,22 +122,22 @@ start_server {
         assert_error "*Invalid stream ID*" {r XREADGROUP GROUP mygroup consumer STREAMS mystream 123-abc}
     }
 
-    test {XREADGROUP: nonexistent group} {
+    test {XREADGROUP nonexistent group} {
         assert_error "*NOGROUP*" {r XREADGROUP GROUP nonexistent consumer STREAMS mystream >}
     }
 
-    test {XREADGROUP: nonexistent stream with existing group} {
+    test {XREADGROUP nonexistent stream with existing group} {
         # Group doesn't exist on the nonexistent stream
         assert_error "*NOGROUP*" {r XREADGROUP GROUP mygroup consumer STREAMS nonexistent >}
     }
 
-    test {XREADGROUP: wrong key type} {
+    test {XREADGROUP wrong key type} {
         r SET wrongtype "not a stream"
         assert_error "*WRONGTYPE*" {r XREADGROUP GROUP mygroup consumer STREAMS wrongtype >}
         r DEL wrongtype
     }
 
-    test {XREADGROUP: boundary value validation} {
+    test {XREADGROUP boundary value validation} {
         # Test COUNT boundaries - values that are too large
         assert_error "*value is not an integer or out of range*" {r XREADGROUP GROUP mygroup consumer COUNT 18446744073709551616 STREAMS mystream >}
         
@@ -145,7 +145,7 @@ start_server {
         assert_error "*timeout is not an integer or out of range*" {r XREADGROUP GROUP mygroup consumer BLOCK 18446744073709551616 STREAMS mystream >}
     }
 
-    test {XREADGROUP: malformed parameter syntax} {
+    test {XREADGROUP malformed parameter syntax} {
         # Unknown parameters
         assert_error "*syntax error*" {r XREADGROUP GROUP mygroup consumer INVALID param STREAMS mystream >}
         assert_error "*syntax error*" {r XREADGROUP GROUP mygroup consumer TIMEOUT 1000 STREAMS mystream >}
@@ -2763,7 +2763,7 @@ start_server {
                     assert_equal $delivery_count 3
                 }
             }
-        }
+        } {} {needs:repl}
 
         test "XREADGROUP CLAIM verify claiming order" {
             r DEL mystream
