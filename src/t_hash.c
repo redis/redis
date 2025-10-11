c745b9c7f43651165eeecbd43039514bc0e23122
debing.sun <debing.sun@redis.com>
2025-10-11 14:43:42 +0800

    Update src/t_hash.c

--- src/t_hash.c
+++ src/t_hash.c
@@ -1266,9 +1266,9 @@ int hashTypeDelete(robj *o, void *field, int isSdsField) {
  */
 unsigned long hashTypeLength(const robj *o, int subtractExpiredFields) {
     unsigned long length = ULONG_MAX;
-    if (server.allow_access_expired) {
+    /* If expired field access is allowed, don't subtract expired fields from the count. */
+    if (server.allow_access_expired)
         subtractExpiredFields = 0;
-    }
 
     if (o->encoding == OBJ_ENCODING_LISTPACK) {
         length = lpLength(o->ptr) / 2;
