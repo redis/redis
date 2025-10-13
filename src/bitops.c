4b06215a89c9657315f3aff7fc78934284bad616
Filipe Oliveira (Redis) <filipe@redis.com>
2025-10-13 10:33:09 +0100

    Update src/bitops.c
    Co-authored-by: debing.sun <debing.sun@redis.com>

--- src/bitops.c
+++ src/bitops.c
@@ -57,6 +57,7 @@ static const uint8_t bitsinbyte[256] = {
     #undef B4
     #undef B2
 };
+
 /* Count number of bits set in the binary array pointed by 's' and long
  * 'count' bytes. The implementation of this function is required to
  * work with an input string length up to 512 MB or more (server.proto_max_bulk_len) */
