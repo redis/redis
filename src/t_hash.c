8f4dca87cf476b11a567937a2c4991140dc053e6
Stav Levi <stav.levi@redis.com>
2025-11-23 15:01:29 +0200

    fix indentation

--- src/t_hash.c
+++ src/t_hash.c
@@ -2277,31 +2277,30 @@ static int parseHashFieldExpireArgs(client *c, int *flags,
                 return C_ERR;
 
             *expire_time_pos = i;
-            } else if (!strcasecmp(c->argv[i]->ptr, "PERSIST")) {
-                if (*flags & (HFE_EX | HFE_EXAT | HFE_PX | HFE_PXAT | HFE_PERSIST))
-                    goto err_expiration;
-                *flags |= HFE_PERSIST;
-            } else if (!strcasecmp(c->argv[i]->ptr, "KEEPTTL")) {
-                if (*flags & (HFE_EX | HFE_EXAT | HFE_PX | HFE_PXAT | HFE_KEEPTTL))
-                    goto err_expiration;
-                *flags |= HFE_KEEPTTL;
-            } else if (!strcasecmp(c->argv[i]->ptr, "FXX")) {
-                if (*flags & (HFE_FXX | HFE_FNX))
-                    goto err_condition;
-                *flags |= HFE_FXX;
-            } else if (!strcasecmp(c->argv[i]->ptr, "FNX")) {
-                if (*flags & (HFE_FXX | HFE_FNX))
-                    goto err_condition;
-                *flags |= HFE_FNX;
-            } else {
-                addReplyErrorFormat(c, "unknown argument: %s", (char*) c->argv[i]->ptr);
-                return C_ERR;
-            }
+        } else if (!strcasecmp(c->argv[i]->ptr, "PERSIST")) {
+            if (*flags & (HFE_EX | HFE_EXAT | HFE_PX | HFE_PXAT | HFE_PERSIST))
+                goto err_expiration;
+            *flags |= HFE_PERSIST;
+        } else if (!strcasecmp(c->argv[i]->ptr, "KEEPTTL")) {
+            if (*flags & (HFE_EX | HFE_EXAT | HFE_PX | HFE_PXAT | HFE_KEEPTTL))
+                goto err_expiration;
+            *flags |= HFE_KEEPTTL;
+        } else if (!strcasecmp(c->argv[i]->ptr, "FXX")) {
+            if (*flags & (HFE_FXX | HFE_FNX))
+                goto err_condition;
+            *flags |= HFE_FXX;
+        } else if (!strcasecmp(c->argv[i]->ptr, "FNX")) {
+            if (*flags & (HFE_FXX | HFE_FNX))
+                goto err_condition;
+            *flags |= HFE_FNX;
+        } else {
+            addReplyErrorFormat(c, "unknown argument: %s", (char*) c->argv[i]->ptr);
+            return C_ERR;
+        }
 
         /* Validate command-specific argument compatibility */
         if ((command_type == HASH_CMD_HGETEX && (*flags & (HFE_KEEPTTL | HFE_FXX | HFE_FNX))) ||
-            (command_type == HASH_CMD_HSETEX && (*flags & HFE_PERSIST)))
-        {
+            (command_type == HASH_CMD_HSETEX && (*flags & HFE_PERSIST))) {
             addReplyErrorFormat(c, "unknown argument: %s", (char*) c->argv[i]->ptr);
             return C_ERR;
         }
@@ -2810,7 +2809,6 @@ void hgetexCommand(client *c) {
         expired += (res == GETF_EXPIRED);
 
         /* Set expiration only if the field exists and not expired lazily. */
-
         if (res == GETF_OK && parse_flags) {
             if (parse_flags & HFE_PERSIST)
                 expire_time = EB_EXPIRE_TIME_INVALID;
