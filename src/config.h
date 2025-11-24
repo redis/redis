f8e35209010e3a68fd9f6a67c1d5ed57e6c2ad26
Andy Pan <i@andypan.me>
2025-11-24 14:11:55 +0800

    Minimize the code changes

--- src/config.h
+++ src/config.h
@@ -87,11 +87,10 @@
 #endif
 
 /* Test for accept4() */
-#if defined(__linux__) || \
+#if defined(__linux__) || defined(OpenBSD5_7) || \
     defined(__FreeBSD__) || \
-    defined(OpenBSD5_7) || \
-    (defined(__DragonFly__) && __DragonFly_version >= 400305) || \
-    (defined(__NetBSD__) && (defined(NetBSD8_0) || __NetBSD_Version__ >= 800000000))
+    (defined(__NetBSD__) && (defined(NetBSD8_0) || __NetBSD_Version__ >= 800000000)) || \
+    (defined(__DragonFly__) && __DragonFly_version >= 400305)
 #define HAVE_ACCEPT4 1
 #endif
 
