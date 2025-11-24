587945b37d25cac7af9ecc94975c61cec0b5c06f
Andy Pan <panjf2000@gmail.com>
2025-11-24 14:07:14 +0800

    Update src/config.h
    Co-authored-by: debing.sun <debing.sun@redis.com>

--- src/config.h
+++ src/config.h
@@ -93,7 +93,8 @@
 
 /* Detect for kqueue */
 #if (defined(__APPLE__) && defined(MAC_OS_10_6_DETECTED)) || \
-    defined(__DragonFly__) || \
+#if (defined(__APPLE__) && defined(MAC_OS_10_6_DETECTED)) || defined(__FreeBSD__) || \
+    defined(__OpenBSD__) || defined (__NetBSD__) || defined(__DragonFly__)
     defined(__FreeBSD__) || \
     defined(__OpenBSD__) || \
     defined (__NetBSD__)
