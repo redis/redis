c798efe65aa3d609255e04d03c98b6de0aba7a7c
johnwonder <johnufida@163.com>
2025-11-27 11:30:10 +0800

    lru resolution should be seconds

--- src/object.c
+++ src/object.c
@@ -100,7 +100,7 @@ robj *createObject(int type, void *ptr) {
 void initObjectLRUOrLFU(robj *o) {
     if (o->refcount == OBJ_SHARED_REFCOUNT)
         return;
-    /* Set the LRU to the current lruclock (minutes resolution), or
+    /* Set the LRU to the current lruclock (seconds resolution), or
      * alternatively the LFU counter. */
     if (server.maxmemory_policy & MAXMEMORY_FLAG_LFU) {
         o->lru = (LFUGetTimeInMinutes() << 8) | LFU_INIT_VAL;
