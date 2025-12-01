6da523fa56ee025e60d108727fdfa0219b61941d
Yuan Wang <yuan.wang@redis.com>
2025-12-01 17:13:37 +0800

    Introduce flushdb option for repl-diskless-load

--- tests/integration/replication.tcl
+++ tests/integration/replication.tcl
@@ -321,7 +321,7 @@ start_server {tags {"repl external:skip"}} {
 }
 
 foreach mdl {no yes} rdbchannel {no yes} {
-    foreach sdl {disabled swapdb} {
+    foreach sdl {disabled swapdb flushdb} {
         start_server {tags {"repl external:skip debug_defrag:skip"} overrides {save {}}} {
             set master [srv 0 client]
             $master config set repl-diskless-sync $mdl
