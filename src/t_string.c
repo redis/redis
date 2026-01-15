50a07bcdb0692dbeaa28a1b9535fc6fea48fb3f0
fcostaoliveira <filipe@redis.com>
2026-01-15 13:59:06 +0000

    Ensuring that within setGenericCommand the command arg optimization is only used for SET command.

--- src/t_string.c
+++ src/t_string.c
@@ -171,7 +171,7 @@ void setGenericCommand(client *c, int flags, robj *key, robj **valref, robj *exp
             /* If command is exactly "SET key value EX/PX/EXAT ttl", we can just
              * replace the expire type and value in-place. Otherwise, we need to
              * rewrite the entire command to strip extra flags (NX, XX, GET, etc). */
-            if (c->argc == 5) {
+            if ((c->cmd->proc == setCommand) && c->argc == 5) {
                 rewriteClientCommandArgument(c, 3, shared.pxat);
                 rewriteClientCommandArgument(c, 4, milliseconds_obj);
             } else {
