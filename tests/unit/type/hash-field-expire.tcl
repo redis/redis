4349dddd2c77639459a5d4202fd8d42588566f66
gentcys <10412450+gentcys@users.noreply.github.com>
2026-03-14 20:17:14 +0800

    update test

--- tests/unit/type/hash-field-expire.tcl
+++ tests/unit/type/hash-field-expire.tcl
@@ -1142,8 +1142,8 @@ start_server {tags {"external:skip needs:debug"}} {
             assert_error {*unknown argument*} {r hsetex myhash nx fields 1 a b}
             assert_error {*unknown argument*} {r hsetex myhash 1 fields 1 a b}
             assert_error {*wrong number of arguments*} {r hsetex myhash fields 1 a}
-            assert_error {*unknown argument*} {r hsetex myhash persist fields 1 a b c}
-            assert_error {*unknown argument*} {r hsetex myhash ex 100 persist fields 1 a b c}
+            assert_error {*unknown argument*} {r hsetex myhash persist fields 1 a b}
+            assert_error {*unknown argument*} {r hsetex myhash ex 100 persist fields 1 a b}
 
             # Only one of FNX or FXX
             assert_error {*Only one of FXX or FNX arguments *} {r hsetex myhash fxx fxx EX 100 fields 1 a b}
