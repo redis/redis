f209c4a7f948306ec40731e46de92d3beaa63d5d
debing.sun <debing.sun@redis.com>
2026-03-18 20:21:04 +0800

    Move fast_float_strtod.* to src dir

---
+++ src/fast_float_strtod.h
@@ -0,0 +1,9 @@
+
+#ifndef __FAST_FLOAT_STRTOD_H__
+#define __FAST_FLOAT_STRTOD_H__
+
+#include <stddef.h>
+
+double fast_float_strtod(const char *in, size_t len,char **out);
+
+#endif /* __FAST_FLOAT_STRTOD_H__ */
