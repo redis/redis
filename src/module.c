a2d1c807371a1914c33b7f954cb39c3e4e1e9555
Zijie Zhao <zzjas98@gmail.com>
2026-03-18 20:30:56 -0500

    Fix ACL OOB for wrong-arity KEYNUM commands (#14847)
    `luaRedisAclCheckCmdPermissionsCommand` and
    `RM_ACLCheckCommandPermissions` now call `commandCheckArity()` to check
    command arity before calling `ACLCheckAllUserCommandPerm`, matching the
    behavior of `processCommand`, `scriptCall`, and `RM_Call`. Without this,
    KEYNUM keyspec commands like EVAL with wrong arity cause out-of-bounds
    argv access during key extraction.
    
    Also fix KEYNUM index calculation (`first + keynumidx`) and add a bounds
    check in genericGetKeys().
    
    Add scripting and module ACL tests for wrong-arity `EVAL` to lock in the
    non-crashing behavior.
    
    Fixes #14843
    
    (cherry picked from commit c4d74587b55383354f53a5c61bd313ccf7867f4b)

--- src/module.c
+++ src/module.c
@@ -6321,7 +6321,7 @@ RedisModuleCallReply *RM_Call(RedisModuleCtx *ctx, const char *cmdname, const ch
             reply = callReplyCreateError(err, ctx);
         goto cleanup;
     }
-    if (!commandCheckArity(c, error_as_call_replies? &err : NULL)) {
+    if (!commandCheckArity(c->cmd, c->argc, error_as_call_replies? &err : NULL)) {
         errno = EINVAL;
         if (error_as_call_replies)
             reply = callReplyCreateError(err, ctx);
@@ -9640,6 +9640,7 @@ RedisModuleUser *RM_GetModuleUserFromUserName(RedisModuleString *name) {
  * REDISMODULE_ERR is returned and errno is set to the following values:
  *
  * * ENOENT: Specified command does not exist.
+ * * EINVAL: Invalid number of arguments for the specified command.
  * * EACCES: Command cannot be executed, according to ACL rules
  */
 int RM_ACLCheckCommandPermissions(RedisModuleUser *user, RedisModuleString **argv, int argc) {
@@ -9649,6 +9650,11 @@ int RM_ACLCheckCommandPermissions(RedisModuleUser *user, RedisModuleString **arg
     /* Find command */
     if ((cmd = lookupCommand(argv, argc)) == NULL) {
         errno = ENOENT;
+        return REDISMODULE_ERR;
+    }
+
+    if (!commandCheckArity(cmd, argc, NULL)) {
+        errno = EINVAL;
         return REDISMODULE_ERR;
     }
 
