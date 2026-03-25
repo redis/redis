053fb82be94648e38bfb868565ce454edd8356b9
debing.sun <debing.sun@redis.com>
2026-03-25 14:35:53 +0800

    Add comment

--- src/sds.c
+++ src/sds.c
@@ -1508,6 +1508,7 @@ int sdsTest(int argc, char **argv, int flags) {
         test_cond("sdsresize() expand len", sdslen(x) == 40);
         test_cond("sdsresize() expand strlen", strlen(x) == 40);
 #if defined(USE_JEMALLOC)
+        /* 224 - hdrlen(3) - 1(\0) */
         test_cond("sdsresize() expand alloc", sdsalloc(x) == 220);
 #endif
         /* Test sdsresize - trim free space */
@@ -1515,6 +1516,7 @@ int sdsTest(int argc, char **argv, int flags) {
         test_cond("sdsresize() shrink len", sdslen(x) == 40);
         test_cond("sdsresize() shrink strlen", strlen(x) == 40);
 #if defined(USE_JEMALLOC)
+        /* 96 - hdrlen(3) - 1(\0) */
         test_cond("sdsresize() shrink alloc", sdsalloc(x) == 92);
 #endif
         /* Test sdsresize - crop used space */
@@ -1522,6 +1524,7 @@ int sdsTest(int argc, char **argv, int flags) {
         test_cond("sdsresize() crop len", sdslen(x) == 30);
         test_cond("sdsresize() crop strlen", strlen(x) == 30);
 #if defined(USE_JEMALLOC)
+        /* 40 - hdrlen(3) - 1(\0) */
         test_cond("sdsresize() crop alloc", sdsalloc(x) == 36);
 #endif
         /* Test sdsresize - extend to different class */
@@ -1529,6 +1532,7 @@ int sdsTest(int argc, char **argv, int flags) {
         test_cond("sdsresize() expand len", sdslen(x) == 30);
         test_cond("sdsresize() expand strlen", strlen(x) == 30);
 #if defined(USE_JEMALLOC)
+        /* 448 - hdrlen(5) - 1(\0) */
         test_cond("sdsresize() expand alloc", sdsalloc(x) == 442);
 #endif
         /* Test sdsresize - shrink to different class */
@@ -1536,6 +1540,7 @@ int sdsTest(int argc, char **argv, int flags) {
         test_cond("sdsresize() crop len", sdslen(x) == 4);
         test_cond("sdsresize() crop strlen", strlen(x) == 4);
 #if defined(USE_JEMALLOC)
+        /* 8 - hdrlen(3) - 1(\0) */
         test_cond("sdsresize() crop alloc", sdsalloc(x) == 4);
 #endif
         sdsfree(x);
