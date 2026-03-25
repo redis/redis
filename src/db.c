366b6f4ac91da598c595133ddef079fd88b03995
Vitah Lin <vitahlin@gmail.com>
2026-03-25 18:10:55 +0800

    Update comment src/db.c
    Co-authored-by: Ozan Tezcan <ozantezcan@gmail.com>

--- src/db.c
+++ src/db.c
@@ -232,7 +232,9 @@ void dbgRunAssertions(redisDb *db) {
     /* Don't assert during RDB loading. Database may be in inconsistent state. */
     if (server.loading || server.async_loading) return;
 
-    /* Don't assert during ASM import or background trim. Data may be in intermediate state. */
+    /* Don't assert during ASM background trim or import.
+     * - During background trim, histogram delta hasn't been applied yet.
+     * - During import, assertions can introduce slowdown and cause ASM tests to fail. */
     if (asmIsBgTrimRunning() || asmImportInProgress()) return;
 
     if (server.dbg_assert_flags & DBG_ASSERT_KEYSIZES)
