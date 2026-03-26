7c3d0b90a3996e2a426a67e661bc163726915ab3
Mincho Paskalev <minchopaskal@gmail.com>
2026-03-26 12:08:00 +0200

    remove unused client flag

--- src/server.h
+++ src/server.h
@@ -471,7 +471,6 @@ extern int configOOMScoreAdjValuesDefaults[CONFIG_OOM_COUNT];
 #define CLIENT_IO_CLOSE_ASAP (1ULL<<4) /* Close this client ASAP in IO thread. */
 #define CLIENT_IO_PENDING_CRON (1ULL<<5)  /* The client is pending cron job, to be processed in main thread. */
 #define CLIENT_IO_COMPRESSION_ENABLED (1ULL<<6)  /* The client compression is enabled for this client*/
-#define CLIENT_IO_READ_DECOMPRESSED_CRON (1ULL<<7)  /* The client is reading only decompressed data inside its querybuf */
 
 /* Definitions for client read errors. These error codes are used to indicate
  * various issues that can occur while reading or parsing data from a client. */
