1955b7353336e4a84ca53519abd6021dca52611e
Sergey Georgiev <s_ggeorgiev@yahoo.com>
2026-04-03 09:38:20 +0300

    fixed: issue from review

--- tests/unit/type/stream-cgroups.tcl
+++ tests/unit/type/stream-cgroups.tcl
@@ -1905,53 +1905,55 @@ start_server {
         }
     }
 
-    start_server {tags {"repl external:skip"}} {
+    start_server {tags {"repl external:skip" "stream"}} {
+        # Verify that XREADGROUP (without NOACK) propagates the newly
+        # created consumer to the replica even when there are no messages
+        # to deliver. The command should return an empty reply but still
+        # replicate the consumer creation so the replica stays in sync.
         test "XREADGROUP without NOACK creates consumer on replica even when no messages exist" {
+            set master [srv 0 client]
+            set master_host [srv 0 host]
+            set master_port [srv 0 port]
+
             start_server {tags {"stream"}} {
-                set master [srv 0 client]
-                set master_host [srv 0 host]
-                set master_port [srv 0 port]
+                set replica [srv 0 client]
 
-                start_server {tags {"stream"}} {
-                    set replica [srv 0 client]
+                $replica replicaof $master_host $master_port
+                wait_for_sync $replica
 
-                    $replica replicaof $master_host $master_port
-                    wait_for_sync $replica
+                $master DEL mystream
+                $master XADD mystream 1-0 f v
+                $master XGROUP CREATE mystream grp 0
 
-                    $master DEL mystream
-                    $master XADD mystream 1-0 f v
-                    $master XGROUP CREATE mystream grp 0
+                wait_for_ofs_sync $master $replica
 
-                    wait_for_ofs_sync $master $replica
+                # Consume the only message so the stream has no
+                # new messages pending for delivery.
+                $master XREADGROUP GROUP grp c1 STREAMS mystream >
+                $master XACK mystream grp 1-0
 
-                    # Consume the only message so the stream has no
-                    # new messages pending for delivery.
-                    $master XREADGROUP GROUP grp setupconsumer STREAMS mystream >
-                    $master XACK mystream grp 1-0
+                wait_for_ofs_sync $master $replica
 
-                    wait_for_ofs_sync $master $replica
+                # Now issue XREADGROUP (without NOACK) for a brand-new
+                # consumer when there are NO messages to deliver.
+                # This must still propagate the consumer to the replica.
+                set reply [$master XREADGROUP GROUP grp c2 STREAMS mystream >]
 
-                    # Now issue XREADGROUP (without NOACK) for a brand-new
-                    # consumer when there are NO messages to deliver.
-                    # This must still propagate the consumer to the replica.
-                    set reply [$master XREADGROUP GROUP grp newconsumer STREAMS mystream >]
+                # The reply should be empty (no new messages).
+                assert_equal $reply {}
 
-                    # The reply should be empty (no new messages).
-                    assert_equal $reply {}
+                # Verify the consumer exists on master.
+                set master_consumers [$master XINFO CONSUMERS mystream grp]
+                set master_names [lmap c $master_consumers {dict get $c name}]
+                assert {[lsearch $master_names "c2"] >= 0}
 
-                    # Verify the consumer exists on master.
-                    set master_consumers [$master XINFO CONSUMERS mystream grp]
-                    set master_names [lmap c $master_consumers {dict get $c name}]
-                    assert {[lsearch $master_names "newconsumer"] >= 0}
+                wait_for_ofs_sync $master $replica
 
-                    wait_for_ofs_sync $master $replica
-
-                    # The consumer must also exist on the replica.
-                    set replica_consumers [$replica XINFO CONSUMERS mystream grp]
-                    set replica_names [lmap c $replica_consumers {dict get $c name}]
-                    if {[lsearch $replica_names "newconsumer"] < 0} {
-                        fail "Consumer 'newconsumer' not found on replica (have: $replica_names)"
-                    }
+                # The consumer must also exist on the replica.
+                set replica_consumers [$replica XINFO CONSUMERS mystream grp]
+                set replica_names [lmap c $replica_consumers {dict get $c name}]
+                if {[lsearch $replica_names "c2"] < 0} {
+                    fail "Consumer 'c2' not found on replica (have: $replica_names)"
                 }
             }
         }
