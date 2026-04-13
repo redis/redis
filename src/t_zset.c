86e8ce64fa3df56e330579b98214b1c4573345fd
debing.sun <debing.sun@redis.com>
2026-04-13 09:32:57 +0800

    Revert "Fix the issue that 2.2250738585072012e-308 can't be parsed" to avoid break change
    This reverts commit d6c93bd2119d44e572a55c1ad06e83ff59627ee7.

--- src/t_zset.c
+++ src/t_zset.c
@@ -711,6 +711,7 @@ zskiplistNode *zslGetElementByRank(zskiplist *zsl, unsigned long rank) {
 
 /* Populate the rangespec according to the objects min and max. */
 static int zslParseRange(robj *min, robj *max, zrangespec *spec) {
+    char *eptr;
     spec->minex = spec->maxex = 0;
 
     /* Parse the min-max interval. If one of the values is prefixed
@@ -722,12 +723,12 @@ static int zslParseRange(robj *min, robj *max, zrangespec *spec) {
     } else {
         size_t len = sdslen(min->ptr);
         if (((char*)min->ptr)[0] == '(') {
-            if (!string2d((char*)min->ptr+1,len-1,&spec->min))
-                return C_ERR;
+            spec->min = fast_float_strtod((char*)min->ptr+1,len-1,&eptr);
+            if (eptr[0] != '\0' || isnan(spec->min)) return C_ERR;
             spec->minex = 1;
         } else {
-            if (!string2d((char*)min->ptr,len,&spec->min))
-                return C_ERR;
+            spec->min = fast_float_strtod((char*)min->ptr,len,&eptr);
+            if (eptr[0] != '\0' || isnan(spec->min)) return C_ERR;
         }
     }
     if (max->encoding == OBJ_ENCODING_INT) {
@@ -735,12 +736,12 @@ static int zslParseRange(robj *min, robj *max, zrangespec *spec) {
     } else {
         size_t len = sdslen(max->ptr);
         if (((char*)max->ptr)[0] == '(') {
-            if (!string2d((char*)max->ptr+1,len-1,&spec->max))
-                return C_ERR;
+            spec->max = fast_float_strtod((char*)max->ptr+1,len-1,&eptr);
+            if (eptr[0] != '\0' || isnan(spec->max)) return C_ERR;
             spec->maxex = 1;
         } else {
-             if (!string2d((char*)max->ptr,len,&spec->max))
-                return C_ERR;
+            spec->max = fast_float_strtod((char*)max->ptr,len,&eptr);
+            if (eptr[0] != '\0' || isnan(spec->max)) return C_ERR;
         }
     }
 
