0efe328dc71d4e76439ee7625dd4dffc31931324
Stav Nachmias <stav.nachmias@redis.com>
2026-04-19 18:32:04 +0300

    script update

--- utils/releasetools/02_upload_tarball.sh
+++ utils/releasetools/02_upload_tarball.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-if [ $# != "1" ]
+if [ $# != "2" ]
 then
-    echo "Usage: ./utils/releasetools/02_upload_tarball.sh <version_tag>"
+    echo "Usage: ./utils/releasetools/02_upload_tarball.sh <version_tag> <--stable|--unstable>"
+    exit 1
+fi
+
+VERSION_TAG=$1
+VERSION_TYPE=$2
+
+# Validate the stability flag
+if [ "$VERSION_TYPE" != "--stable" ] && [ "$VERSION_TYPE" != "--unstable" ]
+then
+    echo "Error: Second parameter must be either --stable or --unstable"
+    echo "Usage: ./utils/releasetools/02_upload_tarball.sh <version_tag> <--stable|--unstable>"
     exit 1
 fi
 
 echo "Uploading..."
-scp /tmp/redis-${1}.tar.gz ubuntu@host.redis.io:/var/www/download/releases/
-echo "Updating web site... "
-echo "Please check the github action tests for the release."
-echo "Press any key if it is a stable release, or Ctrl+C to abort"
-read x
-ssh ubuntu@host.redis.io "cd /var/www/download;
-                          rm -rf redis-${1}.tar.gz;
-                          wget http://download.redis.io/releases/redis-${1}.tar.gz;
-                          tar xvzf redis-${1}.tar.gz;
-                          rm -rf redis-stable;
-                          mv redis-${1} redis-stable;
-                          tar cvzf redis-stable.tar.gz redis-stable;
-                          rm -rf redis-${1}.tar.gz;
-                          shasum -a 256 redis-stable.tar.gz > redis-stable.tar.gz.SHA256SUM;
-                          "
+scp /tmp/redis-${VERSION_TAG}.tar.gz ubuntu@host.redis.io:/var/www/download/releases/
+
+# Only update the stable version if --stable flag is provided
+if [ "$VERSION_TYPE" == "--stable" ]
+then
+    echo "Updating web site... "
+    echo "Please check the github action tests for the release."
+    echo "Press any key if it is a stable release, or Ctrl+C to abort"
+    read x
+    ssh ubuntu@host.redis.io "cd /var/www/download;
+                              rm -rf redis-${VERSION_TAG}.tar.gz;
+                              wget http://download.redis.io/releases/redis-${VERSION_TAG}.tar.gz;
+                              tar xvzf redis-${VERSION_TAG}.tar.gz;
+                              rm -rf redis-stable;
+                              mv redis-${VERSION_TAG} redis-stable;
+                              tar cvzf redis-stable.tar.gz redis-stable;
+                              rm -rf redis-${VERSION_TAG}.tar.gz;
+                              shasum -a 256 redis-stable.tar.gz > redis-stable.tar.gz.SHA256SUM;
+                              "
+else
+    echo "Unstable release - skipping stable version update"
+fi
