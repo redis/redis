77275e4f25b8ca3b90a9aeb88c1a5fbd7dab93d7
Shubham Taple <subbutaple@gmail.com>
2026-05-01 09:20:26 +0530

    Fix listpack backlen tier boundaries - use inclusive limits

--- src/listpack.c
+++ src/listpack.c
@@ -337,20 +337,20 @@ static inline unsigned long lpEncodeBacklen(unsigned char *buf, uint64_t l) {
     if (l <= 127) {
         if (buf) buf[0] = l;
         return 1;
-    } else if (l < 16383) {
+    } else if (l <= 16383) {
         if (buf) {
             buf[0] = l>>7;
             buf[1] = (l&127)|128;
         }
         return 2;
-    } else if (l < 2097151) {
+    } else if (l <= 2097151) {
         if (buf) {
             buf[0] = l>>14;
             buf[1] = ((l>>7)&127)|128;
             buf[2] = (l&127)|128;
         }
         return 3;
-    } else if (l < 268435455) {
+    } else if (l <= 268435455) {
         if (buf) {
             buf[0] = l>>21;
             buf[1] = ((l>>14)&127)|128;
@@ -376,11 +376,11 @@ static inline unsigned long lpEncodeBacklen(unsigned char *buf, uint64_t l) {
 static inline unsigned long lpEncodeBacklenBytes(uint64_t l) {
     if (l <= 127) {
         return 1;
-    } else if (l < 16383) {
+    } else if (l <= 16383) {
         return 2;
-    } else if (l < 2097151) {
+    } else if (l <= 2097151) {
         return 3;
-    } else if (l < 268435455) {
+    } else if (l <= 268435455) {
         return 4;
     } else {
         return 5;
