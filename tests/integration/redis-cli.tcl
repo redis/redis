7a964512cccbf75e95ba7b4a13ba5f26bd049a90
Vitah Lin <vitahlin@gmail.com>
2026-05-20 22:21:49 +0800

    fx

--- tests/integration/redis-cli.tcl
+++ tests/integration/redis-cli.tcl
@@ -42,7 +42,7 @@ start_server {tags {"cli"}} {
 
         # We may have a short read, try to read some more.
         set empty_reads 0
-        while {$empty_reads < 20} {
+        while {$empty_reads < 100} {
             set buf [read $fd]
             if {[string length $buf] == 0} {
                 after 10
@@ -184,15 +184,14 @@ start_server {tags {"cli"}} {
     }
 
     test_interactive_cli_with_prompt "should be ok if there is no result" {
-        puts -nonewline $fd "\x12" ;# CTRL+R
+        puts $fd "\x12" ;# CTRL+R
 
         set now [clock seconds]
-        puts -nonewline $fd "\x12" ;# CTRL+R
+        puts $fd "\x12" ;# CTRL+R
         set result [read_cli $fd]
         assert_equal 1 [regexp {\(reverse-i-search\):} $result]
 
-        puts -nonewline $fd "keys \"$now\"\x0D"
-        set result2 [format_output [read_cli $fd]]
+        set result2 [run_command $fd "keys \"$now\"\x0D"]
         assert_equal 1 [regexp {.*(empty array).*} $result2]
     }
 
