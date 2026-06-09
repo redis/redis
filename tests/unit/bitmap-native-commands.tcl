e9590e4f47daa183598087adcdf7c3bf75b41d55
Antonio Viggiano <agfviggiano@gmail.com>
2026-06-09 09:16:13 -0300

    Expand native bitmap command coverage

--- tests/unit/bitmap-native-commands.tcl
+++ tests/unit/bitmap-native-commands.tcl
@@ -1,3 +1,47 @@
+proc seed_string_bitmap {key bits} {
+    r del $key
+    r set $key ""
+    foreach bit $bits {
+        r setbit $key $bit 1
+    }
+}
+
+proc seed_native_bitmap {key bits} {
+    seed_string_bitmap $key $bits
+    r debug bitmap-force-roaring $key
+}
+
+proc bitmap_raw_or_empty {key} {
+    if {[r exists $key]} {
+        return [r get $key]
+    }
+    return ""
+}
+
+proc assert_native_bitop_matches_string {name op source_bitsets} {
+    set string_dest "bitmap:native:bitop:$name:string:dest"
+    set native_dest "bitmap:native:bitop:$name:native:dest"
+    set string_sources {}
+    set native_sources {}
+
+    for {set i 0} {$i < [llength $source_bitsets]} {incr i} {
+        set string_key "bitmap:native:bitop:$name:string:src:$i"
+        set native_key "bitmap:native:bitop:$name:native:src:$i"
+        seed_string_bitmap $string_key [lindex $source_bitsets $i]
+        seed_native_bitmap $native_key [lindex $source_bitsets $i]
+        lappend string_sources $string_key
+        lappend native_sources $native_key
+    }
+
+    set string_reply [r bitop $op $string_dest {*}$string_sources]
+    set native_reply [r bitop $op $native_dest {*}$native_sources]
+    assert_equal $string_reply $native_reply
+    assert_equal [bitmap_raw_or_empty $string_dest] [bitmap_raw_or_empty $native_dest]
+    if {[r exists $native_dest]} {
+        assert_equal string [r type $native_dest]
+    }
+}
+
 start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
     test {native bitmap read commands preserve type encoding and bytes} {
         set raw [binary format H* 80400100080000]
@@ -20,6 +64,21 @@ start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
         assert_equal $raw [r debug bitmap-raw bitmap:native:read]
     }
 
+    test {SETBIT and GETBIT round trip native bitmap offsets} {
+        seed_native_bitmap bitmap:native:setbit:loop {}
+
+        for {set offset 0} {$offset < 100} {incr offset} {
+            assert_equal 0 [r setbit bitmap:native:setbit:loop $offset 1]
+            assert_equal 1 [r getbit bitmap:native:setbit:loop $offset]
+            assert_equal 1 [r setbit bitmap:native:setbit:loop $offset 0]
+            assert_equal 0 [r getbit bitmap:native:setbit:loop $offset]
+        }
+
+        assert_equal bitmap [r type bitmap:native:setbit:loop]
+        assert_equal bitmap-roaring [r object encoding bitmap:native:setbit:loop]
+        assert_equal 0 [r bitcount bitmap:native:setbit:loop]
+    }
+
     test {SETBIT updates native bitmap values and preserves trailing zero length} {
         r set bitmap:native:setbit [binary format H* 8000]
         r debug bitmap-force-roaring bitmap:native:setbit
@@ -38,6 +97,26 @@ start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
         assert_equal [binary format H* 004000] [r debug bitmap-raw bitmap:native:setbit]
     }
 
+    test {native bitmap BITCOUNT and BITPOS cover redis-roaring integration cases} {
+        seed_native_bitmap bitmap:native:countpos:fib {1 2 3 5 8 13}
+        assert_equal 6 [r bitcount bitmap:native:countpos:fib]
+        assert_equal 1 [r bitpos bitmap:native:countpos:fib 1]
+        assert_equal 0 [r bitpos bitmap:native:countpos:fib 0]
+
+        seed_native_bitmap bitmap:native:countpos:first-one {3 4 6 10 12}
+        assert_equal 3 [r bitpos bitmap:native:countpos:first-one 1]
+
+        seed_native_bitmap bitmap:native:countpos:first-zero {0 1 2 3 4 6}
+        assert_equal 5 [r bitpos bitmap:native:countpos:first-zero 0]
+
+        seed_native_bitmap bitmap:native:countpos:empty {}
+        assert_equal -1 [r bitpos bitmap:native:countpos:empty 0]
+        assert_equal -1 [r bitpos bitmap:native:countpos:empty 1]
+
+        seed_native_bitmap bitmap:native:countpos:single-zero {0}
+        assert_equal 1 [r bitpos bitmap:native:countpos:single-zero 0]
+    }
+
     test {BITFIELD writes native bitmap values through materialization fallback} {
         r set bitmap:native:bitfield [binary format H* 00]
         r debug bitmap-force-roaring bitmap:native:bitfield
@@ -51,6 +130,15 @@ start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
         assert_equal {0} [r bitfield bitmap:native:bitfield SET u1 23 0]
         assert_equal bitmap [r type bitmap:native:bitfield]
         assert_equal [binary format H* 0f0000] [r debug bitmap-raw bitmap:native:bitfield]
+    }
+
+    test {BITFIELD signed INCRBY preserves native bitmap values} {
+        seed_native_bitmap bitmap:native:bitfield:signed {}
+
+        assert_equal {0 1 1} [r bitfield bitmap:native:bitfield:signed SET i5 0 -1 INCRBY i5 0 2 GET i5 0]
+        assert_equal bitmap [r type bitmap:native:bitfield:signed]
+        assert_equal bitmap-roaring [r object encoding bitmap:native:bitfield:signed]
+        assert_equal {1} [r bitfield_ro bitmap:native:bitfield:signed GET i5 0]
     }
 
     test {BITOP accepts native bitmap sources and stores string destinations} {
@@ -67,5 +155,37 @@ start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
         assert_equal 2 [r bitop not bitmap:native:bitop:not bitmap:native:bitop:a]
         assert_equal string [r type bitmap:native:bitop:not]
         assert_equal [binary format H* 0fff] [r get bitmap:native:bitop:not]
+    }
+
+    test {BITOP native bitmap sources match string bitmap results for all operations} {
+        set a {0 4 5 6 20}
+        set b {1 5 6 21}
+        set c {2 3 5 6 7 20}
+
+        assert_native_bitop_matches_string and AND [list $a $b $c]
+        assert_native_bitop_matches_string or OR [list $a $b $c]
+        assert_native_bitop_matches_string xor XOR [list $a $b $c]
+        assert_native_bitop_matches_string diff DIFF [list $a $b $c]
+        assert_native_bitop_matches_string diff1 DIFF1 [list $a $b $c]
+        assert_native_bitop_matches_string andor ANDOR [list $a $b $c]
+        assert_native_bitop_matches_string one ONE [list $a $b $c]
+        assert_native_bitop_matches_string not NOT [list $a]
+    }
+
+    test {BITOP handles native bitmap empty sources and destination aliasing} {
+        seed_native_bitmap bitmap:native:bitop:empty {}
+        assert_equal 0 [r bitop not bitmap:native:bitop:empty-not bitmap:native:bitop:empty]
+        assert_equal 0 [r exists bitmap:native:bitop:empty-not]
+
+        seed_string_bitmap bitmap:native:bitop:alias:string:dest {0 2 4 6}
+        seed_string_bitmap bitmap:native:bitop:alias:string:other {2 6 8}
+        seed_native_bitmap bitmap:native:bitop:alias:native:dest {0 2 4 6}
+        seed_native_bitmap bitmap:native:bitop:alias:native:other {2 6 8}
+
+        set string_reply [r bitop diff bitmap:native:bitop:alias:string:dest bitmap:native:bitop:alias:string:dest bitmap:native:bitop:alias:string:other]
+        set native_reply [r bitop diff bitmap:native:bitop:alias:native:dest bitmap:native:bitop:alias:native:dest bitmap:native:bitop:alias:native:other]
+        assert_equal $string_reply $native_reply
+        assert_equal [r get bitmap:native:bitop:alias:string:dest] [r get bitmap:native:bitop:alias:native:dest]
+        assert_equal string [r type bitmap:native:bitop:alias:native:dest]
     }
 }
