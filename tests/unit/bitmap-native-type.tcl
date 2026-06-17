79e3f22e9811fd9f37fc3cd5980f353456c6797c
Antonio Viggiano <agfviggiano@gmail.com>
2026-06-17 11:44:11 -0300

    Fix bitmap help type test wrapping

--- tests/unit/bitmap-native-type.tcl
+++ tests/unit/bitmap-native-type.tcl
@@ -15,7 +15,7 @@ start_server {tags {"bitmap" "bitmap-native" "needs:debug" "cluster:skip"}} {
         set help [join [r bitmap help] "\n"]
 
         assert_match {*NATIVE changes TYPE to bitmap*} $help
-        assert_match {*STRING changes TYPE back to string*} $help
+        assert_match {*STRING*changes TYPE back to string*} $help
     }
 
     test {bitmap-default-roaring no: SETBIT keeps creating strings} {
