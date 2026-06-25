0dbcd508c3309b7c23abc9e1726c2e0d43b8b3e0
Antonio Viggiano <agfviggiano@gmail.com>
2026-06-25 12:51:52 -0300

    Restore merged bitmap write offset error

--- src/bitops.c
+++ src/bitops.c
@@ -833,7 +833,7 @@ int getBitOffsetFromArgument(client *c, robj *o, uint64_t *offset, int hash, int
 static int bitmapWriteOffsetWithinLimit(client *c, uint64_t maxbit) {
     if (mustObeyClient(c) || (maxbit >> 3) < (uint64_t)server.proto_max_bulk_len)
         return 1;
-    addReplyError(c, "bit offset is not an integer or out of range");
+    addReplyError(c, "bit offset is out of range");
     return 0;
 }
 
