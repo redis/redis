1d852e688487474dbaeb12cc227106c493477297
hassan <hassan.azarbaijani@gmail.com>
2026-07-08 17:19:35 +0300

    kk

--- src/acl.c
+++ src/acl.c
@@ -8,7 +8,7 @@
  * Licensed under your choice of (a) the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
- */
+ */ 
 
 #include "server.h"
 #include "cluster.h"
