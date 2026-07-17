4d94dbcf4faf5b69d459c271d9fbed97a86ae327
debing.sun <debing.sun@redis.com>
2026-07-17 16:46:27 +0800

    Update aof.c

--- src/aof.c
+++ src/aof.c
@@ -2501,7 +2501,8 @@ int rewriteModuleObject(rio *r, robj *key, robj *o, int dbid) {
     if (mt->aof_rewrite == NULL) {
         serverLog(LL_WARNING,
             "Can't rewrite the append only file: the module data type '%s' "
-            "does not implement the aof_rewrite callback.",
+            "does not implement the aof_rewrite callback. Enable "
+            "aof-use-rdb-preamble to rewrite the AOF for this data type.",
             mt->entity.name);
         return 0;
     }
