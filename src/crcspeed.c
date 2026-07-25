1232912dff905df1dcdf8793b94d8c39f5a420af
Vitah Lin <vitahlin@gmail.com>
2026-07-25 22:26:36 +0800

    fx

--- src/crcspeed.c
+++ src/crcspeed.c
@@ -157,7 +157,7 @@ void crcspeed16big_init(crcfn16 fn, uint16_t big_table[8][256]) {
  * reasonable for Intel CPUs made since 2010. Please adjust as necessary if
  * or when your CPU has more load / execute units. We've written benchmark code
  * to help you tune your platform, see crc64Test. */
-#if defined(__i386__) || defined(__x86_64__)
+#if defined(__i386__) || defined(__X86_64__) || defined(__x86_64__)
 static size_t CRC64_TRI_CUTOFF = (2*1024);
 static size_t CRC64_DUAL_CUTOFF = (128);
 #else
