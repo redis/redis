e724a5399fbe1ca9fa4ad26066bd33fb8d7d0725
Ozan Tezcan <ozantezcan@gmail.com>
2026-07-29 10:00:03 +0300

    redo template defrag budget check

--- src/defrag.c
+++ src/defrag.c
@@ -1710,7 +1710,7 @@ static doneStatus defragStageHashTemplates(void *ctx, monotime endtime) {
 
         hashTemplateDefrag(tmpl);
 
-        if (++iterations > 64) {
+        if (++iterations > 8) {
             iterations = 0;
             if (getMonotonicUs() >= endtime) return DEFRAG_NOT_DONE;
         }
