cf5cea1a01f7a773f7f1c02b22374bbe0e0188e7
Vitah Lin <vitahlin@gmail.com>
2026-08-05 15:17:07 +0800

    fix

--- src/crccombine.c
+++ src/crccombine.c
@@ -38,7 +38,9 @@
   madler@alumni.caltech.edu
 */
 
-#define STATIC_ASSERT(VVV) do {int test = 1 / (VVV);test++;} while (0)
+#ifndef static_assert
+#define static_assert(expr, lit) extern char __static_assert_failure[(expr) ? 1 : -1]
+#endif
 
 #if !((defined(__i386__) || defined(__X86_64__) || defined(__x86_64__)))
 
@@ -115,6 +117,11 @@ uint64_t gf2_matrix_times_switch(uint64_t *mat, uint64_t vec) {
 
 #else
 
+static_assert(sizeof(uint64_t) == 8,
+              "CRC vector implementation requires uint64_t to be 8 bytes");
+static_assert(sizeof(long long unsigned int) == 8,
+              "CRC vector implementation requires unsigned long long to be 8 bytes");
+
 /*
 	Warning: here there be dragons involving vector math, and macros to save us
 	from repeating the same information over and over.
@@ -158,8 +165,6 @@ uint64_t gf2_matrix_times_vec2(uint64_t *mat, uint64_t vec) {
 	DO_CHUNK16();
 	DO_CHUNK16();
 
-	STATIC_ASSERT(sizeof(uint64_t) == 8);
-	STATIC_ASSERT(sizeof(long long unsigned int) == 8);
 	return sum[0] ^ sum[1];
 }
 
