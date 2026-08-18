e00395bab009cd96a038bc00ee0a060fb6f87f5d
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-08-18 19:24:21 +0900

    Align LMOVEM notifications with documentation

--- tests/unit/type/list-4.tcl
+++ tests/unit/type/list-4.tcl
@@ -98,20 +98,35 @@ foreach {type large} [array get largevalue] {
     }
 }
 
-    test {LMOVEM same-key notifications match LMOVE order} {
+    test {LMOVEM notifications are pop before push} {
         set orig_notify [lindex [r config get notify-keyspace-events] 1]
-        r config set notify-keyspace-events El
-        r del k{t}
+        r config set notify-keyspace-events Egl
+        r del k{t} src{t} dst{t} last{t} new{t}
         r rpush k{t} a b
+        r rpush src{t} a b
+        r rpush last{t} a
 
         set rd [redis_deferring_client]
         set pattern __keyevent@*__:*
         assert_equal {1} [psubscribe $rd $pattern]
 
         assert_equal {a} [r lmovem k{t} k{t} left right]
-        assert_match "pmessage $pattern __keyevent@*__:rpush k{t}" [$rd read]
         assert_match "pmessage $pattern __keyevent@*__:lpop k{t}" [$rd read]
+        assert_match "pmessage $pattern __keyevent@*__:rpush k{t}" [$rd read]
         assert_equal {b a} [r lrange k{t} 0 -1]
+
+        assert_equal {a} [r lmovem src{t} dst{t} left right]
+        assert_match "pmessage $pattern __keyevent@*__:lpop src{t}" [$rd read]
+        assert_match "pmessage $pattern __keyevent@*__:rpush dst{t}" [$rd read]
+        assert_equal {b} [r lrange src{t} 0 -1]
+        assert_equal {a} [r lrange dst{t} 0 -1]
+
+        assert_equal {a} [r lmovem last{t} new{t} left right]
+        assert_match "pmessage $pattern __keyevent@*__:lpop last{t}" [$rd read]
+        assert_match "pmessage $pattern __keyevent@*__:del last{t}" [$rd read]
+        assert_match "pmessage $pattern __keyevent@*__:rpush new{t}" [$rd read]
+        assert_equal {0} [r exists last{t}]
+        assert_equal {a} [r lrange new{t} 0 -1]
 
         $rd close
         r config set notify-keyspace-events $orig_notify
