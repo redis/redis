0e78a62b994cfa172d69be89d91bdc669bed1909
TalBarYakar <talbaryakar@gmail.com>
2026-09-03 16:41:18 +0300

    add copyright section

--- src/bless.c
+++ src/bless.c
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2026-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of (a) the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ *
  * BLESS - protect keys from eviction ("blessed" keys).
  *
  * Bless is one owner of the generic per-key attribute bitmask (see keyattr.c):
--- src/keyattr.h
+++ src/keyattr.h
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2026-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of (a) the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ *
  * keyattr - generic per-key attributes.
  *
  * A thin framework over keymeta: one keymeta class ("ATTR") stores a uint64_t
