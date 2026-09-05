81fdf327989b3c8cc7ffab34cf703c53e19c08e5
TalBarYakar <talbaryakar@gmail.com>
2026-09-05 17:28:36 +0300

    remove multiple from bless set / clear

--- tests/unit/bless.tcl
+++ tests/unit/bless.tcl
@@ -34,8 +34,8 @@ start_server {tags {"bless"}} {
         # unknown or not-yet-supported flags -> syntax error
         assert_error {*syntax*} {r bless set k bogus}
         assert_error {*syntax*} {r bless set k none}
-        # the same flag more than once is a syntax error
-        assert_error {*syntax*} {r bless set k no-evict no-evict}
+        # exactly one flag token (arity 4); extra tokens are an arity error
+        assert_error {*wrong number*} {r bless set k no-evict no-evict}
         assert_equal 1 [r bless set k no-evict]
         assert_equal {NO-EVICT} [r bless get k]
     }
